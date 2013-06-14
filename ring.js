6d5f99b569ee2b34222bda6f65c35d453e4ae05c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-06-14 12:31:33 +0400

    * dropdown * started fixes on youtrack
    Former-commit-id: 849e58aa3ac24cb8730d760ce25bdddeaf1c77d2

--- ring.js
+++ ring.js
@@ -1,4 +1,4 @@
-(function($) {
+(function($, global) {
     // Figure out CSS location from script location
     var scripts = document.getElementsByTagName('script');
     var this_script = scripts[scripts.length - 1];
@@ -14,10 +14,12 @@
     }
 
     // Still could not find ourselves
-    if (!this_script.src) return;
+//    if (!this_script.src) return;
+//
+//    var script_path = this_script.src;
+//    var script_folder = script_path.substr(0, script_path.lastIndexOf('/') + 1);
 
-    var script_path = this_script.src;
-    var script_folder = script_path.substr(0, script_path.lastIndexOf('/') + 1);
+    var script_folder = 'http://localhost:8000/';
 
     var stylesheet = document.createElement('link');
     stylesheet.type = 'text/css';
@@ -27,33 +29,23 @@
     var head = document.getElementsByTagName('head')[0];
     head.insertBefore(stylesheet, head.firstChild);
 
-    var data = {
-        username: 'Nikolae Chaushesku',
-        services: [
-            {
-                name: 'TeamCity',
-                url: '//teamcity.jetbrains.com'
-            },
-            {
-                name: 'YouTrack',
-                url: '//youtrack.jetbrains.com'
-            },
-            {
-                name: 'Upsource',
-                url: '//upsource.jetbrains.com'
-            }
-        ]
+    // @include ring.templates.js
+    // @include blocks/dropdown/_dropdown.js
+
+    var ring = {
+        init: function(data) {
+            // FIXME
+            data.personal.links = JSON.stringify(data.personal.links);
+
+            var ringLinks = $('' + templates['_stripe'](data) + templates['_header']());
+            $(function() {
+                $('body').prepend(ringLinks)
+            });
+        }
     };
 
-    // @include ring.templates.js
+    global.ring = ring;
 
-    var ringLinks = $('' + templates['_stripe'](data) + templates['_header']());
-    $(function() {
-        $('body').prepend(ringLinks)
-    });
+})(jQuery, this);
 
-    if (/*@cc_on!@*/false) {
-        // @include blocks/font-icon/font-icon.ie7.js
-    }
-
-})(jQuery);
+// @include blocks/font-icon/font-icon.ie7.js
