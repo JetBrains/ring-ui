54c0868da17f5914e78e749148c05885946eac5c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-06-17 20:12:30 +0400

    Some kind of order
    Former-commit-id: 833e6f6203e66392268d00680e6a42a0ce4a4b23

--- ring.js
+++ ring.js
@@ -1,61 +1,23 @@
 (function($, global) {
-    // Figure out CSS location from script location
-    var scripts = document.getElementsByTagName('script');
-    var this_script = scripts[scripts.length - 1];
-
-    // Looks like we are being loaded dynamically
-    if (!this_script.src) {
-        for (var i=0; i<scripts.length; i++) {
-            if (scripts[i].src && scripts[i].src.indexOf('ring.js') > -1) {
-                this_script = scripts[i];
-                break;
-            }
-        }
-    }
-
-    // Still could not find ourselves
-//    if (!this_script.src) return;
-//
-//    var script_path = this_script.src;
-//    var script_folder = script_path.substr(0, script_path.lastIndexOf('/') + 1);
-
-    var script_folder = 'http://localhost:8000/';
-
-    var stylesheet = document.createElement('link');
-    stylesheet.type = 'text/css';
-    stylesheet.rel = 'stylesheet';
-    stylesheet.href = script_folder + 'ring.css';
-
-    var head = document.getElementsByTagName('head')[0];
-    head.insertBefore(stylesheet, head.firstChild);
-
-
+    // Templates
+    //
     // @include node_modules/grunt-contrib-handlebars/node_modules/handlebars/dist/handlebars.runtime.js
-
-    Handlebars.registerHelper('stringify', function(items) {
-        return JSON.stringify(items);
-    });
-
     // @include ring.hbs.js
 
+    // Components
+    //
     // @include blocks/dropdown/_dropdown.js
+    // @include blocks/header/header.js
 
-    var ring = {
-        init: function(data) {
-            data = data || this.data;
+    // IE support
+    //
+    // @include blocks/font-icon/font-icon.ie7.js
 
-            var ringLinks = $(Handlebars.partials['full-header'](data));
-            $(function() {
-                $('body').prepend(ringLinks)
-            });
-        },
-        // Youtrack test
-        data: /* @include youtrack.json*/
-    };
-
+    // Youtrack test
+    // FIXME
     $('.header').hide();
-    global.ring = ring;
+    global.ring.data = /* @include youtrack.json*/
 
 })(jQuery, this);
 
-// @include blocks/font-icon/font-icon.ie7.js
+
