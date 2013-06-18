b73976a954add7a0302965a9a3319eda9ad779bf
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2013-06-18 23:26:18 +0200

    Indent = 2
    Former-commit-id: d40c27c314258abd80c38f0553d16f0da0d0673c

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,179 +1,179 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        // Install & build
-        shell: {
-            clean: {
-                command: [
-                    'rm -rf ./dist',
-                    'rm -rf ./tmp'
-                ].join(';'),
-                options: {
-                    stdout: false,
-                    stderr: false
-                }
-            },
-            install: {
-                command: 'node_modules/bower/bin/bower install',
-                options: {
-                    failOnError: true,
-                    stdout: true,
-                    stderr: true
-                }
-            },
-            uninstall: {
-                command: [
-                    'rm -rf ./node_modules',
-                    'rm -rf ./components'
-                ].join(';'),
-                options: {
-                    stdout: false,
-                    stderr: false
-                }
-            },
-            dist: {
-                command: 'java -jar tools/jruby/jruby-complete-1.7.4.jar -S compile.rb compile --sass-dir bundles --css-dir dist --images-dir . -I blocks',
-                options: {
-                    failOnError: true,
-                    stdout: true,
-                    stderr: true
-                }
-            }
-        },
-        csso: {
-            dist: {
-                files: {
-                    'dist/ring/ring.min.css':         ['dist/ring/ring.css'],
-                    'dist/ring-lib/ring-lib.min.css': ['dist/ring-lib/ring-lib.css']
-                }
-            }
-        },
-        uglify: {
-            dist: {
-                options: {
-                    report: 'gzip'
-                },
-                files: {
-                    'dist/ring/ring.min.js': 'dist/ring/ring.js'
-                }
-            }
-        },
-        compress: {
-            ring: {
-                options: {
-                    archive: './dist/ring.zip'
-                },
-                files: [
-                    {expand: true, cwd: './dist/ring/', src: ['**'], dest: 'ring'}
-                ]
-            },
-            'ring-lib': {
-                options: {
-                    archive: './dist/ring-lib.zip'
-                },
-                files: [
-                    {expand: true, cwd: './dist/ring-lib/', src: ['**'], dest: 'ring-lib'}
-                ]
-            }
-        },
-
-        // Process files
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'bundles',
-                    cssDir: 'dist',
-                    imagesDir: '.',
-                    importPath: 'blocks'
-                }
-            }
-        },
-        handlebars: {
-            compile: {
-                options: {
-                    namespace: false,
-                    node: false
-                },
-                files: {
-                    "tmp/ring.hbs.js": ["blocks/**/*.hbs"]
-                }
-            }
-        },
-        preprocess: {
-            js: {
-                src : 'bundles/ring/ring.js',
-                dest : 'dist/ring/ring.js'
-            }
-        },
-        copy: {
-            fonts: {
-                options: {
-                    processContentExclude: '*.dev.svg'
-                },
-                files: [
-                    {expand: true, src: ['blocks/**/*.woff', 'blocks/**/*.eot', 'blocks/**/*.ttf', 'blocks/**/*.svg', '!blocks/**/*.dev.svg'], dest: 'dist/ring'},
-                    {expand: true, src: ['blocks/**/*.woff', 'blocks/**/*.eot', 'blocks/**/*.ttf', 'blocks/**/*.svg', '!blocks/**/*.dev.svg'], dest: 'dist/ring-lib'}
-                ]
-            }
-        },
-
-        // Development
-        watch: {
-            scss: {
-                files: ['blocks/**/*.scss', 'bundles/**/*.scss'],
-                tasks: ['compass',  'notify:watch'],
-                options: {
-                    livereload: true
-                }
-            },
-            preprocess: {
-                files: ['blocks/**/*.js', 'blocks/**/*.json', 'bundles/**/*.js'],
-                tasks: ['preprocess:js',  'notify:watch'],
-                options: {
-                    livereload: true
-                }
-            },
-            reload: {
-                files: ['*.html'],
-                tasks: ['notify:watch'],
-                options: {
-                    livereload: true
-                }
-            },
-            templates: {
-                files: ['blocks/**/*.hbs'],
-                tasks: ['templates',  'notify:watch'],
-                options: {
-                    livereload: true
-                }
-            }
-        },
-        notify: {
-            watch: {
-                options: {
-                    title: 'grunt',
-                    message: 'Page reloaded'
-                }
-            }
-        },
-        connect: {
-            server: {
-                options: {
-                    port: 8000,
-                    hostname: '*',
-                    keepalive: true
-                }
-            }
+  grunt.initConfig({
+    // Install & build
+    shell: {
+      clean: {
+        command: [
+          'rm -rf ./dist',
+          'rm -rf ./tmp'
+        ].join(';'),
+        options: {
+          stdout: false,
+          stderr: false
         }
-    });
+      },
+      install: {
+        command: 'node_modules/bower/bin/bower install',
+        options: {
+          failOnError: true,
+          stdout: true,
+          stderr: true
+        }
+      },
+      uninstall: {
+        command: [
+          'rm -rf ./node_modules',
+          'rm -rf ./components'
+        ].join(';'),
+        options: {
+          stdout: false,
+          stderr: false
+        }
+      },
+      dist: {
+        command: 'java -jar tools/jruby/jruby-complete-1.7.4.jar -S compile.rb compile --sass-dir bundles --css-dir dist --images-dir . -I blocks',
+        options: {
+          failOnError: true,
+          stdout: true,
+          stderr: true
+        }
+      }
+    },
+    csso: {
+      dist: {
+        files: {
+          'dist/ring/ring.min.css':         ['dist/ring/ring.css'],
+          'dist/ring-lib/ring-lib.min.css': ['dist/ring-lib/ring-lib.css']
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        options: {
+          report: 'gzip'
+        },
+        files: {
+          'dist/ring/ring.min.js': 'dist/ring/ring.js'
+        }
+      }
+    },
+    compress: {
+      ring: {
+        options: {
+          archive: './dist/ring.zip'
+        },
+        files: [
+          {expand: true, cwd: './dist/ring/', src: ['**'], dest: 'ring'}
+        ]
+      },
+      'ring-lib': {
+        options: {
+          archive: './dist/ring-lib.zip'
+        },
+        files: [
+          {expand: true, cwd: './dist/ring-lib/', src: ['**'], dest: 'ring-lib'}
+        ]
+      }
+    },
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    // Process files
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'bundles',
+          cssDir: 'dist',
+          imagesDir: '.',
+          importPath: 'blocks'
+        }
+      }
+    },
+    handlebars: {
+      compile: {
+        options: {
+          namespace: false,
+          node: false
+        },
+        files: {
+          "tmp/ring.hbs.js": ["blocks/**/*.hbs"]
+        }
+      }
+    },
+    preprocess: {
+      js: {
+        src : 'bundles/ring/ring.js',
+        dest : 'dist/ring/ring.js'
+      }
+    },
+    copy: {
+      fonts: {
+        options: {
+          processContentExclude: '*.dev.svg'
+        },
+        files: [
+          {expand: true, src: ['blocks/**/*.woff', 'blocks/**/*.eot', 'blocks/**/*.ttf', 'blocks/**/*.svg', '!blocks/**/*.dev.svg'], dest: 'dist/ring'},
+          {expand: true, src: ['blocks/**/*.woff', 'blocks/**/*.eot', 'blocks/**/*.ttf', 'blocks/**/*.svg', '!blocks/**/*.dev.svg'], dest: 'dist/ring-lib'}
+        ]
+      }
+    },
 
-    grunt.registerTask('install',   ['shell:install']);
-    grunt.registerTask('uninstall', ['shell:uninstall']);
-    grunt.registerTask('clean',     ['shell:clean']);
+    // Development
+    watch: {
+      scss: {
+        files: ['blocks/**/*.scss', 'bundles/**/*.scss'],
+        tasks: ['compass',  'notify:watch'],
+        options: {
+          livereload: true
+        }
+      },
+      preprocess: {
+        files: ['blocks/**/*.js', 'blocks/**/*.json', 'bundles/**/*.js'],
+        tasks: ['preprocess:js',  'notify:watch'],
+        options: {
+          livereload: true
+        }
+      },
+      reload: {
+        files: ['*.html'],
+        tasks: ['notify:watch'],
+        options: {
+          livereload: true
+        }
+      },
+      templates: {
+        files: ['blocks/**/*.hbs'],
+        tasks: ['templates',  'notify:watch'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    notify: {
+      watch: {
+        options: {
+          title: 'grunt',
+          message: 'Page reloaded'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          keepalive: true
+        }
+      }
+    }
+  });
 
-    grunt.registerTask('default',   ['compass', 'handlebars', 'preprocess', 'copy:fonts']);
-    grunt.registerTask('build',     ['shell:dist', 'copy:fonts', 'handlebars', 'preprocess', 'csso', 'uglify', 'compress']);
-    grunt.registerTask('templates', ['handlebars', 'preprocess']);
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask('install',   ['shell:install']);
+  grunt.registerTask('uninstall', ['shell:uninstall']);
+  grunt.registerTask('clean',     ['shell:clean']);
+
+  grunt.registerTask('default',   ['compass', 'handlebars', 'preprocess', 'copy:fonts']);
+  grunt.registerTask('build',     ['shell:dist', 'copy:fonts', 'handlebars', 'preprocess', 'csso', 'uglify', 'compress']);
+  grunt.registerTask('templates', ['handlebars', 'preprocess']);
 };
--- blocks/dropdown/dropdown.js
+++ blocks/dropdown/dropdown.js
@@ -1,62 +1,62 @@
 ;(function($, Handlebars) {
-    var $body;
-    var $doc = $(document);
-    var $dropdown;
-    var target;
+  var $body;
+  var $doc = $(document);
+  var $dropdown;
+  var target;
 
-    var remove = function() {
-        if ($dropdown) {
+  var remove = function() {
+    if ($dropdown) {
 
-            $dropdown.remove();
-            $dropdown = null;
+      $dropdown.remove();
+      $dropdown = null;
 
-            target = null;
+      target = null;
 
-            /* Rare case with opening dropdown from component's descendants nodes
-            while closing another dropdown */
-            var $thisDropdown = $(this).closest('.component-dropdown');
-            if ($thisDropdown[0] !== this) {
-                $thisDropdown.click();
-            }
+      /* Rare case with opening dropdown from component's descendants nodes
+       while closing another dropdown */
+      var $thisDropdown = $(this).closest('.component-dropdown');
+      if ($thisDropdown[0] !== this) {
+        $thisDropdown.click();
+      }
 
-            return false;
-        }
+      return false;
+    }
+  }
+
+  // Using delegate because of compability with Youtrack's jQuery 1.5.1
+
+  $doc.delegate(':not(.component-dropdown)', 'click.close-dropdown', remove);
+
+  $doc.delegate('.component-dropdown', 'click.open-dropdown', function() {
+    var $this = $(this);
+    var sameTarget = (target === this || target === $this.closest('.component-dropdown')[0]);
+
+    remove();
+
+    if (sameTarget) {
+      return false;
     }
 
-    // Using delegate because of compability with Youtrack's jQuery 1.5.1
+    target = this;
+    var data = $this.data('component');
 
-    $doc.delegate(':not(.component-dropdown)', 'click.close-dropdown', remove);
+    if (!$body) {
+      $body = $('body');
+    }
 
-    $doc.delegate('.component-dropdown', 'click.open-dropdown', function() {
-        var $this = $(this);
-        var sameTarget = (target === this || target === $this.closest('.component-dropdown')[0]);
+    if (data) {
+      $dropdown = $(Handlebars.partials['dropdown'](data));
+      $dropdown.appendTo($body);
 
-        remove();
+      var pos = $this.offset();
+      pos.top += $this.outerHeight() + 15;
+      pos.left += $this.outerWidth() / 2 - $dropdown.width() / 2;
 
-        if (sameTarget) {
-            return false;
-        }
+      $dropdown.css(pos);
+    }
 
-        target = this;
-        var data = $this.data('component');
-
-        if (!$body) {
-            $body = $('body');
-        }
-
-        if (data) {
-            $dropdown = $(Handlebars.partials['dropdown'](data));
-            $dropdown.appendTo($body);
-
-            var pos = $this.offset();
-            pos.top += $this.outerHeight() + 15;
-            pos.left += $this.outerWidth() / 2 - $dropdown.width() / 2;
-
-            $dropdown.css(pos);
-        }
-
-        return false;
-    });
+    return false;
+  });
 
 
 }(jQuery, Handlebars));
@@ -75,164 +75,164 @@
  * @license MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
 (function ($) {
-    'use strict';
+  'use strict';
 
-    var escape = /["\\\x00-\x1f\x7f-\x9f]/g,
-        meta = {
-            '\b': '\\b',
-            '\t': '\\t',
-            '\n': '\\n',
-            '\f': '\\f',
-            '\r': '\\r',
-            '"' : '\\"',
-            '\\': '\\\\'
-        },
-        hasOwn = Object.prototype.hasOwnProperty;
+  var escape = /["\\\x00-\x1f\x7f-\x9f]/g,
+    meta = {
+      '\b': '\\b',
+      '\t': '\\t',
+      '\n': '\\n',
+      '\f': '\\f',
+      '\r': '\\r',
+      '"' : '\\"',
+      '\\': '\\\\'
+    },
+    hasOwn = Object.prototype.hasOwnProperty;
 
-    /**
-     * jQuery.toJSON
-     * Converts the given argument into a JSON representation.
-     *
-     * @param o {Mixed} The json-serializable *thing* to be converted
-     *
-     * If an object has a toJSON prototype, that will be used to get the representation.
-     * Non-integer/string keys are skipped in the object, as are keys that point to a
-     * function.
-     *
-     */
-    $.toJSON = typeof JSON === 'object' && JSON.stringify ? JSON.stringify : function (o) {
-        if (o === null) {
-            return 'null';
+  /**
+   * jQuery.toJSON
+   * Converts the given argument into a JSON representation.
+   *
+   * @param o {Mixed} The json-serializable *thing* to be converted
+   *
+   * If an object has a toJSON prototype, that will be used to get the representation.
+   * Non-integer/string keys are skipped in the object, as are keys that point to a
+   * function.
+   *
+   */
+  $.toJSON = typeof JSON === 'object' && JSON.stringify ? JSON.stringify : function (o) {
+    if (o === null) {
+      return 'null';
+    }
+
+    var pairs, k, name, val,
+      type = $.type(o);
+
+    if (type === 'undefined') {
+      return undefined;
+    }
+
+    // Also covers instantiated Number and Boolean objects,
+    // which are typeof 'object' but thanks to $.type, we
+    // catch them here. I don't know whether it is right
+    // or wrong that instantiated primitives are not
+    // exported to JSON as an {"object":..}.
+    // We choose this path because that's what the browsers did.
+    if (type === 'number' || type === 'boolean') {
+      return String(o);
+    }
+    if (type === 'string') {
+      return $.quoteString(o);
+    }
+    if (typeof o.toJSON === 'function') {
+      return $.toJSON(o.toJSON());
+    }
+    if (type === 'date') {
+      var month = o.getUTCMonth() + 1,
+        day = o.getUTCDate(),
+        year = o.getUTCFullYear(),
+        hours = o.getUTCHours(),
+        minutes = o.getUTCMinutes(),
+        seconds = o.getUTCSeconds(),
+        milli = o.getUTCMilliseconds();
+
+      if (month < 10) {
+        month = '0' + month;
+      }
+      if (day < 10) {
+        day = '0' + day;
+      }
+      if (hours < 10) {
+        hours = '0' + hours;
+      }
+      if (minutes < 10) {
+        minutes = '0' + minutes;
+      }
+      if (seconds < 10) {
+        seconds = '0' + seconds;
+      }
+      if (milli < 100) {
+        milli = '0' + milli;
+      }
+      if (milli < 10) {
+        milli = '0' + milli;
+      }
+      return '"' + year + '-' + month + '-' + day + 'T' +
+        hours + ':' + minutes + ':' + seconds +
+        '.' + milli + 'Z"';
+    }
+
+    pairs = [];
+
+    if ($.isArray(o)) {
+      for (k = 0; k < o.length; k++) {
+        pairs.push($.toJSON(o[k]) || 'null');
+      }
+      return '[' + pairs.join(',') + ']';
+    }
+
+    // Any other object (plain object, RegExp, ..)
+    // Need to do typeof instead of $.type, because we also
+    // want to catch non-plain objects.
+    if (typeof o === 'object') {
+      for (k in o) {
+        // Only include own properties,
+        // Filter out inherited prototypes
+        if (hasOwn.call(o, k)) {
+          // Keys must be numerical or string. Skip others
+          type = typeof k;
+          if (type === 'number') {
+            name = '"' + k + '"';
+          } else if (type === 'string') {
+            name = $.quoteString(k);
+          } else {
+            continue;
+          }
+          type = typeof o[k];
+
+          // Invalid values like these return undefined
+          // from toJSON, however those object members
+          // shouldn't be included in the JSON string at all.
+          if (type !== 'function' && type !== 'undefined') {
+            val = $.toJSON(o[k]);
+            pairs.push(name + ':' + val);
+          }
         }
+      }
+      return '{' + pairs.join(',') + '}';
+    }
+  };
 
-        var pairs, k, name, val,
-            type = $.type(o);
 
-        if (type === 'undefined') {
-            return undefined;
+  /**
+   * jQuery.quoteString
+   * Returns a string-repr of a string, escaping quotes intelligently.
+   * Mostly a support function for toJSON.
+   * Examples:
+   * >>> jQuery.quoteString('apple')
+   * "apple"
+   *
+   * >>> jQuery.quoteString('"Where are we going?", she asked.')
+   * "\"Where are we going?\", she asked."
+   */
+  $.quoteString = function (str) {
+    if (str.match(escape)) {
+      return '"' + str.replace(escape, function (a) {
+        var c = meta[a];
+        if (typeof c === 'string') {
+          return c;
         }
-
-        // Also covers instantiated Number and Boolean objects,
-        // which are typeof 'object' but thanks to $.type, we
-        // catch them here. I don't know whether it is right
-        // or wrong that instantiated primitives are not
-        // exported to JSON as an {"object":..}.
-        // We choose this path because that's what the browsers did.
-        if (type === 'number' || type === 'boolean') {
-            return String(o);
-        }
-        if (type === 'string') {
-            return $.quoteString(o);
-        }
-        if (typeof o.toJSON === 'function') {
-            return $.toJSON(o.toJSON());
-        }
-        if (type === 'date') {
-            var month = o.getUTCMonth() + 1,
-                day = o.getUTCDate(),
-                year = o.getUTCFullYear(),
-                hours = o.getUTCHours(),
-                minutes = o.getUTCMinutes(),
-                seconds = o.getUTCSeconds(),
-                milli = o.getUTCMilliseconds();
-
-            if (month < 10) {
-                month = '0' + month;
-            }
-            if (day < 10) {
-                day = '0' + day;
-            }
-            if (hours < 10) {
-                hours = '0' + hours;
-            }
-            if (minutes < 10) {
-                minutes = '0' + minutes;
-            }
-            if (seconds < 10) {
-                seconds = '0' + seconds;
-            }
-            if (milli < 100) {
-                milli = '0' + milli;
-            }
-            if (milli < 10) {
-                milli = '0' + milli;
-            }
-            return '"' + year + '-' + month + '-' + day + 'T' +
-                hours + ':' + minutes + ':' + seconds +
-                '.' + milli + 'Z"';
-        }
-
-        pairs = [];
-
-        if ($.isArray(o)) {
-            for (k = 0; k < o.length; k++) {
-                pairs.push($.toJSON(o[k]) || 'null');
-            }
-            return '[' + pairs.join(',') + ']';
-        }
-
-        // Any other object (plain object, RegExp, ..)
-        // Need to do typeof instead of $.type, because we also
-        // want to catch non-plain objects.
-        if (typeof o === 'object') {
-            for (k in o) {
-                // Only include own properties,
-                // Filter out inherited prototypes
-                if (hasOwn.call(o, k)) {
-                    // Keys must be numerical or string. Skip others
-                    type = typeof k;
-                    if (type === 'number') {
-                        name = '"' + k + '"';
-                    } else if (type === 'string') {
-                        name = $.quoteString(k);
-                    } else {
-                        continue;
-                    }
-                    type = typeof o[k];
-
-                    // Invalid values like these return undefined
-                    // from toJSON, however those object members
-                    // shouldn't be included in the JSON string at all.
-                    if (type !== 'function' && type !== 'undefined') {
-                        val = $.toJSON(o[k]);
-                        pairs.push(name + ':' + val);
-                    }
-                }
-            }
-            return '{' + pairs.join(',') + '}';
-        }
-    };
-
-
-    /**
-     * jQuery.quoteString
-     * Returns a string-repr of a string, escaping quotes intelligently.
-     * Mostly a support function for toJSON.
-     * Examples:
-     * >>> jQuery.quoteString('apple')
-     * "apple"
-     *
-     * >>> jQuery.quoteString('"Where are we going?", she asked.')
-     * "\"Where are we going?\", she asked."
-     */
-    $.quoteString = function (str) {
-        if (str.match(escape)) {
-            return '"' + str.replace(escape, function (a) {
-                var c = meta[a];
-                if (typeof c === 'string') {
-                    return c;
-                }
-                c = a.charCodeAt();
-                return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
-            }) + '"';
-        }
-        return '"' + str + '"';
-    };
+        c = a.charCodeAt();
+        return '\\u00' + Math.floor(c / 16).toString(16) + (c % 16).toString(16);
+      }) + '"';
+    }
+    return '"' + str + '"';
+  };
 
 }(jQuery));
 
 (function($, Handlebars) {
-    Handlebars.registerHelper('stringify', function(items) {
-        return $.toJSON(items);
-    });
+  Handlebars.registerHelper('stringify', function(items) {
+    return $.toJSON(items);
+  });
 }(jQuery, Handlebars))
