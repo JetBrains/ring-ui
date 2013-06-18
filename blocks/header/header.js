4410dd4d0e0cfd97712945cc3b0bdd631751b6fa
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-06-18 21:18:44 +0400

    new ring API
    Former-commit-id: fb3c5d938a5c47078c744e77053e5df58ad46814

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,14 +1,73 @@
 ;(function($, Handlebars, global) {
-    var ring = {
-        init: function(data) {
-            data = data || this.data;
+  var $body;
+  var data;
+  var html;
 
-            var ringLinks = $(Handlebars.partials['full-header'](data));
-            $(function() {
-                $('body').prepend(ringLinks)
-            });
-        }
-    };
+  var SELECTOR_PREFIX = '.ring-';
+  var template = 'full-header'; // Default template
 
-    global.ring = ring;
+  var init = function(initData, component) {
+    if (component) {
+      template = component;
+    }
+
+    data = initData || this.data;
+
+    $(updateHtml);
+  };
+
+  var render = function(template, data) {
+    return Handlebars.partials[template](data)
+  }
+
+  var clean = function() {
+    $(SELECTOR_PREFIX + template).remove();
+  }
+
+  var updateHtml = function(cb) {
+    if (!data) {
+      return;
+    }
+
+    if (!$body) {
+      $body = $('body');
+    }
+
+    html = render(template, data);
+    if (html) {
+      if (typeof cb == 'function') {
+        cb();
+      }
+
+      $body.prepend(html);
+    }
+
+  }
+
+  var update = function(path, value) {
+    var part;
+    var newData = {};
+    var pathParts = path.split('.');
+
+    while (part = pathParts.pop()) {
+      if (isNaN(Number(part))) {
+        newData = {};
+      } else {
+        newData = [];
+      }
+      newData[part] = value;
+      value = newData;
+    }
+
+    $.extend(true, data, newData);
+
+    updateHtml(clean);
+  };
+
+  global.ring = {
+    init: init,
+    update: update,
+    render: render
+  };
+
 }(jQuery, Handlebars, this));
