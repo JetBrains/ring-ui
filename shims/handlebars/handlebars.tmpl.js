96a8e9d479b8442342e801f291258cc511edb69a
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-06-21 17:11:45 +0400

    Moving to requirejs. Whoa
    Former-commit-id: 0c952c46dc9245664a6c4f92ebf5bd994434e80e

---
+++ shims/handlebars/handlebars.tmpl.js
@@ -0,0 +1,8 @@
+define(['jquery-json'], function() {
+  /*global Handlebars*/
+  // @include ../../node_modules/grunt-contrib-handlebars/node_modules/handlebars/dist/handlebars.runtime.js
+  // @include templates.js
+  // @include helpers.js
+
+  return Handlebars;
+});
