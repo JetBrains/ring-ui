9837533d820b3a7c3c92c450a7453136e0ffb6e7
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-06-24 19:20:33 +0400

    Don't wait dom ready (w/ profiler)
    Former-commit-id: 2f746c6dca191f3e775db5a7ada0e27b38093afb

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -4,20 +4,35 @@ define(['jquery', 'handlebars', 'dropdown/dropdown', 'font-icon/font-icon'], fun
   var $body;
   var data;
   var html;
+  var debug;
 
   var $global = $(window);
   var SELECTOR_PREFIX = '.ring-';
   var template = 'full-header'; // Default template
 
-  var init = function(initData, component) {
+  var init = function(initData, dontWaitDom, component) {
     if (component) {
       template = component;
     }
 
     data = initData || this.data;
 
-    // Render on DOM ready
-    $(updateHtml);
+    if (!dontWaitDom) {
+      $(updateHtml);
+    } else {
+      bodyReady(updateHtml);
+    }
+  };
+
+  var bodyReady = function(cb) {
+    $body = $('body');
+    if ($body && $body[0] && $body[0].firstChild) {
+      cb();
+    } else {
+      setTimeout(function() {
+        bodyReady(cb);
+      }, 10);
+    }
   };
 
   var render = function(template, data) {
@@ -45,7 +60,7 @@ define(['jquery', 'handlebars', 'dropdown/dropdown', 'font-icon/font-icon'], fun
 
       $body.prepend(html);
       $global.trigger('ring:header:updated');
-
+      console.timeEnd('ring');
       return true;
     }
 
