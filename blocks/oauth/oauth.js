7f01443c1065d1e314d30ff9563cddd1711c56ce
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2013-06-25 21:05:46 +0400

    #RD-1 in progress
    Former-commit-id: 015326fe63492d7838dd355a7a748609b75d5e35

--- blocks/oauth/oauth.js
+++ blocks/oauth/oauth.js
@@ -4,12 +4,24 @@ define(['jso', 'jquery', 'header/header'], function (jso, $, header) {
   var config;
   var headerInit = header.init;
 
-  var oauthInit = function (initialConfig, dontWaitDom, component) {
-    config = jQuery.extend(initialConfig, {});
+  var oauthInit = function (initialConfig, baseData, dontWaitDom, component) {
+    if (!(initialConfig) || !(initialConfig.serverUri)) {
+      throw "Server URI is not defined!";
+    }
+    config = jQuery.extend({
+        client_id: "dafb2157-a3ac-4f8c-92fa-450c3c903189",
+        redirect_uri: window.location.href,
+        authorization: initialConfig.serverUri + '/rest/oauth2/auth'
+      }, {
+        client_id: initialConfig.clientId,
+        redirect_uri: initialConfig.redirectUri
+      }
+    );
+    console.log(config);
     jso.configure({
       'default': config
     });
-    $.oajax({url: config.headerDataUrl,
+    $.oajax({url:initialConfig.serverUri + '/rest/services',
       jso_provider: 'default',
       //TODO: use string scopes instead of ids
       jso_scopes: ['dafb2157-a3ac-4f8c-92fa-450c3c903189'],
@@ -18,7 +30,7 @@ define(['jso', 'jquery', 'header/header'], function (jso, $, header) {
       success: function (data) {
         console.log("Response (default):");
         console.log(data);
-        headerInit(data, dontWaitDom, component);
+        headerInit(baseData, dontWaitDom, component);
       }
     });
   };
