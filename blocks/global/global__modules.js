e8c1a9335c0c540ca806ffcb3000a61082ed9a8e
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-07-22 20:59:26 +0400

    Fix inspections
    Former-commit-id: 86aa300f504f3ab3036fb0ed4dae0788effa66c0

--- blocks/global/global__modules.js
+++ blocks/global/global__modules.js
@@ -42,17 +42,23 @@ define(['jquery', 'global/global__events'], function($, Event) {
         dontEnumsLength = dontEnums.length;
 
       return function (obj) {
-        if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) throw new TypeError('Object.keys called on non-object');
+        if (typeof obj !== 'object' && typeof obj !== 'function' || obj === null) {
+          throw new TypeError('Object.keys called on non-object');
+        }
 
         var result = [];
 
         for (var prop in obj) {
-          if (hasOwnProperty.call(obj, prop)) result.push(prop);
+          if (hasOwnProperty.call(obj, prop)) {
+            result.push(prop);
+          }
         }
 
         if (hasDontEnumBug) {
           for (var i=0; i < dontEnumsLength; i++) {
-            if (hasOwnProperty.call(obj, dontEnums[i])) result.push(dontEnums[i]);
+            if (hasOwnProperty.call(obj, dontEnums[i])) {
+              result.push(dontEnums[i]);
+            }
           }
         }
         return result;
