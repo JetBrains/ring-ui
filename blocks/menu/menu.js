e20f9d7d32b5cde21ba94984a4bf183e5861bb96
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-07-29 18:48:58 +0400

    Method to make an item active #RG-4 fixed
    Former-commit-id: ffaba39f89195fd32f6028a7aaa72f9d11dc1eb7

--- blocks/menu/menu.js
+++ blocks/menu/menu.js
@@ -1,14 +1,31 @@
 define(['jquery',  'global/global__modules', 'global/global__views', 'dropdown/dropdown', 'font-icon/font-icon'], function($, Module, View) {
   'use strict';
 
+  var activePath = '';
+  var module = 'menu';
+
+  var getActivePath = function(label, part) {
+    if (label.indexOf('.') === -1) {
+      label = ('left.' || part) + label;
+    }
+    label += '.active';
+
+    return label;
+  };
+
   // Turn order value into elements order
-  var processItems = function(obj) {
+  var processItems = function(obj, name) {
     var items = [];
     var orderedItems = [];
     var item;
 
     for (var id in obj) {
       item = obj[id];
+
+      // Save current active path
+      if (item.active) {
+        activePath = getActivePath(id, name);
+      }
 
       if (item.order) {
         delete item.order;
@@ -31,22 +48,39 @@ define(['jquery',  'global/global__modules', 'global/global__views', 'dropdown/d
 
   var process = function(data) {
     if (data.left) {
-      data.left = processItems(data.left);
+      data.left = processItems(data.left, 'left');
     }
 
     if (data.right) {
-      data.right = processItems(data.right);
+      data.right = processItems(data.right, 'right');
     }
 
     return data;
   };
 
-  var module = 'menu';
+  var setActive = function(label) {
+    label = getActivePath(label);
+
+    if (activePath === label) {
+      // TODO logging
+      return null;
+    }
+
+    var update = [module, process, label, 1];
+
+    if (activePath) {
+      update.push(activePath, 0);
+    }
+
+    activePath = label;
+
+    View.update.apply(View, update);
+  };
+
 
   Module.add(module, {
     init: View.init.bind(View, module, process),
     update: View.update.bind(View, module, process),
-    setActive: function() {}
+    setActive: setActive
   });
-
 });
