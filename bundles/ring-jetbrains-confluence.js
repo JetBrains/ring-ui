b013975481ff4c155d75a294aac8cd5c94611abb
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-08-06 15:54:46 +0400

    Speed up confluence
    Former-commit-id: c4550d134cbe218dc2db2d1e56267ba0c9a635a8

--- bundles/ring-jetbrains-confluence.js
+++ bundles/ring-jetbrains-confluence.js
@@ -8,29 +8,8 @@ define([
   // Remove all jQuery variables from the global scope
   $.noConflict();
 
-  // Figure out CSS location from script location
-  var scripts = document.getElementsByTagName('script');
-  var ringScript;
-
-  for (var i = scripts.length; i > 0; i--) {
-    ringScript = scripts[i];
-
-    if (ringScript && ringScript && ringScript.src.indexOf('ring-jetbrains-confluence.min.js') > -1) {
-      break;
-    } else {
-      ringScript = null;
-    }
-  }
-
-  // Stop if we could not find ourselves
-  if (!ringScript || !ringScript.src) {
-    return ring;
-  }
-
-  // Append stylesheet to document
-  var ringPath = ringScript.src;
-  var ringFolder = ringPath.substr(0, ringPath.lastIndexOf('/') + 1);
-  $('<link rel="stylesheet" type="text/css" href="' + ringFolder + 'ring.min.css"/>').insertAfter(ringScript);
+  var header = ring('header');
+  header('init', {});
 
   // Render header
   $(function(){
@@ -40,11 +19,7 @@ define([
       .hide()
       .next('.ajs-drop-down').css('top', 0);
 
-    ring('header')('init', {
-      user: {
-        name: $username.text()
-      }
-    });
+    header.update('user.name', $username.text());
 
     $(document).on('click.ring.personal', '.ring-header__personal', function() {
       $username.click();
