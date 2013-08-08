f44bbeb4d5962e93374bb39b5c49e4e25dc2df30
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-08 18:44:09 +0400

    Create class for DiffTools and file with helpers, update configs, add codemirror.js as dependency. Add index-diff file for testing.
    Former-commit-id: c7ef5ec8e0bf65ae72dc9d561fb930ece55971e6

---
+++ bundles/ring-diff.js
@@ -0,0 +1,8 @@
+/**
+ * @fileoverview Bundle which, exports DiffTool.
+ * @author igor.alexeenko (Igor Alekseyenko)
+ */
+
+define(['global/global', 'diff/diff'], function(global, DiffTool) {
+  return DiffTool;
+});
