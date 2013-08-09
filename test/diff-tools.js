5798511c4e9cc729cf25eb46392b63dccc420165
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-09 14:02:24 +0400

    Add tests for utils method and fix inheritance in utils.
    Former-commit-id: 64588d9778079313a7995527d10164dda9ac10dc

--- test/diff-tools.js
+++ test/diff-tools.js
@@ -1,8 +1,64 @@
-/*global describe:false, it:false, expect:false*/
+/*global describe:false, it:false, expect:false, randomUninitializedObject:false*/
 'use strict';
 (function() {
-  var diffToolModule = window.ring('diff');
-  var diffToolUtils = diffToolModule.invoke('getDiffToolUtils');
+  describe('DiffTool utils basics.', function() {
+    it('DiffTool utils loads from ring module', function() {
+      expect(window.ring('diff')).to.be.a('function');
+    });
 
+    it('Module returns diffTool namespace.', function() {
+      var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+      expect(diffTool).to.be.an('object');
+    });
+  });
 
+  describe('DiffTool utils functionality', function() {
+    var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+
+    it('diffTool.isDef checks whether object is defined or not', function() {
+      expect(diffTool.isDef(window.randomUninitializedObject)).to.equal(false);
+      expect(diffTool.isDef(diffTool)).to.equal(true);
+    });
+
+    it('diffTool.abstractMethod raises an error when called', function() {
+      expect(diffTool.abstractMethod).to.throw(Error,
+          /This method is not implemented yet\./);
+    });
+
+    // todo(igor.alekseenko): Solid way to check whether the function is empty.
+    it('diffTool.nullFunction is an empty function', function() {
+      expect(diffTool.nullFunction.toString().replace(/\s/g, '')).to.equal(
+          'function(){}');
+    });
+  });
+
+  describe('DiffTool utils inheritance', function() {
+    var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+
+    var ParentClass = function() {};
+    ParentClass.prototype.parentProperty = true;
+
+    var ChildClass = function() {};
+    diffTool.inherit(ChildClass, ParentClass);
+
+    var childClassInstance = new ChildClass();
+
+    it('diffTool.inherit inherits childClass from parentClass', function() {
+      expect(childClassInstance).to.be.an.instanceof(ChildClass);
+      expect(childClassInstance).to.be.an.instanceof(ParentClass);
+      expect(ChildClass.prototype.parentProperty).to.equal(true);
+    });
+
+    it('diffTool.inherit appends link to parent class to a ' +
+        'child\'s static property super_.', function() {
+      expect(ChildClass.super_).to.equal(ParentClass.prototype);
+    });
+
+    it('diffTool.inherit changes nothing in parent, so changes in child ' +
+        'does not cause changes in parent.', function() {
+      ChildClass.prototype.parentProperty = false;
+      expect(ChildClass.prototype.parentProperty).to.equal(false);
+      expect(ParentClass.prototype.parentProperty).to.equal(true);
+    });
+  });
 })();
