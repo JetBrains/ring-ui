3b39d77bc5ad8e9af3f18ac318204d50f4fd48a0
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-12 10:43:41 +0400

    Rewrite tests accordingly to new system with requirejs
    Former-commit-id: ecedfcc9cdc40c145ddbc25856da504961d75a09

--- test/diff-tools.js
+++ test/diff-tools.js
@@ -1,19 +1,21 @@
-/*global describe:false, it:false, expect:false, randomUninitializedObject:false*/
-'use strict';
-(function() {
+define(['global/global', 'chai'], function(ring, chai) {
+  'use strict';
+
+  var expect = chai.expect;
+
   describe('DiffTool utils basics.', function() {
     it('DiffTool utils loads from ring module', function() {
-      expect(window.ring('diff')).to.be.a('function');
+      expect(ring('diff')).to.be.a('function');
     });
 
     it('Module returns diffTool namespace.', function() {
-      var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+      var diffTool = ring('diff').invoke('getDiffToolUtils');
       expect(diffTool).to.be.an('object');
     });
   });
 
   describe('DiffTool utils functionality', function() {
-    var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+    var diffTool = ring('diff').invoke('getDiffToolUtils');
 
     it('diffTool.isDef checks whether object is defined or not', function() {
       expect(diffTool.isDef(window.randomUninitializedObject)).to.equal(false);
@@ -22,7 +24,7 @@
 
     it('diffTool.abstractMethod raises an error when called', function() {
       expect(diffTool.abstractMethod).to.throw(Error,
-          /This method is not implemented yet\./);
+        /This method is not implemented yet\./);
     });
 
     // todo(igor.alekseenko): Solid way to check whether the function is empty.
@@ -32,7 +34,7 @@
   });
 
   describe('DiffTool utils inheritance', function() {
-    var diffTool = window.ring('diff').invoke('getDiffToolUtils');
+    var diffTool = ring('diff').invoke('getDiffToolUtils');
 
     var ParentClass = function() {};
     ParentClass.prototype.parentProperty = true;
@@ -49,15 +51,15 @@
     });
 
     it('diffTool.inherit appends link to parent class to a ' +
-        'child\'s static property super_.', function() {
+      'child\'s static property super_.', function() {
       expect(ChildClass.super_).to.equal(ParentClass.prototype);
     });
 
     it('diffTool.inherit changes nothing in parent, so changes in child ' +
-        'does not cause changes in parent.', function() {
+      'does not cause changes in parent.', function() {
       ChildClass.prototype.parentProperty = false;
       expect(ChildClass.prototype.parentProperty).to.equal(false);
       expect(ParentClass.prototype.parentProperty).to.equal(true);
     });
   });
-})();
+});
