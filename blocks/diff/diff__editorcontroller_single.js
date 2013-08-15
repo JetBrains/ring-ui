54497da5eef857206c5342e3c8a4e71e102568c4
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-15 18:29:28 +0400

    Create parser of response of API, remove CodeMirror, make simple output.
    Former-commit-id: 8845114ebe097711c918db6e96fd2ae0e5994a70

--- blocks/diff/diff__editorcontroller_single.js
+++ blocks/diff/diff__editorcontroller_single.js
@@ -3,8 +3,8 @@
  * @author igor.alexeenko (Igor Alexeenko)
  */
 
-define(['codemirror', 'diff/diff__tools',
-  'diff/diff__editorcontroller'], function(CodeMirror, diffTool) {
+define(['diff/diff__tools',
+  'diff/diff__editorcontroller'], function(diffTool) {
   'use strict';
 
   /**
@@ -19,28 +19,183 @@ define(['codemirror', 'diff/diff__tools',
   diffTool.inherit(diffTool.SingleEditorController, diffTool.EditorController);
 
   /**
+   * API response of number of changed lines and kinds of changes with them.
+   * @type {{
+   *   lines: number=,
+   *   newLines: number=,
+   *   oldLines: number=,
+   *   ranges: Array.<diffTool.SingleEditorController.InlineModification>,
+   *   type: diffTool.SingleEditorController.ModificationType
+   * }}
+   */
+  diffTool.SingleEditorController.LineModification = {};
+
+  /**
+   * Part of {@link LineModification}, which contains modifications with
+   * number of chars inside of particular line.
+   * @type {{
+   *   chars: number=,
+   *   newChars: number=,
+   *   oldChars: number=,
+   *   type: diffTool.SingleEditorController.ModificationType
+   * }}
+   */
+  diffTool.SingleEditorController.InlineModification = {};
+
+  /**
+   * @enum {string}
+   */
+  diffTool.SingleEditorController.ModificationType = {
+    /**
+     * Lines or chars was changed.
+     */
+    MODIFIED: 'modified',
+
+    /**
+     * Lines or chars was not changed.
+     */
+    UNCHANGED: 'unchanged'
+  };
+
+  /**
+   * Number of lines, which will be taken before and after changed code to
+   * display changed code in context of file.
+   * @type {number}
+   * @const
+   */
+  diffTool.SingleEditorController.UNCHANGED_GAP = 3;
+
+  /**
+   * @type {Array.<diffTool.SingleEditorController.BufferLine>}
+   */
+  diffTool.SingleEditorController.Buffer = [];
+
+  /**
+   * @type {{
+   *   codeType: diffTool.SingleEditorController.ModificationType,
+   *   line: string,
+   *   lineNumber: number
+   * }}
+   */
+  diffTool.SingleEditorController.BufferLine = {};
+
+
+  /**
+   * @enum {string}
+   */
+  diffTool.SingleEditorController.CodeType = {
+    /**
+     * Line from original code.
+     */
+    ORIGINAL: 'original',
+
+    /**
+     * Line from modified code.
+     */
+    MODIFIED: 'modified',
+
+    /**
+     * Unchanged line. Does not matter, to which code it belongs, because
+     * after merge it won't be changed. This lines, should not be highlighted
+     * in output.
+     */
+    UNCHANGED: 'unchanged'
+  };
+
+  /**
+   * @static
+   * @param {string} original
+   * @param {string} modified
+   * @param {Array.<Object>} diff
+   * @return {diffTool.SingleEditorController.Buffer}
+   */
+  diffTool.SingleEditorController.parseCode_ = function(original, modified,
+                                                        diff) {
+    // todo(igor.alexeenko): Not sure about lines separator.
+    var linesOriginal = original.split(/\n/);
+    var linesModified = modified.split(/\n/);
+
+    var output = /** @type {diffTool.SingleEditorController.Buffer} */ ([]);
+
+    /**
+     * Number of current line in original code.
+     * @type {number}
+     */
+    var cursorOriginal = 0;
+
+    /**
+     * Number of current line if modified code.
+     * @type {number}
+     */
+    var cursorModified = 0;
+
+    var offsetOriginal, offsetModified;
+
+    diff.forEach(function(change) {
+      switch (change.type) {
+      case diffTool.SingleEditorController.ModificationType.UNCHANGED:
+        cursorOriginal += change.lines;
+        cursorModified += change.lines;
+        break;
+
+      case diffTool.SingleEditorController.ModificationType.MODIFIED:
+        offsetOriginal = cursorOriginal + change.oldLines;
+        offsetModified = cursorModified + change.newLines;
+
+        var from = diffTool.clamp(
+            cursorOriginal - diffTool.SingleEditorController.UNCHANGED_GAP,
+            0, linesOriginal.length - 1);
+
+        var to = offsetOriginal +
+            diffTool.SingleEditorController.UNCHANGED_GAP;
+
+        for (var i = from; i < cursorOriginal; i++) {
+          output.push({
+            codeType: diffTool.SingleEditorController.CodeType.UNCHANGED,
+            line: linesOriginal[i],
+            lineNumber: i + 1
+          });
+        }
+
+        for (i = cursorOriginal; i < offsetOriginal; i++) {
+          output.push({
+            codeType: diffTool.SingleEditorController.CodeType.ORIGINAL,
+            line: linesOriginal[i],
+            lineNumber: i + 1
+          });
+        }
+
+        for (i = cursorModified; i < offsetModified; i++) {
+          output.push({
+            codeType: diffTool.SingleEditorController.CodeType.MODIFIED,
+            line: linesModified[i],
+            lineNumber: i + 1
+          });
+        }
+
+        for (i = offsetOriginal; i < to; i++) {
+          output.push({
+            codeType: diffTool.SingleEditorController.CodeType.UNCHANGED,
+            line: linesOriginal[i],
+            lineNumber: i + 1
+          });
+        }
+
+        break;
+      }
+    }, this);
+
+    return output;
+  };
+
+  /**
    * @override
    */
   diffTool.SingleEditorController.prototype.setEnabledInternal = function(
       enabled) {
     if (enabled) {
-      var editorElement = document.createElement('div');
-      var codeMirrorOptions = diffTool.EditorController.getCodeMirrorOptions(
-          this.isEditable());
 
-      this.element_.appendChild(editorElement);
-
-      /**
-       * @type {CodeMirror}
-       * @private
-       */
-      this.codeMirror_ = new CodeMirror(editorElement, codeMirrorOptions);
     } else {
-      // todo(igor.alexeenko): find out, how to destroy {@link CodeMirror}
-      // properly.
-      this.codeMirror_ = null;
-
-      // todo(igor.alexeenko): solid way to cleanup element.
       this.element_.innerHTML = '';
     }
   };
@@ -48,18 +203,32 @@ define(['codemirror', 'diff/diff__tools',
   /**
    * @override
    */
-  diffTool.SingleEditorController.prototype.setEditableInternal = function(
-      editable) {
-    if (this.codeMirror_) {
-      this.codeMirror_.setOption('readOnly', !editable);
-    }
-  };
+  diffTool.SingleEditorController.prototype.setEditableInternal =
+      diffTool.nullFunction;
 
   /**
    * @override
    */
   diffTool.SingleEditorController.prototype.setContentInternal = function(
-      original) {
-    this.codeMirror_.setValue(original);
+      original, modified, diff) {
+    // todo(igor.alexeenko): Pretty naive way to display diff. Add template.
+    var parsedContent = diffTool.SingleEditorController.parseCode_(original,
+        modified, diff);
+    var output = [];
+    var codeTypeToSymbol = diffTool.createObject(
+        diffTool.SingleEditorController.CodeType.UNCHANGED, ' ',
+        diffTool.SingleEditorController.CodeType.ORIGINAL, '-',
+        diffTool.SingleEditorController.CodeType.MODIFIED, '+');
+
+    output.push('<pre>');
+
+    parsedContent.forEach(function(line) {
+      output.push([line.lineNumber, codeTypeToSymbol[line.codeType],
+          line.line].join(' '));
+    });
+
+    output.push('</pre>');
+
+    this.element_.innerHTML = output.join('\n');
   };
 });
