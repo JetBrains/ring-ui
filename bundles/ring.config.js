dd00a2b300b6e3a71cf8a1b3e7389ad7df56faa6
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-08-15 13:18:11 +0400

    Replace json3 with json2 to support IE7
    Former-commit-id: 7406dcf8605743eedaf75063cc252b458b22875c

--- bundles/ring.config.js
+++ bundles/ring.config.js
@@ -5,7 +5,7 @@ require.config({
     'jquery'     : '../shims/jquery/jquery',
     'handlebars' : '../tmp/handlebars',
     'storage'    : '../components/polyfill/storage',
-    'json3'      : '../components/json3/lib/json3',
+    'json'       : '../components/json2/json2',
     'jso'        : '../components/jso/jso'
   },
   shim: {
@@ -13,7 +13,7 @@ require.config({
       exports: '$'
     },
     'jso': {
-      deps: ['jquery', 'json3', 'storage'],
+      deps: ['jquery', 'json', 'storage'],
       exports: 'jso_configure',
       init: function(){
         /* jshint camelcase:false */
--- shims/handlebars/handlebars.js
+++ shims/handlebars/handlebars.js
@@ -1,4 +1,4 @@
-define(['json3'], function() {
+define(['json'], function() {
   /*global Handlebars*/
   // @include ../../node_modules/grunt-contrib-handlebars/node_modules/handlebars/dist/handlebars.runtime.js
   // @include ../../tmp/templates.js
