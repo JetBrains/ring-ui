d9412ad164976363262bca3ca364567ebd6f3a5f
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-20 17:51:44 +0400

    Add double-paned mode. Separate styles for single- and double-paned modes in two different files.
    Former-commit-id: 20c86be7a5eda6dd93e4d27d9cf8a343bc3b920a

--- blocks/diff/diff__editorcontroller_double.js
+++ blocks/diff/diff__editorcontroller_double.js
@@ -3,7 +3,8 @@
  * @author igor.alexeenko (Igor Alexeenko)
  */
 
-define(['diff/diff__tools', 'diff/diff__editorcontroller'], function(diffTool) {
+define(['diff/diff__tools', 'codemirror', 'handlebars',
+  'diff/diff__editorcontroller'], function(diffTool, CodeMirror, Handlebars) {
   /**
    * @param {Element} element
    * @constructor
@@ -14,4 +15,56 @@ define(['diff/diff__tools', 'diff/diff__editorcontroller'], function(diffTool) {
         true);
   };
   diffTool.inherit(diffTool.DoubleEditorController, diffTool.EditorController);
+
+  /**
+   * IDs of templates for {@link Handlebars}.
+   * @enum {string}
+   */
+  diffTool.DoubleEditorController.Template = {
+    BASE: 'diff__doublepane'
+  };
+
+  /**
+   * @enum {string}
+   */
+  diffTool.DoubleEditorController.CssClass = {
+    BASE: 'diff_doublepane',
+    ORIGINAL: 'diff__original',
+    MODIFIED: 'diff__modified'
+  };
+
+  /**
+   * @override
+   */
+  diffTool.DoubleEditorController.prototype.setEnabledInternal = function(
+      enabled) {
+    if (enabled) {
+      this.element_.innerHTML = Handlebars.partials[
+          diffTool.DoubleEditorController.Template.BASE]();
+
+      this.originalElement_ = this.element_.querySelector(
+          '.' + diffTool.DoubleEditorController.CssClass.ORIGINAL);
+      this.modifiedElement_ = this.element_.querySelector(
+          '.' + diffTool.DoubleEditorController.CssClass.MODIFIED);
+
+      this.codeMirrorOriginal_ = new CodeMirror(this.originalElement_);
+      this.codeMirrorModified_ = new CodeMirror(this.modifiedElement_);
+    } else {
+      this.codeMirrorOriginal_ = null;
+      this.codeMirrirModified_ = null;
+
+      this.element_.innerHTML = '';
+    }
+  };
+
+  /**
+   * @override
+   */
+  diffTool.DoubleEditorController.prototype.setContentInternal = function(
+      original, modified, diff) {
+    this.codeMirrorOriginal_.setValue(original);
+    this.codeMirrorModified_.setValue(modified);
+
+    this.diff_ = diff; // temporary note, to prevent jshint errors.
+  };
 });
