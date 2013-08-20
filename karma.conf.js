b4e215ab6d75aecdbe86a9d1d28695afd3d4164b
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-08-20 10:48:25 +0400

    Revert "Separate Handlebars, which is loaded as requirejs module and Handlebars, which is used in tests to fix tests of classes, using Handlebars."
    This reverts commit 2261334a22635b14e747dd0d3693f4ce74b17eed.
    
    
    Former-commit-id: 5e426b126fc731c2748ea9f65168a661df7efccf

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,6 @@ module.exports = function(karma) {
     files: [
       'components/jquery/jquery.js',
       'components/codemirror/lib/codemirror.js',
-      'tmp/handlebars-test.js',
 
       {pattern: 'blocks/**/*.js', included: false},
       {pattern: 'test/**/*.js', included: false},
