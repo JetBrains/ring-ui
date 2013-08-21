57edf7810566766260e6511bff0f84cd4b52e9f6
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-08-21 18:16:11 +0400

    ring-diff bundle
    Former-commit-id: 7db7bc987d3c3ab648c4fe00b9cfb7536cbed732

--- bundles/ring-diff.js
+++ bundles/ring-diff.js
@@ -1,8 +1,13 @@
 /**
- * @fileoverview Bundle that DiffTool.
+ * @fileoverview Bundle with DiffTool
  * @author igor.alexeenko (Igor Alekseyenko)
  */
 
-define(['global/global', 'diff/diff'], function(ring) {
+define([
+  'header/header',
+  'menu/menu',
+  'footer/footer',
+  'diff/diff'
+], function(ring) {
   return ring;
 });
