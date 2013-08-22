f7ccf7d1426d3f40a8d8b9cd6dbb1d97e1435d90
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-08-22 14:02:43 +0400

    Display two editors, bind scrolls.
    Former-commit-id: e612d40339ac4aa088e3538d53d4912b2fddd4b4

--- blocks/diff/diff__editorcontroller_double.js
+++ blocks/diff/diff__editorcontroller_double.js
@@ -4,7 +4,9 @@
  */
 
 define(['diff/diff__tools', 'codemirror', 'handlebars',
-  'diff/diff__editorcontroller'], function(diffTool, CodeMirror, Handlebars) {
+  'diff/diff__editorcontroller',
+  'diff/diff__parser_doublepane'], function(diffTool, CodeMirror, Handlebars) {
+
   /**
    * @param {Element} element
    * @constructor
@@ -12,7 +14,7 @@ define(['diff/diff__tools', 'codemirror', 'handlebars',
    */
   diffTool.DoubleEditorController = function(element) {
     diffTool.DoubleEditorController.super_.constructor.call(this, element,
-        true);
+        true, diffTool.ParserDoublePane.getInstance());
   };
   diffTool.inherit(diffTool.DoubleEditorController, diffTool.EditorController);
 
@@ -39,6 +41,19 @@ define(['diff/diff__tools', 'codemirror', 'handlebars',
   };
 
   /**
+   * @static
+   * @return {Object}
+   */
+  diffTool.DoubleEditorController.getEditorOptions = function() {
+    return {
+      lineNumbers: true,
+      matchBrackets: true,
+      mode: 'text/x-java',
+      readOnly: true
+    };
+  };
+
+  /**
    * @override
    */
   diffTool.DoubleEditorController.prototype.setEnabledInternal = function(
@@ -52,13 +67,25 @@ define(['diff/diff__tools', 'codemirror', 'handlebars',
       this.modifiedElement_ = this.element_.querySelector(
           diffTool.DoubleEditorController.CssSelector.MODIFIED);
 
-      this.codeMirrorOriginal_ = new CodeMirror(this.originalElement_);
-      this.codeMirrorModified_ = new CodeMirror(this.modifiedElement_);
+      this.codeMirrorOriginal_ = new CodeMirror(this.originalElement_,
+          diffTool.DoubleEditorController.getEditorOptions());
+      this.codeMirrorModified_ = new CodeMirror(this.modifiedElement_,
+          diffTool.DoubleEditorController.getEditorOptions());
     } else {
+      this.unbindEditors_();
+
       this.codeMirrorOriginal_ = null;
       this.codeMirrorModified_ = null;
 
+      this.originalLines_ = null;
+      this.modifiedLines_ = null;
+
+      this.originalOffsets_ = null;
+      this.modifiedOffsets_ = null;
+
       this.element_.innerHTML = '';
+
+      this.lineToLine_ = null;
     }
   };
 
@@ -67,9 +94,128 @@ define(['diff/diff__tools', 'codemirror', 'handlebars',
    */
   diffTool.DoubleEditorController.prototype.setContentInternal = function(
       original, modified, diff) {
+    this.unbindEditors_();
+
     this.codeMirrorOriginal_.setValue(original);
     this.codeMirrorModified_.setValue(modified);
 
-    this.diff_ = diff; // temporary note, to prevent jshint errors.
+    this.bindEditors_(original, modified, diff);
+  };
+
+  /**
+   * Add unified scroll for both editors and draw a connectors between
+   * them.
+   * @param {string} original
+   * @param {string} modified
+   * @param {diffTool.Parser.Diff} diff
+   * @private
+   */
+  diffTool.DoubleEditorController.prototype.bindEditors_ = function(
+      original, modified, diff) {
+    CodeMirror.on(this.codeMirrorOriginal_, 'scroll',
+        diffTool.bindContext(this.onScroll_, this));
+    CodeMirror.on(this.codeMirrorModified_, 'scroll',
+        diffTool.bindContext(this.onScroll_, this));
+
+    this.originalLines_ = this.codeParser_.getLines(original, diff,
+        diffTool.ParserDoublePane.CodeType.ORIGINAL);
+    this.modifiedLines_ = this.codeParser_.getLines(modified, diff,
+        diffTool.ParserDoublePane.CodeType.MODIFIED);
+
+    this.originalOffsets_ = this.getLinesOffset_(this.originalLines_,
+        this.codeMirrorOriginal_);
+    this.modifiedOffsets_ = this.getLinesOffset_(this.modifiedLines_,
+        this.codeMirrorModified_);
+  };
+
+  /**
+   * Remove scroll handlers and destroy connectors.
+   * @private
+   */
+  diffTool.DoubleEditorController.prototype.unbindEditors_ = function() {
+    CodeMirror.off(this.codeMirrorOriginal_, 'scroll');
+    CodeMirror.off(this.codeMirrorModified_, 'scroll');
+  };
+
+  /**
+   * @param {CodeMirror} target
+   * @private
+   */
+  diffTool.DoubleEditorController.prototype.onScroll_ = function(target) {
+    var oppositeElement;
+    var elementOffsets;
+    var oppositeElementOffsets;
+
+    if (target === this.codeMirrorOriginal_) {
+      oppositeElement = this.codeMirrorModified_;
+      elementOffsets = this.originalOffsets_;
+      oppositeElementOffsets = this.modifiedOffsets_;
+    } else {
+      oppositeElement = this.codeMirrorOriginal_;
+      elementOffsets = this.modifiedOffsets_;
+      oppositeElementOffsets = this.originalOffsets_;
+    }
+
+    var scrollPosition = target.getScrollInfo();
+
+    var equatorOffset = scrollPosition.clientHeight / 2;
+    var equator = scrollPosition.top + equatorOffset;
+
+    var currentOffsetIndex = this.getCurrentOffset_(equator, elementOffsets);
+    var currentOffset = elementOffsets[currentOffsetIndex];
+
+    var ratio = (equator - currentOffset.top) /
+                (currentOffset.bottom - currentOffset.top);
+
+    var currentOppositeOffset = oppositeElementOffsets[currentOffsetIndex];
+
+    var targetPosition = (currentOppositeOffset.top - equatorOffset) +
+                         ratio * (currentOppositeOffset.bottom -
+                             currentOppositeOffset.top);
+
+    oppositeElement.scrollTo(currentOffset.left, targetPosition);
+  };
+
+  /**
+   * @param {Array.<Object>} lines
+   * @param {CodeMirror} editor
+   * @return {Array.<Object>}
+   * @private
+   */
+  diffTool.DoubleEditorController.prototype.getLinesOffset_ = function(lines,
+      editor) {
+    var offsets = [];
+
+    lines.forEach(function(line) {
+      offsets.push({
+        bottom: editor.heightAtLine(line.bottom, 'local'),
+        type: line.type,
+        top: editor.heightAtLine(line.top, 'local')
+      });
+    }, this);
+
+    return offsets;
+  };
+
+  /**
+   * Returns number of used offset.
+   * @param {number} scrollPosition
+   * @param {Array.<Object>} offsets
+   * @return {number}
+   * @private
+   */
+  diffTool.DoubleEditorController.prototype.getCurrentOffset_ = function(
+      scrollPosition, offsets) {
+    var currentOffset;
+
+    offsets.forEach(function(offset, i) {
+      if (!diffTool.isDef(currentOffset) &&
+          offset.top <= scrollPosition &&
+          offset.bottom >= scrollPosition) {
+        currentOffset = i;
+      }
+    });
+
+    return diffTool.isDef(currentOffset) ? currentOffset : -1;
   };
 });
