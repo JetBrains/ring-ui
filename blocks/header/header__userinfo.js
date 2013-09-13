7378573cc94e52f8a10a0efd37fd326cf928fd46
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-09-13 13:25:02 +0400

    Wait also for header:init:done when updating header from auth
    Former-commit-id: a6e842327e0b9ee509118d74eb2282f85e4e00fc

--- blocks/header/header__userinfo.js
+++ blocks/header/header__userinfo.js
@@ -3,13 +3,21 @@ define(['jquery', 'global/global__modules', 'global/global__views', 'header/head
   'use strict';
 
   var auth = Module.get('auth');
+  var header = Module.get('header');
 
-  auth.on('init:done', function() {
-    auth('ajax', '/rest/users/me')
-      .done(function(me) {
-        if (me && me.name) {
-          View.update('header', 'user.name', me.name);
-        }
-      });
-  });
+  var authInited = $.Deferred();
+  var headerInited = $.Deferred();
+
+  auth.on('init:done', authInited.resolve.bind(authInited));
+  header.on('init:done', headerInited.resolve.bind(headerInited));
+
+  $.when(headerInited, authInited)
+    .then(function() {
+      return auth('ajax', '/rest/users/me');
+    })
+    .then(function(me) {
+      if (me && me.name) {
+        View.update('header', 'user.name', me.name);
+      }
+    });
 });
