94ad071917f33303f8a0d0fd9576e4baf8fd0cce
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-09-13 16:46:30 +0400

    Profile links
    Former-commit-id: 032dd34c833464a4db746a3eacfb8ca4607af2f5

--- blocks/header/header__userinfo.js
+++ blocks/header/header__userinfo.js
@@ -11,13 +11,36 @@ define(['jquery', 'global/global__modules', 'global/global__views', 'header/head
   auth.on('init:done', authInited.resolve.bind(authInited));
   header.on('init:done', headerInited.resolve.bind(headerInited));
 
+  header.on('logout', function() {
+    auth.trigger('logout');
+    View.update('header', 'user', null);
+  });
+
   $.when(headerInited, authInited)
     .then(function() {
       return auth('ajax', '/rest/users/me');
     })
     .then(function(me) {
       if (me && me.name) {
-        View.update('header', 'user.name', me.name);
+        var config = auth.get('config');
+
+        var data = {
+          authLinks: {
+            profile: {
+              label: 'Profile',
+              url: config.serverUrl + '/jpapUser'
+            },
+            logout: {
+              label: 'Log out',
+              event: 'header:logout'
+            }
+          },
+          user: {
+            name: me.name
+          }
+        };
+
+        View.update('header', '.', data);
       }
     });
 });
