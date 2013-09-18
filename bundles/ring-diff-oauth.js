7939380fd521cb6f9e06d0603b3036bca65d670d
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-09-18 13:38:41 +0400

    ring-diff-oauth bundle
    Former-commit-id: d63fb5f0c458e3bf542166e5463e763fd9f0ca84

---
+++ bundles/ring-diff-oauth.js
@@ -0,0 +1,12 @@
+define([
+  'global/global',
+  'auth/auth',
+  'menu/menu',
+  'header/header',
+  'header/header__userinfo',
+  'header/header__hub-services',
+  'footer/footer',
+  'diff/diff'
+], function(ring) {
+  return ring;
+});
