35ee7cf57a172b43d4fb03bcc404accb04e82296
Maxim Mazin <maxim.mazin@jetbrains.com>
2013-10-09 20:14:58 +0200

    Fixed jshint warnings
    Former-commit-id: d491d50f4c198af1d0df2af773f675849d941ec3

--- blocks/delayed-listener/delayed-listener.ng.js
+++ blocks/delayed-listener/delayed-listener.ng.js
@@ -1,66 +1,66 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    /**
-     * <input type="text" listen-delayed="250"
-     *      on-delayed-caret-move="onCaretMove(value,caret)"
-     *      on-delayed-change="onValueChange(value,caret)">
-     */
-    angular.module('Ring.delayed-listener', []).
-        directive('listenDelayed', function () {
-            return {
-                scope: {
-                    listenDelayed: '@listenDelayed',
-                    onDelayedCaretMove: '&onDelayedCaretMove',
-                    onDelayedChange: '&onDelayedChange'
-                },
-                link: function (scope, iElement, iAttrs) {
-                    var timeoutHandler;
-                    var lastPolledCaretPosition;
-                    var lastTriggeredCaretPosition;
-                    var lastPolledValue;
-                    var lastTriggeredValue;
-                    var pollCaretPosition = function () {
-                        var caret = iElement.caret();
-                        var value = iElement.val();
+  /**
+   * <input type="text" listen-delayed="250"
+   *      on-delayed-caret-move="onCaretMove(value,caret)"
+   *      on-delayed-change="onValueChange(value,caret)">
+   */
+  angular.module('Ring.delayed-listener', []).
+    directive('listenDelayed', function () {
+      return {
+        scope: {
+          listenDelayed: '@listenDelayed',
+          onDelayedCaretMove: '&onDelayedCaretMove',
+          onDelayedChange: '&onDelayedChange'
+        },
+        link: function (scope, iElement) {
+          var timeoutHandler;
+          var lastPolledCaretPosition;
+          var lastTriggeredCaretPosition;
+          var lastPolledValue;
+          var lastTriggeredValue;
+          var pollCaretPosition = function () {
+            var caret = iElement.caret();
+            var value = iElement.val();
 
-                        // If caret position changed during the last tick
-                        //   Then save it and wait for the next tick
-                        //   Else trigger caret-move-event
-                        if (lastPolledCaretPosition !== caret || lastPolledValue !== value) {
-                            lastPolledCaretPosition = caret;
-                            lastPolledValue = value;
-                        } else if (value !== lastTriggeredValue) {
-                            lastTriggeredCaretPosition = caret;
-                            lastTriggeredValue = value;
-                            scope.onDelayedChange({value: value, caret: caret});
-                        } else if (caret !== lastTriggeredCaretPosition) {
-                            lastTriggeredCaretPosition = caret;
-                            lastTriggeredValue = value;
-                            scope.onDelayedCaretMove({value: value, caret: caret});
-                        }
-                    };
-                    var startListen = function () {
-                        lastTriggeredCaretPosition = undefined;
-                        lastPolledCaretPosition = undefined;
-                        timeoutHandler = setInterval(pollCaretPosition, scope.listenDelayed || 250);
-                    };
-                    var stopListen = function () {
-                        if (timeoutHandler) {
-                            clearInterval(timeoutHandler);
-                        }
-                    };
+            // If caret position changed during the last tick
+            //   Then save it and wait for the next tick
+            //   Else trigger caret-move-event
+            if (lastPolledCaretPosition !== caret || lastPolledValue !== value) {
+              lastPolledCaretPosition = caret;
+              lastPolledValue = value;
+            } else if (value !== lastTriggeredValue) {
+              lastTriggeredCaretPosition = caret;
+              lastTriggeredValue = value;
+              scope.onDelayedChange({value: value, caret: caret});
+            } else if (caret !== lastTriggeredCaretPosition) {
+              lastTriggeredCaretPosition = caret;
+              lastTriggeredValue = value;
+              scope.onDelayedCaretMove({value: value, caret: caret});
+            }
+          };
+          var startListen = function () {
+            lastTriggeredCaretPosition = undefined;
+            lastPolledCaretPosition = undefined;
+            timeoutHandler = setInterval(pollCaretPosition, scope.listenDelayed || 250);
+          };
+          var stopListen = function () {
+            if (timeoutHandler) {
+              clearInterval(timeoutHandler);
+            }
+          };
 
-                    iElement.bind('focus', function () {
-                        startListen();
-                    });
-                    iElement.bind('blur', function () {
-                        stopListen();
-                    });
-                    if (iElement.is(':focus')) {
-                        startListen();
-                    }
-                }
-            };
-        });
+          iElement.bind('focus', function () {
+            startListen();
+          });
+          iElement.bind('blur', function () {
+            stopListen();
+          });
+          if (iElement.is(':focus')) {
+            startListen();
+          }
+        }
+      };
+    });
 }());
