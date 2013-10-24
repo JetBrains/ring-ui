981c510a9c0bae72c01c0888b5be0058986b8314
Maxim Mazin <maxim.mazin@jetbrains.com>
2013-10-24 17:42:34 +0200

    AngularJS directive for header
    Former-commit-id: 044c73385f6150adbb7961c1bb3cedb4aede23b6

---
+++ blocks/header/header.ng.js
@@ -0,0 +1,45 @@
+(function () {
+  'use strict';
+
+  /**
+   * <div header>
+   *   <header-auth-link key="login" url="/login" label="Log in"></header-auth-link>
+   *   <header-auth-link key="logout" url="/logout" label="Log out"></header-auth-link>
+   * </div>
+   */
+  angular.module('Ring.header', []).
+    directive('header', [function () {
+      return {
+        scope: true,
+        controller: ['$scope', function ($scope) {
+          var ctrl = this;
+          $scope.header = {
+            authLinks: []
+          };
+          ctrl.addAuthLink = function (key, link) {
+            $scope.header.authLinks[key] = link;
+          };
+        }],
+        link: function (scope, iElement) {
+          ring('header', 'init')(scope.header, iElement, 'replace');
+        }
+      };
+    }]).
+    directive('headerAuthLink', [function () {
+      return {
+        require: '^header',
+        restrict: 'E',
+        scope: {
+          'key': '@key',
+          'url': '@url',
+          'label': '@label'
+        },
+        link: function (scope, iElement, iAttrs, headerCtrl) {
+          headerCtrl.addAuthLink(scope.key, {
+            'url': scope.url,
+            'label': scope.label
+          });
+        }
+      };
+    }]);
+}());
