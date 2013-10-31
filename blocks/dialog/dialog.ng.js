d26a429167bcbc962fc2e9dd1591c67519860232
Maxim Mazin <maxim.mazin@jetbrains.com>
2013-10-31 11:51:24 +0100

    Improved dialog
    Former-commit-id: 5e0e5cb114dfa513b58ebe37f1b7ff82d10f15c8

--- blocks/dialog/dialog.ng.js
+++ blocks/dialog/dialog.ng.js
@@ -8,11 +8,13 @@
         'scope': true,
         'replace': true,
         'templateUrl': 'dialog/dialog.ng.html',
-        'controller': ['$scope', 'dialog', function($scope, dialog) {
-          $scope.onButtonClick = function(button) {
+        'controller': ['$scope', 'dialog', function ($scope, dialog) {
+          $scope.onButtonClick = function (button) {
             var dontClose = false;
             if (button.action) {
-              dontClose = button.action($scope.data, button) === false;
+              dontClose = button.action($scope.data, button, function (errorMessage) {
+                $scope.error = errorMessage;
+              }) === false;
             }
             if (!dontClose && (button.close !== false)) {
               $scope.active = false;
@@ -22,7 +24,7 @@
         }]
       };
     }]).
-    service('dialog', [function() {
+    service('dialog', [function () {
       var dialogScope;
       return {
         /**
@@ -36,13 +38,13 @@
          *     <li>label — [required String] a text on a button</li>
          *     <li>default — [optional Boolean, default false] if true then button is highlighted as default</li>
          *     <li>close — [optional Boolean, default true] if true then click on the button closes the dialog unless action of the button returned false</li>
-         *     <li>action — [optional function(data, button)] is executed on button click. If returns false then window shouldn't be closed</li>
+         *     <li>action — [optional function(data, button, errorCallback(errorMessage))] is executed on button click. If returns false then window shouldn't be closed</li>
          *     <li>Also any field may be added to the button and later read in action function</li>
          *   </ul></li>
          * </ul>
          *
          */
-        'show': function(config) {
+        'show': function (config) {
           if (!dialogScope) {
             console.error('No dialog directive is found');
             return;
@@ -58,12 +60,12 @@
         /**
          * Hides dialog
          */
-        'hide': function() {
+        'hide': function () {
           if (dialogScope) {
             dialogScope.active = false;
           }
         },
-        'register': function(scope) {
+        'register': function (scope) {
           dialogScope = scope;
         }
       };
