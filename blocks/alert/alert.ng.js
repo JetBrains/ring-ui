f4609452a3ef677fba6878ffd167d208f3245e4b
Maxim Mazin <maxim.mazin@jetbrains.com>
2013-11-06 16:39:33 +0100

    AngularJS alert service
    Former-commit-id: 107b05aaf54874ada64fd941323c45d501d7b081

---
+++ blocks/alert/alert.ng.js
@@ -0,0 +1,25 @@
+(function () {
+  'use strict';
+
+  angular.module('Ring.alert', []).
+  /**
+   * Service to add alerts to the stack of alert messages.
+   *
+   * Usage:
+   * alert.message("Hello"); - shows message "Hello"
+   * alert.warning("Beware", 5000); - shows warning "Beware" for 5 seconds.
+   * alert.error("Fail :("); - shows error message
+   * alert.success("Successfully processed everything", 10000); shows success message for 10 seconds
+   */
+    service('alert', [function () {
+      var service = {};
+
+      ['message', 'error', 'warning', 'success'].forEach(function (type) {
+        service[type] = function (message, opt_timeout) {
+          return ring('alerts', 'add')(message, type, opt_timeout);
+        };
+      });
+
+      return service;
+    }]);
+})();
