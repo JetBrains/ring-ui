05fc028d88afaa1a11acd66ffbd0fa36670e8770
Igor Alexeenko <alexeenko.igor@gmail.com>
2013-11-19 15:54:05 +0400

    Implement new mode for DiffTool: PLAIN. It shows plain content of file in case, when there are content of only one file (for example: file is deleted or created).
    Former-commit-id: 5b27f1279caa7874b9ed8d5883195a786f4828b5

---
+++ blocks/diff/diff__editorcontroller_plain.js
@@ -0,0 +1,84 @@
+/**
+ * @fileoverview Controller for not changed files.
+ * @author igor.alexeenko (Igor Alekseyenko)
+ */
+
+define([
+  'diff/diff__tools',
+  'codemirror',
+  'diff/diff__editorcontroller'
+], function(d, CodeMirror) {
+  /**
+   * @param {Element} element
+   * @constructor
+   * @extends {d.EditorController}
+   */
+  d.PlainEditorController = function(element) {
+    d.PlainEditorController.super_.constructor.call(this, element);
+  };
+  d.inherit(d.PlainEditorController, d.EditorController);
+
+  /**
+   * @type {number}
+   * @const
+   */
+  d.PlainEditorController.MAX_LINES = 15;
+
+  /**
+   * @enum {string}
+   */
+  d.PlainEditorController.ClassName = {
+    EDITOR: 'diff__plain-editor'
+  };
+
+  /**
+   * @type {CodeMirror}
+   * @private
+   */
+  d.PlainEditorController.prototype.editor_ = null;
+
+  /**
+   * @override
+   */
+  d.PlainEditorController.prototype.setEnabledInternal = function(enabled) {
+    if (enabled === true) {
+      /**
+       * @type {Element}
+       * @private
+       */
+      this.editorElement_ = document.createElement('div');
+      this.editorElement_.className = d.PlainEditorController.ClassName.EDITOR;
+      this.element_.appendChild(this.editorElement_);
+
+      this.editor_ = new CodeMirror(this.editorElement_,
+          d.PlainEditorController.getCodeMirrorOptions());
+    } else {
+      this.editorElement_.parent.removeChild(this.editorElement_);
+      this.editorElement_ = null;
+    }
+  };
+
+  /**
+   * @override
+   */
+  d.PlainEditorController.prototype.setContentInternal = function(original,
+      modified, diff) {
+    var usedContent = diff[0].oldLines === 0 ? modified : original;
+    this.editor_.setValue(usedContent);
+  };
+
+  /**
+   * Returns {@code Object} which represents parameters for {@link CodeMirror}
+   * editor's instance.
+   * @static
+   * @return {Object}
+   */
+  d.PlainEditorController.getCodeMirrorOptions = function() {
+    return {
+      lineNumbers: true,
+      readOnly: true
+    };
+  };
+
+  return d.PlainEditorController;
+});
