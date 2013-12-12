21f78cafeb07fdeb50c7b96a2e9834743a86f75a
Maxim Mazin <maxim.mazin@jetbrains.com>
2013-12-12 17:09:52 +0400

    Added params to breadcrumb directive
    Former-commit-id: 98e649f56e5ae71258b8e36f09481a94df62bb9e

--- blocks/breadcrumb/breadcrumb.ng.js
+++ blocks/breadcrumb/breadcrumb.ng.js
@@ -11,6 +11,22 @@
         scope: { path: '&' },
         link: function($scope) {
           $scope.calculatedPath = $scope.path();
+          angular.forEach($scope.calculatedPath, function (pe) {
+            if (pe.params) {
+              var result = pe.href;
+              var first = true;
+              angular.forEach(pe.params, function (pValue, pName) {
+                if (first) {
+                  result += '?';
+                  first = false;
+                } else {
+                  result += '&';
+                }
+                result += pName + '=' + pValue;
+              });
+              pe.href = result;
+            }
+          });
         }
       };
     }]);
