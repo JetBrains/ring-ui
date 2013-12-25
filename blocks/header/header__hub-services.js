eb8d162d7512197291aa9cea3bb9493d9941f2db
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2013-12-25 19:13:26 +0400

    Paths mess continues
    Former-commit-id: 7b0375cacf8e2bd78b939d78ed61276fea5fdb32

--- blocks/header/header__hub-services.js
+++ blocks/header/header__hub-services.js
@@ -27,7 +27,7 @@ define(['jquery', 'global/global__modules', 'header/header', 'auth/auth'], funct
   $.when(headerInited, authInited)
     .then(function() {
       header.trigger('services');
-      return auth('get', '/api/rest/services');
+      return auth('get', 'api/rest/services?query=is:+verified');
     })
     .then(function(services) {
       var list = services && services.services;
