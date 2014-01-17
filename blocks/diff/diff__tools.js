6a0338e497d15c06b5af83651c635daf89051dfd
Igor Alexeenko <alexeenko.igor@gmail.com>
2014-01-17 16:11:43 +0400

    Add tools to read/write cookies.
    Former-commit-id: 6aa1eb8dec5283c9968654ef65eb535d7ebdcc57

--- blocks/diff/diff__tools.js
+++ blocks/diff/diff__tools.js
@@ -595,5 +595,52 @@ define([
     return d.os.currentOS_;
   };
 
+  /**
+   * Namespace for reading/writing cookies.
+   */
+  d.cookies = {};
+
+  /**
+   * Returns value of cookie with given name.
+   * @param {string} name
+   * @return {string}
+   */
+  d.cookies.get = function(name) {
+    var nameEQ = name + '=';
+    var ca = document.cookie.split(';');
+
+    for(var i = 0;i < ca.length; i++) {
+      var c = ca[i];
+
+      while(c.charAt(0) === ' ') {
+        c = c.substring(1,c.length);
+      }
+
+      if (c.indexOf(nameEQ) === 0) {
+        return c.substring(nameEQ.length,c.length);
+      }
+    }
+
+    return null;
+  };
+
+  /**
+   * Sets a cookie.
+   * @param {string} name
+   * @param {string} value
+   * @param {number} days
+   */
+  d.cookies.set = function(name, value, days) {
+    var expires = '';
+
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime()+(days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toGMTString();
+    }
+
+    document.cookie = name + '=' + value + expires + '; path=/';
+  };
+
   return d;
 });
