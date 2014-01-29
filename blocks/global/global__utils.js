145f12cff00fe9442ed28ceb2b4532069db4ad46
Maxim Barinov <barinovmaxim@gmail.com>
2014-01-29 13:17:46 +0400

    add to global_utils throttle
    Former-commit-id: f5a69d2145f5f005f52a24a1b13962a12102cf19

--- blocks/global/global__utils.js
+++ blocks/global/global__utils.js
@@ -35,5 +35,27 @@ define(function() {
   };
   //@endexclude
 
+  utils.throttle = function(fn, threshhold, scope) {
+    threshhold || (threshhold = 250);
+    var last,
+      deferTimer;
+    return function () {
+      var context = scope || this;
+
+      var now = +new Date,
+        args = arguments;
+      if (last && now < last + threshhold) {
+        clearTimeout(deferTimer);
+        deferTimer = setTimeout(function () {
+          last = now;
+          fn.apply(context, args);
+        }, threshhold);
+      } else {
+        last = now;
+        fn.apply(context, args);
+      }
+    };
+  }
+
   return utils;
 });
