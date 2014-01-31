7191bc884ceaf8bb00fc566b4fd08d1e39dde063
Anton Byrna <it.release@gmail.com>
2014-01-31 12:19:40 +0400

    Update remove/add/refresh methods
    Former-commit-id: 487cf1971cc249934e9f7eb170dc04e066da1ff9

--- blocks/tree/tree.js
+++ blocks/tree/tree.js
@@ -3,7 +3,7 @@ define([
   'handlebars',
   'jquery'
 ], function (Module, Handlebars, $) {
-
+  
   'use strict';
 
   function traverse (tree, fn) {
@@ -31,13 +31,14 @@ define([
     }
   }
 
+  var cache = {};
+
   var Tree = function (list, options) {
     this.list_ = (list && list.length) ? list.slice(0) : [];
-    this.options_ = options || {
-      message: 'No files'
-    };
     this.tree_ = null;
-    this.prepare(list);
+    this.$el = null;
+    this.options_ = options || { message: 'List is empty' };
+    this.prepare();
   };
 
   Tree.TEMPLATE = {
@@ -51,93 +52,36 @@ define([
     DIR: '.ring-tree__item-dir'
   };
 
-  Tree.prototype.refresh = function (list) {
-    var i, l, cb,
-        listItemTpl = this.options_.listItemTemplate || Handlebars.partials[Tree.TEMPLATE.LIST_ITEM];
-
-    cb = function (item, index, arr) {
-      if (item.type === 'file' && list[i].path === item.value.path) {
-        item.$el.remove();
-
-        for (var prop in list[i]) {
-          if ({}.hasOwnProperty.call(list[i], prop)) {
-            item.value[prop] = list[i][prop];
-          }
-        }
-
-        item.$el = $(listItemTpl(item));
-        arr.$el.append(item.$el);
-      }
-    };
-
-    for (i=0, l=list.length; i<l; i++) {
-      traverse(this.tree_, cb);
-    }
-  };
-
-  Tree.prototype.add = function (list) {
-    var newList = (list && list.length) ? list : [];
-    this.list_ = this.list_.concat(newList);
-    this.prepare(newList);
-    this.render();
-  };
-
-  Tree.prototype.remove = function (list) {
-    var i, l, j, jl, indexes = [];
-
-    var cb = function (item, index, arr) {
-      if (item.type === 'file' && list[i].path === item.value.path) {
-        item.$el.remove();
-        item.$el = null;
-        arr.splice(index, 1);
-      }
-    };
-
-    for (i=0, l=list.length; i<l; i++) {
-      traverse(this.tree_, cb);
-
-      for (j=0, jl=this.list_; j<jl; j++) {
-        if (this.list_[j].path === list[i].path) {
-          indexes.push(j);
-        }
-      }
-    }
-
-    for (i=0, l=indexes.length; i<l; i++) {
-      this.list_.splice(indexes[i], 1);
-    }
-  };
-
-  Tree.prototype.prepare = function (list) {
-    if (!list || list.length === 0) {
-      if (!this.list_ || this.list_.length === 0) {
-        this.tree_ = null;
-      }
+  Tree.prototype.prepare = function () {
+    if (this.list_.length === 0) {
+      this.clear();
       return;
     }
 
-    var i, l, item,
-        tree = this.tree_ || [];
+    var i, l,
+        tree = [];
 
     function prepareItem (item) {
-      var i, l,
-          partPath,
-          node,
-          splitPath = item.path.replace(/^\/|\/$/g, '').split('/'),
+      var i, l, partPath, node,
+          path = item.path.replace(/^\/|\/$/g, '').split('/'),
           curTree = tree;
 
-      for (i=0, l=splitPath.length; i<l; i++) {
-        partPath = splitPath[i];
+      for (i=0, l=path.length; i<l; i++) {
+        partPath = path[i];
         node = {
           name: partPath,
           type: i === (l-1) ? 'file' : 'dir'
         };
 
+        if (node.type === 'dir' && !cache[node.name + ':' + node.type]) {
+          cache[node.name + ':' + node.type] = { opened: false };
+        }
+
         if (node.type === 'dir') {
           if (!curTree[partPath]) {
             curTree[partPath] = node;
             node.children = [];
-            node.index = curTree.push(node) - 1;
+            curTree.push(node);
           } else {
             node.children = curTree[partPath].children;
           }
@@ -149,42 +93,155 @@ define([
       }
     }
 
-    for (i=0, l=list.length; i<l; i++) {
-      item = list[i];
-      prepareItem(item);
+    for (i=0, l=this.list_.length; i<l; i++) {
+      prepareItem(this.list_[i]);
     }
 
     this.tree_ = tree;
   };
 
-  Tree.prototype.bindListeners = function () {
-    var self = this;
-
-    if (this.binded_) {
+  Tree.prototype.add = function (list) {
+    if (!list || list.length === 0) {
       return;
     }
 
-    this.binded_ = true;
+    this.list_ = this.list_.concat(list);
 
-    this.tree_.$el.on('click', Tree.SELECTOR.DIR, function (e) {
+    this.prepare();
+    this.render();
+  };
+
+  Tree.prototype.remove = function (list) {
+    if (!list || list.length === 0) {
+      return;
+    }
+
+    var i, il, j, jl,
+        indexes = [];
+
+    for (i=0, il=list.length; i<il; i++) {
+      for (j=0, jl=this.list_.length; j<jl; j++) {
+        if (this.list_[j].path === list[i].path) {
+          this.list_.splice(j, 1);
+          jl -= 1;
+          j -= 1;
+          indexes.push(j);
+        }
+      }
+    }
+
+    if (indexes.length) {
+      this.prepare();
+      this.render();
+    }
+  };
+
+  Tree.prototype.refresh = function (list) {
+    var i, l;
+
+    if (list.path) {
+      for (i=0, l=this.list_.length; i<l; i++) {
+        if (this.list_[i].path === list.path) {
+          this.list_[i] = list;
+          break;
+        }
+      }
+    }
+
+    else if (!list || list.length === 0) {
+      return;
+    }
+
+    else if (list.length) {
+      this.list_ = list;
+    }
+
+    this.prepare();
+    this.render();
+  };
+
+  Tree.prototype.clear = function () {
+    this.list_ = [];
+    this.tree_ = null;
+
+    this.render();
+  };
+
+  Tree.prototype.render = function () {
+    var listTpl = this.options_.listTemplate || Handlebars.partials[Tree.TEMPLATE.LIST],
+        listItemTpl = this.options_.listItemTemplate || Handlebars.partials[Tree.TEMPLATE.LIST_ITEM];
+
+    var $rootEl = this.$el;
+
+    if (!this.tree_) {
+      var emptyTpl = this.options_.emptyTemplate || Handlebars.partials[Tree.TEMPLATE.EMPTY],
+          $el = $(emptyTpl(this.options_));
+
+      if ($rootEl) {
+        $rootEl.replaceWith($el);
+      }
+
+      this.$el = $el;
+
+      return $el;
+    }
+
+    traverse(this.tree_, function (item, index, arr) {
+      if (!arr.$el) {
+        arr.$el = $(listTpl());
+      }
+
+      item.$el = $(listItemTpl(item));
+
+      if (item.type === 'dir') {
+        item.$el.data('name', item.name);
+        item.children.$el = $(listTpl());
+        item.$el.append(item.children.$el);
+
+        var state = cache[item.name + ':dir'];
+
+        if (state.opened) {
+          item.$el.addClass('ring-tree__item-dir--opened');
+        }
+      }
+
+      arr.$el.append(item.$el);
+    });
+
+    this.$el = this.tree_.$el;
+
+    this.bindListeners();
+
+    if ($rootEl) {
+      $rootEl.replaceWith(this.$el);
+    }
+
+    return this.$el;
+  };
+
+  Tree.prototype.bindListeners = function () {
+    var self = this;
+
+    this.$el.on('click', Tree.SELECTOR.DIR, function (e) {
       self.onDirClick_(e);
     });
 
-    this.tree_.$el.on('click', Tree.SELECTOR.FILE, function (e) {
+    this.$el.on('click', Tree.SELECTOR.FILE, function (e) {
       self.onFileClick_(e);
     });
 
-    this.tree_.$el.on('keydown', [Tree.SELECTOR.DIR, Tree.SELECTOR.FILE], function (e) {
+    $(document).on('keydown', [Tree.SELECTOR.DIR, Tree.SELECTOR.FILE], function (e) {
       var SPACE_KEY = 32;
 
       if (e.keyCode === SPACE_KEY) {
-        e.preventDefault();
 
         if ($(e.target).hasClass('ring-tree__item-dir')) {
+          e.preventDefault();
           self.onDirClick_(e);
         }
 
         else if ($(e.target).hasClass('ring-tree__item-file')) {
+          e.preventDefault();
           self.onFileClick_(e);
         }
       }
@@ -199,62 +256,14 @@ define([
         $(e.currentTarget) : null;
 
     if ($target) {
+      var state = cache[$target.data('name') + ':dir'];
+      state.opened = !state.opened;
       $target.toggleClass('ring-tree__item-dir--opened');
     }
   };
 
   Tree.prototype.onFileClick_ = function (e) {
-    e.stopPropagation();
-  };
-
-  Tree.prototype.render = function () {
-    var listTpl = this.options_.listTemplate || Handlebars.partials[Tree.TEMPLATE.LIST],
-        listItemTpl = this.options_.listItemTemplate || Handlebars.partials[Tree.TEMPLATE.LIST_ITEM];
-
-    if (!this.tree_) {
-      var emptyTpl = this.options_.emptyTemplate || Handlebars.partials[Tree.TEMPLATE.EMPTY];
-      return $(emptyTpl(this.options_));
-    }
-
-    traverse(this.tree_, function (item, index, arr) {
-      if (!arr.$el) {
-        arr.$el = $(listTpl());
-      }
-
-      if (item.type === 'dir' && !item.$el) {
-        item.$el = $(listItemTpl(item));
-        item.children.$el = $(listTpl());
-        item.$el.append(item.children.$el);
-        arr.$el.append(item.$el);
-      }
-
-      if (item.type === 'file') {
-        if (item.$el) {
-          item.$el.remove();
-        }
-
-        item.$el = $(listItemTpl(item));
-        arr.$el.append(item.$el);
-      }
-    });
-
-    this.bindListeners();
-    return this.tree_.$el;
-  };
-
-  Tree.prototype.clear = function () {
-    traverse(this.tree_, function (item, index, arr) {
-      if (item.$el) {
-        item.$el.remove();
-        item.$el = null;
-      }
-
-      if (arr.$el) {
-        arr.$el.remove();
-        arr.$el = null;
-      }
-    });
-    this.binded_ = false;
+    console.log('onFileClick_', e);
   };
 
   Module.add('tree', {
