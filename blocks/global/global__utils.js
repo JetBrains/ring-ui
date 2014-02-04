0ee7a4bf3dcc575793589561d7ee35139196069c
Anton Byrna <it.release@gmail.com>
2014-02-04 12:59:19 +0400

    Add method to global utils that generate uuid. Add method to tree component that will return subtree items using tree uuid.
    Former-commit-id: 6e388afaaf3c17ab2f43726e27d444a8d0347006

--- blocks/global/global__utils.js
+++ blocks/global/global__utils.js
@@ -58,5 +58,15 @@ define(function() {
     };
   };
 
+  utils.uuid = function () {
+    var d = new Date().getTime(),
+        uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = (d + Math.random()*16)%16 | 0;
+          d = Math.floor(d/16);
+          return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+        });
+    return uuid;
+  };
+
   return utils;
 });
