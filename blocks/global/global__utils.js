ae8717fa4b2a5bdc01315deb832b62bcf3f50d41
Anton Byrna <it.release@gmail.com>
2014-02-05 18:08:28 +0400

    Walk through tree with arrow keys
    Former-commit-id: 60872c6294129a1fa422eb696c7371fab1c4ce49

--- blocks/global/global__utils.js
+++ blocks/global/global__utils.js
@@ -63,7 +63,7 @@ define(function() {
         uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
           var r = (d + Math.random()*16)%16 | 0;
           d = Math.floor(d/16);
-          return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+          return (c==='x' ? r : (r&0x7|0x8)).toString(16);
         });
     return uuid;
   };
