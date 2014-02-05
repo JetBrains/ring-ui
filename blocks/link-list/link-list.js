54e217e11488d74cc5b76421fda18678a960b722
Maxim Barinov <barinovmaxim@gmail.com>
2014-02-05 20:14:59 +0400

    link-list
    Former-commit-id: d3e5ffab30ef7150448d297ad7f61c75af0f83c3

--- blocks/link-list/link-list.js
+++ blocks/link-list/link-list.js
@@ -5,7 +5,7 @@ define([
   'global/global__events',
   'global/global__utils',
   'popup/popup'
-], function ($, View, Module) {
+], function ($, View, Module, utils) {
   'use strict';
 
   var MODULE = 'link-list';
@@ -13,49 +13,70 @@ define([
 
   var popup = Module.get('popup');
   var $body = $('body');
+  var $el;
 
   var create = function (data) {
-    var linkList = Module.get(MODULE);
+    var wrapper = popup('create', data);
+
+    popup('remove');
+    dataSource(wrapper.target, data).then(function (data) {
+      var $el;
+      if (data instanceof $ || utils.isNode(data)) {
+        $el = data;
+      }
+      var $el = $(View.render('dropdown__items', data));
+      wrapper.insertHTML(wrapper, $el);
+    });
+  };
+
+  var remove = function () {
+    if ($el) {
+      $el.remove();
+    }
+    popup('remove');
+  };
+
+  var dataSource = function ($target, data) {
+    var dfd = $.Deferred();
 
     if (!data) {
-      linkList.trigger('show:fail');
-      return false;
+      data = $target.data('ring-dropdown');
     }
 
-    return $(View.render(MODULE, data));
+    if ($.isArray(data)) {
+      data = {items: data};
+    }
+
+    if (typeof data === 'string') {
+      data = {html: data};
+    }
+
+    dfd.resolve(data);
+    return dfd.promise();
   };
 
   $(document).delegate('*', 'click.ring-dropdown', function (e) {
     var $target = $(e.currentTarget).closest(COMPONENT_SELECTOR);
 
     if ($target.length) {
-      var wrapper = popup('create', {
-        target: $target
-      });
-      $body.append(wrapper.el);
-
-      /**
-       *
-       * @ToDo Add dataSource in create func
-       */
-      var q = $(View.render('dropdown__items', {items: [
-        {label: 'test'},
-        {label: 'test2'}
-      ]}));
-
-      wrapper.el.
-        html(q).
-        css(wrapper.getPos());
+      create($target);
       return false;
     }
 
     e.stopPropagation();
   });
 
-  // Public methods
   Module.add(MODULE, {
     create: {
       method: create,
+      override: true
+    },
+    remove: {
+      method: remove,
+      override: true
+    },
+    dataSource: {
+      method: dataSource,
       override: true
     }
   });
