4eb1ff0de4cb6e5f885c2b7c2b20f583be15fa49
Maxim Barinov <barinovmaxim@gmail.com>
2014-02-11 19:43:50 +0400

    RG-240 Implement action list in dropdown
    Former-commit-id: 1cbb64983dcc69238b2a2bb5b9c2e93190d1e027

--- blocks/action-list/action-list.js
+++ blocks/action-list/action-list.js
@@ -6,20 +6,27 @@ define([
   'global/global__utils',
   'shortcuts/shortcuts',
   'popup/popup'
-], function ($, View, Module) {
+], function ($, View, Module, events) {
   'use strict';
 
   var $target,
     $el,
+    items,
     popup = Module.get('popup'),
+    shortcuts = Module.get('shortcuts'),
     actionList;
 
-  var MODULE = 'action-list';
-  var COMPONENT_SELECTOR = '.ring-js-action-list';
+  var MODULE = 'action-list',
+    COMPONENT_SELECTOR = '.ring-js-action-list',
+    ITEM_ACTION_SELECTOR = '.ring-dropdown__item_action',
+    ACTIVE_CLASS = 'active',
+    ACTIVE_SELECTOR = '.active';
 
   var create = function (data, config) {
     var wrapper = popup('create', config);
     actionList = Module.get(MODULE);
+
+    shortcuts('pushScope', MODULE);
 
     if (!$target instanceof $) {
       actionList.trigger('init:fail');
@@ -28,12 +35,26 @@ define([
       if (config.data) {
         $.extend(data, config.data);
       }
+      items = data.items;
       $el = $(View.render('action-list', data));
+
       wrapper.insertHTML(wrapper, $el);
+
+
     });
 
     actionList.trigger('init:done');
 
+    $el.bind('mouseenter', function (e) {
+      events.domEventHandler(e);
+
+      $(e.currentTarget)
+        .addClass(ACTIVE_CLASS)
+        .siblings()
+        .removeClass(ACTIVE_CLASS);
+    });
+
+    return $el;
   };
 
   var dataSource = function (target, data) {
@@ -52,6 +73,40 @@ define([
     return dfd.promise();
   };
 
+  var remove = function () {
+    if ($el) {
+      $el.remove();
+    }
+    popup('remove');
+  };
+
+  var action_ = function () {
+    var $active = $el.parent().find(ACTIVE_SELECTOR);
+
+    if ($active.length) {
+      actionList.trigger('action', (items[$el.parent().find(ITEM_ACTION_SELECTOR).index($active)] || false));
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  var navigate_ = function (e, key) {
+    var up = (key === 'up'),
+      $active = $el.parent().find(ITEM_ACTION_SELECTOR + ACTIVE_SELECTOR),
+      $next = $active[up ? 'prev' : 'next'](ITEM_ACTION_SELECTOR);
+
+    $active.removeClass(ACTIVE_CLASS);
+
+    if ($next.length) {
+      $next.addClass(ACTIVE_CLASS);
+    } else {
+      $el.parent().find(ITEM_ACTION_SELECTOR)[up ? 'last' : 'first']().addClass(ACTIVE_CLASS);
+    }
+
+    return false;
+  };
+
   $(document).delegate('*', 'click.ring-js-action-list', function (e) {
     var $target = $($(e.currentTarget).closest(COMPONENT_SELECTOR));
 
@@ -63,14 +118,30 @@ define([
         }
       });
       return false;
+    } else {
+      shortcuts('popScope', MODULE);
     }
 
     e.stopPropagation();
   });
 
+  shortcuts('bindList', {
+    scope: MODULE
+  }, {
+    'esc': remove,
+    'enter': action_,
+    'tab': action_,
+    'up': navigate_,
+    'down': navigate_
+  });
+
   Module.add(MODULE, {
     create: {
       method: create,
+      override: true
+    },
+    remove: {
+      method: remove,
       override: true
     },
     dataSource: {
