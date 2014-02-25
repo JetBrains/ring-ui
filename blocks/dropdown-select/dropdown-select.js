947f926417bc369b5ed59551d5752590fd4bd4e7
Maxim Barinov <barinovmaxim@gmail.com>
2014-02-25 15:25:32 +0400

    rename modules
    Former-commit-id: c701c63679a92ea6ba7547d308ddc15c3e6711f5

--- blocks/dropdown-select/dropdown-select.js
+++ blocks/dropdown-select/dropdown-select.js
@@ -4,179 +4,141 @@ define([
   'global/global__modules',
   'global/global__events',
   'global/global__utils',
-  'dropdown/dropdown',
-  'shortcuts/shortcuts'
+  'delayed-listener/delayed-listener',
+  'action-list/action-list'
 ], function ($, View, Module, events, utils) {
   'use strict';
 
-  var $target,
+  var $el,
+    actionList = Module.get('action-list'),
+    delayedListener = Module.get('delayed-listener'),
+    select,
     dataSource,
-    resourceTop,
-    Config;
+    Config,
+    uid = 0;
 
-  var dropdown = Module.get('dropdown');
-  var MODULE = 'dropdown-select';
-  var state = 'add';
+  var MODULE = 'select',
+    RESULT_COUNT = 10;
 
-  var DROPDOWN__SELECTOR = '.ring-dropdown';
-  var ITEMS_CONTAINER__SELECTOR = '.ring-dropdown__items';
-  var SEARCH_FIELD__SELECTOR = '.ring-js-dropdown-input';
-  var THROTTLE_INTERVAL = 250;
-  var INFINITE_SCROLL_TOP = 20;
-
+//  config = {
+//    target:DOM,
+//    dataSource: {return promise with action-list compatible data},
+//  }
   var init = function (config) {
-    if (!config || !config.targetElem || !config.dataSource) {
-      utils.log('dropdown-select init params missing');
-      return false;
-    }
-
-    dataSource = config.dataSource;
-    $target = $(config.targetElem);
-    resourceTop = INFINITE_SCROLL_TOP;
-
-
-    $target.on('click', function (e) {
-      e.stopPropagation();
-
-      dataSource(null, resourceTop).then(function (items) {
-        renderDropdown($target, items);
-        loadMoreEventBind();
-      });
-
-      $(document).on('keyup', SEARCH_FIELD__SELECTOR, utils.throttle(function (e) {
-        var query = $(e.currentTarget).val();
-        renderItems(query, resourceTop);
-      }, THROTTLE_INTERVAL));
-    });
-  };
-
-  var renderDropdown = function ($target, items) {
-    if (!$target.length || !items.length) {
-      utils.log('dropdown-select renderDropdown params missing');
-      return false;
-    }
-
-    dropdown('hide');
-    return dropdown('show', {
-      'type': 'dropdown-select',
-      'header-title':'Add to',
-      'footer-title':'Remove from',
-      'items': items
-    }, {
-      preventRemove: true,
-      target: $target
-    });
-  };
-
-  var renderItems = function (query, top) {
-    dataSource(query, top).then(function (items) {
-      var $items = $(View.render('dropdown__items', {
-        'items': items
-      }));
-      /**
-       * @todo refactor
-       */
-      $(DROPDOWN__SELECTOR).find(ITEMS_CONTAINER__SELECTOR).html($items);
-    });
-  };
-
-  var loadMoreEventBind = function () {
-    $(ITEMS_CONTAINER__SELECTOR).on('scroll', utils.throttle(function () {
-      loadMoreItems(this);
-    }, THROTTLE_INTERVAL));
-  };
-
-  var loadMoreItems = function (context) {
-    if ($(context).outerHeight() + $(context).scrollTop() === $(context).prop('scrollHeight')) {
-      resourceTop = resourceTop + INFINITE_SCROLL_TOP;
-      renderItems(null, resourceTop);
-      loadMoreEventBind();
-    }
-  };
-
-  var configureUrl = function (config, query, resourceTop) {
-    var auth = Module.get('auth'),
-      url;
-
-    if (state === 'add') {
-      url = config.add.url +
-        (resourceTop ? '&$top=' + resourceTop : '' ) +
-        (query ? '&query=name:' + query + ' or ' + query + '*' : '');
-    } else if (state === 'remove') {
-      auth('getUser').then(function (user) {
-        url = config.remove.url.replace('#{userId}', user.id);
-      });
-    }
-
-    return url;
-  };
-
-  var remoteDataSource = function (config) {
-    var auth = Module.get('auth'),
-      dropdownData = [];
+    select = Module.get(MODULE);
     Config = config;
 
-    return function (query, resourceTop) {
-      var dfd = $.Deferred(),
-        restUrl = configureUrl(config, query, resourceTop);
+    if (!Config && (!Config.target || !Config.dataSource)) {
+      utils.log('select: init params missing');
+      select.trigger('init:fail');
+      return false;
+    }
 
-      auth('get', restUrl).then(function (data) {
-        var groups;
+    dataSource = Config.dataSource;
 
-        if (data.groups) {
-          groups = data.groups;
-        } else if (data.total && data.usergroups) {
-          groups = data.usergroups;
-        } else {
-          dfd.resolve([
-            {
-              action: true,
-              label: 'No results'
-            }
-          ]);
-        }
+    uid += 1;
 
-        groups.forEach(function (group) {
-          dropdownData.push({
-            label: group.name,
-            url: group.url,
-            event: {
-              name: 'dropdown:select',
-              data: {
-                id: group.id
-              }
-            }
-          });
+    $(Config.target).
+      one('focus', function () {
+        delayedListener('init', {
+          target: $(Config.target),
+          onDelayedChange: function (data) {
+            _renderSuggest(data.value);
+          },
+          onDelayedCaretMove: function (data) {
+            _renderSuggest(data.value);
+          }
         });
-
-        dfd.resolve(dropdownData);
-        dropdownData = [];
+        $(Config.target).focus();
+      }).
+      on('blur', function () {
+        delayedListener('remove');
       });
 
-      return dfd.promise();
+    select.trigger('init:done', {});
+    return $el;
+  };
+
+  var remoteDataSource = function (remoteDataSourceConfig) {
+    var auth = Module.get('auth');
+
+    if (!remoteDataSourceConfig && (!remoteDataSourceConfig.hubResource || !remoteDataSourceConfig.url)) {
+      utils.log('Select: remoteDataSource params missing');
+      select.trigger('init:fail');
+      return false;
+    }
+
+    return function (query) {
+      var defer = $.Deferred(),
+        restUrl = remoteDataSourceConfig.url,
+        substr = ['query', '$top'],
+        suggestArgs = [encodeURI(query)];
+
+      substr.forEach(function (item, index) {
+        restUrl = restUrl.replace('#{' + item  + '}', suggestArgs[index] ? suggestArgs[index] : '');
+      });
+
+      restUrl = restUrl + '&$top=' + RESULT_COUNT;
+
+      auth('get', restUrl).then(function (data, state, jqXHR) {
+        var items = [];
+        if (data[remoteDataSourceConfig.hubResource]) {
+          items = data[remoteDataSourceConfig.hubResource].map(function (val) {
+            return {
+              'action': true,
+              'label': val.name
+            };
+          });
+        }
+        defer.resolve(items, state, jqXHR);
+      }).fail(function () {
+        defer.reject.apply(defer, arguments);
+      });
+      return defer.promise();
     };
   };
 
-  dropdown.on('select', function (data) {
-    Config[state].callback.call(this, data);
-    dropdown('hide');
-  });
+  var remove = function () {
+    if ($el) {
+      $el.remove();
+      $el = null;
+      actionList('remove');
+      select.trigger('remove:done');
+    }
+  };
 
-  dropdown.on('toggle', function () {
-    console.log(arguments);
-    /**
-     * @ToDo Toggle func exec
-     */
-  });
-
-  dropdown.on('hide:done', function () {
-    $(ITEMS_CONTAINER__SELECTOR).unbind();
-  });
+  var _renderSuggest = function (query) {
+    dataSource(query).then(function (data) {
+      if (data.length) {
+        actionList('init', {
+          target: $(Config.target),
+          type:['bound'],
+          width: 'auto',
+          items: data
+        });
+      }
+    });
+  };
 
   Module.add(MODULE, {
-    init: init,
+    init: {
+      method: init,
+      override: true
+    },
+    remove: {
+      method: remove,
+      override: true
+    },
     remoteDataSource: {
       method: remoteDataSource,
+      override: true
+    },
+    trigger: {
+      method: Module.triggerInstance.bind(null, MODULE, uid),
+      override: true
+    },
+    on: {
+      method: Module.onInstance.bind(null, MODULE, uid),
       override: true
     }
   });
