ea786659c1c0b8dab4959a6152b959e384c5a1a6
Maxim Barinov <barinovmaxim@gmail.com>
2014-02-26 04:00:28 +0400

    RG-255 Searchable block for dropdown
    Former-commit-id: 619a7c0de6bebc734fdc675c1eefb34b72cc10c4

--- blocks/dropdown-searchable/dropdown-searchable.js
+++ blocks/dropdown-searchable/dropdown-searchable.js
@@ -4,120 +4,93 @@ define([
   'global/global__modules',
   'global/global__events',
   'global/global__utils',
-  'dropdown/dropdown',
   'shortcuts/shortcuts',
-  'popup/popup',
+  'action-list/action-list',
   'delayed-listener/delayed-listener'
 ], function ($, View, Module, events, utils) {
   'use strict';
 
-  var DROPDOWN__SELECTOR = '.ring-dropdown',
-    ITEMS_CONTAINER__SELECTOR = '.ring-dropdown__items',
-    SEARCH_FIELD__SELECTOR = '.ring-js-dropdown-input',
-    THROTTLE_INTERVAL = 250,
-    INFINITE_SCROLL_TOP = 20;
+  var RESULT_COUNT = 5,
+    POPUP_INPUT_SELECTOR = '.ring-js-popup-input';
 
-  var dropdown = Module.get('dropdown'),
-    popup = Module.get('popup'),
-    MODULE = 'dropdown-select';
-
-  var $target,
-    dataSource,
-    resourceTop = INFINITE_SCROLL_TOP,
-    Config,
-    wrapper,
-    state = 'add';
-
+  var popup = Module.get('popup'),
+    MODULE = 'dropdown-searchable';
 
   var init = function (config) {
-    if (!config || !config.target || !config.dataSource) {
-      utils.log('dropdown-select init params missing');
+    var $target,
+      wrapper;
+
+    if (!config || !config.target || !config.actions) {
+      utils.log('dropdown-searchable: init params missing');
       return false;
     }
 
-    dataSource = config.dataSource;
     $target = $(config.target);
 
     $target.on('click', function (e) {
       wrapper = popup('init', config);
       _bindRemoveEvent(wrapper);
 
-      dataSource(null, resourceTop).then(function (items) {
-        renderDropdown(wrapper, $target, items);
-        loadMoreEventBind();
-      });
-
-      $(document).on('keyup', SEARCH_FIELD__SELECTOR, utils.throttle(function (e) {
-        var query = $(e.currentTarget).val();
-        renderItems(query, resourceTop);
-      }, THROTTLE_INTERVAL));
-
+      for (var i = 0; i < config.actions.length; i++) {
+        var obj = config.actions[i];
+        _render(obj);
+      }
       e.stopPropagation();
     });
-  };
 
-  var renderDropdown = function (wrapper, $target, items) {
-    if (!$target.length || !items.length) {
-      utils.log('dropdown-select renderDropdown params missing');
-      return false;
-    }
-
-//    var actionList = Module.get(MODULE);
-    var title = $(View.render('popup-control', {title: ''}));
-
-    wrapper.appendHTML(title);
-
-//    dropdown('hide');
-//    return dropdown('show', {
-//      'type': 'dropdown-select',
-//      'header-title': 'Add to',
-//      'footer-title': 'Remove from',
-//      'items': items
-//    }, {
-//      preventRemove: true,
-//      target: $target
-//    });
-  };
-
-  var renderItems = function (query, top) {
-    dataSource(query, top).then(function (items) {
-      var $items = $(View.render('dropdown__items', {
-        'items': items
+    var _render = function (action) {
+      var title = $(View.render('popup-control', {
+        title: action.title,
+        type: action.type,
+        input: true
       }));
-      /**
-       * @todo refactor
-       */
-      $(DROPDOWN__SELECTOR).find(ITEMS_CONTAINER__SELECTOR).html($items);
-    });
+
+      action.dataSource('').then(function (data) {
+        var actionList = Module.get('action-list');
+
+        var items = actionList('init', {
+          items: data
+        });
+        wrapper.appendHTML(title);
+        wrapper.appendHTML(items);
+        _bindDelayedListener(title.find(POPUP_INPUT_SELECTOR), action, items);
+      });
+    };
+
+    var _bindDelayedListener = function ($el, action) {
+      var delayedListener = Module.get('delayed-listener');
+      if ($el) {
+        delayedListener('init', {
+          target: $el,
+          onDelayedChange: function (data) {
+            action.dataSource(data.value).then(function (data) {
+              var actionList = Module.get('action-list');
+              var items = actionList('init', {
+                items: data
+              });
+              console.log(items);
+            });
+          }
+        });
+      }
+    };
   };
 
-  var loadMoreEventBind = function () {
-    $(ITEMS_CONTAINER__SELECTOR).on('scroll', utils.throttle(function () {
-      loadMoreItems(this);
-    }, THROTTLE_INTERVAL));
-  };
+  var configureUrl = function (config, query) {
+    /**
+     * @TODO add field expression for "users"
+     */
+    var url,
+      auth = Module.get('auth');
 
-  var loadMoreItems = function (context) {
-    if ($(context).outerHeight() + $(context).scrollTop() === $(context).prop('scrollHeight')) {
-      resourceTop = resourceTop + INFINITE_SCROLL_TOP;
-      renderItems(null, resourceTop);
-      loadMoreEventBind();
-    }
-  };
-
-  var configureUrl = function (config, query, resourceTop) {
-    var auth = Module.get('auth'),
-      url;
-
-    if (state === 'add') {
-      url = config.add.url +
-        (resourceTop ? '&$top=' + resourceTop : '' ) +
-        (query ? '&query=name:' + query + ' or ' + query + '*' : '');
-    } else if (state === 'remove') {
+    if (config.url.indexOf('#{userId}') !== -1) {
       auth('getUser').then(function (user) {
-        url = config.remove.url.replace('#{userId}', user.id);
+        config.url = config.url.replace('#{userId}', user.id);
       });
     }
+    url = config.url +
+      ('&$top=' + RESULT_COUNT) +
+      (query ? '&query=name:' + query + ' or ' + query + '*' : '');
 
     return url;
   };
@@ -125,19 +98,16 @@ define([
   var remoteDataSource = function (config) {
     var auth = Module.get('auth'),
       dropdownData = [];
-    Config = config;
 
-    return function (query, resourceTop) {
+    return function (query) {
       var dfd = $.Deferred(),
-        restUrl = configureUrl(config, query, resourceTop);
+        restUrl = configureUrl(config, query);
 
       auth('get', restUrl).then(function (data) {
-        var groups;
+        var items;
 
-        if (data.groups) {
-          groups = data.groups;
-        } else if (data.total && data.usergroups) {
-          groups = data.usergroups;
+        if (data[config.hubResource]) {
+          items = data[config.hubResource];
         } else {
           dfd.resolve([
             {
@@ -147,18 +117,21 @@ define([
           ]);
         }
 
-        groups.forEach(function (group) {
-          dropdownData.push({
-            label: group.name,
-            url: group.url,
-            event: {
-              name: 'dropdown:select',
-              data: {
-                id: group.id
+        if (items) {
+          items.forEach(function (item) {
+            dropdownData.push({
+              label: item.name,
+              url: item.url || '',
+              event: {
+                name: 'dropdown:select',
+                data: {
+                  id: item.id,
+                  name: item.name
+                }
               }
-            }
+            });
           });
-        });
+        }
 
         dfd.resolve(dropdownData);
         dropdownData = [];
@@ -168,7 +141,6 @@ define([
     };
   };
 
-
   var _bindRemoveEvent = function (wrapper) {
 
     $(document).one('click', function () {
@@ -176,27 +148,11 @@ define([
       popup('remove');
     });
 
-    $(wrapper.el).one('click', function (event) {
+    $(wrapper.el).on('click', function (event) {
       event.stopPropagation();
     });
 
   };
-
-  dropdown.on('select', function (data) {
-    Config[state].callback.call(this, data);
-    dropdown('hide');
-  });
-
-  dropdown.on('toggle', function () {
-    console.log(arguments);
-    /**
-     * @ToDo Toggle func exec
-     */
-  });
-
-  dropdown.on('hide:done', function () {
-    $(ITEMS_CONTAINER__SELECTOR).unbind();
-  });
 
   Module.add(MODULE, {
     init: init,
