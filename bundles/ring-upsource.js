0206def89719f260ee66bc5220e6dcc5d5fa54c1
Maxim Barinov <barinovmaxim@gmail.com>
2014-04-08 18:46:58 +0400

    fix jshint in ring-upsource bundle
    Former-commit-id: b2ac1270b8e2d3aee754c672639411d91680f1e5

--- bundles/ring-upsource.js
+++ bundles/ring-upsource.js
@@ -1,15 +1,15 @@
 define([
-    'global/global',
-    'auth/auth',
-    'menu/menu',
-    'header/header',
-    'header/header__userinfo',
-    'header/header__jetbrains-services',
-    'footer/footer',
-    'query-assist/query-assist',
-    'dropdown-menu/dropdown-menu',
-    'dropdown-filter/dropdown-filter',
-    'dropdown-select/dropdown-select'
-], function(ring) {
-    return ring;
+  'global/global',
+  'auth/auth',
+  'menu/menu',
+  'header/header',
+  'header/header__userinfo',
+  'header/header__jetbrains-services',
+  'footer/footer',
+  'query-assist/query-assist',
+  'dropdown-menu/dropdown-menu',
+  'dropdown-filter/dropdown-filter',
+  'dropdown-select/dropdown-select'
+], function (ring) {
+  return ring;
 });
