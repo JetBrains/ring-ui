c7b1473927a7d7b41c12e2c1d658430e39629bce
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-05-01 18:31:11 +0200

    fix for JPS-1138 option to allow unsafe message
    Former-commit-id: 3b417e7764ec0aff9f6b08b2dec1532c0468b8a1

--- blocks/alert/alert.ng.js
+++ blocks/alert/alert.ng.js
@@ -13,15 +13,19 @@
    */
     service('alert', [function () {
       var service = {};
+      var REPLACE_TABLE = {'&': '&amp;', '<': '&lt;', '>': '&gt;'};
 
       $.each(ring('alerts', 'getAlertTypes')(), function (name, type) {
-        service[type] = function (message, opt_timeout) {
+        service[type] = function (message, opt_timeout, opt_unsafe) {
+
           // Escape message
-          var tagsToReplace = {'&': '&amp;', '<': '&lt;', '>': '&gt;'};
-          message = message.replace(/[&<>]/g, function (tag) {
-              return tagsToReplace[tag] || tag;
-            }
-          );
+          if (opt_unsafe !== true) {
+            //TODO: Why don't we use angular $sanitize here?
+            message = message.replace(/[&<>]/g, function (tag) {
+                return REPLACE_TABLE[tag] || tag;
+              }
+            );
+          }
 
           return ring('alerts', 'add')(message, type, true, opt_timeout);
         };
