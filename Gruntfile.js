c30498fa21542d582760aad41906bad3b36a2a78
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-05-06 13:19:12 +0400

    RG-293 Run tests on Fx and Chrome instead of PhantomJS: remove PhantomJS
    Former-commit-id: dad220998330da6628479f747107fec43afd60d0

--- Gruntfile.js
+++ Gruntfile.js
@@ -206,7 +206,7 @@ module.exports = function(grunt) {
       },
       dist: {
         singleRun: true,
-        browsers: ['Firefox', 'ChromeNoSandbox', 'PhantomJS'],
+        browsers: ['Firefox', 'ChromeNoSandbox'],
         reporters: ['teamcity', 'coverage']
       },
       dev: {
