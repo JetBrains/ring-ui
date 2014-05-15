e3835273e490332e2c44dd14ba5c3964cce3224b
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-05-15 15:29:01 +0200

    #JPS-1355 fixed
    Former-commit-id: c71e4810d2ef0f80648f9cc508811c8d852149c9

--- blocks/breadcrumb/breadcrumb.ng.js
+++ blocks/breadcrumb/breadcrumb.ng.js
@@ -1,34 +1,20 @@
 (function () {
   'use strict';
-
+  /**
+   * <ring-breadcrumb title="{{title}} link="{{link}}">
+   *   <ring-breadcrumb title="{{title}} link="{{link}}">content</ring-breadcrumb>
+   * </ring-breadcrumb>">
+   */
   angular.module('Ring.breadcrumb', []).
-    directive('ringBreadcrumb', [function() {
+    directive('ringBreadcrumb', [function () {
       return {
         templateUrl: 'breadcrumb/breadcrumb.ng.html',
         replace: true,
         transclude: true,
         restrict: 'E',
-        scope: { path: '&' },
-        link: function($scope) {
-          $scope.calculatedPath = $scope.path();
-          angular.forEach($scope.calculatedPath, function (pe) {
-            if (pe.params) {
-              var result = pe.href;
-              var first = true;
-              angular.forEach(pe.params, function (pValue, pName) {
-                if (pValue) {
-                  if (first) {
-                    result += '?';
-                    first = false;
-                  } else {
-                    result += '&';
-                  }
-                  result += pName + '=' + pValue;
-                }
-              });
-              pe.href = result;
-            }
-          });
+        scope: {
+          label: '@',
+          title: '@'
         }
       };
     }]);
