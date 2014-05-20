5afd8ace63f181cc7ba26918cd9448d96559ebad
Maxim Barinov <barinovmaxim@gmail.com>
2014-05-20 15:28:47 +0400

    RG-266 update after code review
    Former-commit-id: 98839ebe37a22b639070441e6c1e90b431e2d4e4

--- blocks/confirm/confirm.ng.js
+++ blocks/confirm/confirm.ng.js
@@ -6,43 +6,40 @@
       'dialog',
       '$q',
       function (dialog, $q) {
-        this.show = function (message, description, actionTitle) {
+        this.show = function (message, description, actionTitle, cancelTitle) {
           var defer = $q.defer();
 
-          if (!message) {
-            message = 'Confirm action?';
-          }
-          if (!actionTitle) {
-            actionTitle = 'Confirm';
-          }
-
-          dialog.show({
-            content: 'confirm/confirm.ng.html',
-            data: {
-              message: message,
-              description: description
-            },
-            buttons: [
-              {
-                'label': actionTitle,
-                'default': true,
-                'close': false,
-                'action': function () {
-                  defer.resolve(true);
-                  dialog.hide();
-                  return true;
-                }
+          if (!message || !actionTitle || !cancelTitle) {
+            defer.reject();
+          } else {
+            dialog.show({
+              content: 'confirm/confirm.ng.html',
+              data: {
+                message: message,
+                description: description
               },
-              {
-                'label': 'Cancel',
-                'action': function () {
-                  defer.resolve(false);
-                  dialog.hide();
-                  return false;
+              buttons: [
+                {
+                  'label': actionTitle,
+                  'default': true,
+                  'close': false,
+                  'action': function () {
+                    defer.resolve(true);
+                    dialog.hide();
+                    return true;
+                  }
+                },
+                {
+                  'label': cancelTitle,
+                  'action': function () {
+                    defer.resolve(false);
+                    dialog.hide();
+                    return false;
+                  }
                 }
-              }
-            ]
-          });
+              ]
+            });
+          }
 
           return defer.promise;
         };
