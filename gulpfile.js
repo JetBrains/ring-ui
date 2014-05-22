b1988e8bac2c50497e2e4902ef664bf771b8a1b4
Maxim Barinov <barinovmaxim@gmail.com>
2014-05-23 01:28:33 +0400

    RG-282 commit in honnor of Leonya (versions instead of version in the gulp-autoprefixer)
    Former-commit-id: caed2942f4e99107b0c5818081f3277dc9786a7a

--- gulpfile.js
+++ gulpfile.js
@@ -108,7 +108,7 @@ gulp.task('styles', function () {
     pipe(scss({
       sourcemap: true
     })).
-    pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')).
+    pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')).
     pipe(minifycss()).
     pipe(gulp.dest(PATH.dist));
 });
