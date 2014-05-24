de9335fc1bfa0f2af1148786aa9a568ad43c7215
Maxim Barinov <barinovmaxim@gmail.com>
2014-05-24 18:01:05 +0400

    RG-282 gulp clean up && gulp build task
    Former-commit-id: d91c77ad72d9d53aa52fd12b2b8bb1301f008a17

--- blocks/hello-world/hello-world.jsx
+++ blocks/hello-world/hello-world.jsx
@@ -9,7 +9,7 @@ require('./hello-world.scss');
 var helloWorld = React.createClass({
   render: function () {
     return (
-      <div className='contaifffner'>
+      <div className='container'>
         <span className='page-header hello-world__title'>Hello</span>, <span className='hello-world__sub-title hello-world__first'>{this.props.firstPart}</span><span className='hello-world__sub-title hello-world__second'>{this.props.secondPart}!</span>
       </div>
       );
