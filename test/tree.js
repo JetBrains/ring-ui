fa15f706b80fa14d620d79a9e98dcebc1a9998cd
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-06-05 01:37:55 +0400

    Fix test for tree #RG-309 fixed
    Former-commit-id: b342e03146d9adb9afdae4d07e80f6b8fc1fa35e

--- test/tree.js
+++ test/tree.js
@@ -1,7 +1,5 @@
-define(['global/global', 'chai', 'tree/tree', 'chai-as-promised'], function(ring, chai, Tree) {
+define(['tree/tree'], function(Tree) {
   'use strict';
-
-  var expect = chai.expect;
 
   describe('Tree', function() {
     beforeEach(function() {
@@ -13,15 +11,13 @@ define(['global/global', 'chai', 'tree/tree', 'chai-as-promised'], function(ring
     it('should render element after create instance', function() {
       var tree = new Tree();
 
-//      expect(tree.$el.html()).to.be.ok();
-      expect(tree).to.be.a('object');
+      tree.$el.html().should.be.ok;
     });
 
     it('should render element if we pass not emply list', function() {
       var tree = new Tree([this.fileItemMock]);
 
-//      expect(tree.$el.html()).to.be.ok();
-      expect(tree).to.be.a('object');
+      tree.$el.html().should.be.ok;
     });
   });
 });
