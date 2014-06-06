cb88d6e265156543aa990a00ea0f4058b7b7c5c6
Igor Shevchenko <cyberskunk@gmail.com>
2014-06-06 14:52:52 +0400

    avoid unnecessary copying in "gulp test"
    Former-commit-id: 73b569d2633a4693473f4fc2ed8ff5ba9245ed5f

--- blocks/hello-world/__tests__/hello-world.test.js
+++ blocks/hello-world/__tests__/hello-world.test.js
@@ -2,12 +2,12 @@
 
 /** @jsx React.DOM */
 /* jshint ignore:start */
-jest.dontMock('../../../blocks/hello-world/hello-world.jsx');
+jest.dontMock('../hello-world.jsx');
 
 describe('hello-world', function () {
   it('test test', function () {
     var React = require('react/addons');
-    var HelloWorld = require('../../../blocks/hello-world/hello-world.jsx');
+    var HelloWorld = require('../hello-world.jsx');
     var TestUtils = React.addons.TestUtils;
 
     var helloworld = HelloWorld({
