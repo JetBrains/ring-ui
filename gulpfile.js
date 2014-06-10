2080f57b480814b7260d25c096c2ceee24af0797
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-06-10 21:08:23 +0400

    cleanup
    Former-commit-id: 218f0d0498f888e9792ad78070c1600cca0d260f

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,9 @@ var gulp = require('gulp'),
   path = require('path');
 var karma = require('gulp-karma');
 
+// Read configuration from package.json
+var pkgConfig = Object.create(require('./package.json'));
+
 var webpackConfig = {
   cache: true,
   output: {
@@ -36,29 +39,29 @@ var webpackConfig = {
       },
       // the url-loader uses DataUrls.
       // the file-loader emits files.
-      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf$/,    loader: "file-loader" },
-      { test: /\.eot$/,    loader: "file-loader" },
-      { test: /\.svg$/,    loader: "file-loader" }
+      { test: /\.woff$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/, loader: "file-loader" },
+      { test: /\.eot$/, loader: "file-loader" },
+      { test: /\.svg$/, loader: "file-loader" }
     ],
     noParse: /\.min\.js/
   },
   plugins: []
 };
 
-// The development server (the recommended option for development)
-//gulp.task("default", ["webpack-dev-server"]);
+//The development server (the recommended option for development)
+gulp.task("default", ["webpack-dev-server"]);
 
 // Build and watch cycle (another option for development)
 // Advantage: No server required, can run app from filesystem
 // Disadvantage: Requests are not blocked until bundle is available,
 //               can serve an old app on refresh
 gulp.task("build-dev", ["webpack:build-dev"], function () {
-  gulp.watch(["app/**/*"], ["webpack:build-dev"]);
+  gulp.watch(["src/**/*"], ["webpack:build-dev"]);
 });
 
 // Production build
-//gulp.task("build", ["webpack:build"]);
+gulp.task("build", ["test", "webpack:build"]);
 
 gulp.task("webpack:build", function (callback) {
   // modify some webpack config options
@@ -72,6 +75,9 @@ gulp.task("webpack:build", function (callback) {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   );
+
+  //Out filename
+  webpackConfig.output.filename = 'bundle.js';
 
   // run webpack
   webpack(webpackConfig, function (err, stats) {
@@ -100,9 +106,9 @@ gulp.task("webpack:build-dev", function (callback) {
   });
 });
 
-gulp.task("webpack-dev-server", function (callback) {
+gulp.task("webpack-dev-server", function () {
   // modify some webpack config options
-  var myConfig = Object.create(webpackConfig);
+  var myConfig = webpackConfig;
   myConfig.devtool = "eval";
   myConfig.debug = true;
 
@@ -116,15 +122,6 @@ gulp.task("webpack-dev-server", function (callback) {
       if (err) throw new gutil.PluginError("webpack-dev-server", err);
       gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
     });
-});
-
-gulp.task('clean', function () {
-  return gulp.src([
-    'dist'
-  ], {
-    read: false
-  }).
-    pipe(clean());
 });
 
 gulp.task('server', function () {
@@ -142,76 +139,15 @@ gulp.task('server', function () {
     beep();
 });
 
-gulp.task('watch', [
-  'webpack',
-  'fonts'
-], function () {
-  gulp.watch([
-    'blocks/**/*.jsx',
-    'blocks/**/*.js',
-    'blocks/**/*.scss'
-  ], function (file) {
-    gulp.start('webpack', function () {
-      gulp.src(file.path).
-        pipe(connect.reload());
-    });
-  });
-
-  gulp.start('server');
-});
-
-gulp.task('fonts', function () {
-  gulp.src([
-    'blocks/font-icon/**/*.woff',
-    'blocks/font-icon/**/*.eot',
-    'blocks/font-icon/**/*.ttf',
-    'blocks/font-icon/**/*.svg'
-  ]).
-    pipe(gulp.dest('dist/fonts'));
-});
-
-gulp.task('styles', function () {
-  return gulp.src('bundles/**/*.scss').
-    pipe(scss({
-      sourcemap: true
-    })).
-    pipe(autoprefixer('last 2 versions', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4')).
-    pipe(minifycss()).
-    pipe(gulp.dest('dist'));
-});
-
-
-gulp.task('webpack', function (cb) {
-  webpack(webpackConfig, function (err) {
-    if (err) {
-      throw new gutil.PluginError('execWebpack', err);
-    }
-    cb();
-  });
-});
-
-gulp.task('build', function () {
-  gulp.start('clean', 'fonts', 'test');
-  webpackConfig.plugins = webpackConfig.plugins.concat(new webpack.optimize.UglifyJsPlugin());
-  webpackConfig.output.filename = 'bundle.js';
-  gulp.start('webpack');
-});
-
-gulp.task('test', function() {
+gulp.task('test', function () {
   // Be sure to return the stream
   return gulp.src(['test/**/*.js'])
     .pipe(karma({
       configFile: 'karma.conf.js',
       action: 'run'
     }))
-    .on('error', function(err) {
+    .on('error', function (err) {
       // Make sure failed tests cause gulp to exit non-zero
       throw err;
     });
-});
-
-gulp.task('default', [
-  'clean'
-], function () {
-  gulp.start('watch');
 });
