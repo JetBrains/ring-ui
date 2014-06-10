1598533badbd2fc72f40b1b8a51052d29fb565ea
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-06-10 20:51:11 +0400

    fix deps
    Former-commit-id: 2bccbfa79579fc89209746495d7d5d9876c28804

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,13 @@ var webpackConfig = {
       {
         test: /\.js$/,
         loader: 'jsx'
-      }
+      },
+      // the url-loader uses DataUrls.
+      // the file-loader emits files.
+      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/,    loader: "file-loader" },
+      { test: /\.eot$/,    loader: "file-loader" },
+      { test: /\.svg$/,    loader: "file-loader" }
     ],
     noParse: /\.min\.js/
   },
