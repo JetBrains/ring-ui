a1e58a6ac8773b2e2e698ae299fa8fef62543e59
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-06-10 12:41:35 +0400

    gulpfile in progress
    Former-commit-id: 8e96506f7adbf94b2b56cfdba903de6c418713de

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,106 @@ var gulp = require('gulp'),
   spawn = require('child_process').spawn,
   path = require('path');
 
+var webpackConfig = {
+  cache: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'Ring'
+  },
+  resolve: {
+    modulesDirectories: [
+      'node_modules'
+    ]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style!css!autoprefixer-loader?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4!sass?outputStyle=expanded!'
+      },
+      {
+        test: /\.js$/,
+        loader: 'jsx'
+      }
+    ],
+    noParse: /\.min\.js/
+  },
+  plugins: []
+};
+
+// The development server (the recommended option for development)
+//gulp.task("default", ["webpack-dev-server"]);
+
+// Build and watch cycle (another option for development)
+// Advantage: No server required, can run app from filesystem
+// Disadvantage: Requests are not blocked until bundle is available,
+//               can serve an old app on refresh
+gulp.task("build-dev", ["webpack:build-dev"], function () {
+  gulp.watch(["app/**/*"], ["webpack:build-dev"]);
+});
+
+// Production build
+//gulp.task("build", ["webpack:build"]);
+
+gulp.task("webpack:build", function (callback) {
+  // modify some webpack config options
+  webpackConfig.plugins = webpackConfig.plugins.concat(
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  );
+
+  // run webpack
+  webpack(webpackConfig, function (err, stats) {
+    if (err) throw new gutil.PluginError("webpack:build", err);
+    gutil.log("[webpack:build]", stats.toString({
+      colors: true
+    }));
+    callback();
+  });
+});
+
+gulp.task("webpack:build-dev", function (callback) {
+  // modify some webpack config options
+  var myDevConfig = Object.create(webpackConfig);
+  myDevConfig.devtool = "sourcemap";
+  myDevConfig.debug = true;
+
+  // create a single instance of the compiler to allow caching
+  // run webpack
+  webpack(myDevConfig).run(function (err, stats) {
+    if (err) throw new gutil.PluginError("webpack:build-dev", err);
+    gutil.log("[webpack:build-dev]", stats.toString({
+      colors: true
+    }));
+    callback();
+  });
+});
+
+gulp.task("webpack-dev-server", function (callback) {
+  // modify some webpack config options
+  var myConfig = Object.create(webpackConfig);
+  myConfig.devtool = "eval";
+  myConfig.debug = true;
+
+  // Start a webpack-dev-server
+  new WebpackDevServer(webpack(myConfig), {
+    publicPath: "/" + myConfig.output.publicPath,
+    stats: {
+      colors: true
+    }
+  }).listen(8080, "localhost", function (err) {
+      if (err) throw new gutil.PluginError("webpack-dev-server", err);
+      gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+});
+
 gulp.task('clean', function () {
   return gulp.src([
     'dist'
@@ -73,37 +173,6 @@ gulp.task('styles', function () {
     pipe(gulp.dest('dist'));
 });
 
-var webpackConfig = {
-  entry: './site/webpack.js',
-  target: 'web',
-  debug: false,
-  devtool: false,
-  watch: false,
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    library: 'Ring'
-  },
-  resolve: {
-    modulesDirectories: [
-      'node_modules'
-    ]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        loader: 'style!css!autoprefixer-loader?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4!sass?outputStyle=expanded!'
-      },
-      {
-        test: /\.js$/,
-        loader: 'jsx'
-      }
-    ],
-    noParse: /\.min\.js/
-  },
-  plugins: []
-};
 
 gulp.task('webpack', function (cb) {
   webpack(webpackConfig, function (err) {
