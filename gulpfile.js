6e4a42c4b1132311431ca142f691d12becfddcc9
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-06-13 14:28:34 +0400

    fix tests cleanup and stuff
    Former-commit-id: 4c26c96a2fd710068f4aaae4b23a5a0820b509f8

--- gulpfile.js
+++ gulpfile.js
@@ -14,35 +14,13 @@ var karma = require('gulp-karma');
 // Read configuration from package.json
 var pkgConfig = Object.create(require('./package.json'));
 
-var webpackConfig = {
-  cache: true,
-  entry: path.join(__dirname, pkgConfig.src, 'blocks/hello-world/hello-world.jsx'),
-  output: {
-    path: path.join(__dirname, pkgConfig.dist),
-    filename: 'bundle.js',
-    library: 'Ring'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.scss$/,
-        loader: 'style!css!autoprefixer-loader?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4!sass?outputStyle=expanded!'
-      },
-      //jsx loader
-      {
-        test: /\.jsx$/,
-        loader: 'jsx-loader'
-      },
-      //images loader
-      { test: /\.png$/, loader: "file-loader" },
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
-      { test: /\.woff$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf$/, loader: "file-loader" },
-      { test: /\.eot$/, loader: "file-loader" },
-      { test: /\.svg$/, loader: "file-loader" }
-    ]
-  }
+var webpackConfig = Object.create(require('./webpack.conf.js'));
+
+webpackConfig.entry = path.join(__dirname, pkgConfig.src, 'blocks/hello-world/hello-world.jsx');
+webpackConfig.output = {
+  path: path.join(__dirname, pkgConfig.dist),
+  filename: 'bundle.js',
+  library: 'Ring'
 };
 
 //The development server (the recommended option for development)
