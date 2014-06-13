7bb859c3f0d8dceb372c8cac9a16d75f5baccc52
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-06-13 13:45:20 +0400

    fix dev-server
    Former-commit-id: e8d786dd01b7f4267c771bd9c62cd9fcb4cbb37f

--- src/blocks/hello-world/hello-world.jsx
+++ src/blocks/hello-world/hello-world.jsx
@@ -6,14 +6,27 @@ var React = require('react');
 
 require('./hello-world.scss');
 
+var url = require('file!./yeoman.png');
+
 var helloWorld = React.createClass({
   render: function () {
     return (
       <div className='container'>
-        <span className='page-header hello-world__title'>Hello</span>, <span className='hello-world__sub-title hello-world__first'>{this.props.firstPart}</span><span className='hello-world__sub-title hello-world__second'>{this.props.secondPart}</span>!
+        <span className='page-header hello-world__title'>Hello</span>,
+        <span className='hello-world__sub-title hello-world__first'>{this.props.firstPart}</span>
+        <span className='hello-world__sub-title hello-world__second'>{this.props.secondPart}</span>
+        <img src={url}/>
+      !
       </div>
       );
+  },
+  append: function (element) {
+
   }
 });
 
-module.exports = helloWorld;
+module.exports = {
+  helloWorld: function(params, element) {
+    React.renderComponent(helloWorld(params), element);
+  }
+};
