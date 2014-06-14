2bdacf32b10cb4c7263a0e8568a2a8c1574d583a
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-06-14 13:17:39 +0200

    Improve utils promises tests
    Former-commit-id: 94575567604d6b104eb745b0407eb2f29fd20e1b

--- test/utils.js
+++ test/utils.js
@@ -2,23 +2,32 @@ define(['global/global__utils', 'jquery', 'q'], function(utils, $, q) {
   'use strict';
 
   describe('Utils', function () {
-    describe('Promises', function () {
-      it('isDeferred', function () {
+    describe('Promises utils', function () {
+      it('isDeferred should detect jQuery Deferreds', function () {
         utils.isDeferred($.Deferred()).should.be.true;
         utils.isDeferred($.Deferred().promise()).should.be.true; // Yes, it's jQuery
+      });
+
+      it('isDeferred should not detect any other deferreds and promises', function () {
         utils.isDeferred(q.defer()).should.be.false;
         utils.isDeferred(q.defer().promise).should.be.false;
       });
 
-
-      it('isPromise', function () {
+      it('isPromise should detect jQuery Deferreds', function () {
         utils.isPromise($.Deferred()).should.be.true; // Yes, it's jQuery
-        utils.isDeferred($.Deferred().promise()).should.be.true;
+        utils.isPromise($.Deferred().promise()).should.be.true;
+      });
+
+      it('isPromise should not detect other deferreds', function () {
         utils.isPromise(q.defer()).should.be.false;
+      });
+
+      it('isPromise should detect other promises', function () {
         utils.isPromise(q.defer().promise).should.be.true;
       });
 
-      it('warpPromise', function () {
+
+      it('wrapPromise should properly wrap proper promise', function () {
         var dfd = q.defer();
         var handler = sinon.spy();
 
@@ -32,6 +41,16 @@ define(['global/global__utils', 'jquery', 'q'], function(utils, $, q) {
         return dfd.promise.then(function() {
           handler.should.have.been.calledTwice;
         });
+      });
+
+      it('wrapPromise should properly wrap object', function () {
+        var object = {};
+
+        return utils
+          .wrapPromise(object)
+          .done(function(result) {
+            result.should.equal(object);
+          });
       });
     });
   });
