f100138f32e62d718e3b9eca99705e0ec9a6e52b
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2014-07-02 17:27:44 +0200

    RG-325 Port diff graph renderer from Upsource - type annotations, fixes
    Former-commit-id: 8964afe63cef51f579b47399a6268d267be2b06a

--- src/blocks/revision-graph/revision-graph.js
+++ src/blocks/revision-graph/revision-graph.js
@@ -1,204 +1,255 @@
 function RevisionGraph() {
   var POSITION_WIDTH = 16;                          // Single position width (px)
-  var MAX_COLOR = 10;
+  var MAX_COLOR = 10;                               // Color map size
   var ARROW_DOWN = "l0,10 l-5,-5 l5,5.5 l5,-5.5 ";  // .5 because line width is 2px
   var ARROW_UP = "l0,-10 l5,5 l-5,-5.5 l-5,5.5 ";   // .5 because line width is 2px
 
-  var nodeTypes = { NODE: 0x01, UP_ARROW: 0x02, DOWN_ARROW: 0x02};
+  /**
+   *  Node types
+   *  @enum {Number}
+   */
+  var nodeTypes = { NODE: 0x01, ARROW_UP: 0x02, ARROW_DOWN: 0x02};
 
-  // Elements
-  var myRevisionsContainer;
-  var mySvg;
+  /**
+   * @type {Element}
+   */
+  var revisionsContainer_;
 
-  // Adaptive UI: window resize support
-  var myResizeCommand;
-  var myWindowResizeHandler;
-  var myResizeEventHandler;
+  /**
+   * @type {Element}
+   */
+  var svg_;
 
-  var myGraph;
-  var myRevisions = [];
+  /**
+   * @type {Function}
+   */
+  var resizeCommand_;
 
-  // Graph + revisions list width (px)
-  var myFullWidth;
-  // Graph height (px)
-  var myGraphHeight;
-  // Item number to item offset map
-  var myOffsetsCache = {};
+  /**
+   * Graph data
+   */
+  var graph_;
 
-  function RevisionGraph(revisionsContainer, eventBus) {
-    myRevisionsContainer = revisionsContainer;
-    mySvg = createSvgElement("svg");
+  /**
+   * Revisions data
+   * @type {Array}
+   */
+  var revisions_ = [];
+
+  /**
+   * Graph + revisions list width (px)
+   * @type {Number}
+   */
+  var fullWidth_;
+
+  /**
+   * Graph height (px)
+   * @type {Number}
+   */
+  var graphHeight_;
+
+  /**
+   * Item number to item offset map
+   * @type {Object}
+   */
+  var offsetsCache_ = {};
+
+  /**
+   * @param {Element} revisionsContainer
+   * @constructor
+   */
+  function RevisionGraph(revisionsContainer) {
+    revisionsContainer_ = revisionsContainer;
+    svg_ = createSvgElement("svg");
 
     // SVG is not supported
-    if (mySvg == null) {
+    if (svg_ == null) {
       return;
     }
 
     setStyleName("revision-graph");
-    getElement().appendChild(mySvg);
+    getElement().appendChild(svg_);
 
-    myResizeCommand = $.debounce(function() {
+    resizeCommand_ = $.debounce(function () {
       // Check width, reset offsets cache if differs
-      var fullWidth = myRevisionsContainer.getParent().getOffsetWidth();
-      if (fullWidth != myFullWidth) {
-        myFullWidth = fullWidth;
-        myOffsetsCache = {};
+      var fullWidth = $(revisionsContainer_).parent().width();
+      if (fullWidth != fullWidth_) {
+        fullWidth_ = fullWidth;
+        offsetsCache_ = {};
       }
 
       // Check height, re-render the graph if differs
-      if (getElement().getOffsetHeight() != myGraphHeight) {
+      if (getElement().getOffsetHeight() != graphHeight_) {
         adjustRevisionsContainer();
         var graphHeight = getElement().getOffsetHeight();
-        myGraphHeight = graphHeight;
-        mySvg.getStyle().setHeight(graphHeight);
+        graphHeight_ = graphHeight;
+        svg_.style.height = graphHeight + "px";
         render();
       }
     }, 50, false);
 
-    myWindowResizeHandler = $(window).resize(function() {
-        myResizeCommand();
+    $(window).on('resize.revisionGraph', function () {
+      resizeCommand_();
     });
-
-    myResizeEventHandler = function() {
-      resize(true);
-    };
   }
 
   function setGraph(graph) {
-    myGraph = graph;
+    graph_ = graph;
   }
 
   function setRevisions(revisions, append) {
     if (!append) {
-      myRevisions = [];
+      revisions_ = [];
     }
 
-    myRevisions.concat(revisions);
+    revisions_.concat(revisions);
   }
 
   function resize(force) {
     if (force) {
-      myOffsetsCache = {};
+      offsetsCache_ = {};
     }
-    if (myResizeCommand != null) {
-      myResizeCommand();
-    }
+
+    resizeCommand_ && resizeCommand_();
   }
 
   // Implementation
 
   function onUnload() {
-    if (myWindowResizeHandler != null) {
-      myWindowResizeHandler.removeHandler();
-      myWindowResizeHandler = null;
-    }
+    $(window).off('resize.revisionGraph');
 
-    if (myResizeEventHandler != null) {
-      myResizeEventHandler.removeHandler();
-      myResizeEventHandler = null;
-    }
-
-    myGraph = null;
-    myRevisions = [];
-    myOffsetsCache = {};
+    graph_ = null;
+    revisions_ = [];
+    offsetsCache_ = {};
   }
 
   function adjustRevisionsContainer() {
     var graphWidth = getGraphWidth();
     var marginLeft = graphWidth > 0 ? graphWidth + 8 : 0;
-    myRevisionsContainer.getElement().getStyle().setProperty("marginLeft", marginLeft);
+    revisionsContainer_.style.marginLeft = marginLeft + "px";
   }
 
   function render() {
-    if (myGraph == null) {
+    if (graph_ == null) {
       return;
     }
 
-    mySvg.removeAllChildren();
-    mySvg.getStyle().setWidth(getGraphWidth());
+    while (svg_.firstChild) {
+      svg_.removeChild(svg_.firstChild);
+    }
 
-    var nodeGroupIndex = {}; // int, element
-    var solidEdgeGroupIndex = {}; // int, element
-    var dashedEdgeGroupIndex = {}; // int, element
-    var buildersIndex = {}; // element, string
+    svg_.style.width = getGraphWidth() + "px";
 
-    var rowsArray = myGraph.getRowsArray();
-    if (rowsArray == null || rowsArray.length == 0) { return; }
+    /**
+     * @type {{Number, Element}}
+     */
+    var nodeGroupIndex = {};
+
+    /**
+     * @type {{Number, Element}}
+     */
+    var solidEdgeGroupIndex = {};
+
+    /**
+     * @type {{Number, Element}}
+     */
+    var dashedEdgeGroupIndex = {};
+
+    /**
+     * @type {{Element, String}}
+     */
+    var buildersIndex = {};
+
+    var rowsArray = graph_.getRowsArray();
+    if (rowsArray == null || rowsArray.length == 0) {
+      return;
+    }
+
     for (var rowIdx = 0; rowIdx < rowsArray.length; ++rowIdx) {
-      var rowDTO = rowsArray[rowIdx];
+      var rowData = rowsArray[rowIdx];
 
-      var nodesArray = rowDTO.getNodesArray();
-      if (nodesArray == null || nodesArray.length == 0) { continue; }
+      var nodesArray = rowData.getNodesArray();
+      if (nodesArray == null || nodesArray.length == 0) {
+        continue;
+      }
       for (var j = 0; j < nodesArray.length; ++j) {
-        var nodeDTO = nodesArray[j];
-        var position = nodeDTO.getPosition();
-        var color = nodeDTO.getColor();
-        var type = nodeDTO.getType();
+        var nodeData = nodesArray[j];
+        var nodePosition = nodeData.getPosition();
+        var nodeColor = nodeData.getColor();
+        var nodeType = nodeData.getType();
 
-        var groupElement = getGroupByColor(color, true, null, nodeGroupIndex);
-        if (type == nodeTypes.NODE) {
+        var nodeGroupElement = getGroupByColor(nodeColor, true, null, nodeGroupIndex);
+        if (nodeType == nodeTypes.NODE) {
           var commitNode = createNodeElement();
-          commitNode.setAttribute("cx", calculateOffsetLeft(position));
+          commitNode.setAttribute("cx", calculateOffsetLeft(nodePosition));
           commitNode.setAttribute("cy", getOffsetTop(rowIdx));
 
-          if (myRevisions.length > rowIdx) {
-            var revision = myRevisions[rowIdx];
+          if (revisions_.length > rowIdx) {
+            var revision = revisions_[rowIdx];
             commitNode.setAttribute("title", getNodeTitle(revision));
             commitNode.setAttribute("id", "node-" + revision.getRevisionIdShort());
           }
 
-          groupElement.appendChild(commitNode);
+          nodeGroupElement.appendChild(commitNode);
         } else {
-          var builder = getBuilderByElement(groupElement, buildersIndex);
-          var moveX = calculateOffsetLeft(position);
-          var moveY = getOffsetTop(rowIdx);
-          moveTo(moveX, moveY, builder);
-          builder.append(type == nodeTypes.DOWN_ARROW ? ARROW_DOWN : ARROW_UP);
+          var nodeBuilder = getBuilderByElement(nodeGroupElement, buildersIndex);
+          var nodeMoveX = calculateOffsetLeft(nodePosition);
+          var nodeMoveY = getOffsetTop(rowIdx);
+          moveTo(nodeMoveX, nodeMoveY, nodeBuilder);
+          nodeBuilder.append(nodeType == nodeTypes.ARROW_DOWN ? ARROW_DOWN : ARROW_UP);
         }
       }
 
-      var edgesArray = rowDTO.getEdgesArray();
-      if (edgesArray == null || edgesArray.length == 0) { continue; }
+      var edgesArray = rowData.getEdgesArray();
+      if (edgesArray == null || edgesArray.length == 0) {
+        continue;
+      }
       for (var k = 0; k < edgesArray.length; ++k) {
-        var edgeDTO = edgesArray[k];
-        var position = edgeDTO.getPosition();
-        var toPosition = edgeDTO.getToPosition();
-        var isUp = edgeDTO.getIsUp();
-        var color = edgeDTO.getColor();
-        var isSolid = edgeDTO.getIsSolid();
+        var edgeData = edgesArray[k];
+        var edgePosition = edgeData.getPosition();
+        var edgeToPosition = edgeData.getToPosition();
+        var edgeColor = edgeData.getColor();
 
-        var groupElement = getGroupByColor(color, false, null, nodeGroupIndex); // Make sure the node colors are
-        var className = groupElement.getAttribute("class");                     // consistent with edge colors.
+        var isUp = edgeData.getIsUp();
+        var isSolid = edgeData.getIsSolid();
+
+        var edgeGroupElement = getGroupByColor(edgeColor, false, null, nodeGroupIndex); // Make sure the node colors are
+        var className = edgeGroupElement.getAttribute("class");                         // consistent with edge colors.
         if (!isSolid) {
-          groupElement = getGroupByColor(color, false, className + " revision-graph__dashed", dashedEdgeGroupIndex);
+          edgeGroupElement = getGroupByColor(edgeColor, false, className + " revision-graph__dashed", dashedEdgeGroupIndex);
         } else {
-          groupElement = getGroupByColor(color, false, className, solidEdgeGroupIndex);
+          edgeGroupElement = getGroupByColor(edgeColor, false, className, solidEdgeGroupIndex);
         }
-        var builder = getBuilderByElement(groupElement, buildersIndex);
+        var edgeBuilder = getBuilderByElement(edgeGroupElement, buildersIndex);
 
-        var moveX = calculateOffsetLeft(position);
-        var moveY = getOffsetTop(rowIdx);
-        moveTo(moveX, moveY, builder);
+        var edgeMoveX = calculateOffsetLeft(edgePosition);
+        var edgeMoveY = getOffsetTop(rowIdx);
+        moveTo(edgeMoveX, edgeMoveY, edgeBuilder);
 
-        var lineX = calculateOffsetLeft(toPosition);
+        var lineX = calculateOffsetLeft(edgeToPosition);
         var lineY = getOffsetTop(isUp ? rowIdx - 1 : rowIdx + 1);
-        lineTo(lineX, lineY, builder);
+        lineTo(lineX, lineY, edgeBuilder);
       }
     }
 
     for (var entry in buildersIndex) {
-      var groupElement = entry.getKey();
-      var builder = entry.getValue();
+      if (buildersIndex.hasOwnProperty(entry)) {
+        var groupElement = entry.getKey();
+        var builder = entry.getValue();
 
-      if (builder.length > 0) {
-        builder = builder.substring(0, builder.length - 2); // cut the last space off
-        var element = createPathElement();
-        element.setAttribute("d", builder);
-        groupElement.appendChild(element);
+        if (builder.length > 0) {
+          builder = builder.substring(0, builder.length - 2); // cut the last space off
+          var element = createPathElement();
+          element.setAttribute("d", builder);
+          groupElement.appendChild(element);
+        }
       }
     }
   }
 
+  /**
+   * @returns {Element}
+   */
   function createNodeElement() {
     var node = createSvgElement("circle");
     node.setAttribute("class", "revision-graph__node");
@@ -206,44 +257,75 @@ function RevisionGraph() {
     return node;
   }
 
+  /**
+   * @returns {Element}
+   */
   function createPathElement() {
     var line = createSvgElement("path");
     line.setAttribute("class", "revision-graph__line");
     return line;
   }
 
-  function createSvgElement(name){
+  /**
+   * @param {String} name
+   * @returns {Element|null}
+   */
+  function createSvgElement(name) {
     if (Modernizr.svg) {
       return null;
     }
     return document.createElementNS("http://www.w3.org/2000/svg", name);
-   }
+  }
 
+  /**
+   * @param {Number} lineX
+   * @param {Number} lineY
+   * @param {String} builder
+   * @returns {String}
+   */
   function lineTo(lineX, lineY, builder) {
     builder += 'L' + lineX + ',' + lineY + ' ';
     return builder;
   }
 
+  /**
+   * @param {Number} moveX
+   * @param {Number} moveY
+   * @param {String} builder
+   * @returns {String}
+   */
   function moveTo(moveX, moveY, builder) {
     builder += 'M' + moveX + ',' + moveY + ' ';
     return builder;
   }
 
+  /**
+   * @param {String} color
+   * @param {Boolean} isAppend
+   * @param {String} classValue
+   * @param {Number} groupIndex
+   * @returns {Element}
+   */
   function getGroupByColor(color, isAppend, classValue, groupIndex) {
     var element = groupIndex[color];
     if (element == null) {
       element = createSvgElement("g");
       element.setAttribute("class", classValue != null ? classValue : "revision-graph__group_" + (groupIndex.length % MAX_COLOR));
-      groupIndex.put(color, element);
+      groupIndex[color] = element;
       if (isAppend) {
-        mySvg.appendChild(element);
+        svg_.appendChild(element);
       } else {
-        mySvg.insertFirst(element);
+        svg_.insertBefore(element, svg_.firstChild);
       }
     }
     return element;
   }
 
+  /**
+   * @param {Element} element
+   * @param buildersIndex
+   * @returns {String}
+   */
   function getBuilderByElement(element, buildersIndex) {
     var builder = buildersIndex[element];
     if (builder == null) {
@@ -253,12 +335,20 @@ function RevisionGraph() {
     return builder;
   }
 
+  /**
+   * @param revision
+   * @returns {String}
+   */
   function getNodeTitle(revision) {
     var revisionId = revision.getRevisionIdShort();
     var branchLabels = getBranchLabels(revision);
     return branchLabels.length > 0 ? revisionId + " (" + branchLabels.join(", ") + ")" : revisionId;
   }
 
+  /**
+   * @param revision
+   * @returns {Array}
+   */
   function getBranchLabels(revision) {
     var branchAvailability = revision.getBranchAvailability();
     if (branchAvailability != null) {
@@ -271,23 +361,38 @@ function RevisionGraph() {
     return [];
   }
 
+  /**
+   * @returns {Number}
+   */
   function getGraphWidth() {
-    return myGraph == null ? 0 : myGraph.offsetWidth * POSITION_WIDTH;
+    return graph_ == null ? 0 : graph_.offsetWidth * POSITION_WIDTH;
   }
 
+  /**
+   * @param {Number} position
+   * @returns {Number}
+   */
   function calculateOffsetLeft(position) {
     return POSITION_WIDTH * position + POSITION_WIDTH / 2;
   }
 
+  /**
+   * @param {Number} nodeIndex
+   * @returns {Number}
+   */
   function getOffsetTop(nodeIndex) {
-    var activityItemOffsetTop = myOffsetsCache[nodeIndex];
+    var activityItemOffsetTop = offsetsCache_[nodeIndex];
     if (activityItemOffsetTop == null) {
       activityItemOffsetTop = calculateOffsetTop(nodeIndex);
-      myOffsetsCache[nodeIndex] = activityItemOffsetTop;
+      offsetsCache_[nodeIndex] = activityItemOffsetTop;
     }
     return activityItemOffsetTop;
   }
 
+  /**
+   * @param {Number} index
+   * @returns {Number}
+   */
   function calculateOffsetTop(index) {
     var item = $('.revision-item_revision').eq(index);
     return item.length > 0 ? parseInt(item.position().top) + parseInt(item.css('padding-top')) + 9 : 0;
