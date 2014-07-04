583ab5788462ce42469ac5755a3685f9873cdd90
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-07-04 16:17:02 +0400

    Port checkbox on react
    Former-commit-id: d497837560e8edd3647373c0c3b999929ab67cab

---
+++ src/blocks/checkbox/checkbox.jsx
@@ -0,0 +1,77 @@
+/** @jsx React.DOM */
+
+'use strict';
+
+require('./checkbox.scss');
+
+var React = require('react');
+var ReactPropTypes = React.PropTypes;
+
+var idPrefix = '\\x0';
+var generateUniqueId = (function (prefix, idCounter) {
+  return function () {
+    var id = String(idCounter++);
+    return prefix + id;
+  };
+}(idPrefix, 0));
+
+var Checkbox = React.createClass({
+  propTypes: {
+    name: ReactPropTypes.string,
+
+    /**
+     * Add custom class for checkbox
+     */
+    className: ReactPropTypes.string,
+
+    /**
+     * Add id for component. If user does not pass id
+     * we generate unique id for correct work checkbox
+     */
+    id: ReactPropTypes.string
+  },
+
+  getDefaultProps: function () {
+    return {
+      id: generateUniqueId(),
+      className: ''
+    };
+  },
+
+  /**
+   * Return native input node
+   * @returns {DOMElement}
+   */
+  getInputDOMNode: function () {
+    return this.refs.input.getDOMNode();
+  },
+
+  /**
+   * Return native label node
+   * which is responsible for the look of checkbox
+   * @returns {DOMElement}
+   */
+  getLabelDOMNode: function () {
+    return this.refs.label.getDOMNode();
+  },
+
+  render: function () {
+    return (
+      <span className={this.props.className}>
+        {this.transferPropsTo(<input
+        ref="input"
+        className="ring-checkbox"
+        type="checkbox"
+        />)}
+
+        <label
+        ref="label"
+        className="ring-checkbox__label"
+        htmlFor={this.props.id}>
+        </label>
+      </span>
+      );
+  }
+});
+
+module.exports = Checkbox;
