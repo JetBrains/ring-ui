7f0691639e83cd4c1c2c79a14d8d8d764f0b344a
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-07-04 19:56:20 +0400

    Add example progress-bar and checkbox
    Former-commit-id: 6e5c02a017f5f845ffdb9e7f65c9e2c6f6f80ae6

--- src/ring.js
+++ src/ring.js
@@ -1,4 +1,7 @@
+global.react = require('react');
+
 module.exports = {
   ProgressBar: require('./blocks/progress-bar/progress-bar.jsx'),
+  Checkbox: require('./blocks/checkbox/checkbox.jsx'),
   helloWorld: require('./blocks/hello-world/hello-world.jsx').helloWorld
 };
