35312cd2be45063606640d7a99f8efff4fdb61a6
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-07-07 19:21:18 +0400

    Get rind of hello-world
    Former-commit-id: 2e02e3030bbd3e4a8d1fe2d400a2a6a30269cc10

--- src/ring.js
+++ src/ring.js
@@ -3,6 +3,5 @@ global.react = require('react');
 module.exports = {
   Checkbox: require('./components/checkbox/checkbox.jsx'),
   DropdownMixin: require('./components/dropdown/dropdown.jsx'),
-  ProgressBar: require('./components/progress-bar/progress-bar.jsx'),
-  helloWorld: require('./components/hello-world/hello-world.jsx').helloWorld
+  ProgressBar: require('./components/progress-bar/progress-bar.jsx')
 };
