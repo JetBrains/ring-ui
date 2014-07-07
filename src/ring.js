d3b44010b51ef50aa06e46a0c9a759551025870d
Igor Alexeenko <alexeenko.igor@gmail.com>
2014-07-07 13:21:41 +0400

    Add basic dropdown mixin.
    Former-commit-id: 8927c7b06db9dacd396ee1b55e30f01eabbbef1c

--- src/ring.js
+++ src/ring.js
@@ -1,7 +1,8 @@
 global.react = require('react');
 
 module.exports = {
-  ProgressBar: require('./blocks/progress-bar/progress-bar.jsx'),
   Checkbox: require('./blocks/checkbox/checkbox.jsx'),
+  DropdownMixin: require('./blocks/dropdown/dropdown.jsx'),
+  ProgressBar: require('./blocks/progress-bar/progress-bar.jsx'),
   helloWorld: require('./blocks/hello-world/hello-world.jsx').helloWorld
 };
