a33321eaed3aca1305a5c1040f184271546a2d1a
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-07-14 16:30:33 +0400

    feat(textarea): Create textarea component
    Former-commit-id: ad41a05e027bc49c570fd4db27182ccdc8f74d0e

--- src/ring.js
+++ src/ring.js
@@ -3,5 +3,6 @@ global.react = require('react');
 module.exports = {
   Checkbox: require('./components/checkbox/checkbox.jsx'),
   DropdownMixin: require('./components/dropdown/dropdown.jsx'),
-  ProgressBar: require('./components/progress-bar/progress-bar.jsx')
+  ProgressBar: require('./components/progress-bar/progress-bar.jsx'),
+  Textarea: require('./components/textarea/textarea.jsx')
 };
