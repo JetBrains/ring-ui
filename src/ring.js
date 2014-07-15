c77b42eb5c0ba45089b9eac763788cc4dc0f68e4
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-07-15 16:00:03 +0400

    feat(input): Input component skeleton
    Former-commit-id: 4c81407fce373246e03c71d6acc220dad11f61dd

--- src/ring.js
+++ src/ring.js
@@ -4,5 +4,6 @@ module.exports = {
   Checkbox: require('./components/checkbox/checkbox.jsx'),
   DropdownMixin: require('./components/dropdown/dropdown.jsx'),
   ProgressBar: require('./components/progress-bar/progress-bar.jsx'),
+  Input: require('./components/input/input.jsx'),
   Textarea: require('./components/textarea/textarea.jsx')
 };
