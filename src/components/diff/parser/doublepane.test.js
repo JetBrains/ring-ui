afc7eb144e83948b1e9be3919032156c7728741c
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-07-18 17:59:29 +0400

    feat: Add diff component
    Former-commit-id: f7a67e840ee01f484a0b3a34683249fbf687b7f3

---
+++ src/components/diff/parser/doublepane.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+describe('Diff.DoublePaneParser', function () {
+  var parsers = require('./doublepane');
+
+  it('should export module', function () {
+    expect(parsers).toBeDefined();
+  });
+});
