85dfed3c5fbb130a46c8d68d4d75f8506ad7573a
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-07-22 16:49:49 +0400

    RG-339 Correct localStorage fallback with tests
    Former-commit-id: 5843d90fc3961b3834a460470c33242f48f5c4da

---
+++ blocks/storage/storage__fallback.js
@@ -0,0 +1,122 @@
+/**
+ * Cookie storage fallback (IE7- and IE10+ in protected mode)
+ *
+ * Adopted from http://github.com/inexorabletash/polyfill/blob/master/obsolete/storage.js
+ */
+define(['jquery', 'json'], function ($) {
+  'use strict';
+
+  var DEFAULT_COOKIE_NAME = 'localStorage';
+
+  function createCookie(name, value, days) {
+    var date, expires;
+
+    if (days) {
+      date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = '; expires=' + date.toGMTString();
+    } else {
+      expires = ';';
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
+  }
+
+  function readCookie(name) {
+    var nameEQ = name + '=';
+    var cookies = document.cookie.split(';');
+
+    for (var i = 0, cookie; i < cookies.length; i++) {
+      cookie = cookies[i];
+      while (cookie.charAt(0) === ' ') {
+        cookie = cookie.substring(1, cookie.length);
+      }
+
+      if (cookie.indexOf(nameEQ) === 0) {
+        return cookie.substring(nameEQ.length, cookie.length);
+      }
+    }
+  }
+
+  var FallbackStorage = function(config) {
+    this.cookieName = config && config.cookieName || DEFAULT_COOKIE_NAME;
+  };
+
+  var proto = FallbackStorage.prototype;
+
+  proto.read_ = function() {
+    var rawData = readCookie(this.cookieName);
+    var dfd = $.Deferred();
+    var data = {};
+
+    try {
+      data = JSON.parse(decodeURIComponent(rawData));
+    } catch(e) {}
+
+    dfd.resolve(data);
+    return dfd;
+  };
+
+  proto.write_ = function(data) {
+    try {
+      var stringData = encodeURIComponent(JSON.stringify(data));
+      createCookie(this.cookieName, stringData === '{}' ? '' : stringData, 365);
+    } catch (e) {
+      return $.Deferred().reject(e);
+    }
+  };
+
+  proto.get = function(key) {
+    return this.read_().then(function(data) {
+      var value = data[key];
+
+      if (value != null) {
+        return value;
+      } else {
+        return $.Deferred().reject(new Error('Callback is not a function'));
+      }
+    });
+  };
+
+  proto.set = function(key, value) {
+    var self = this;
+
+    return this.read_().then(function(data) {
+      if (key) {
+        if (value != null) {
+          data[key] = value;
+        } else {
+          delete data[key];
+        }
+      }
+
+      return self.write_(data);
+    });
+  };
+
+  proto.remove = function(key) {
+    return this.set(key, null);
+  };
+
+  proto.each = function(callback) {
+    var dfd = $.Deferred();
+
+    if (typeof callback !== 'function') {
+      return dfd.reject(new Error('Callback is not a function'));
+    }
+
+    return this.read_().then(function(data) {
+      var count = 0;
+
+      for (var key in data) {
+        count++;
+        callback(key, data[key]);
+      }
+
+      if (count === 0) {
+        return dfd.reject(new Error('There is no items'));
+      }
+    });
+  };
+
+  return FallbackStorage;
+});
--- test/storage.js
+++ test/storage.js
@@ -1,23 +1,41 @@
-define(['global/global', 'chai', 'q', 'chai-as-promised', 'storage/storage'], function(ring, chai, q) {
+define(['global/global', 'chai', 'q', 'storage/storage', 'storage/storage__fallback', 'chai-as-promised'], function(ring, chai, q, Storage, FallbackStorage) {
   'use strict';
-
-  var storage = ring('storage');
 
   describe('Storage', function () {
     beforeEach(function(){
       localStorage.clear();
     });
 
+    testStorage(new Storage());
+  });
+
+  describe('Fallback storage', function () {
+    var cookieName = 'testCookie';
+
+    beforeEach(function(){
+      document.cookie = cookieName + '=;';
+    });
+
+    testStorage(new FallbackStorage({cookieName: cookieName}));
+  });
+
+  function testStorage(storage) {
     describe('Set', function () {
-      it('Should set items', function () {
-        return q(storage('set', 'empty', {})).should.be.fulfilled;
+      it('Correct set call should be fulfilled', function () {
+        return q(storage.set('empty', {})).should.be.fulfilled;
+      });
+
+      it('Should correctly set url incopatible characters', function () {
+        storage.set('test;', 'value;');
+
+        return q(storage.get('test;')).should.eventually.equal('value;');
       });
 
       it('Set should fail on wrong input (e.g. on circular objects)', function () {
         var circular = {};
         circular.circular = circular;
 
-        return q(storage('set', 'circular', circular)).should.be.rejected;
+        return q(storage.set('circular', circular)).should.be.rejected;
       });
     });
 
@@ -25,80 +43,73 @@ define(['global/global', 'chai', 'q', 'chai-as-promised', 'storage/storage'], fu
       var test = {a: 666};
 
       it('Should get items', function () {
-        storage('set', 'test2', test);
+        storage.set('test2', test);
 
-        return q(storage('get', 'test2')).should.eventually.deep.equal(test);
+        return q(storage.get('test2')).should.eventually.deep.equal(test);
       });
 
       it('Should not return same objects', function () {
-        storage('set', 'test', test);
+        storage.set('test', test);
 
-        return q(storage('get', 'test')).should.not.eventually.equal(test);
+        return q(storage.get('test')).should.not.eventually.equal(test);
       });
 
       it('Should fail when there is no item', function () {
-        return q(storage('get', 'test')).should.be.rejected;
-      });
-    });
-
-    describe('Has', function () {
-      it('Should see present items', function () {
-        storage('set', 'empty', {});
-        storage('has', 'empty').should.be.true;
-      });
-
-      it('Should not see absent items', function () {
-        storage('has', 'empty').should.not.be.ok;
+        return q(storage.get('test')).should.be.rejected;
       });
     });
 
     describe('Remove', function () {
       it('Should remove present items', function () {
-        storage('set', 'empty', {});
-        return q(storage('remove', 'empty')).should.fulfilled;
+        storage.set('empty', {});
+        storage.remove('empty');
+
+        return q(storage.get('empty')).should.be.rejected;
       });
 
-      it('Should not remove absent items', function () {
-        return q(storage('remove', 'empty')).should.rejected;
+      it('Correct remove should be fulfilled', function () {
+        storage.set('empty', {});
+        return q(storage.remove('empty')).should.be.fulfilled;
       });
     });
 
     describe('Each', function () {
       it('Should iterate over items', function () {
-        storage('set', 'test', 'value');
+        storage.set('test', 'value');
         var iterator = sinon.spy();
 
-        storage('each', iterator);
+        storage.each(iterator);
         iterator.should.have.been.calledWith('test', 'value');
       });
 
       it('Correct iteration should be fulfilled', function () {
-        storage('set', 'test1', '');
+        storage.set('test1', '');
 
-        return q(storage('each', function() {})).should.be.fulfilled;
+        return q(storage.each(function () {
+        })).should.be.fulfilled;
       });
 
       it('Should not iterate without items', function () {
-        return q(storage('each', function() {})).should.be.rejected;
+        return q(storage.each(function () {
+        })).should.be.rejected;
       });
 
       it('Should iterate over all items', function () {
         var iterator = sinon.spy();
 
-        storage('set', 'test1', '');
-        storage('set', 'test2', '');
-        storage('set', 'test3', '');
-        storage('each', iterator);
+        storage.set('test1', '');
+        storage.set('test2', '');
+        storage.set('test3', '');
+        storage.each(iterator);
 
         iterator.should.have.been.calledThrice;
       });
 
       it('Should fail on wrong callback', function () {
-        storage('set', 'test', '');
+        storage.set('test', '');
 
-        return q(storage('each')).should.be.rejected;
+        return q(storage.each()).should.be.rejected;
       });
     });
-
-  });
+  }
 });
