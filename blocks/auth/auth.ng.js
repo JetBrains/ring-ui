78aa8ef0f81bc5835e0bf8c012d10ece460f507a
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-07-28 19:29:12 +0200

    #RG-342 fixed
    Former-commit-id: 3bde321022280f167e766950cf5b8ad03b91e82e

--- blocks/auth/auth.ng.js
+++ blocks/auth/auth.ng.js
@@ -24,53 +24,57 @@
    * </pre>
    */
   angular.module('Ring.auth', []).
-    provider('auth', [function () {
+    provider('auth', ['$hhtpProvider', function ($httpProvider) {
       var authFuture;
-      this.config = function (config) {
+      var authConfig = {};
+      this.config = function (config, doNotInstallHeader) {
+        authConfig = config;
         // Authorize
         //   redirects to /oauth if it's required,
         //   parses oauth response if any
         authFuture = auth('init', config);
-      };
 
-      this.$get = ['$injector', function ($injector) {
-        return $injector.instantiate(['$location', '$http', '$q', function ($location, $http, $q) {
-          var authConfig = {};
+        authFuture.done(function (absUrl) {
+          authConfig = auth.get('config');
 
-          authFuture.done(function (absUrl) {
-            authConfig = auth.get('config');
-
-            if (absUrl) {
-              // Skipping common prefix
-              for (var i = 0, minLength = Math.min(absUrl.length, authConfig.redirect_uri.length) + 1; i < minLength; i++) {
-                if (absUrl.charAt(i) !== authConfig.redirect_uri.charAt(i)) {
-                  $location.url(absUrl.substring(i - 1)).replace();
-                  break;
-                }
+          if (absUrl) {
+            // Skipping common prefix
+            for (var i = 0, minLength = Math.min(absUrl.length, authConfig.redirect_uri.length) + 1; i < minLength; i++) {
+              if (absUrl.charAt(i) !== authConfig.redirect_uri.charAt(i)) {
+                $location.url(absUrl.substring(i - 1)).replace();
+                break;
               }
             }
-          });
+          }
 
-          return {
-            'installAuthHeader': function () {
-              // Install Authorization header getter
-              authFuture.done(function () {
-                $http.defaults.headers.common['Authorization'] = function () {
-                  return 'Bearer ' + auth('getToken', true);
-                };
-              });
-            },
-            'getUser': function () {
-              return auth('getUser');
-            },
-            'getClientId': function () {
-              return authConfig['client_id'];
-            },
-            'initDone': function () {
-              return $q.when(authFuture);
-            }
-          };
-        }]);
+          if (!doNotInstallHeader) {
+            // Install Authorization header getter
+            $httpProvider.defaults.headers.common['Authorization'] = function () {
+              return 'Bearer ' + auth('getToken', true);
+            };
+          }
+        });
+
+        return $q.when(authFuture);
+      };
+
+      this.$get = ['$location', '$http', '$q', function ($location, $http, $q) {
+        return {
+          'installAuthHeader': function () {
+            // TODO: Remove me (header is installed on provider config phase)
+          },
+          'getUser': function () {
+            return auth('getUser');
+          },
+          'getClientId': function () {
+            return authConfig['client_id'];
+          },
+          'initDone': function () {
+            //@Deprecated (use promise from config method)
+            //TODO: Remove!!!
+            return $q.when(authFuture);
+          }
+        };
       }];
     }]);
 }());
