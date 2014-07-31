9b8088203cc91dfb0daeab4421a24adcef9c2cd4
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-07-31 19:17:17 +0200

    fix tests
    Former-commit-id: 40da3fd38632c64072811085359555d893211cbf

--- src/components/auth/auth.test.js
+++ src/components/auth/auth.test.js
@@ -1,13 +1,26 @@
 describe('auth', function () {
   var Auth = require('./auth');
 
+  var callNew = function (fun) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return function () {
+      function F() {
+        return fun.apply(this, args);
+      }
+
+      F.prototype = fun.prototype;
+      return new F();
+    };
+  };
+
 
   beforeEach(function () {
   });
 
-  describe('init', function() {
+  describe('init', function () {
     it('should provide config', function () {
-      expect(Auth);
+      expect(callNew(Auth)).toThrow('Config is required');
+      expect(callNew(Auth, {})).toThrow('Property serverUri is required');
     });
     it('should fix serverUri', function () {
       expect(Auth);
