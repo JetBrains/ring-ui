7522e3f0e0225e4216cad969b0306e62f406b54e
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-07-31 20:03:07 +0200

    RG-344 Storage test
    Former-commit-id: 0db31908b807f4fad75457347678515a37e03358

---
+++ src/components/storage/storage.test.js
@@ -0,0 +1,116 @@
+function testStorage(storage) {
+  describe('Set', function () {
+    it('Correct set call should be fulfilled', function () {
+      return q(storage.set('empty', {})).should.be.fulfilled;
+    });
+
+    it('Should correctly set url incopatible characters', function () {
+      storage.set('test;', 'value;');
+
+      return q(storage.get('test;')).should.eventually.equal('value;');
+    });
+
+    it('Set should fail on wrong input (e.g. on circular objects)', function () {
+      var circular = {};
+      circular.circular = circular;
+
+      return q(storage.set('circular', circular)).should.be.rejected;
+    });
+  });
+
+  describe('Get', function () {
+    var test = {a: 666};
+
+    it('Should get items', function () {
+      storage.set('test2', test);
+
+      return q(storage.get('test2')).should.eventually.deep.equal(test);
+    });
+
+    it('Should not return same objects', function () {
+      storage.set('test', test);
+
+      return q(storage.get('test')).should.not.eventually.equal(test);
+    });
+
+    it('Should fail when there is no item', function () {
+      return q(storage.get('test')).should.be.rejected;
+    });
+  });
+
+  describe('Remove', function () {
+    it('Should remove present items', function () {
+      storage.set('empty', {});
+      storage.remove('empty');
+
+      return q(storage.get('empty')).should.be.rejected;
+    });
+
+    it('Correct remove should be fulfilled', function () {
+      storage.set('empty', {});
+      return q(storage.remove('empty')).should.be.fulfilled;
+    });
+  });
+
+  describe('Each', function () {
+    it('Should iterate over items', function () {
+      storage.set('test', 'value');
+      var iterator = sinon.spy();
+
+      storage.each(iterator);
+      iterator.should.have.been.calledWith('test', 'value');
+    });
+
+    it('Correct iteration should be fulfilled', function () {
+      storage.set('test1', '');
+
+      return q(storage.each(function () {
+      })).should.be.fulfilled;
+    });
+
+    it('Should not iterate without items', function () {
+      return q(storage.each(function () {
+      })).should.be.rejected;
+    });
+
+    it('Should iterate over all items', function () {
+      var iterator = sinon.spy();
+
+      storage.set('test1', '');
+      storage.set('test2', '');
+      storage.set('test3', '');
+      storage.each(iterator);
+
+      iterator.should.have.been.calledThrice;
+    });
+
+    it('Should fail on wrong callback', function () {
+      storage.set('test', '');
+
+      return q(storage.each()).should.be.rejected;
+    });
+  });
+}
+
+describe('Storage', function () {
+  beforeEach(function(){
+    localStorage.clear();
+  });
+
+  var Storage = require('./storage');
+
+  testStorage(new Storage());
+});
+
+
+describe('Fallback storage', function () {
+  var cookieName = 'testCookie';
+
+  beforeEach(function(){
+    document.cookie = cookieName + '=;';
+  });
+
+  var FallbackStorage = require('./storage_fallback');
+
+  testStorage(new FallbackStorage({cookieName: cookieName}));
+});
