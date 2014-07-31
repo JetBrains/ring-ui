a7ee3f9af78b525f919a5ff451efdc041a32421c
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-07-31 16:47:18 +0200

    RG-344 Auth module in Ring 2.0
    Former-commit-id: 6733452ac747cf799040d7749ee082e47aae3a2a

---
+++ src/components/auth/auth.test.js
@@ -0,0 +1,18 @@
+describe('auth', function () {
+  var Auth = require('./auth');
+
+
+  beforeEach(function () {
+  });
+
+  it('should init', function () {
+    it('should provide config', function () {
+
+    });
+    it('should fix serverUri', function () {
+
+    });
+//    expect(Auth.addons.TestUtils.isComponentOfType(this.button, Button)).toEqual(true);
+  });
+
+});
