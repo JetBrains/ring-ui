3b97defb8084387611944ddcccd7efd522bf083d
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-07-31 17:19:12 +0200

    minor
    Former-commit-id: 90765ee2f364054f9b00f0a61c9fd1338feb4bee

--- webpack.conf.js
+++ webpack.conf.js
@@ -42,13 +42,13 @@ module.exports = {
         loader: 'jsx-loader'
       },
       //images loader
-      { test: /\.png$/, loader: "file-loader" },
+      { test: /\.png$/, loader: 'file-loader' },
       // the url-loader uses DataUrls.
       // the file-loader emits files.
-      { test: /\.woff$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-      { test: /\.ttf$/, loader: "file-loader" },
-      { test: /\.eot$/, loader: "file-loader" },
-      { test: /\.svg$/, loader: "file-loader" }
+      { test: /\.woff$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+      { test: /\.ttf$/, loader: 'file-loader' },
+      { test: /\.eot$/, loader: 'file-loader' },
+      { test: /\.svg$/, loader: 'file-loader' }
     ]
   },
   plugins: []
