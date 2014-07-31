032ed082f50b4bcce6cf9c8d995a3bbf9e7f756e
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-07-31 18:26:29 +0200

    RG-344 Local storage
    Former-commit-id: 89f35b34cda306e9c91edaf4a1c4487e1e67805c

---
+++ src/components/storage/storage.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var LocalStorage = require('./storage_local.js');
+var FallbackStorage = require('./storage_fallback.js');
+
+var Storage = LocalStorage;
+
+// Using try/catch here because of IE10+ protected mode and other browsers' quirks
+// See https://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
+try {
+  var test = 'testStorage';
+  localStorage.setItem(test, test);
+  localStorage.removeItem(test);
+} catch(e) {
+  Storage = FallbackStorage;
+}
+
+module.exports = Storage;
