e5cba7cd3cdb208c79b11959cf9fcfc948893fd4
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-07-31 19:50:33 +0200

    init tests
    Former-commit-id: 2d5306f2e7518a29edf7628bd1f831915e52627e

--- src/components/auth/auth.test.js
+++ src/components/auth/auth.test.js
@@ -1,5 +1,6 @@
 describe('auth', function () {
   var Auth = require('./auth');
+  var $ = require('jquery');
 
   var callNew = function (fun) {
     var args = Array.prototype.slice.call(arguments, 1);
@@ -21,9 +22,26 @@ describe('auth', function () {
     it('should provide config', function () {
       expect(callNew(Auth)).toThrow('Config is required');
       expect(callNew(Auth, {})).toThrow('Property serverUri is required');
+      expect(callNew(Auth, {serverUri: ''})).toThrow('Property serverUri is required');
     });
     it('should fix serverUri', function () {
-      expect(new Auth({serverUri: 'http://localhost'}).config).toEqual({serverUri: 'http://localhost/'});
+      expect(new Auth({serverUri: 'http://localhost'}).config.serverUri).toEqual('http://localhost/');
+      expect(new Auth({serverUri: '.'}).config.serverUri).toEqual('./');
+    });
+    it('should add defaults', function () {
+      var config = {
+        serverUri: 'http://localhost/'
+      };
+      expect(new Auth(config).config).toEqual($.extend({}, Auth.DEFAULT_CONFIG, config));
+
+      config.redirect_uri = 'http://redirect.to';
+      config.client_id = '7-7-7-7-7';
+      expect(new Auth(config).config).toEqual($.extend({}, Auth.DEFAULT_CONFIG, config));
+
+      config.redirect_uri = 'http://redirect.to';
+      config.client_id = '7-7-7-7-7';
+      config.scope = ['test', 'test2'];
+      expect(new Auth(config).config).toEqual($.extend({scope: ['test1', 'test2', '0-0-0-0-0']}, Auth.DEFAULT_CONFIG, config));
     });
   });
 
