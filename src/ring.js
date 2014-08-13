5e18ecec4e74a5439300ccd96401a918383b832a
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-08-13 10:38:03 +0200

    moved to popup-menu folder #RG-348 fixed
    Former-commit-id: 1b533669714b23ee7852c0f687096829b05c1fcc

--- src/ring.js
+++ src/ring.js
@@ -4,7 +4,7 @@ module.exports = {
   Checkbox: require('./components/checkbox/checkbox.jsx'),
   PopupMixin: require('./components/popup/popup-mixin.jsx'),
   Popup: require('./components/popup/popup.jsx'),
-  PopupMenu: require('./components/popup/popup-menu.jsx'),
+  PopupMenu: require('./components/popup-menu/popup-menu.jsx'),
   ProgressBar: require('./components/progress-bar/progress-bar.jsx'),
   Input: require('./components/input/input.jsx'),
   Textarea: require('./components/textarea/textarea.jsx'),
