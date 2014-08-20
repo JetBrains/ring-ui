acc1d13e9b969607cca58e45c30af8e6ead66f6a
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-08-20 09:51:57 +0400

    fix: Remove state from progress bar
    Former-commit-id: f4f7fa6f8bfd0bcbb38e2fa2f85100c18dacb843

--- src/components/progress-bar/progress-bar.test.js
+++ src/components/progress-bar/progress-bar.test.js
@@ -16,33 +16,33 @@ describe('progress-bar', function() {
 
   describe('default value for attributes', function() {
     it('should set default value for max attribute', function() {
-      this.progress.state.max.should.equal(1.0);
+      this.progress.props.max.should.equal(1.0);
     });
 
     it('should not define attribute value by default', function() {
-      expect(this.progress.state.value).not.to.exist;
+      expect(this.progress.props.value).not.to.exist;
     });
   });
 
   describe('client interaction with progress bar API', function() {
     it('should set max value for progress bar', function() {
-      this.progress.setState({
+      this.progress.setProps({
         max: 100
       });
 
-      this.progress.state.max.should.equal(100);
+      this.progress.props.max.should.equal(100);
     });
 
     it('should set progress task value', function() {
-      this.progress.setState({
+      this.progress.setProps({
         value: 0.5
       });
 
-      this.progress.state.value.should.equal(0.5);
+      this.progress.props.value.should.equal(0.5);
     });
 
     it('should set additional classes(modifiers) to the component', function() {
-      this.progress.setState({
+      this.progress.setProps({
         className: 'ring-progress-bar_global'
       });
 
@@ -60,7 +60,7 @@ describe('progress-bar', function() {
     });
 
     it('should update max value in DOM', function() {
-      this.progress.setState({
+      this.progress.setProps({
         max: 100
       });
 
@@ -68,7 +68,7 @@ describe('progress-bar', function() {
     });
 
     it('should update progress value in DOM', function() {
-      this.progress.setState({
+      this.progress.setProps({
         value: 0.5
       });
 
@@ -77,7 +77,7 @@ describe('progress-bar', function() {
     });
 
     it('should set width equal 100% if progress value more than max value', function() {
-      this.progress.setState({
+      this.progress.setProps({
         max: 1.0,
         value: 10
       });
@@ -86,8 +86,8 @@ describe('progress-bar', function() {
     });
 
     it('should not set width if value is not a number', function() {
-      this.progress.setState({
-        value: 'string'
+      this.progress.setProps({
+        value: null
       });
 
       $(this.progress.refs.progressbar.getDOMNode()).should.have.attr('style').match(/width:;/);
