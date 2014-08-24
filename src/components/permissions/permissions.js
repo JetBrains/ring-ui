f726082e8dbd15867b81b16052ab486da3d658c4
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-08-24 22:00:19 +0200

    RG-353 Tests
    Former-commit-id: 1d0ec040d2e5f75ba7a9f0e83cf96733fe850537

--- src/components/permissions/permissions.js
+++ src/components/permissions/permissions.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var Auth = require('../auth/auth');
+var PermissionCache = require('./permissions__cache');
 
 /**
  * <code>
- *   var permissions = new Permissions(auth, {prefx: 'jetbrains.jetpass.', serviceId: auth.serviceId})
+ *   var permissions = new Permissions(auth, {prefix: 'jetbrains.jetpass.', serviceId: auth.serviceId})
  *   permissions.load().then(function (p) {
  *     var canReadUser = p.has('read-user');
  *     ...
@@ -17,19 +18,18 @@ var Auth = require('../auth/auth');
  *
  * @param {Auth} auth instance of well configured Auth object
  * @param {{
- *  prefx: string?,
+ *  prefix: string?,
  *  serviceId: string?
- * }?} config permissions loaded configuration. <code>prefx</code> if provided then this prefix is removed
+ * }?} config permissions loaded configuration. <code>prefix</code> if provided then this prefix is removed
  * from the permissions names. <code>serviceId</code> if provided then permissions only for the service are loaded.
  * @constructor
  */
 var Permissions = function (auth, config) {
   config = config || {};
   this.query = config.serviceId && ('service: {' + config.serviceId + '}');
-  this.prefix = config.prefx;
+  this.prefix = config.prefix;
 
   this._auth = auth;
-  this._permissionCache = {};
   this._promise = null;
 };
 
@@ -41,7 +41,7 @@ var API_PERMISSION_CACHE_PATH = Auth.API_PATH + '/permissions/cache';
  */
 Permissions.prototype.load = function () {
   if (this._promise) {
-    return this._promise.promise();
+    return this._promise;
   }
 
   var self = this;
@@ -52,47 +52,11 @@ Permissions.prototype.load = function () {
     };
     return self.getSecure(API_PERMISSION_CACHE_PATH, accessToken, params).
       then(function (cachedPermissions) {
-        self._permissionCache = Permissions._initPermissionCache(cachedPermissions, self.prefix);
-        return self;
+        return new PermissionCache(cachedPermissions, self.prefix);
       });
   });
 
   return this._promise;
-};
-
-/**
- * Checks if the current user has the given permissions in the space with the given id.
- * If this method is called before <code>load()</code> then it always returns false.
- *
- * @param {string} permissions  space separated list of permissions
- * @param {string?} spaceId     optional spaceId. If absent the method checks
- *  if the given permission is granted in any space.
- *
- * @return {boolean}
- */
-Permissions.prototype.has = function (permissions, spaceId) {
-  if (!permissions) {
-    return true;
-  }
-  var permissionList = permissions.split(/\s+/);
-
-  for (var i = permissionList.length - 1, cachedPermission; i >= 0; i--) {
-    cachedPermission = this._permissionCache[permissionList[i]];
-    // Has no the permission in any space
-    if (!cachedPermission) {
-      return false;
-    }
-    // The permission is global or is given in the global space
-    if (cachedPermission.global) {
-      return true;
-    }
-    // spaceId is specified but the permission isn't given in the space
-    if (spaceId && !(cachedPermission.spaceIdSet && cachedPermission.spaceIdSet[spaceId])) {
-      return false;
-    }
-  }
-
-  return true;
 };
 
 /**
@@ -107,55 +71,9 @@ Permissions.prototype.has = function (permissions, spaceId) {
  */
 Permissions.prototype.check = function (permissions, spaceId) {
   this.load().
-    then(function (self) {
-      return self.has(permissions, spaceId);
+    then(function (permissionCache) {
+      return permissionCache.has(permissions, spaceId);
     });
-};
-
-/**
- * Convert an array of spaces to a set of space ids.
- *
- * @param {{id: string}?} spaces
- * @return {object} a set of space ids
- * @private
- */
-Permissions._toSpaceIdSet = function (spaces) {
-  var spaceIdSet = null;
-  if (spaces) {
-    spaceIdSet = {};
-    for (var i = 0; i < spaces.length; i++) {
-      spaceIdSet[spaces[i].id] = true;
-    }
-  }
-  return spaceIdSet;
-};
-
-/**
- * Convert an array of cached permissions to a permissionCache that is a map of a permission key
- * to the respective cached permission.
- *
- * @param { {
- *  permission: {key: string},
- *  global: boolean?,
- *  spaces: {id: string}[]?
- * }[] } cachedPermissions
- * @param {string?} prefix a substring to chop off from the beginning of the permission key
- * @return {object} permission cache
- * @private
- */
-Permissions._initPermissionCache = function (cachedPermissions, prefix) {
-  var permissionCache = {};
-  for (var i = 0; i < cachedPermissions.length; i++) {
-    var cachedPermission = cachedPermissions[i];
-    var key = cachedPermission.permission.key;
-    if (prefix && key.indexOf(prefix) === 0) {
-      key = key.substr(prefix);
-    }
-    cachedPermission.spaceIdSet = Permissions._toSpaceIdSet(cachedPermission.spaces);
-    permissionCache[key] = cachedPermission;
-  }
-
-  return permissionCache;
 };
 
 module.exports = Permissions;
---
+++ src/components/permissions/permissions.test.js
@@ -0,0 +1,68 @@
+describe('permissions', function () {
+  var Auth = require('../auth/auth');
+  var Permissions = require('./permissions');
+  var PermissionCache = require('./permissions__cache');
+
+
+  describe('construction', function () {
+    var auth = new Auth({serverUri: ''});
+
+    it('shouldn\'t build query if no spaceId provided', function () {
+      expect(new Permissions(auth).query).to.equal(undefined);
+    });
+
+    it('should build query if spaceId provided', function () {
+      expect(new Permissions(auth, {serviceId: '123'}).query).to.equal('service: {123}');
+    });
+
+    it('should pass permission key prefix', function () {
+      expect(new Permissions(auth, {prefix: 'jetbrains.jetpass.'}).prefix).to.equal('jetbrains.jetpass.');
+    });
+  });
+
+  describe('cache', function () {
+    var permissionCache = new PermissionCache([
+      {permission: {key: 'jetbrains.jetpass.space-create'}, global: true},
+      {permission: {key: 'jetbrains.jetpass.space-read'}, global: true},
+      {permission: {key: 'jetbrains.jetpass.space-update'}, spaces: [{id: '123'}]},
+      {permission: {key: 'jetbrains.upsource.permission.project.admin'}, global: true}
+    ], 'jetbrains.jetpass.');
+
+    it('should not permit unlisted permission', function () {
+      expect(permissionCache.has('role-update')).to.equal(false);
+      expect(permissionCache.has('role-update', '123')).to.equal(false);
+    });
+
+    it('should permit global permission', function () {
+      expect(permissionCache.has('space-read')).to.equal(true);
+      expect(permissionCache.has('space-read', '123')).to.equal(true);
+      expect(permissionCache.has('space-read', '456')).to.equal(true);
+    });
+
+    it('should permit if user has permission in space', function () {
+      expect(permissionCache.has('space-update')).to.equal(true);
+      expect(permissionCache.has('space-update', '123')).to.equal(true);
+    });
+
+    it('should not permit if user has no permission in space', function () {
+      expect(permissionCache.has('space-update', '456')).to.equal(false);
+    });
+
+    it('should match full permission key if it doesn\'t start with the provided prefix', function () {
+      expect(permissionCache.has('jetbrains.upsource.permission.project.admin', '456')).to.equal(true);
+    });
+
+    it('should permit if user has all permissions', function () {
+      expect(permissionCache.has('space-read space-update', '123')).to.equal(true);
+    });
+
+    it('should not permit if user doesn\'t have some permissions', function () {
+      expect(permissionCache.has('space-read space-update', '456')).to.equal(false);
+      expect(permissionCache.has('space-read role-update')).to.equal(false);
+    });
+  });
+
+  // Check _permissionCache that is built after request
+// .check resolves to a boolean as expected
+
+});
