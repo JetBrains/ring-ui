dcc5e03f43e03c6b85f76f20aa1e45aa221e0581
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-08-25 16:22:51 +0200

    RG-353 permission, permissionIf, somePermissions angular directives
    Former-commit-id: 40265b5ab42c2661b6314da7d0b0d12e1b08dea0

--- src/components/permissions-ng/permissions-ng.js
+++ src/components/permissions-ng/permissions-ng.js
@@ -41,4 +41,113 @@ permissionsModule.provider('userPermissions', [function () {
   }];
 }]);
 
+var registerPermission = function (element) {
+  var somePermissionsCtrl = element.controller('somePermissions');
+  return somePermissionsCtrl && somePermissionsCtrl.registerPermission() || angular.noop;
+};
+
+permissionsModule.directive('permission', [
+  'userPermissions',
+  function (userPermissions) {
+    return {
+      controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+        this.permitted = false;
+
+        var self = this;
+        userPermissions.check($attrs.permission, $scope.$eval($attrs.inSpace)).
+          then(function (permitted) {
+            self.permitted = permitted;
+            if (permitted) {
+              $element.show();
+            } else {
+              $element.hide();
+            }
+            return permitted;
+          }).
+          then(registerPermission($element));
+      }]
+    };
+  }
+]);
+
+permissionsModule.directive('permissionIf', [
+  '$animate',
+  'userPermissions',
+  function ($animate, userPermissions) {
+    return {
+      transclude: 'element',
+      priority: 600,
+      terminal: true,
+      restrict: 'A',
+      require: '^?somePermissions',
+      link: function (scope, iElement, iAttrs, somePermittedCtrl, $transclude) {
+        var block, childScope;
+
+        userPermissions.check(iAttrs.permissionIf, scope.$eval(iAttrs.inSpace)).
+          then(function (permitted) {
+            if (permitted) {
+              if (!childScope) {
+                childScope = scope.$new();
+                $transclude(childScope, function (clone) {
+                  block = {
+                    startNode: clone[0],
+                    endNode: clone[clone.length++] = document.createComment(' end permissionIf: ' + iAttrs.permissionIf + ' ')
+                  };
+                  $animate.enter(clone, iElement.parent(), iElement);
+                });
+              }
+            } else {
+              if (childScope) {
+                childScope.$destroy();
+                childScope = null;
+              }
+
+              if (block) {
+                /* global getBlockElements: false */
+                $animate.leave(getBlockElements(block));
+                block = null;
+              }
+            }
+          }).
+          then(registerPermission(iElement));
+      }
+    };
+  }
+]);
+
+permissionsModule.directive('somePermissions', [
+  function () {
+    return {
+      scope: {
+        'somePermissions': '='
+      },
+      controller: ['$scope', function ($scope) {
+        var permissions = [];
+        $scope.somePermissions = false;
+
+        var checkPermissions = function () {
+          for (var i = permissions.length - 1; i >= 0; i--) {
+            if (permissions[i].permitted) {
+              $scope.somePermissions = true;
+              return;
+            }
+          }
+
+          $scope.somePermissions = false;
+        };
+
+        this.registerPermission = function () {
+          var permission = {permitted: false};
+          permissions.push(permission);
+
+          return function (permitted) {
+            permission.permitted = permitted;
+            checkPermissions();
+          };
+        };
+      }]
+    };
+  }
+]);
+
 module.exports = permissionsModule;
