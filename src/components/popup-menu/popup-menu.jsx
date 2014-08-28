54f380ee672153f53aee054970439822959fef87
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-08-28 21:08:19 +0400

    RG-303 Query-assist 2.0: extract list component
    Former-commit-id: f8d048576bad1dbf70475a7e53109d8f2b1cae17

--- src/components/popup-menu/popup-menu.jsx
+++ src/components/popup-menu/popup-menu.jsx
@@ -3,92 +3,9 @@
  * @jsx React.DOM
  */
 
-var $ = require('jquery');
-var Popup = require('popup/popup');
 var React = require('react');
-
-/**
- * @enum {number}
- */
-var Type = {
-  SEPARATOR: 0,
-  MENU_LINK: 1,
-  MENU_ITEM: 2
-};
-
-/**
- * @constructor
- * @extends {ReactComponent}
- */
-var MenuSeparator = React.createClass({
-  render: function () {
-    /* jshint ignore:start */
-    return (
-      <span className="ring-popup__separator"></span>
-    );
-    /* jshint ignore:end */
-  }
-});
-
-/**
- * @constructor
- * @extends {ReactComponent}
- */
-var MenuItem = React.createClass({
-  render: function () {
-    /* jshint ignore:start */
-    return this.transferPropsTo(
-      <span className="ring-popup__item ring-popup__item_action">{this.props.label}</span>
-    );
-    /* jshint ignore:end */
-  }
-});
-
-/**
- * @constructor
- * @extends {ReactComponent}
- */
-var MenuLink = React.createClass({
-  render: function () {
-    var el = this.props.href ? React.DOM.a : React.DOM.span;
-    return this.
-      transferPropsTo(el({className: 'ring-popup__item ring-link'}, this.props.label));
-  }
-});
-
-var MenuList = React.createClass({
-  render: function () {
-    return React.DOM.div(
-      {
-        className: 'ring-popup__i'
-      },
-      this.props.data.map(function (item) {
-        var props = $.extend({'type': Type.MENU_ITEM}, item);
-        if (item.url) {
-          props.href = item.url;
-        }
-        if (props.href) {
-          props.type = Type.MENU_LINK;
-        }
-        var element;
-        switch (props.type) {
-          case Type.SEPARATOR:
-            element = MenuSeparator;
-            break;
-          case Type.MENU_LINK:
-            element = MenuLink;
-            break;
-          case Type.MENU_ITEM:
-            element = MenuItem;
-            break;
-          default:
-            throw new Error('Unknown menu element type: ' + item.type);
-        }
-        return element(props, null);
-      })
-    );
-  }
-});
+var Popup = require('popup/popup');
+var List = require('list/list');
 
 /**
  * @constructor
@@ -98,8 +15,9 @@ var MenuList = React.createClass({
 var PopupMenu = React.createClass({
   mixins: [Popup.Mixin],
 
+  // TODO Find a better way
   statics: {
-    Type: Type
+    Type: List.Type
   },
 
   /** @override */
@@ -109,20 +27,13 @@ var PopupMenu = React.createClass({
     className: React.PropTypes.string
   },
 
-  getInitialState: function () {
+  getDefaultProps: function () {
     return {data: []};
   },
 
   /** @override */
-  getDefaultProps: function () {
-    return {
-      anchorElement: document.body
-    };
-  },
-
-  /** @override */
   getInternalContent: function () {
-    return new MenuList({data: this.state.data}, null);
+    return new List({data: this.props.data}, null);
   }
 });
 
