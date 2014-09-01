5f493bb3a637bc509fceb7b9b854df7dbe050180
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-09-01 19:25:39 +0400

    RG-303 Query-assist 2.0: start component
    Former-commit-id: b084ce76da4d95de418281346fecf523885b6707

--- src/ring.js
+++ src/ring.js
@@ -5,6 +5,7 @@ module.exports = {
   Popup: require('popup/popup'),
   PopupMenu: require('popup-menu/popup-menu'),
   List: require('list/list'),
+  QueryAssist: require('query-assist/query-assist'),
   ProgressBar: require('progress-bar/progress-bar'),
   Input: require('input/input'),
   Textarea: require('textarea/textarea'),
