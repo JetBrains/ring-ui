d2f6d9532e110f6292f9f1139c331f9c5b913be9
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-09-02 15:21:29 +0400

    RG-303 Query-assist 2.0: controlled input
    Former-commit-id: 484152e0c82ab946cb1cd241592d0d25c3459ba4

--- src/components/query-assist/query-assist.jsx
+++ src/components/query-assist/query-assist.jsx
@@ -4,6 +4,8 @@
  */
 
 var React = require('react');
+var $ = require('jquery');
+require('jquery-caret');
 //var Popup = require('popup/popup');
 //var List = require('list/list');
 
@@ -19,22 +21,72 @@ var QueryAssist = React.createClass({
   /** @override */
   propTypes: {
     className: React.PropTypes.string,
+    dataSource: React.PropTypes.func,
     placeholder: React.PropTypes.string,
     onApply: React.PropTypes.func,
     onFocusChange: React.PropTypes.func,
     onClose: React.PropTypes.func
   },
 
+  generateStateObject: function(props) {
+    props = props || this.props;
+
+    return {
+      letters: props.query.split(''),
+      query: props.query,
+      caret: props.caret != null ? props.caret : props.query && props.query.length || 0
+    };
+  },
+
+  getInitialState: function() {
+    return this.generateStateObject();
+  },
+
+  componentWillReceiveProps: function() {
+    this.setState(this.generateStateObject());
+  },
+
+  getQuery: function() {
+    return $(this.refs.input.getDOMNode()).text();
+  },
+
+  handleInput: function() {
+    var query = this.getQuery();
+    var caret = $(this.refs.input.getDOMNode()).caret();
+
+    if (query.length > this.state.query.length) {
+      caret += 1;
+    }
+
+    this.setState(this.generateStateObject({
+      query: query,
+      caret: caret
+    }));
+  },
+
   /** @override */
   render: function () {
     /* jshint ignore:start */
+    var query = this.state.letters.length && React.renderComponentToStaticMarkup(
+      <span>{this.state.letters.map(function(letter) {
+       // In spite of werining we don't need key here because of renderComponentToStaticMarkup
+       return <span className="ring-query-assist__letter ring-query-assist__letter_field-name">{letter}</span>
+      })}</span>
+    ) || '';
+
     return (
       <div className="ring-query-assist">
-        <div className="ring-query-assist__input ring-input ring-js-shortcuts" spellCheck="false" contentEditable="true">{this.props.query}</div>
+        <div className="ring-query-assist__input ring-input ring-js-shortcuts" ref="input" onInput={this.handleInput} spellCheck="false" contentEditable="true" dangerouslySetInnerHTML={{__html: query}}></div>
         {this.props.placeholder && <span className="ring-query-assist__placeholder">{this.props.placeholder}</span>}
       </div>
     );
     /* jshint ignore:end */
+  },
+
+  componentDidUpdate: function() {
+    if (this.refs.input.getDOMNode().firstChild) {
+      $(this.refs.input.getDOMNode()).caret(this.state.caret);
+    }
   }
 });
 
