4e5c85154228c2c704ab8e1c78d8c3245c2a56ae
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2014-09-05 13:35:16 +0400

    loaders changed
    Former-commit-id: 7eb027e3a5ce5572f575e63b5a9ab0daffbc3793

--- webpack.config.js
+++ webpack.config.js
@@ -56,13 +56,14 @@ module.exports = {
         loader: 'imports-loader?window=>{}!exports-loader?window.Mousetrap'
       },
       //images loader
-      { test: /\.png$/, loader: 'file-loader' },
+      { test: /\.png$/, loader: 'url-loader?limit=10000' },
+      { test: /\.gif$/, loader: 'url-loader?limit=10000' },
       // the url-loader uses DataUrls.
       // the file-loader emits files.
       { test: /\.woff$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
       { test: /\.ttf$/, loader: 'file-loader' },
       { test: /\.eot$/, loader: 'file-loader' },
-      { test: /\.svg$/, loader: 'file-loader' }
+      { test: /\.svg$/, loader: 'url-loader?limit=10000' }
     ]
   },
   plugins: []
