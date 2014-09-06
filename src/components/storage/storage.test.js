6df342ad7f57f671756d22b25212107c648d041a
Maxim Mazin <maxim.mazin@jetbrains.com>
2014-09-06 21:00:09 +0200

    RG-359 Fixed storage not to fail on element absence
    Former-commit-id: 8e2ca441d7d84d599f2295d03178fab8e04f1e20

--- src/components/storage/storage.test.js
+++ src/components/storage/storage.test.js
@@ -1,10 +1,10 @@
 function testStorage(storage) {
-  describe('Set', function () {
-    it('Correct set call should be fulfilled', function () {
+  describe('set', function () {
+    it('should be fulfilled', function () {
       return storage.set('empty', {}).should.be.fulfilled;
     });
 
-    it('Should correctly set url incompatible characters', function () {
+    it('should correctly set url incompatible characters', function () {
       return storage.set('test;', 'value;').
         then(function () {
           return storage.get('test;');
@@ -12,7 +12,7 @@ function testStorage(storage) {
         should.eventually.equal('value;');
     });
 
-    it('Set should fail on wrong input (e.g. on circular objects)', function () {
+    it('should fail on wrong input (e.g. on circular objects)', function () {
       var circular = {};
       circular.circular = circular;
 
@@ -20,10 +20,10 @@ function testStorage(storage) {
     });
   });
 
-  describe('Get', function () {
+  describe('get', function () {
     var test = {a: 666};
 
-    it('Should get items', function () {
+    it('should get items', function () {
       return storage.set('test2', {a: 666}).
         then(function () {
           return storage.get('test2');
@@ -31,7 +31,7 @@ function testStorage(storage) {
         should.become({a: 666});
     });
 
-    it('Should not return same objects', function () {
+    it('should not return same objects', function () {
       return storage.set('test', test).
         then(function () {
           return storage.get('test');
@@ -39,13 +39,13 @@ function testStorage(storage) {
         should.not.eventually.equal(test);
     });
 
-    it('Should fail when there is no item', function () {
-      return storage.get('test').should.be.rejected;
+    it('should return null when there is no item', function () {
+      return storage.get('test').should.become(null);
     });
   });
 
-  describe('Remove', function () {
-    it('Should remove present items', function () {
+  describe('remove', function () {
+    it('should remove present items', function () {
       return storage.set('empty', {}).
         then(function () {
           return storage.remove('empty');
@@ -53,10 +53,10 @@ function testStorage(storage) {
         then(function () {
           return storage.get('empty');
         }).
-        should.be.rejected;
+        should.become(null);
     });
 
-    it('Correct remove should be fulfilled', function () {
+    it('should be fulfilled when is correct', function () {
       return storage.set('empty', {}).
         then(function () {
           return storage.remove('empty');
@@ -65,11 +65,19 @@ function testStorage(storage) {
     });
   });
 
-  describe('Each', function () {
+  describe('each', function () {
     var noop = function () {
     };
 
-    it('Should iterate over items', function () {
+    it('should be fulfilled', function () {
+      return storage.set('test1', '').
+        then(function () {
+          return storage.each(noop);
+        }).
+        should.be.fulfilled;
+    });
+
+    it('should iterate over items', function () {
       var iterator = sinon.spy();
       return storage.set('test', 'value').
         then(function () {
@@ -80,19 +88,15 @@ function testStorage(storage) {
         });
     });
 
-    it('Correct iteration should be fulfilled', function () {
-      return storage.set('test1', '').
+    it('should not iterate without items', function () {
+      var iterator = sinon.spy();
+      return storage.each(iterator).
         then(function () {
-          return storage.each(noop);
-        }).
-        should.be.fulfilled;
+          iterator.should.not.been.called;
+        });
     });
 
-    it('Should not iterate without items', function () {
-      return storage.each(noop).should.be.rejected;
-    });
-
-    it('Should iterate over all items', function () {
+    it('should iterate over all items', function () {
       var iterator = sinon.spy();
       return storage.set('test1', '').
         then(function () {
@@ -109,7 +113,7 @@ function testStorage(storage) {
         });
     });
 
-    it('Should fail on wrong callback', function () {
+    it('should fail on wrong callback', function () {
       return storage.set('test', '').
         then(function () {
           return storage.each();
