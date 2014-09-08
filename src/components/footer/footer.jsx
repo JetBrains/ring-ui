f3aa360c89f786acdc2e247009ad86e72013f500
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-09-08 17:03:19 +0400

    #RG-360 in progress
    Former-commit-id: c7c52cd112c8a56a46a238643741ec380d9d3825

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,6 +5,7 @@
 
 require('./footer.scss');
 var React = require('react');
+var $ = require('jquery');
 
 /**
  * @enum {string}
@@ -19,7 +20,7 @@ var Position = {
  * @constructor
  * @extends {ReactComponent}
  */
-var FooteColumn = React.createClass({
+var FooterColumn = React.createClass({
   propTypes: {
     position: React.PropTypes.string
   },
@@ -27,11 +28,39 @@ var FooteColumn = React.createClass({
   render: function () {
     /* jshint ignore:start */
     return (
-      <div className="ring-footer__column ring-footer__column_{this.props.position}">
+      <div className="{['ring-footer__column', 'ring-footer__column_' + this.props.position].join(' ')}">
         <ul className="ring-footer__column__i">
           {this.props.children}
         </ul>
       </div>
+      );
+    /* jshint ignore:end */
+  }
+});
+
+/**
+ * @constructor
+ * @extends {ReactComponent}
+ */
+var FooterItem = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object
+  },
+
+  render: function () {
+    /* jshint ignore:start */
+    var element;
+    var item = this.props.item;
+    element = (item.copyright ? '&cprt' : '') + item.label;
+    if(item.url) {
+      element = <a href={item.url}>{element}</a>
+    }
+    return (
+      <li className="ring-footer__line">
+        <ul className="ring-footer__column__i">
+          {this.props.children}
+        </ul>
+      </li>
       );
     /* jshint ignore:end */
   }
