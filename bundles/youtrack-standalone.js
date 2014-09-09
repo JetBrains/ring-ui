b82731f44e95b8bab572a7373927cfdfe7bd073e
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2014-09-09 18:28:16 +0400

    loader added to youtrack-standalone
    Former-commit-id: 658418a9109795ab1dfad467d7d741451dc61048

--- bundles/youtrack-standalone.js
+++ bundles/youtrack-standalone.js
@@ -2,7 +2,8 @@ define([
   'global/global',
   'menu/menu',
   'footer/footer',
-  'dropdown-select/dropdown-select'
+  'dropdown-select/dropdown-select',
+  'loader/loader'
 ], function(ring) {
   return ring;
 });
