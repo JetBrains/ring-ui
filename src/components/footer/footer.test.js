4f23e560450f337cff1862ae8c6b740f257baaac
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-09-09 14:51:56 +0400

    #RG-360 fixed tests
    Former-commit-id: 4488aa861f5f0b948d310ace196b30a9ccd6a6db

--- src/components/footer/footer.test.js
+++ src/components/footer/footer.test.js
@@ -25,5 +25,40 @@ describe('footer', function () {
       $(footer.getDOMNode()).should.have.class('myClass');
     });
 
+    it('add left column one line', function () {
+      footer.setProps({'left': ['One Line']});
+
+      $(footer.getDOMNode()).should.contain('One Line');
+      $(footer.getDOMNode()).find('li').should.not.be.empty;
+      $(footer.getDOMNode()).find('li').length.should.equal(1);
+    });
+
+    it('add left column two lines', function () {
+      footer.setProps({'left': ['One Line','Second Line']});
+
+      $(footer.getDOMNode()).find('li').should.not.be.empty;
+      $(footer.getDOMNode()).find('li').length.should.equal(2);
+    });
+
+    it('add three columns two lines', function () {
+      footer.setProps({
+        'left': ['One Line','Second Line'],
+        'center': ['One Line','Second Line'],
+        'right': ['One Line','Second Line']
+      });
+      function assertLines(lines, count) {
+        lines.should.not.be.empty;
+        lines.length.should.equal(count);
+      }
+
+      var root = $(footer.getDOMNode());
+      var ul = root.find('ul');
+      ul.length.should.equal(3);
+
+      assertLines(ul.eq(0).find('li'), 2);
+      assertLines(ul.eq(1).find('li'), 2);
+      assertLines(ul.eq(2).find('li'), 2);
+    });
+
   });
 });
