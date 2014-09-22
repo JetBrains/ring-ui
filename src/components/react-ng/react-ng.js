37799f6a4a04fd47264efbd76067996f8d37e366
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-09-22 14:24:35 +0400

    RG-323 Angular react directive: handle callbacks
    Former-commit-id: a0aaabcfe32116388ea14717dd8c49bc072b46a7

--- src/components/react-ng/react-ng.js
+++ src/components/react-ng/react-ng.js
@@ -12,61 +12,87 @@ var reactModule = angular.module('Ring.react-ng', []);
 var Ring = {};
 var directiveName = 'react';
 
-var domAttrsNames = {};
-domAttrsNames['for'] = 'htmlFor';
-domAttrsNames['class'] = 'className';
+var specialDOMAttrs = {
+  'for': 'htmlFor',
+  'class': 'className'
+};
 
 reactModule.directive(directiveName, [
-  '$interpolate',
   '$parse',
-  function ($interpolate, $parse) {
+  function ($parse) {
     return {
       restrict: 'A',
-      link: function (scope, element, attrs) {
-        var name = attrs[directiveName];
+      link: function (scope, iElement, iAttrs) {
+        var name = iAttrs[directiveName];
 
         var ComponentClass = Ring[name];
         if (!ComponentClass) {
           throw Error('Component ' + name + ' is not registered');
+        } else if (!React.isValidClass(ComponentClass)) {
+          throw Error('Property ' + name + ' is not valid component');
         }
 
         var props = {};
 
-        angular.forEach(attrs, function(value, name) {
-          if (attrs.hasOwnProperty(name) && name !== directiveName && typeof value === 'string') {
-            // Detect major native DOM attributes
-            var domName = domAttrsNames[name];
-
-            // Detect interpolation
-            var expression = $interpolate(value, true);
-
-            if (!expression && !domName) {
-              expression = $parse(value);
+        function getUpdater(name) {
+          return function updatePropsFromWatch(value, oldValue) {
+            // Don't pass undefined on init
+            if (angular.isUndefined(value) && angular.isUndefined(oldValue)) {
+              return;
             }
 
+            var props = {};
+            props[name] = value;
+            component.setProps(props);
+          };
+        }
+
+        angular.forEach(iAttrs, function (value, name) {
+          if (iAttrs.hasOwnProperty(name) && name !== directiveName && typeof value === 'string') {
             // Use React DOM attributes names
-            var propName = domName || name;
+            var specialDOMAttrName = specialDOMAttrs[name];
+            var propName = specialDOMAttrName || name;
 
-            if (expression) {
-              scope.$watch(expression, function(result) {
-                var props = {};
-                props[propName] = result;
-                component.setProps(props);
-              });
+            // Detect interpolation
+            var interpolated = iElement.attr(iAttrs.$attr[name]) !== value;
 
-              props[propName] = expression(scope);
+            // Check if component expects callback
+            var expectsCallback = ComponentClass.propTypes &&
+              (ComponentClass.propTypes[propName] === React.PropTypes.func ||
+              ComponentClass.propTypes[propName] === React.PropTypes.func.isRequired);
+
+            // Parse as expression
+            var parsedExpression = !specialDOMAttrName && !interpolated && $parse(value);
+
+            if (interpolated) {
+              iAttrs.$observe(name, getUpdater(propName));
+            } else if (parsedExpression && expectsCallback) {
+              props[propName] = function (param) {
+                var locals = typeof param === 'object' ? angular.copy(param) : {};
+                locals.arguments = Array.prototype.slice.call(arguments, 0);
+
+                return parsedExpression(scope, locals);
+              };
+            } else if (parsedExpression) {
+              scope.$watch(parsedExpression, getUpdater(propName));
+
+              props[propName] = parsedExpression(scope);
             } else {
               props[propName] = value;
             }
           }
         });
 
-        var component = React.renderComponent(new ComponentClass(props), element[0]);
+        var component = React.renderComponent(new ComponentClass(props), iElement[0]);
+
+        iElement.on('$destroy', function() {
+          React.unmountComponentAtNode(iElement[0]);
+        });
       }
     };
   }
 ]);
 
-module.exports = function(ring_components) {
-  Ring = ring_components;
+module.exports = function (ringComponents) {
+  Ring = ringComponents;
 };
