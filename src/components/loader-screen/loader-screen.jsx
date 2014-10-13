abda2473b38b08d7eece89a1f3d503a040d981bb
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2014-10-13 14:50:26 +0200

    loader-screen - initial
    Former-commit-id: 942ab2ecca31cfb3f29a98240599f7ae7d568278

---
+++ src/components/loader-screen/loader-screen.jsx
@@ -0,0 +1,31 @@
+/** @jsx React.DOM */
+
+var React = require('react');
+var Loader = require('../loader/loader');  // jshint -W098
+require('./loader-screen.scss');
+
+/**
+ * @constructor
+ * @extends {ReactComponent}
+ <example>
+ <div id="loader-screen">
+ </div>
+
+ <script>
+ React.renderComponent(LoaderScreen(), document.getElementById('loader-screen'));
+ </script>
+ </example>
+ */
+var LoaderScreen = React.createClass({
+  render: function() {
+    /*jshint ignore:start*/
+    return (
+      <div className="ring-loader-screen">
+        <Loader/>
+      </div>
+    );
+    /*jshint ignore:end*/
+  }
+});
+
+module.exports = LoaderScreen;
