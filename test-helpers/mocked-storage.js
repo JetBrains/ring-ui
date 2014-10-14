856add50a27c2b07a3b334aed31bad68809cd7c4
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-10-14 16:43:00 +0400

    RG-396 Persistent storage events
    Former-commit-id: 06cc4b1fbea7bc881977612e5f27484257f1fcc0

---
+++ test-helpers/mocked-storage.js
@@ -0,0 +1,103 @@
+var mixIn = require('mout/object/mixin');
+
+/**
+ *  Create window as event target
+ */
+// jshint -W079
+var window = mixIn({}, sinon.EventTarget);
+// jshint +W079
+
+/**
+ * Custom event
+ * @constructor
+ */
+var StorageEvent = function StorageEvent(type, customData, target) {
+  this.initEvent(type, false, false, target);
+  mixIn(this, customData);
+};
+
+StorageEvent.prototype = new sinon.Event();
+
+StorageEvent.prototype.constructor = StorageEvent;
+
+/**
+ * Mocked storage (initially borrowed from https://github.com/azu/mock-localstorage)
+ * @constructor
+ */
+function MockedStorage() {
+  var storage = {};
+
+  var defaultProps = {
+    writable: false,
+    configurable: false,
+    enumerable: false
+  };
+
+  function dispatchEvent(key, value) {
+    var storageEvent = new StorageEvent('storage', {
+      key: key,
+      oldValue: storage[key],
+      newValue: value,
+      url: '/'
+    });
+
+    window.dispatchEvent(storageEvent);
+  }
+
+  Object.defineProperty(storage, 'getItem', mixIn({
+    value: function (key) {
+      if (arguments.length === 0) {
+        throw new TypeError('Failed to execute \'getItem\' on \'Storage\': 1 argument required, but only 0 present.');
+      }
+      return storage[key.toString()] || null;
+    }
+  }, defaultProps));
+
+  Object.defineProperty(storage, 'setItem', mixIn({
+    value: function (key, value) {
+      if (arguments.length < 2) {
+        throw new TypeError('Failed to execute \'setItem\' on \'Storage\': 1 argument required, but only ' + arguments.length + ' present.');
+      }
+
+      var stringKey = String(key);
+      var stringValue = String(value);
+
+      dispatchEvent(stringKey, stringValue);
+      storage[stringKey] = stringValue;
+    }
+  }, defaultProps));
+
+  Object.defineProperty(storage, 'removeItem', mixIn({
+    value: function (key) {
+      if (arguments.length === 0) {
+        throw new TypeError('Failed to execute \'removeItem\' on \'Storage\': 1 argument required, but only 0 present.');
+      }
+
+      var stringKey = String(key);
+      dispatchEvent(stringKey, null);
+      delete storage[stringKey];
+    }
+  }, defaultProps));
+
+  Object.defineProperty(storage, 'length', {
+    get: function () {
+      return Object.keys(storage).length;
+    },
+    configurable: false,
+    enumerable: false
+  });
+
+  Object.defineProperty(storage, 'clear', mixIn({
+    value: function () {
+      Object.keys(storage).forEach(function (key) {
+        storage.removeItem(key);
+      });
+    }
+  }, defaultProps));
+
+  return storage;
+}
+
+window.localStorage = new MockedStorage();
+
+module.exports = window;
