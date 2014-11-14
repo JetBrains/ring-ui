4f6602174dd375234418643f89101efcb80ed025
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2014-11-14 16:10:25 +0300

    fix
    Former-commit-id: 8207f4f79fcc3666a15142d2f530500249d880db

--- src/components/alert-ng/alert-ng.js
+++ src/components/alert-ng/alert-ng.js
@@ -10,12 +10,12 @@
 var React = require('react/addons');
 
 angular.module('Ring.alert', []).provider('alert', function() {
-  var reactAlert = require('alert/alert');
-  var reactAlerts = require('alert/alerts');
+  var ReactAlert = require('../alert/alert');
+  var ReactAlerts = require('../alert/alerts');
   var container = null;
 
   this.init = function(containerElement) {
-    container = React.renderComponent(reactAlerts, containerElement);
+    container = React.renderComponent(new ReactAlerts(), containerElement);
   };
 
   this.$get = function() {
@@ -27,26 +27,27 @@ angular.module('Ring.alert', []).provider('alert', function() {
     };
   };
 
-  function error(text, ttl) {
-    _add(text, reactAlert.Type.ERROR, ttl);
-  }
-
-  function warning(text, ttl) {
-    _add(text, reactAlert.Type.WARNING, ttl);
-  }
-
-  function message(text, ttl) {
-    _add(text, reactAlert.Type.MESSAGE, ttl);
-  }
-
-  function success(text, ttl) {
-    _add(text, reactAlert.Type.SUCCESS, ttl);
-  }
 
   function _add(text, type, ttl) {
     if(!container) {
       throw Error('Ring2 alert is not configured for use');
     }
-    return reactAlerts.add(text, type, ttl);
+    return container.add(text, type, ttl);
+  }
+
+  function error(text, ttl) {
+    _add(text, ReactAlert.Type.ERROR, ttl);
+  }
+
+  function warning(text, ttl) {
+    _add(text, ReactAlert.Type.WARNING, ttl);
+  }
+
+  function message(text, ttl) {
+    _add(text, ReactAlert.Type.MESSAGE, ttl);
+  }
+
+  function success(text, ttl) {
+    _add(text, ReactAlert.Type.SUCCESS, ttl);
   }
 });
