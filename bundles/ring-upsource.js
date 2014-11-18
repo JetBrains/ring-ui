46f4f4c04f099853fac2c0843b4b48981b16a6a2
Leonid Khachaturov <Leonid.Khachaturov@jetbrains.com>
2014-11-18 20:40:03 +0100

    Upsource bundle: remove components that were migrated to 2.0
    Former-commit-id: 29886deffa0c3286d602ea33211297af90d8ab71

--- bundles/ring-upsource.js
+++ bundles/ring-upsource.js
@@ -1,11 +1,5 @@
 define([
   'global/global',
-  'auth/auth',
-  'menu/menu',
-  'header/header',
-  'header/header__userinfo',
-  'header/header__jetbrains-services',
-  'footer/footer',
   'query-assist/query-assist',
   'dropdown-menu/dropdown-menu',
   'dropdown-filter/dropdown-filter',
