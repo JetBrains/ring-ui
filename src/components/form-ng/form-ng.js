30bb5568fb4f2f94be5916c463bdb5bdf1a15b1b
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-11-24 12:36:11 +0300

    RG-399 Copy angular components to Ring 2.0
    Former-commit-id: 3f4cf0b9609702cac41869b1a4510d28b9497986

---
+++ src/components/form-ng/form-ng.js
@@ -0,0 +1,153 @@
+/* global angular: false */
+
+require('input/input.scss');
+
+angular.module('Ring.form', []).
+/**
+ * <div error-bubble="form.name"></div>
+ * @param {object} error-bubble Ng-model for this control
+ *
+ * Where form.name is a reference to angularJS form input
+ */
+  directive('errorBubble', ['errorBubbleMessageBundle', function (bundle) {
+    return {
+      scope: {
+        'errorBubble': '&errorBubble'
+      },
+      replace: true,
+      template: require('./form-ng__error-bubble.html'),
+      link: function (scope, iElement) {
+        scope.style = {};
+        var element = iElement.siblings('input, textarea');
+
+        scope.$watch(function() {
+          var result = scope.errorBubble()
+
+          return result.$invalid && result.$dirty;
+        }, function(active) {
+          scope.active = active;
+
+          if (active && element.length) {
+            scope.style.left = element.outerWidth() + 2;
+          }
+        });
+
+        scope.msg = function (id) {
+          return bundle[id] && bundle[id]();
+        };
+      }
+    };
+  }]).
+/**
+ * This service is used by errorBubble directive to correctly show messages.
+ * You can decorate this service to provide your own localized messages.
+ */
+  service('errorBubbleMessageBundle', [function () {
+    this.required = function () {
+      return 'Value is required';
+    };
+    this.url = function () {
+      return 'Should be a valid URL';
+    };
+    this.email = function () {
+      return 'Should be a valid email';
+    };
+    this.maxlength = function () {
+      return 'Is too long';
+    };
+    this.minlength = function () {
+      return 'Is too short';
+    };
+    this.pattern = function () {
+      return 'Doesn\'t match the pattern';
+    };
+    this.equalvalue = function () {
+      return 'Is not the same';
+    };
+    this.unique = function () {
+      return 'Is not unique';
+    };
+  }]).
+/**
+ * <input name="confirm" type="password" equal-value="data.password" ng-model="data.confirm">
+ * Constraint to be user for confirm password fields.
+ */
+  directive('equalValue', [function () {
+    return {
+      require: 'ngModel',
+      link: function (scope, iElement, iAttrs, ngModelCtrl) {
+
+        function assertEqual(thisValue, thatValue) {
+          ngModelCtrl.$setValidity('equalvalue', thisValue === thatValue);
+        }
+
+        scope.$watch(iAttrs.equalValue, function (value) {
+          assertEqual(iElement.val(), value);
+        });
+
+        iElement.on('keyup', function () {
+          var thatValue = scope.$eval(iAttrs.equalValue);
+          scope.$apply(function () {
+            assertEqual(iElement.val(), thatValue);
+          });
+        });
+      }
+    };
+  }]).
+/**
+ * <input name="name" required type="text" ng-class="form.name | inputClass:submitted" ng-model="name">
+ *
+ * Is intended to be used for the value of ng-class. Accepts a reference to an angularJS form input
+ */
+  filter('inputClass', [function () {
+    return function (input, submitted) {
+      return {
+        'ring-input': true,
+        'ring-input_error': input.$invalid && (input.$dirty || submitted),
+        'ring-input_correct': !input.$invalid && (input.$dirty || submitted)
+      };
+    };
+  }])
+/**
+ * <form form-autofill-fix ...>
+ *
+ * Fixes Chrome bug: https://github.com/angular/angular.js/issues/1460
+ */
+  .directive('formAutofillFix', ['$timeout', function ($timeout) {
+    return {
+      require: '?form',
+      priority: 10,
+      link: function ($scope, element, attrs, form) {
+        if (form) {
+          var promise;
+          var count = 0;
+
+          (function poll() {
+            var filled;
+
+            angular.forEach(element.find('input'), function (elem) {
+              var $elem = angular.element(elem);
+              var controller = $elem.controller('ngModel');
+              var val = $elem.val();
+              var type = $elem.attr('type');
+
+              if (controller && val && type !== 'checkbox' && type !== 'radio') {
+                controller.$setViewValue(val);
+                filled = true;
+              }
+            });
+
+            if (!filled || count < 5) {
+              promise = $timeout(poll, 150);
+              count++;
+            }
+          }());
+
+          element.on('$destroy', function () {
+            $timeout.cancel(promise);
+          });
+
+        }
+      }
+    };
+  }]);
