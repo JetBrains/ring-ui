9b11a4c5131b03541fb64ad80dc67ca90d409aee
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-11-24 12:23:55 +0300

    RG-354 Implement components used in Hub in 2.0: remove copied in RG-399 components from ring-hub.css
    Former-commit-id: 71791e56924c670e3c89e99eb8c38dcd1586c9dc

--- bundles/ring-hub2.js
+++ bundles/ring-hub2.js
@@ -4,7 +4,6 @@ define([
   'header/header',
   'header/header__userinfo',
   'header/header__hub-services',
-  'footer/footer',
   'dropdown-menu/dropdown-menu',
   'dropdown-filter/dropdown-filter',
   'dropdown-select/dropdown-select',
