7d9e67e971b57f2660b08dd116710e582215320b
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2014-11-25 20:18:45 +0300

    RG-430 Provide React components constructors and instances in Angular
    Former-commit-id: 1e0b383902e11bda81ea92a364b9d7a31ea19316

--- src/components/react-ng/react-ng.js
+++ src/components/react-ng/react-ng.js
@@ -6,12 +6,46 @@
 /* global angular: false */
 
 var React = require('react/addons');
+var mixIn = require('mout/object/mixIn');
 
 var reactModule = angular.module('Ring.react-ng', []);
 
-var Ring = {};
-var directiveName = 'react';
+/**
+ * Ring components map
+ * @type {{}}
+ */
+var ringComponents = {};
 
+/**
+ * React component getter
+ * @param name
+ * @return {*}
+ */
+function getComponent(name) {
+  return ringComponents[name];
+}
+
+reactModule.service('ringComponents', function () {
+  return getComponent;
+});
+
+/**
+ * React component register
+ * @param componentsMap
+ */
+function registerComponents(componentsMap) {
+  mixIn(ringComponents, componentsMap);
+}
+
+module.exports = registerComponents;
+
+/**
+ * Directive to render React components
+ * <example>
+ *   <div react="ProgressBar" value="searchProgress.percent"></div>
+ * </example>
+ */
+var directiveName = 'react';
 var specialDOMAttrs = {
   'for': 'htmlFor',
   'class': 'className'
@@ -24,8 +58,9 @@ reactModule.directive(directiveName, [
       restrict: 'A',
       link: function (scope, iElement, iAttrs) {
         var name = iAttrs[directiveName];
+        var instanceAttr = 'reactInstance';
 
-        var ComponentClass = Ring[name];
+        var ComponentClass = ringComponents[name];
         if (!ComponentClass) {
           throw Error('Component ' + name + ' is not registered');
         } else if (!React.isValidClass(ComponentClass)) {
@@ -48,7 +83,7 @@ reactModule.directive(directiveName, [
         }
 
         angular.forEach(iAttrs, function (value, name) {
-          if (iAttrs.hasOwnProperty(name) && name !== directiveName && typeof value === 'string') {
+          if (iAttrs.hasOwnProperty(name) && name !== directiveName && name !== instanceAttr && typeof value === 'string') {
             // Use React DOM attributes names
             var specialDOMAttrName = specialDOMAttrs[name];
             var propName = specialDOMAttrName || name;
@@ -85,14 +120,18 @@ reactModule.directive(directiveName, [
 
         var component = React.renderComponent(new ComponentClass(props), iElement[0]);
 
-        iElement.on('$destroy', function() {
+        if (iAttrs[instanceAttr]) {
+          var instanceProp = $parse(iAttrs[instanceAttr])(scope);
+
+          if (typeof instanceProp === 'string') {
+            scope[instanceProp] = component;
+          }
+        }
+
+        iElement.on('$destroy', function () {
           React.unmountComponentAtNode(iElement[0]);
         });
       }
     };
   }
 ]);
-
-module.exports = function (ringComponents) {
-  Ring = ringComponents;
-};
