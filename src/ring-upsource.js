aa3d4456c0ee4c78b2184f173d7e6d98365fe302
Igor Alexeenko <alexeenko.igor@gmail.com>
2014-11-25 14:55:05 +0300

    Add upsource bundle to build.
    Former-commit-id: 441956275602d87b6aa256e3d95c0913b56b88e4

---
+++ src/ring-upsource.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview Ring 2.0 bundle for Upsource.
+ * @author igor.alexeenko@jetbrains.com (Igor Alekseenko)
+ */
+
+window.React = require('react');
+
+module.exports = {
+  Auth: require('auth/auth'),
+  Footer: require('footer/footer'),
+  Global: require('global/global'),
+  Header: require('header/header'),
+  Popup: require('popup/popup'),
+  PopupMenu: require('popup-menu/popup-menu')//,
+};
