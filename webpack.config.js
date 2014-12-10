f8932a3d52f0d5fcdf68927bcb1f9b534fe76a64
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2014-12-10 13:09:22 +0300

    Add include path for sass-loader to simplify including ring global in projects (just @import "global/global")
    Former-commit-id: bc669ad5ddfe62308d3e09d39c4d0b236f0c2535

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
           'style',
           'css',
           'autoprefixer?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4',
-          'sass?outputStyle=expanded'
+          'sass?outputStyle=expanded&includePaths[]=' + (path.resolve(__dirname, './components'))
         ]
       },
       //jsx loader
