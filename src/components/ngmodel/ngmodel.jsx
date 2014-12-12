416fd5332deda3823a8a7cb2a438bd955d9e777b
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2014-12-12 14:10:56 +0300

    fixes
    Former-commit-id: 393257924414efa598949389062d12769a387f38

--- src/components/ngmodel/ngmodel.jsx
+++ src/components/ngmodel/ngmodel.jsx
@@ -1,12 +1,23 @@
 var NgModelMixin = {
   componentDidUpdate: function() {
     if (this.props._onModelChange) {
-      var model = this.state;
-      if (this.ngModelStateField && this.state[this.ngModelStateField] !== undefined) {
-        model = this.state[this.ngModelStateField];
+      var data;
+      if (this.ngModelStateField) {
+        if(typeof this.ngModelStateField === 'string' && this.state[this.ngModelStateField] !== undefined) {
+          data = this.state[this.ngModelStateField];
+        } else if(typeof this.ngModelStateField === 'object') {
+          data = {};
+          for(var stateFieldName in this.state) {
+            if(this.state.hasOwnProperty(stateFieldName) && this.ngModelStateField[stateFieldName]) {
+              data[stateFieldName] = this.state[stateFieldName];
+            }
+          }
+        } else {
+          return;
+        }
       }
 
-      this.props._onModelChange(model);
+      this.props._onModelChange(data);
     }
   }
 };
