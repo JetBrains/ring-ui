09e72257d996a2a6c24b4e17425153bce9ffad86
Evgenii Schepotiev <evgenii.schepotiev@gmail.com>
2014-12-16 18:39:59 +0100

    footer and popup-menu examples and documentation
    Former-commit-id: b2139403c8c1b9475ac4f27d5a4645412c2f0c10

--- src/components/popup-menu/popup-menu.jsx
+++ src/components/popup-menu/popup-menu.jsx
@@ -11,6 +11,31 @@ var List = require('list/list');
  * @constructor
  * @mixes {Popup.Mixin}
  * @extends {ReactComponent}
+ <example name="Popup Menu">
+ <file name="index.html">
+ <div>
+ <div id="popup"></div>
+ </div>
+ </file>
+ <file name="index.js" webpack="true">
+ var PopupMenu = require('./popup-menu.jsx');
+
+ var popupMenu = PopupMenu.renderComponent(PopupMenu({
+    anchorElement: document.getElementById('popup'),
+    corner: PopupMenu.PopupProps.Corner.TOP_LEFT,
+    classNames: ['additional', 'class', 'names']
+  }, null));
+
+ popupMenu.setProps({data: [
+    {'label': 'One'},
+    {'label': 'Two', 'href': 'http://www.jetbrains.com'},
+    {'label': 'Three', 'type': PopupMenu.ListProps.Type.ITEM, 'href': 'http://www.jetbrains.com'},
+    {'type': PopupMenu.ListProps.Type.SEPARATOR},
+    {'label': 'Four', 'type': PopupMenu.ListProps.Type.LINK},
+    {'label': 'Five', 'type': PopupMenu.ListProps.Type.LINK, 'href': 'http://www.jetbrains.com', 'className': 'test'}
+  ]});
+ </file>
+ </example>
  */
 var PopupMenu = React.createClass({
   mixins: [Popup.Mixin, List.Mixin],
