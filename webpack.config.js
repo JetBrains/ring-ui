f2fde4c58605d33df9eb92fddf4c3ac96d49cc8b
Ekaterina.Zaikina <katriyna@gmail.com>
2014-12-16 15:06:15 +0300

    run tests on windows
    Former-commit-id: 4e93a7ab242861a0aa169937a6bab91150b17964

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
     extensions: ['', '.js', '.jsx'],
     fallback: [
       path.join(__dirname, 'components'),
-      path.join(__dirname, 'src/components')
+      path.join(__dirname, 'src', 'components')
     ]
   },
   resolveLoader: {
