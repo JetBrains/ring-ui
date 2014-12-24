72e408812efc95323d7533959f12e685846804ea
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2014-12-24 13:15:03 +0300

    Skip freezing tests
    Former-commit-id: c0ddee8447651a264036c68e322ff9f134651497

--- karma.conf.js
+++ karma.conf.js
@@ -82,7 +82,7 @@ module.exports = function (karma) {
     customLaunchers: {
       ChromeNoSandbox: {
         base: 'Chrome',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--test-type']
       }
     },
 
