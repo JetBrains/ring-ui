7cfce448ce7008d98987e726bbf00be65657d427
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-12-29 17:58:36 +0300

    chore: Support deferred object in dropdown
    Former-commit-id: 22c19663629609da207bf424ffbbd4543a556061

--- src/components/dropdown-ng/dropdown-ng.test.js
+++ src/components/dropdown-ng/dropdown-ng.test.js
@@ -5,17 +5,18 @@ require('./dropdown-ng');
 
 describe('DropdownNg', function () {
 
-  var scope, directiveScope, element;
+  var scope, directiveScope, element, itemsMock;
 
   beforeEach(window.module('Ring.dropdown'));
 
   /* global inject */
   beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
-    scope.items = [
+    itemsMock = [
       {id: 1, name: 'test1'},
       {id: 2, name: 'test2'}
     ];
+    scope.items = itemsMock;
 
     element = $compile('<button class="ring-btn" ring-dropdown items="items" on-item-select="onSelect" label-field="name"></button>')(scope);
     scope.$digest();
@@ -43,4 +44,15 @@ describe('DropdownNg', function () {
 
     expect(directiveScope.popupMenuInstance.props.data[0].label).to.equal(scope.items[0].name);
   });
+
+  it('should defer popup render until items is loaded', inject(function($q) {
+    scope.items = $q.defer();
+    scope.$digest();
+    expect(directiveScope.popupMenuInstance).not.to.be.defined;
+
+    scope.items.resolve(itemsMock);
+    scope.$digest();
+
+    expect(directiveScope.popupMenuInstance).to.be.defined;
+  }));
 });
