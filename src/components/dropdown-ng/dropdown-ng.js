688bbd07b4d061776ccc1f2f46245e4d44243912
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-12-29 17:58:36 +0300

    chore: Support deferred object in dropdown
    Former-commit-id: 22c19663629609da207bf424ffbbd4543a556061

--- src/components/dropdown-ng/dropdown-ng.js
+++ src/components/dropdown-ng/dropdown-ng.js
@@ -29,10 +29,10 @@ angular.module('Ring.dropdown', [])
         labelField: '@',
         onItemSelect: '='
       },
-      controller: ['$scope', '$element', function ($scope, $element) {
+      controller: ['$scope', '$element', '$q', function($scope, $element, $q) {
         $scope.popupMenuInstance = null;
 
-        $scope.renderPopup = function (items) {
+        $scope.renderPopup = function(items) {
           $scope.popupMenuInstance = PopupMenu.renderComponent(new PopupMenu({
             corner: PopupMenu.PopupProps.Corner.BOTTOM_LEFT,
             direction: PopupMenu.PopupProps.Directions.BOTTOM,
@@ -45,12 +45,25 @@ angular.module('Ring.dropdown', [])
           }));
         };
 
-        var convertItemsForPopup = function (items) {
-          return items.map(function (item) {
+        /**
+         * @return {q.promise} The deferred object which represent popup items
+         */
+        var getItems = function() {
+          if ($scope.items && $scope.items.then) {
+            return $scope.items;
+          }
+
+          var defer = $q.defer();
+          defer.resolve($scope.items);
+          return defer.promise;
+        };
+
+        var convertItemsForPopup = function(items) {
+          return items.map(function(item) {
             return {
               label: item[$scope.labelField] || item,
-              onClick: function () {
-                $scope.$apply(function () {
+              onClick: function() {
+                $scope.$apply(function() {
                   $scope.onItemSelect(item);
                 });
                 $scope.popupMenuInstance.hide();
@@ -59,19 +72,27 @@ angular.module('Ring.dropdown', [])
           });
         };
 
-        $scope.$watch('items', function (items) {
-          if (angular.isArray(items)){
+        $scope.$watch('items', function() {
+          getItems().then(function(items) {
+            if (!angular.isArray(items)) {
+              return;
+            }
+
             if (!$scope.popupMenuInstance) {
               $scope.renderPopup(convertItemsForPopup(items));
             } else {
-              $scope.popupMenuInstance.setProps({data: convertItemsForPopup(items)});
+              $scope.popupMenuInstance.setProps({
+                data: convertItemsForPopup(items)
+              });
             }
-          }
+          });
         });
 
-        $element.on('click', function ($event) {
-          $scope.popupMenuInstance.show();
-          $event.stopPropagation();
+        $element.on('click', function($event) {
+          getItems().then(function() {
+            $scope.popupMenuInstance.show();
+            $event.stopPropagation();
+          });
         });
       }]
     };
