3146303720a6ab637221dc09e57be2119533525c
Ekaterina.Zaikina <katriyna@gmail.com>
2015-01-20 19:22:58 +0300

    error page messages extracted to message-bundle-ng
    Former-commit-id: 32dc990198a0205110ddb45b99ffe4fb8c4f3000

--- src/components/message-bundle-ng/message-bundle-ng.js
+++ src/components/message-bundle-ng/message-bundle-ng.js
@@ -41,4 +41,34 @@ angular.module('Ring.message-bundle', []).
     this.avatareditor_add = function() {
       return 'Add Image';
     };
+    this.errorpage_seriouslywrong = function() {
+      return 'Oh-oh... Something went seriously wrong.';
+    };
+    this.errorpage_offline = function() {
+      return 'There\'s nothing we can do: The server seems to be offline.';
+    };
+    this.errorpage_disconnected = function() {
+      return 'Disconnected';
+    };
+    this.errorpage_disconnectedmsg = function() {
+      return 'No, no one\'s there.';
+    };
+    this.errorpage_403 = function() {
+      return 'Woah, you can\'t touch this!';
+    };
+    this.errorpage_403msg = function() {
+      return 'Unfortunately, you are not allowed to access the page you\'ve requested. It seems you don\'t have sufficient permissions.';
+    };
+    this.errorpage_404 = function() {
+      return 'Nope, can\'t find it!';
+    };
+    this.errorpage_404msg = function() {
+      return 'Despite our best efforts, there\'s nothing here to show you with the URL you requested. Most likely the URL is invalid or you don\'t have permissions to access the page.';
+    };
+    this.errorpage_500 = function() {
+      return 'Oh-oh... Something went seriously wrong';
+    };
+    this.errorpage_500msg = function() {
+      return 'Despite our best efforts, the server is not working properly.';
+    };
   }]);
