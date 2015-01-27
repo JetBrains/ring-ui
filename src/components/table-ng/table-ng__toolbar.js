9c9e1939e56c134c5f759ec5df72b7f2742e915b
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-01-27 11:28:13 +0300

    #RG-471 add height for toolbar container when controls is fixed
    Former-commit-id: a3b31c41db1e68d31b058a476cba2507e51fc538

--- src/components/table-ng/table-ng__toolbar.js
+++ src/components/table-ng/table-ng__toolbar.js
@@ -2,6 +2,15 @@
 var debounce = require('mout/function/debounce');
 var $ = require('jquery');
 
+/**
+ * Stickable toolbar, usable for tables, but can be used elsewhere.
+ * <example>
+     <rg-table-toolbar stick>
+      <ng-include src="'project-list/project-list__toolbar.tpl.html'"></ng-include>
+     </rg-table-toolbar>
+  </example>
+ */
+
 angular.module('Ring.table.toolbar', [])
   .directive('rgTableToolbar', ['$window', function ($window) {
     var DEBOUNCE_INTERVAL = 10;
@@ -10,22 +19,27 @@ angular.module('Ring.table.toolbar', [])
       restrict: 'E',
       replace: true,
       transclude: true,
-      template: '<div class="table__toolbar__controls" ng-transclude></div>',
+      template: '<div class="table__toolbar"><div class="table__toolbar__controls" ng-transclude></div></div>',
       link: function (scope, element, attrs) {
         var $wrappedWindow = $($window);
         var $element = $(element);
+        var $controls = $element.find('.table__toolbar__controls');
+
         var savedToolbarTop;
 
         var toolbarScrollListener = debounce(function () {
           var toolbarTop = savedToolbarTop || $element.offset().top;
           var scrolledTop = $window.scrollY;
 
-          if (scrolledTop > toolbarTop) {
-            savedToolbarTop = toolbarTop;
-            $element.addClass('table__toolbar__controls_fixed');
-          } else {
+          if (scrolledTop > toolbarTop && !savedToolbarTop) {
+              //save height to style to prevent collapsing after fixing controls
+              $element.css('height', $element.height());
+              savedToolbarTop = toolbarTop;
+              $controls.addClass('table__toolbar__controls_fixed');
+          } else if (scrolledTop <= toolbarTop && savedToolbarTop >= 0) {
             savedToolbarTop = null;
-            $element.removeClass('table__toolbar__controls_fixed');
+            $element.css('height', null);
+            $controls.removeClass('table__toolbar__controls_fixed');
           }
         }, DEBOUNCE_INTERVAL);
 
