6839701546603be6e03ff256dbe87eacf704d7f1
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-01-27 13:53:41 +0300

    #RG-471 extract sidebar toggle button
    Former-commit-id: d3363c8c42600f16f458037c0f7d106428723502

--- src/components/sidebar-ng/sidebar-ng.js
+++ src/components/sidebar-ng/sidebar-ng.js
@@ -1,6 +1,10 @@
 require('../sidebar/sidebar.scss');
 var debounce = require('mout/function/debounce');
 var $ = require('jquery');
+require('../react-ng/react-ng')({
+  Icon: require('../icon/icon.jsx')
+});
+
 /**
  * A sidebar directive.
  */
@@ -50,12 +54,26 @@ angular.module('Ring.sidebar', [])
 
         if (scope.placeUnderSibling) {
           var syncWith = $element.next(scope.placeUnderSibling);
-          if (syncWith) {
+          if (syncWith && syncWith.length > 0) {
             syncPositionWith(syncWith);
           } else {
             throw 'Sidebar can\'t find element to sync with.';
           }
         }
       }
+    };
+  }])
+  .directive('rgSidebarToggleButton', [function () {
+    return {
+      replace: true,
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        model: '='
+      },
+      template: '<button class="ring-btn" ng-click="model = !model">' +
+      '<span react="Icon" size="14" glyph="model ? \'chevron-right\' : \'chevron-left\'"></span>' +
+      '<span ng-transclude></span>' +
+      '</button>'
     };
   }]);
