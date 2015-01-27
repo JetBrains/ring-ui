cc1f98a72bf98a1a63553d283b94992562dc9030
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-01-27 11:11:43 +0300

    #RG-471 sticky toolbar
    Former-commit-id: 7022683327f61cd1c2427ec3424ed7cff98b060c

--- src/components/table-ng/table-ng__toolbar.js
+++ src/components/table-ng/table-ng__toolbar.js
@@ -1,11 +1,42 @@
 /*global angular*/
+var debounce = require('mout/function/debounce');
+var $ = require('jquery');
 
 angular.module('Ring.table.toolbar', [])
-  .directive('rgTableToolbar', [function () {
+  .directive('rgTableToolbar', ['$window', function ($window) {
+    var DEBOUNCE_INTERVAL = 10;
+
     return {
       restrict: 'E',
       replace: true,
       transclude: true,
-      template: '<div class="table__toolbar" ng-transclude></div>'
+      template: '<div class="table__toolbar__controls" ng-transclude></div>',
+      link: function (scope, element, attrs) {
+        var $wrappedWindow = $($window);
+        var $element = $(element);
+        var savedToolbarTop;
+
+        var toolbarScrollListener = debounce(function () {
+          var toolbarTop = savedToolbarTop || $element.offset().top;
+          var scrolledTop = $window.scrollY;
+
+          if (scrolledTop > toolbarTop) {
+            savedToolbarTop = toolbarTop;
+            $element.addClass('table__toolbar__controls_fixed');
+          } else {
+            savedToolbarTop = null;
+            $element.removeClass('table__toolbar__controls_fixed');
+          }
+        }, DEBOUNCE_INTERVAL);
+
+        //Stick toolbar if sticking is enabled
+        if (attrs.stick !== undefined) {
+          $wrappedWindow.on('scroll', toolbarScrollListener);
+
+          scope.$on('$destroy', function () {
+            $wrappedWindow.off('scroll', toolbarScrollListener);
+          });
+        }
+      }
     };
   }]);
