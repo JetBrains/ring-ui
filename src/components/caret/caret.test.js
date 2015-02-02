29866e5a5f23f8f66cc77dffd8a7e030b76010f0
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-02-02 19:23:01 +0300

    RG-480 Query Assist: js exception: improve tests
    Former-commit-id: 4e1158206511bdd7271c9b08fc27f43d0ac857b9

--- src/components/caret/caret.test.js
+++ src/components/caret/caret.test.js
@@ -2,72 +2,76 @@
 var Caret = require('./caret');
 
 describe('Caret', function () {
-  function createRange (start, end) {
-    var range = document.createRange();
-    range.setStart(this.target, start);
-    range.setEnd(this.target, end);
-    window.getSelection().addRange(range);
-  }
-
   beforeEach(function () {
     this.target = document.createElement('div');
-    this.target.innerHTML = 'this.target = document.createElement(<b>textarea</b>)';
+    this.target.innerHTML = 'this.target = document.createElement(textarea)';
+    this.target.style = 'position: absolute; left: 0; top: 0;';
     this.target.contentEditable = true;
-    this.container = document.createElement('div');
 
-    this.container.appendChild(this.target);
-    document.body.appendChild(this.container);
+    document.body.appendChild(this.target);
+
+    var range = document.createRange();
+    range.selectNodeContents(this.target);
+    window.getSelection().addRange(range);
 
     this.caret = new Caret(this.target);
   });
 
   afterEach(function () {
-    document.body.removeChild(this.container);
     window.getSelection().removeAllRanges();
-    this.container = null;
+    document.body.removeChild(this.target);
     this.target = null;
   });
 
-  it('Default position should be 0', function () {
-    this.caret.getPosition().should.equal(0);
+  describe('getPosition', function () {
+    it('Default position should be 0', function () {
+      window.getSelection().removeAllRanges();
+
+      this.caret.getPosition().should.equal(0);
+    });
+
+    it('Get of selection should return -1', function () {
+      var range = document.createRange();
+      range.setStart(this.target, 0);
+      range.setEnd(this.target, 1);
+      window.getSelection().addRange(range);
+
+      this.caret.getPosition().should.equal(-1);
+    });
+
+    it('Should get correct positions', function () {
+      window.getSelection().collapse(this.target.firstChild, 10);
+
+      this.caret.getPosition().should.equal(10);
+    });
   });
 
-  it('Get of selection should return -1', function () {
-    createRange.call(this, 0, 1);
+  describe('setPosition', function () {
+    it('Shouldn\'t change caret position if we don\'t position', function () {
+      should.not.exist(this.caret.setPosition());
 
-    this.caret.getPosition().should.equal(-1);
+      this.caret.getPosition().should.equal(0);
+    });
+
+    it('Should change caret position if we position', function () {
+      this.caret.setPosition(15);
+
+      this.caret.getPosition().should.equal(15);
+    });
   });
 
-  it('Should get correct positions', function () {
-    createRange.call(this, 0, 0);
-    window.getSelection().collapse(this.target.firstChild, 10);
+  describe('getOffset', function () {
+    it('Should get correct zero offset', function () {
+      window.getSelection().collapse(this.target.firstChild, 0);
 
-    this.caret.getPosition().should.equal(10);
-  });
+      this.caret.getOffset().should.equal(0);
+    });
 
-  it('Shouldn\'t change caret position if we don\'t position', function () {
-    should.not.exist(this.caret.setPosition());
+    it('Should get correct offset', function () {
+      window.getSelection().collapse(this.target.firstChild, 10);
 
-    this.caret.getPosition().should.equal(0);
-  });
-
-  it('Should change caret position if we position', function () {
-    this.caret.setPosition(15);
-
-    this.caret.getPosition().should.equal(15);
-  });
-
-  it('Should get correct zero', function () {
-    createRange.call(this, 0, 0);
-    window.getSelection().collapse(this.target.firstChild, 0);
-
-    this.caret.getOffset().should.equal(0);
-  });
-
-  it('Should get correct offset', function () {
-    createRange.call(this, 0, 1);
-    window.getSelection().collapse(this.target.firstChild, 10);
-
-    this.caret.getOffset().should.equal(57);
+      // Test caret offset should be about 60px (PhantomJS has small differences)
+      (Math.ceil(this.caret.getOffset() / 10) * 10).should.equal(60);
+    });
   });
 });
