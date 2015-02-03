47d29a67ebcbe8fc150f0b839d665214850388ba
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2015-02-03 11:08:53 +0300

    Sanitize mechanizm added to alert-ng
    Former-commit-id: 5b8e360e92137b6f97f3ac61c1829fc1fdc805b9

--- src/components/alert-ng/alert-ng.js
+++ src/components/alert-ng/alert-ng.js
@@ -14,8 +14,10 @@ angular.module('Ring.alert', []).provider('alert', function() {
   var ReactAlert = require('../alert/alert');
   var ReactAlerts = require('../alert/alerts');
   var container = null;
+  var allowedTags = {'A': ['href', 'class']};
 
   this.init = init;
+  this.setAllowedTags = setAllowedTags;
 
   this.$get = function() {
     return {
@@ -35,11 +37,62 @@ angular.module('Ring.alert', []).provider('alert', function() {
     container = React.renderComponent(new ReactAlerts(), containerElement.get(0));
   }
 
+  function setAllowedTags(tags) {
+    allowedTags = tags;
+  }
+
+  function _checkText(text) {
+    var dummyElement = document.createElement('div');
+    dummyElement.innerHTML = text;
+
+    var goDeeper = function(DOMNode) {
+      for (var j = 0; j < DOMNode.childNodes.length; j++) {
+        var child = DOMNode.childNodes[j];
+
+        if (child.nodeName === '#text') {
+          continue;
+        }
+
+        var tagName = child.nodeName || child.tagName;
+        if (allowedTags[tagName]) {
+          var allowedAttrs = allowedTags[tagName];
+
+          for (var i = 0; i < child.attributes.length; i++) {
+            var attr = child.attributes[i];
+            var attrName = attr.nodeName;
+            var attrValue = attr.nodeValue || attr.value;
+            if (allowedAttrs.indexOf(attrName) === -1) {
+              // remove unallowed attribute
+              child.removeAttribute(attrName);
+            } else if (attrName === 'href' && attrValue.match(/javascript:/ig)) {
+              // special processor for "href" attribute
+              /* jshint ignore:start */
+              child.setAttribute('href', 'javascript:;');
+              /* jshint ignore:end */
+            }
+          }
+
+          goDeeper(child);
+        } else {
+          // escape unallowed tags
+          var outerHTML = child.outerHTML;
+          outerHTML = outerHTML.replace(/</ig, '&lt;').replace(/>/ig, '&gt;');
+          child.outerHTML = outerHTML;
+        }
+      }
+    };
+
+    goDeeper(dummyElement);
+
+    return dummyElement.innerHTML;
+  }
+
   function _add(text, type, ttl) {
     if(!container) {
       init();
     }
-    return container.add(text, type, ttl);
+
+    return container.add(_checkText(text), type, ttl);
   }
 
   function error(text, ttl) {
@@ -57,7 +110,7 @@ angular.module('Ring.alert', []).provider('alert', function() {
   function success(text, ttl) {
     return _add(text, ReactAlert.Type.SUCCESS, ttl);
   }
-  
+
   function setRemoveCallback(removeCallback){
     if(!container) {
       init();
