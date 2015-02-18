aff24c34617c55bda523d315ba43864681c5f93d
Ekaterina.Zaikina <katriyna@gmail.com>
2015-02-18 15:39:51 +0300

    analytics refactoring
    Former-commit-id: 8ed04b41f50ba32d1967e8962dcdec67cb80a7c3

--- src/components/analytics/analytics.test.js
+++ src/components/analytics/analytics.test.js
@@ -1,5 +1,7 @@
 describe('analytics singleton', function() {
   var analytics = require('./analytics');
+  var AnalyticsGAPlugin = require('./analytics__ga-plugin');
+  var AnalyticsCustomPlugin = require('./analytics__custom-plugin');
   var Analytics = analytics.constructor;
 
   it('should be created', function() {
@@ -10,6 +12,45 @@ describe('analytics singleton', function() {
     expect(analytics.trackPageView).should.exist;
     expect(analytics.trackEvent).should.exist;
     expect(analytics.track).should.exist;
+  });
+
+  describe('ga plugin', function() {
+    var gaPlugin;
+    beforeEach(function() {
+      gaPlugin = new AnalyticsGAPlugin();
+    });
+
+    it('should init ga', function() {
+      expect(window.ga).should.exist;
+    });
+
+    it('should export interface', function() {
+      expect(gaPlugin.trackPageView).should.exist;
+      expect(gaPlugin.trackEvent).should.exist;
+    });
+
+    it('should send pageview event', function() {
+      var rememberGA = window.ga;
+      window.ga = this.sinon.spy();
+      gaPlugin.trackPageView('some-path');
+
+      window.ga.should.calledWith('send', 'pageview', 'some-path');
+
+      window.ga = rememberGA;
+    });
+
+    it('should send action event', function() {
+      var rememberGA = window.ga;
+      window.ga = this.sinon.spy();
+      gaPlugin.trackEvent('some-category', 'some-action');
+
+      window.ga.should.calledWith('send', 'event', {
+        eventCategory: 'some-category',
+        eventAction: 'some-action'
+      });
+
+      window.ga = rememberGA;
+    });
   });
 
   describe('tracking events', function() {
@@ -27,10 +68,8 @@ describe('analytics singleton', function() {
 
     describe('#enabled', function() {
       beforeEach(function() {
-        analytics.config({
-          analyticsIsAllowed: true,
-          send: send
-        });
+        var customPlugin = new AnalyticsCustomPlugin(send);
+        analytics.config([customPlugin]);
       });
 
       it('should send request to statistics server on tracking event', function() {
@@ -72,10 +111,7 @@ describe('analytics singleton', function() {
     });
     describe('#disabled', function() {
       beforeEach(function() {
-        analytics.config({
-          analyticsIsAllowed: false,
-          send: send
-        });
+        analytics.config([]);
       });
 
       it('should not send request to statistics server', function() {
---
+++ src/components/analytics/analytics__ga-plugin.js
@@ -0,0 +1,47 @@
+'use sctrict';
+
+/**
+ *
+ * @param {string?} gaId Google analytics id (should be undefined in development)
+ * @constructor
+ */
+var AnalyticsGAPlugin = function(gaId) {
+  /* jshint ignore:start */
+  (function (i, s, o, g, r, a, m) {
+    i.GoogleAnalyticsObject = r;
+    i[r] = i[r] || function () {
+      (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+      m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+  /* jshint ignore:end */
+  /**
+   * UA-57284711-1 - ga key for development purpose
+   */
+  var key = gaId || 'UA-57284711-1';
+  /* global ga */
+  ga('create', key, (!gaId ? {'cookieDomain': 'none'} : {}));
+  console.log('Analytics initialized');
+};
+
+AnalyticsGAPlugin.prototype.trackEvent = function(category, action) {
+  if (window.ga) {
+    var eventOptions = {
+      eventCategory: category,
+      eventAction: action
+    };
+    ga('send', 'event', eventOptions);
+  }
+};
+
+AnalyticsGAPlugin.prototype.trackPageView = function(path) {
+  if (window.ga) {
+    ga('send', 'pageview', path);
+  }
+};
+
+module.exports = AnalyticsGAPlugin;
