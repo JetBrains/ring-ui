9903f6cfa925dd7bea54d066a113f948029f7de7
Ekaterina.Zaikina <katriyna@gmail.com>
2015-03-03 15:31:27 +0300

    add table-ng__selection-navigate-actions (improvements after review)
    Former-commit-id: abd3ea57a70ab7c83209373b458ea1671903054e

--- components/table-ng/table-ng__selection-navigate-actions.js
+++ components/table-ng/table-ng__selection-navigate-actions.js
@@ -1,98 +1,102 @@
+var deepMixIn = require('mout/object/deepMixIn');
+
 /**
  * Class with default hotkeys navigation actions (e.g. select, clear selection, move up/down)
  * @param {Selection?} tableSelection selection, assosiated with rg-table
  * (if tableSelection is not defined, use method setSelection to configure instance)
  */
-var SelectionNavigateActions = function(tableSelection) {
+var SelectionNavigateActions = function (tableSelection) {
   this._selection = tableSelection;
   this._addSelectionMode = null;
 };
 
-SelectionNavigateActions.prototype.setSelection = function(tableSelection) {
-  this._selection = tableSelection;
-};
+deepMixIn(SelectionNavigateActions.prototype, {
+  setSelection: function (tableSelection) {
+    this._selection = tableSelection;
+  },
 
-SelectionNavigateActions.prototype.moveUp = function () {
-  if (!this._selection) {
+  moveUp: function () {
+    if (!this._selection) {
+      return false;
+    }
+    var activeItem = this._selection.activatePreviousItem();
+    return !activeItem;
+  },
+
+  moveDown: function () {
+    if (this._selection) {
+      var activeItem = this._selection.activateNextItem();
+      if (!activeItem) {
+        this._selection.setActiveItemIndex(0);
+      }
+    }
     return false;
-  }
-  var activeItem = this._selection.activatePreviousItem();
-  return !activeItem;
-};
+  },
 
-SelectionNavigateActions.prototype.moveDown = function() {
-  if (this._selection) {
-    var activeItem = this._selection.activateNextItem();
+  reset: function () {
+    this._addSelectionMode = null;
+    return !!this._selection;
+  },
+
+  selectUp: function () {
+    if (!this._selection) {
+      return false;
+    }
+    this._changeCheckedItemsArray();
+    return this.moveUp();
+  },
+
+  selectDown: function () {
+    if (!this._selection) {
+      return false;
+    }
+    this._changeCheckedItemsArray();
+    return this.moveDown();
+  },
+
+  selectCurrent: function () {
+    if (!this._selection) {
+      return false;
+    }
+    var activeItem = this._selection.getActiveItem();
+    if (!activeItem) {
+      return true;
+    } else {
+      this._selection.toggleCheck(activeItem);
+      return false;
+    }
+  },
+
+  clearSelection: function () {
+    if (!this._selection) {
+      return false;
+    }
+    var activeItem = this._selection.getActiveItem();
+
+    if (activeItem && this._selection.items.length >= 0) {
+      this._selection.clearSelection();
+      return false;
+    }
+
     if (!activeItem) {
       this._selection.setActiveItemIndex(0);
+      return false;
+    } else {
+      this._selection.clearActivity();
+      return true;
     }
-  }
-  return false;
-};
+  },
 
-SelectionNavigateActions.prototype.reset = function () {
-  this._addSelectionMode = null;
-  return !!this._selection;
-};
-
-SelectionNavigateActions.prototype.selectUp = function () {
-  if (!this._selection) {
-    return false;
+  _changeCheckedItemsArray: function () {
+    if (!this._selection) {
+      return false;
+    }
+    var activeItem = this._selection.getActiveItem();
+    if (activeItem && !this._addSelectionMode) {
+      this._addSelectionMode = activeItem.checked ? 'uncheckItem' : 'checkItem';
+    }
+    this._selection[this._addSelectionMode](activeItem);
   }
-  this._changeCheckedItemsArray();
-  return this.moveUp();
-};
-
-SelectionNavigateActions.prototype.selectDown = function () {
-  if (!this._selection) {
-    return false;
-  }
-  this._changeCheckedItemsArray();
-  return this.moveDown();
-};
-
-SelectionNavigateActions.prototype.selectCurrent = function () {
-  if (!this._selection) {
-    return false;
-  }
-  var activeItem = this._selection.getActiveItem();
-  if (!activeItem) {
-    return true;
-  } else {
-    this._selection.toggleCheck(activeItem);
-    return false;
-  }
-};
-
-SelectionNavigateActions.prototype.clearSelection = function () {
-  if (!this._selection) {
-    return false;
-  }
-  var activeItem = this._selection.getActiveItem();
-
-  if (activeItem && this._selection.items.length >= 0) {
-    this._selection.clearSelection();
-    return false;
-  }
-
-  if (!activeItem) {
-    this._selection.setActiveItemIndex(0);
-    return false;
-  } else {
-    this._selection.clearActivity();
-    return true;
-  }
-};
-
-SelectionNavigateActions.prototype._changeCheckedItemsArray = function() {
-  if (!this._selection) {
-    return false;
-  }
-  var activeItem = this._selection.getActiveItem();
-  if (activeItem && !this._addSelectionMode) {
-    this._addSelectionMode = activeItem.checked ? 'uncheckItem' : 'checkItem';
-  }
-  this._selection[this._addSelectionMode](activeItem);
-};
+});
 
 module.exports = SelectionNavigateActions;
