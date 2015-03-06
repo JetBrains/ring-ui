be8d7f50ba9ab5431ac5d8a51cd705d7a088cc30
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-03-06 15:58:40 +0300

    RG-280 replace extending with direct adding to prototype
    Former-commit-id: e6b7f4491829adc33fac1a8fcdbbbf81aee50bdd

--- components/analytics/analytics__custom-plugin.js
+++ components/analytics/analytics__custom-plugin.js
@@ -1,7 +1,6 @@
 'use sctrict';
 
 var AnalyticsCustomPluginUtils = require('./analytics__custom-plugin-utils');
-var deepMixIn = require('mout/object/deepMixIn');
 
 var AnalyticsCustomPlugin = function (send, isDevelopment, flushInterval) {
   this._data = [];
@@ -16,61 +15,59 @@ var AnalyticsCustomPlugin = function (send, isDevelopment, flushInterval) {
   this._flushInterval = flushInterval || 10000;
 };
 
-deepMixIn(AnalyticsCustomPlugin.prototype, {
-  trackEvent: function (category, action) {
-    this._processEvent(category, action);
-    /**
-     * Save last user action to track 'pageview-last-action'
-     */
-    this._lastUserEventPresentation = category + '$' + action;
-  },
+AnalyticsCustomPlugin.prototype.trackEvent = function (category, action) {
+  this._processEvent(category, action);
+  /**
+   * Save last user action to track 'pageview-last-action'
+   */
+  this._lastUserEventPresentation = category + '$' + action;
+};
 
-  trackPageView: function (path) {
-    this._trackPageViewAdditionalInfo(path);
-    this._processEvent('page', path);
-    this._processEvent('navigator_user-agent', AnalyticsCustomPluginUtils.getUserAgentPresentation());
-    this._processEvent('navigator_platform', navigator.platform);
-    this._processEvent('navigator_lang', navigator.language);
-  },
+AnalyticsCustomPlugin.prototype.trackPageView = function (path) {
+  this._trackPageViewAdditionalInfo(path);
+  this._processEvent('page', path);
+  this._processEvent('navigator_user-agent', AnalyticsCustomPluginUtils.getUserAgentPresentation());
+  this._processEvent('navigator_platform', navigator.platform);
+  this._processEvent('navigator_lang', navigator.language);
+};
 
-  _initSendSchedule: function () {
-    var self = this;
-    window.addEventListener('beforeunload', function () {
-      self._trackPageViewAdditionalInfo();
-      return self._flush();
-    });
-    setInterval(self._flush, self._flushInterval);
-    this._hasSendSchedule = true;
-  },
+AnalyticsCustomPlugin.prototype._initSendSchedule = function () {
+  var self = this;
+  window.addEventListener('beforeunload', function () {
+    self._trackPageViewAdditionalInfo();
+    return self._flush();
+  });
+  setInterval(self._flush, self._flushInterval);
+  this._hasSendSchedule = true;
+};
 
-  _processEvent: function (category, action) {
-    if (!this._hasSendSchedule && this._flush) {
-      this._initSendSchedule();
-    }
-    if (this._isDevelopment) {
-      /* eslint-disable no-console*/
-      console.log('TRACKING DATA = ', category, action);
-      /* eslint-enable no-console*/
-    }
-    this._data.push({
-      category: AnalyticsCustomPluginUtils.reformatString(category),
-      action: AnalyticsCustomPluginUtils.reformatString(action)
-    });
-  },
-
-  _trackPageViewAdditionalInfo: function (newPagePath) {
-    var currentTime = (new Date()).getTime();
-    if (this._lastPagePath) {
-      this._processEvent('pageview-last-action_' + this._lastPagePath, this._lastUserEventPresentation || 'left-with-no-action');
-      if (this._lastPageViewTime) {
-        var duration = AnalyticsCustomPluginUtils.getPageViewDurationPresentation(currentTime - this._lastPageViewTime);
-        this._processEvent('pageview-duration_' + this._lastPagePath, duration);
-      }
-    }
-    this._lastPageViewTime = currentTime;
-    this._lastPagePath = newPagePath;
-    this._lastUserEventPresentation = undefined;
+AnalyticsCustomPlugin.prototype._processEvent = function (category, action) {
+  if (!this._hasSendSchedule && this._flush) {
+    this._initSendSchedule();
   }
-});
+  if (this._isDevelopment) {
+    /* eslint-disable no-console*/
+    console.log('TRACKING DATA = ', category, action);
+    /* eslint-enable no-console*/
+  }
+  this._data.push({
+    category: AnalyticsCustomPluginUtils.reformatString(category),
+    action: AnalyticsCustomPluginUtils.reformatString(action)
+  });
+};
+
+AnalyticsCustomPlugin.prototype._trackPageViewAdditionalInfo = function (newPagePath) {
+  var currentTime = (new Date()).getTime();
+  if (this._lastPagePath) {
+    this._processEvent('pageview-last-action_' + this._lastPagePath, this._lastUserEventPresentation || 'left-with-no-action');
+    if (this._lastPageViewTime) {
+      var duration = AnalyticsCustomPluginUtils.getPageViewDurationPresentation(currentTime - this._lastPageViewTime);
+      this._processEvent('pageview-duration_' + this._lastPagePath, duration);
+    }
+  }
+  this._lastPageViewTime = currentTime;
+  this._lastPagePath = newPagePath;
+  this._lastUserEventPresentation = undefined;
+};
 
 module.exports = AnalyticsCustomPlugin;
