e81415f512b67a0940abf432e0f836cf69939b09
Maksim Ryzhikov <rv.maksim@gmail.com>
2015-03-24 16:53:37 +0300

    fix: test for caret in PhantomJS
    Former-commit-id: ffff906dc9702f2296b1463f7a115f391d727472

--- components/caret/caret.test.js
+++ components/caret/caret.test.js
@@ -75,10 +75,10 @@ describe('Caret', function () {
     });
 
     it('Should get correct offset', function () {
+      var isPhantomJS = window.navigator.appVersion.search('PhantomJS') > -1;
       window.getSelection().collapse(this.target.firstChild, 10);
 
-      // Test caret offset should be about 50px (PhantomJS has small differences)
-      (Math.ceil(this.caret.getOffset() / 10) * 10).should.equal(50);
+      (Math.ceil(this.caret.getOffset() / 10) * 10).should.equal(isPhantomJS ? 60 : 50);
     });
   });
 });
