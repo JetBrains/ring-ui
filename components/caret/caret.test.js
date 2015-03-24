2455fa3b68dc709f18d08ccc43d9850fa792f77f
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-03-24 17:50:11 +0300

    Proper caret.getOffest test fix
    Former-commit-id: 9148b1636fe3428bf3f3e6ba4908fb6071c89407

--- components/caret/caret.test.js
+++ components/caret/caret.test.js
@@ -75,14 +75,10 @@ describe('Caret', function () {
     });
 
     it('Should get correct offset', function () {
-      var isPhantomJS = window.navigator.appVersion.search('PhantomJS') > -1;
-
-      if (isPhantomJS) {
-        return true;
-      }
-
       window.getSelection().collapse(this.target.firstChild, 10);
-      (Math.ceil(this.caret.getOffset() / 10) * 10).should.equal(50);
+
+      // Test caret offset should be about 50px (browsers have some small differences)
+      this.caret.getOffset().should.be.closeTo(50, 2);
     });
   });
 });
