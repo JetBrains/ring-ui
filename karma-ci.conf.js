9e3317b635f20b425f27957a1d524adf19af4e8e
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-03-31 17:10:58 +0300

    Run unit tests on CI using Selenium Grid
    Former-commit-id: a6ded836a07195f55e08842293dfe2cd9397fb18

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -5,7 +5,8 @@ var generateConfig = require('./karma.conf.js');
 module.exports = function(karma) {
   var config = merge(generateConfig(karma), {
     action: 'run',
-    browsers: ['ChromeNoSandbox', 'Firefox'],
+    // Something is broken in IE9 in hardly debuggable way, so we'll fix it someday
+    browsers: ['wdIE11', /*'wdIE9', */'wdFirefox', 'wdChrome'],
     reporters: 'teamcity',
     webpackServer: {}
   });
