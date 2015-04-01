1466152b0d71aa1d4a889f04b4b2ae146ffc4ad8
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-04-01 15:02:31 +0300

    Manual placing render-into-document hook in root mocha suite
    Former-commit-id: 9bf26f1a5313441d9e44351ababd5277369072ce

--- test-helpers/render-into-document.js
+++ test-helpers/render-into-document.js
@@ -1,8 +1,11 @@
 var React = require('react/addons');
 
 var containerNode;
-
-afterEach(function() {
+/**
+ * Add after each to top-level suite to be sure that the hook will be called before each test
+ */
+/*global mocha*/
+mocha.suite.afterEach(function() {
   var firstChild;
 
   if (!containerNode || !containerNode.firstChild) {
