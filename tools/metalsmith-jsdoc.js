22963676915b5d0cd8c2523291f140a817fe0e4b
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-04-11 01:19:12 +0300

    RG-624 Docsite: enable livereload — extract jsdoc-example-processor
    Former-commit-id: 6f96a3ee8e972e022dcffd11d558911fa9188466

--- tools/metalsmith-jsdoc.js
+++ tools/metalsmith-jsdoc.js
@@ -1,34 +1,13 @@
 /* eslint-env node */
 var path = require('path');
-var beautify = require('js-beautify');
 
 var fillIn = require('mout/object/fillIn');
 var arrayFind = require('mout/array/find');
 var pluck = require('mout/array/pluck');
 var flatten = require('mout/array/flatten');
-var filter = require('mout/array/filter');
 
 var dox = require('dox');
 var slug = require('slug');
-var parseXML = require('xml-parser');
-var buildXML = require('./xml-builder');
-
-function slugify(str) {
-  return slug(str).toLowerCase();
-}
-
-// Meant to be beautify methods compatible
-var langMap = {
-  '.js': 'js',
-  '.jsx': 'js',
-  '.html': 'html',
-  '.css': 'css',
-  '.scss': 'scss'
-};
-
-var beautifyOptions = {
-  'indent_size': 2
-};
 
 function simpleProcessor(tagContext) {
   return tagContext.tag.string;
@@ -38,124 +17,15 @@ function emptyProcessor() {
   return '';
 }
 
-function wrapHtml(head, body) {
-  return [{
-    name: 'html',
-    attributes: {},
-    children: [{
-      name: 'head',
-      attributes: {},
-      children: head || [],
-      content: ''
-    }, {
-      name: 'body',
-      attributes: {},
-      children: body || [],
-      content: ''
-    }],
-    content: ''
-  }];
-}
-
-
-function exampleProcessor(tagContext, metalsmithContext) {
-  var example = parseXML(tagContext.tag.string).root;
-  var header = '\n\n### Example: ' + example.attributes.name + '\n';
-
-  var files = example.children.map(function (exampleFile) {
-    exampleFile.directory = 'example-' + slugify(example.attributes.name || name + '-' + tagContext.index) + '/';
-    exampleFile.fileName = exampleFile.attributes.name || 'index.js';
-    exampleFile.fileExt = path.extname(exampleFile.fileName);
-    exampleFile.fileBase = path.basename(exampleFile.fileName, exampleFile.fileExt);
-    exampleFile.fileBuiltName = exampleFile.fileName;
-    exampleFile.lang = langMap[exampleFile.fileExt];
-
-    if (exampleFile.attributes.webpack) {
-      exampleFile.fileBuiltName = exampleFile.fileBase + '.build' + exampleFile.fileExt;
-      exampleFile.webpack = {
-        entry: path.resolve('..', 'docs', exampleFile.directory, exampleFile.fileName),
-        output: {
-          path: path.resolve('..', 'docs', exampleFile.directory),
-          filename: exampleFile.fileBuiltName,
-          pathinfo: true
-        }
-      };
-    }
-
-    return exampleFile;
-  });
-
-  var webpackEntries = pluck(filter(files, 'webpack'), 'webpack');
-
-  if (webpackEntries.length) {
-    var metadata = metalsmithContext.metalsmith.metadata();
-    metadata.webpackEntries = metadata.webpackEntries ?
-      metadata.webpackEntries.concat(webpackEntries) :
-      webpackEntries;
-  }
-
-  var scripts = filter(files, {fileExt: '.js'}).map(function (file) {
-    return {
-      name: 'script',
-      attributes: {
-        src: file.fileBuiltName,
-        type: 'text/javascript'
-      },
-      content: ''
-    };
-  });
-
-  var runnableExamples = filter(files, {fileExt: '.html'}).map(function (file) {
-    var link = file.directory + file.fileName;
-    return '<div class="markdown-example">' +
-      '<iframe src="' + link + '" class="markdown-example__frame"></iframe>' +
-      '<a href="' + link + '" class="markdown-example__link">Example</a>' +
-      '</div>';
-  }).join('');
-
-  files.forEach(function (file) {
-    var content = file.content;
-    var children = file.children;
-
-    if (file.fileExt === '.html' && scripts.length) {
-      var title = {
-        name: 'title',
-        attributes: {},
-        content: example.attributes.name
-      };
-
-      children = wrapHtml([title], file.children.concat(scripts));
-    }
-
-    if (content === '' && children.length) {
-      content = buildXML(children);
-    }
-
-    metalsmithContext.files[file.directory + file.fileName] = {
-      contents: new Buffer(beautify[file.lang](content, beautifyOptions))
-    };
-  });
-
-  var contents = files.map(function (file) {
-    var content = file.content;
-
-    if (content === '' && file.children.length) {
-      content = buildXML(file.children);
-    }
-
-    return '\n\n```' + file.lang + '\n' + beautify[file.lang](content, beautifyOptions) + '\n```';
-  }).join('');
-
-  return header + runnableExamples + contents;
-}
-
-var processors = {
+var defaultProcessors = {
   description: simpleProcessor,
-  default: emptyProcessor,
-  example: exampleProcessor
+  default: emptyProcessor
 };
 
-function configure() {
+function configure(options) {
+  options = options || {};
+
+  var processors = fillIn({}, options.tags || {}, defaultProcessors);
 
   return function parse(files, metalsmith, done) {
     setImmediate(done);
@@ -195,7 +65,7 @@ function configure() {
         });
       });
 
-      files[slugify(name) + '.md'] = fillIn({
+      files[slug(name).toLowerCase() + '.md'] = fillIn({
         title: name,
         contents: new Buffer(contents.join(''))
       }, files[file]);
