80c9bfeed36aab39475d34bdcaae41c180fee87c
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-04-29 01:15:24 +0300

    #RG-296 gemini runner script
    Former-commit-id: 21ac6607e7c423df743fb155702ccfb89d9380a3

--- tools/gemini-runner.js
+++ tools/gemini-runner.js
@@ -1,14 +1,44 @@
+/* eslint-env node */
 var Gemini = require('gemini/api');
 
-var gemini = new Gemini('.gemini.yml',
-  {
-    rootUrl: require('os').hostname() + ':9999'
-  }
-);
+var docsiteUrl = require('os').hostname() + ':9999';
+var isGather = process.argv.indexOf('--gather') !== -1;
+var isHtmlReporter = process.argv.indexOf('--html') !== -1;
 
-gemini.test(['components/alert/alert.gemini.js'], {
-  //reporters: ['html']
-})
-  .done(function (res) {
-    console.log('Done', res);
-  });
+var gemini = new Gemini('.gemini.yml', {
+  rootUrl: docsiteUrl
+});
+
+function getFilesFromArguments() {
+  var startIndex = process.argv.indexOf('--files') + 1;
+  return process.argv.slice(startIndex);
+}
+
+function handleGeminiError(error) {
+  console.error('Error: ' + error.message);
+  if (error.advice) {
+    console.error('To fix:', error.advice);
+  }
+}
+
+var files = getFilesFromArguments();
+
+if (isGather) {
+  if (files.length > 1) {
+    throw new Error('You should gather only 1 file at time, received: ' + files.join(','));
+  }
+  gemini.gather(files, {})
+    .then(function (res) {
+      console.log('Gather done', res);
+    })
+    .fail(handleGeminiError);
+
+} else {
+  gemini.test(files, {
+    reporters: isHtmlReporter ? ['html'] : null
+  })
+    .then(function (res) {
+      console.log('Test done', res);
+    })
+    .fail(handleGeminiError);
+}
