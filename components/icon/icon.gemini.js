a6a8edb3cee7e5b3a4c48765a62e8f865ef8dd3d
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-04-29 15:15:18 +0300

    #RG-296 use child suits for icon tests
    Former-commit-id: fc009f2fa20ee19276184d6d85ef825c869f99d1

--- components/icon/icon.gemini.js
+++ components/icon/icon.gemini.js
@@ -1,10 +1,17 @@
 var gemini = require('gemini');
 
-gemini.suite('Icon', function(suite) {
-  suite
-    .setUrl('/example-icon/')
-    .setCaptureElements('#some-icons')
-    .capture('some icons')
-    .setCaptureElements('#all-icons')
-    .capture('all icons');
+gemini.suite('Icon', function(parent) {
+  parent.setUrl('/example-icon/');
+
+  gemini.suite('Example icons', function (child) {
+    child
+      .setCaptureElements('#some-icons')
+      .capture('some icons');
+  });
+
+  gemini.suite('All icons', function (child) {
+    child
+      .setCaptureElements('#all-icons')
+      .capture('all icons');
+  });
 });
