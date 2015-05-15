c6bd0315369e906f9dac9cd7cab3bfbdb5b66d6a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-15 17:53:05 +0300

    #RG-698 Basic visual tests for select
    Former-commit-id: 8fc38078bfcd79e43a605ac1b3c9fbfdbf386a9a

---
+++ components/select/select.gemini.js
@@ -0,0 +1,31 @@
+var gemini = require('gemini');
+
+gemini.suite('Select', function () {
+
+  gemini.suite('Disabled select', function (child) {
+    child
+      .setUrl('/example-disabled-select')
+      .setCaptureElements(['#demo1', '#demo2'])
+      .capture('plain');
+
+  });
+
+  gemini.suite('Input based select', function (child) {
+    child
+      .setUrl('/example-simple-input-based-select')
+      .setCaptureElements(['.ring-select', '.ring-popup'])
+      .capture('selectPopup', function (actions, find) {
+        actions.click(find('.ring-input'));
+      });
+  });
+
+  gemini.suite('Select with filter', function (child) {
+    child
+      .setUrl('/example-simple-select-with-default-filter-mode')
+      .setCaptureElements(['.ring-select', '.ring-popup'])
+      .capture('selectPopup', function (actions, find) {
+        actions.click(find('.ring-select'));
+      });
+  });
+
+});
