5aec1edd880b8c870a60abacb1e5afa81bf90b42
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-15 18:44:24 +0300

    #RG-698 fix visual tests for select
    Former-commit-id: d41324137169204ea2cf0aadd44ef1267484db1b

--- components/select/select.gemini.js
+++ components/select/select.gemini.js
@@ -2,18 +2,17 @@ var gemini = require('gemini');
 
 gemini.suite('Select', function () {
 
-  gemini.suite('Disabled select', function (child) {
-    child
-      .setUrl('/example-disabled-select')
-      .setCaptureElements(['#demo1', '#demo2'])
+  gemini.suite('Disabled select button', function (child) {
+    child.setUrl('/example-disabled-select')
+      .setCaptureElements('#demo1')
+      .ignoreElements('.ring-select__icons')
       .capture('plain');
-
   });
 
   gemini.suite('Input based select', function (child) {
     child
       .setUrl('/example-simple-input-based-select')
-      .setCaptureElements(['.ring-select', '.ring-popup'])
+      .setCaptureElements('.ring-select', '.ring-popup')
       .capture('selectPopup', function (actions, find) {
         actions.click(find('.ring-input'));
       });
@@ -22,9 +21,10 @@ gemini.suite('Select', function () {
   gemini.suite('Select with filter', function (child) {
     child
       .setUrl('/example-simple-select-with-default-filter-mode')
-      .setCaptureElements(['.ring-select', '.ring-popup'])
+      .setCaptureElements('.ring-select', '.ring-popup')
       .capture('selectPopup', function (actions, find) {
         actions.click(find('.ring-select'));
+        actions.click(find('.ring-popup .ring-input'));
       });
   });
 
