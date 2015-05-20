88ca11d19aac80dbc160143fbdef54c3ea93bd0c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-05-20 11:20:43 +0300

    Prevent broken package publication, take #3
    Former-commit-id: 2e9e62f30a5b0695e191e2b71ec5d0b759fde495

--- tools/message-build-fail.js
+++ tools/message-build-fail.js
@@ -1,3 +1,6 @@
 /* eslint-env node */
 
-console.log('##teamcity[buildStatus status=\'FAILURE\' text=\'{build.status.text}, webpack.config.js is missing\']');
+var message = 'webpack.config.js is missing';
+console.log('##teamcity[buildStatus status=\'FAILURE\' text=\'{build.status.text},' + message + '\']');
+
+throw new Error(message);
