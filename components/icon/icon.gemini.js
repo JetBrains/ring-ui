3ba05eb94f5f2afb8b35dc292b1eeef6e5236df1
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-21 18:55:24 +0300

    Retake icons images for chrome because of changed render in selenium browsers
    Former-commit-id: 0a84c49e7d465f337c42b524bccd329e81468a6f

--- components/icon/icon.gemini.js
+++ components/icon/icon.gemini.js
@@ -2,6 +2,7 @@ var gemini = require('gemini');
 
 gemini.suite('Icon', function(parent) {
   parent.setUrl('/example-icon/');
+  parent.setTolerance(4); //Increase tolerance to avoid wrong render artifacts detecting
 
   gemini.suite('Example icons', function (child) {
     child
