a8c4f69abfb9bbab180a18e0dc0dbb40c487d8b5
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-25 17:42:40 +0300

    #JPS-1525 refactoring on supported-browsers block: extract smiles, create example of markup usage
    Former-commit-id: daa2ef5550e403459dff1030184de6c7f1d67489

---
+++ components/supported-browsers/supported-browsers__smiles.js
@@ -0,0 +1,40 @@
+
+var smileChanges = 0;
+var MAX_SMILE_CHANGES = 50;
+
+function changeSmileClickListener(event) {
+  smileChanges++;
+
+  var eyes = ['O', 'o', '-', '>', '<'];
+
+  var rand = function (min, max) {
+    return Math.round((Math.random() * (max - min))) + min;
+  };
+
+  var getRandomEye = function() {
+    return eyes[rand(0, (eyes.length - 1))];
+  };
+
+
+  var getRandomSmile = function() {
+    if (smileChanges >= MAX_SMILE_CHANGES) {
+      return '\\\\ (x_x) //';
+    }
+
+    return '{{ (' + getRandomEye() + '_' + getRandomEye() + ') }}';
+  };
+
+  var target = event.target || event.srcElement;
+  target.innerHTML = getRandomSmile();
+}
+
+function attachSmileClickListener (smileNode) {
+  if (smileNode.addEventListener) {
+    smileNode.addEventListener('click', changeSmileClickListener);
+  } else if (smileNode.attachEvent) {
+    smileNode.addEventListener('onclick', changeSmileClickListener);
+  }
+}
+
+module.exports = attachSmileClickListener;
+
