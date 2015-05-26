46651a6a9cd8291fd3aa100e6573168e75146c73
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-26 14:28:15 +0300

    #JPS-1525 add methods to listen global errors and test for old-browsers-message
    Former-commit-id: dbe8efb1e8a47e24eed3693761e6a4856c3daa57

---
+++ components/old-browsers-message/old-browsers-message.gemini.js
@@ -0,0 +1,8 @@
+var gemini = require('gemini');
+
+gemini.suite('Old-browser-message', function(suite) {
+  suite
+    .setUrl('/example-old-browsers-message/')
+    .setCaptureElements('.ring-old-browsers-message')
+    .capture('plain');
+});
