41bed534cff8d8e340291117d4fc98dd792db676
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-05-27 15:11:00 +0300

    Revert minification flag for css loader
    Former-commit-id: b9398ae0401ea4e21a1ef6529b24dbb79221da30

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
         test: /\.scss$/,
         loaders: [
           'style',
-          'css?minimize',
+          'css',
           'autoprefixer?browsers=last 2 versions, safari 5, ie 8, ie 9, opera 12.1, ios 6, android 4',
           'sass?outputStyle=expanded&includePaths[]=' + (path.resolve(__dirname, './components'))
         ]
