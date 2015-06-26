1e5b14139ffb7f730ce9d9aa5f634eb92761ea1e
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-06-26 08:29:43 +0300

    RG-737 TeamCity ring header causes error logging in JS console (Firefox): remove redundant sketch stuff
    Former-commit-id: 3b2c0cfd0bb33a4216ec918de3be10cee402786b

--- components/icon/icon__template.js
+++ components/icon/icon__template.js
@@ -194,8 +194,7 @@ var transformSVG = function(fileContents) {
     'svg': {
       '$': {
         'xmlns': 'http://www.w3.org/2000/svg',
-        'xmlns:xlink': 'http://www.w3.org/1999/xlink',
-        'xmlns:sketch': 'http://www.bohemiancoding.com/sketch/ns'
+        'xmlns:xlink': 'http://www.w3.org/1999/xlink'
       }
     }
   };
