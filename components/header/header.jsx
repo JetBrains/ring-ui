e456904a4eadbf53863b3a4e11e3215b6df6cde6
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-07-01 12:52:13 +0300

    RG-701 Add tooltips for header icons
    Former-commit-id: eaeddc733e1ac97409548b1ca292c2df1c32591d

--- components/header/header.jsx
+++ components/header/header.jsx
@@ -443,7 +443,7 @@ var Header = React.createClass({
         headerClassName.getClassName('user-menu-item', 'login'), true));
 
     var menuItems = Global.createObject(
-        MenuItemType.SETTINGS, (<HeaderItem key="settings" ref="settings" glyph="cog1" href={this.props.settingsLink} onOpen={this.props.onSettingsOpen} onClose={this.props.onSettingsClose} title="Settings" />),
+        MenuItemType.SETTINGS, (<HeaderItem key="settings" ref="settings" glyph="cog1" href={this.props.settingsLink} onOpen={this.props.onSettingsOpen} onClose={this.props.onSettingsClose} title="Administration" />),
         MenuItemType.HELP, (<HeaderItem key="help" ref="help" glyph="help" href={this.props.helpLink} onOpen={this.props.onHelpOpen} onClose={this.props.onHelpClose} title="Help" />),
         MenuItemType.SERVICES, (<HeaderItem key="services" ref="services" glyph="services" onOpen={this._onServicesOpen} onClose={this._onServicesClose} title="Services" />),
         MenuItemType.USER_MENU, (<HeaderItem key="userMenu" ref="userMenu" glyph="user1" onOpen={this.props.onUserMenuOpen} onClose={this.props.onUserMenuClose} />),
