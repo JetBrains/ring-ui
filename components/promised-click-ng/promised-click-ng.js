105c7fd61832aa728ea70c2723629646e3181562
Max Maximov <max.maximov@gmail.com>
2015-08-18 15:07:19 +0300

    I think that would be better in the tabular display interface (when we have a sidebar) all pop-ups move to the sidebar! #JPS-1327 Fixed
    Former-commit-id: 07f0d3f0ea48a7f4a17417cffb2215f00390df6b

---
+++ components/promised-click-ng/promised-click-ng.js
@@ -0,0 +1,78 @@
+/**
+ * @name Promised Click Ng
+ * @description Directive for a button active state control
+ * @example
+<example name="promised-click-ng">
+  <file name="index.html">
+    <div class="button-example" ng-app="button-test">
+      <button class="ring-btn" rg-promised-click="onClick()" ng-controller="testController">Simple use</button>
+      <button class="ring-btn" rg-promised-click test-directive>Via the controller</button>
+    </div>
+  </file>
+  <file name="index.js" webpack="true">
+    require('button/button.scss');
+    require('angular/angular.min.js');
+    require('promised-click-ng/promised-click-ng');
+
+    var buttonTestModule = angular.module('button-test', ['Ring.promised-click']);
+
+    buttonTestModule.controller('testController', function($scope, $timeout) {
+      $scope.onClick = function () {
+        return $timeout(angular.noop, 1000);
+      }
+    });
+
+    buttonTestModule.directive('testDirective', ['$timeout', function($timeout) {
+      return {
+        require: 'rgPromisedClick',
+        link: function (scope, iElement, iAttrs, rgPromisedClick) {
+          iElement.on('click', function ($event) {
+            rgPromisedClick.onPromisedClick(function ($event) {
+              return $timeout(angular.noop, 1000);
+            }, $event);
+          });
+        }
+      }
+    }]);
+  </file>
+</example>
+*/
+
+/* global angular: false */
+var promisedClickModule = angular.module('Ring.promised-click', []);
+
+promisedClickModule.directive('rgPromisedClick', function ($parse) {
+  return {
+    restrict: 'A',
+    controller: function ($scope, $element) {
+      var active = false;
+      this.onPromisedClick = function (callback, $event) {
+        if (active) {
+          if ($event) {
+            $event.preventDefault();
+          }
+        } else {
+          var promise = callback($event);
+          if (promise) {
+            $scope.$apply(function () {
+              active = true;
+              $element.addClass('ring-btn_active');
+              promise.finally(function () {
+                active = false;
+                $element.removeClass('ring-btn_active');
+              });
+            });
+          }
+        }
+      };
+    },
+    link: function (scope, iElement, iAttrs, controller) {
+      if (iAttrs.rgPromisedClick) {
+        var onClick = $parse(iAttrs.rgPromisedClick);
+        iElement.on('click', controller.onPromisedClick.bind(controller, function ($event) {
+          return onClick(scope, { '$event': $event });
+        }));
+      }
+    }
+  };
+});
