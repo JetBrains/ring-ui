4766bc251cb7f5df8b3b59825b0021d6053cccb7
Max Maximov <max.maximov@gmail.com>
2015-08-23 02:55:23 +0300

    diff was removed
    Former-commit-id: ef2880a0a5ec128c9345108657d4e90d8a7b7555

--- components/diff/__parser/diff__parser_doublepane.test.js
+++
@@ -1,9 +0,0 @@
-describe('Diff', function () {
-  describe('ParserDoublePane', function () {
-    var parsers = require('./diff__parser_doublepane');
-
-    it('should export module', function () {
-      parsers.should.exist;
-    });
-  });
-});
