b4f5cc11d41bb068aaf052d59f99fefcdf2d81bd
Max Maximov <max.maximov@gmail.com>
2015-08-23 02:55:23 +0300

    diff was removed
    Former-commit-id: 035408542391189a3aee773c9a2754a5a2c069d7

--- components/diff/__parser/diff__parser_singlepane.test.js
+++
@@ -1,9 +0,0 @@
-describe('Diff', function () {
-  describe('ParserSinglePane', function () {
-    var parser = require('./diff__parser_singlepane');
-
-    it('should export module', function () {
-      parser.should.exist;
-    });
-  });
-});
