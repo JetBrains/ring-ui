c11e032f962e3b7a24cff2c68e7346438302ffd1
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2015-09-03 17:24:25 +0300

    rgTable: pager added, Button: light-blue button for page, upper modifier, fixes
    Former-commit-id: 83dc7d53abe7e08ec1e25214e3ff85e7549a07e3

--- components/table-ng/table-ng.js
+++ components/table-ng/table-ng.js
@@ -495,6 +495,117 @@ angular.module('Ring.table', ['Ring.table.toolbar', 'Ring.react-ng', 'Ring.place
     };
   })
 /**
+ * Pagination
+ */
+  .directive('rgTablePager', ['$location', 'i18n', function($location, i18n) {
+    return {
+      restrict: 'E',
+      template: require('./table-ng__pager.html'),
+      scope: {
+        skip: '=',
+        top: '=',
+        total: '=',
+        onPageChange: '&'
+      },
+      replace: true,
+      link: function(scope, element, attrs) {
+        scope.maxPages = attrs.maxPages || 7;
+        scope.selectedPageNum = 1;
+
+        if (scope.maxPages % 2 === 0) {
+          scope.maxPages++;
+        }
+
+        var openPageAfterInit = $location.search()['page'];
+
+        scope.calculatePageClass = function(pageNum) {
+          var condition = pageNum === scope.selectedPageNum;
+          return {
+            'ring-btn_light-blue': condition,
+            'ring-btn_active': condition,
+            'ring-btn_upper': condition
+          };
+        };
+
+        scope.getTopOptionLabel = function(itemsPerPage) {
+          return itemsPerPage + i18n(' per page');
+        };
+
+        scope.$watchGroup(['skip', 'total', 'top'], function() {
+          scope.show = false;
+          var top = $location.search()['top'] || scope.top;
+          var total = scope.total;
+          var skip = scope.skip;
+
+          scope.top = top;
+          scope.topOptions = [20, 50, 100];
+
+          if (total !== undefined && skip !== undefined && top !== undefined) {
+            if (total > 0 && total > top) {
+              scope.totalPages = parseInt(total / top, 10);
+              scope.show = true;
+              scope.itemsPerPage = top;
+
+              scope.startPage = 1;
+
+              if (scope.maxPages < scope.totalPages) {
+                scope.endPage = scope.maxPages;
+                scope.sideBySidePages = (scope.maxPages - 1) / 2;
+                var freeSpaceLeft = (scope.selectedPageNum - 1) - scope.sideBySidePages;
+                var freeSpaceRight = scope.totalPages - (scope.selectedPageNum + scope.sideBySidePages);
+
+                if (freeSpaceLeft > 0) {
+                  scope.startPage = freeSpaceLeft + 1 + (freeSpaceRight < 0 ? freeSpaceRight : 0);
+                  scope.endPage = scope.startPage + scope.maxPages - 1;
+                } else {
+                  scope.endPage = scope.selectedPageNum + scope.sideBySidePages + (freeSpaceLeft < 0 ? freeSpaceLeft * -1 : 0);
+                }
+              } else {
+                scope.endPage = scope.totalPages;
+              }
+
+              scope.pages = [];
+              for (var i = scope.startPage; i <= scope.endPage; i++) {
+                scope.pages.push(i);
+              }
+
+              scope.loadPage = function(pageNum, firstLoad) {
+                pageNum = parseInt(pageNum, 10);
+                if (pageNum < 1) {
+                  pageNum = 1;
+                } else if (pageNum > scope.totalPages) {
+                  pageNum = scope.totalPages;
+                }
+
+                scope.selectedPageNum = pageNum;
+
+                scope.onPageChange({
+                  pageNum: scope.selectedPageNum,
+                  itemsPerPage: scope.top,
+                  firstLoad: firstLoad
+                });
+
+                $location.search('page', pageNum);
+              };
+
+              scope.topChange = function(newItemsPerPage) {
+                scope.top = newItemsPerPage;
+                $location.search('top', newItemsPerPage);
+                scope.loadPage(1);
+              };
+
+              if (openPageAfterInit) {
+                scope.selectedPageNum = +openPageAfterInit;
+                scope.loadPage(openPageAfterInit, true);
+                openPageAfterInit = null;
+              }
+            }
+          }
+        });
+      }
+    };
+  }])
+/**
  * Class with default hotkeys navigation actions (e.g. select, clear selection, move up/down)
  */
   .constant('SelectionNavigateActions', require('./table-ng__selection-navigate-actions'));
