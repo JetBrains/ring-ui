3c8bc0da1561a7f0e1af90228a5f7bbaec36d6dd
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2015-09-08 12:22:28 +0300

    RING-UI-CR-580: Refactoring + review fixes
    Former-commit-id: 7a6587f2b5786a71eddc2c7a357c2f4768c27f90

--- components/table-ng/table-ng.js
+++ components/table-ng/table-ng.js
@@ -1,15 +1,13 @@
 require('../table/table.scss');
 var TableSelection = require('./table-ng__selection');
 require('./table-ng__toolbar');
+require('./table-ng__pager');
 require('../place-under-ng/place-under-ng');
 var debounce = require('mout/function/debounce');
 
 require('../react-ng/react-ng')({
   Checkbox: require('../checkbox/checkbox.jsx')
 });
-
-require('message-bundle-ng/message-bundle-ng');
-
 /*global angular*/
 
 /** @name Table Ng
@@ -151,7 +149,7 @@ require('message-bundle-ng/message-bundle-ng');
   </file>
 </example>
 */
-angular.module('Ring.table', ['Ring.table.toolbar', 'Ring.react-ng', 'Ring.place-under', 'Ring.message-bundle'])
+angular.module('Ring.table', ['Ring.table.toolbar', 'Ring.table.pager', 'Ring.react-ng', 'Ring.place-under'])
   .directive('rgTable', function () {
     return {
       restrict: 'E',
@@ -496,122 +494,6 @@ angular.module('Ring.table', ['Ring.table.toolbar', 'Ring.react-ng', 'Ring.place
       }
     };
   })
-/**
- * Pagination
- */
-  .directive('rgTablePager', ['$location', 'RingMessageBundle', function($location, RingMessageBundle) {
-    return {
-      restrict: 'E',
-      template: require('./table-ng__pager.html'),
-      scope: {
-        skip: '=',
-        top: '=',
-        total: '=',
-        onPageChange: '&'
-      },
-      replace: true,
-      link: function(scope, element, attrs) {
-        scope.maxPages = attrs.maxPages || 7;
-        scope.selectedPageNum = 1;
-
-        if (scope.maxPages % 2 === 0) {
-          scope.maxPages++;
-        }
-
-        scope.nextPageText = RingMessageBundle.next_page();
-        scope.previousPageText = RingMessageBundle.previous_page();
-        scope.firstPageText = RingMessageBundle.first_page();
-        scope.lastPageText = RingMessageBundle.last_page();
-
-        var openPageAfterInit = $location.search()['page'];
-
-        scope.calculatePageClass = function(pageNum) {
-          var condition = pageNum === scope.selectedPageNum;
-          return {
-            'ring-btn_light-blue': condition,
-            'ring-btn_active': condition,
-            'ring-btn_upper': condition
-          };
-        };
-
-        scope.getTopOptionLabel = function(itemsPerPage) {
-          return itemsPerPage + ' ' + RingMessageBundle.items_per_page();
-        };
-
-        scope.$watchGroup(['skip', 'total', 'top'], function() {
-          scope.show = false;
-          var top = $location.search()['top'] || scope.top;
-          var total = scope.total;
-          var skip = scope.skip;
-
-          scope.top = top;
-          scope.topOptions = [20, 50, 100];
-
-          if (total !== undefined && skip !== undefined && top !== undefined) {
-            if (total > 0 && total > top) {
-              scope.totalPages = parseInt(total / top, 10);
-              scope.show = true;
-              scope.itemsPerPage = top;
-
-              scope.startPage = 1;
-
-              if (scope.maxPages < scope.totalPages) {
-                scope.endPage = scope.maxPages;
-                scope.sideBySidePages = (scope.maxPages - 1) / 2;
-                var freeSpaceLeft = (scope.selectedPageNum - 1) - scope.sideBySidePages;
-                var freeSpaceRight = scope.totalPages - (scope.selectedPageNum + scope.sideBySidePages);
-
-                if (freeSpaceLeft > 0) {
-                  scope.startPage = freeSpaceLeft + 1 + (freeSpaceRight < 0 ? freeSpaceRight : 0);
-                  scope.endPage = scope.startPage + scope.maxPages - 1;
-                } else {
-                  scope.endPage = scope.selectedPageNum + scope.sideBySidePages + (freeSpaceLeft < 0 ? freeSpaceLeft * -1 : 0);
-                }
-              } else {
-                scope.endPage = scope.totalPages;
-              }
-
-              scope.pages = [];
-              for (var i = scope.startPage; i <= scope.endPage; i++) {
-                scope.pages.push(i);
-              }
-
-              scope.loadPage = function(pageNum, firstLoad) {
-                pageNum = parseInt(pageNum, 10);
-                if (pageNum < 1) {
-                  pageNum = 1;
-                } else if (pageNum > scope.totalPages) {
-                  pageNum = scope.totalPages;
-                }
-
-                scope.selectedPageNum = pageNum;
-
-                scope.onPageChange({
-                  pageNum: scope.selectedPageNum,
-                  itemsPerPage: scope.top,
-                  firstLoad: firstLoad
-                });
-
-                $location.search('page', pageNum);
-              };
-
-              scope.topChange = function(newItemsPerPage) {
-                scope.top = newItemsPerPage;
-                $location.search('top', newItemsPerPage);
-                scope.loadPage(1);
-              };
-
-              if (openPageAfterInit) {
-                scope.selectedPageNum = +openPageAfterInit;
-                scope.loadPage(openPageAfterInit, true);
-                openPageAfterInit = null;
-              }
-            }
-          }
-        });
-      }
-    };
-  }])
 /**
  * Class with default hotkeys navigation actions (e.g. select, clear selection, move up/down)
  */
---
+++ components/table-ng/table-ng__pager.js
@@ -0,0 +1,118 @@
+/*global angular*/
+require('message-bundle-ng/message-bundle-ng');
+
+angular.module('Ring.table.pager', ['Ring.message-bundle'])
+  .directive('rgTablePager', ['$location', 'RingMessageBundle', function($location, RingMessageBundle) {
+    return {
+      restrict: 'E',
+      template: require('./table-ng__pager.html'),
+      scope: {
+        skip: '=',
+        top: '=',
+        total: '=',
+        onPageChange: '&'
+      },
+      replace: true,
+      link: function(scope, element, attrs) {
+        var defaultMaxPagesToShow = 7;
+        scope.maxPages = attrs.maxPages || defaultMaxPagesToShow;
+        scope.selectedPageNum = 1;
+
+        if (scope.maxPages % 2 === 0) {
+          scope.maxPages++;
+        }
+
+        scope.nextPageText = RingMessageBundle.next_page();
+        scope.previousPageText = RingMessageBundle.previous_page();
+        scope.firstPageText = RingMessageBundle.first_page();
+        scope.lastPageText = RingMessageBundle.last_page();
+
+        var openPageAfterInit = $location.search()['page'];
+
+        scope.calculatePageClass = function(pageNum) {
+          var condition = pageNum === scope.selectedPageNum;
+          return {
+            'ring-btn_light-blue': condition,
+            'ring-btn_active': condition,
+            'ring-btn_upper': condition
+          };
+        };
+
+        scope.getTopOptionLabel = function(itemsPerPage) {
+          return itemsPerPage + ' ' + RingMessageBundle.items_per_page();
+        };
+
+        scope.loadPage = function(pageNum, firstLoad) {
+          pageNum = parseInt(pageNum, 10);
+          if (pageNum < 1) {
+            pageNum = 1;
+          } else if (pageNum > scope.totalPages) {
+            pageNum = scope.totalPages;
+          }
+
+          scope.selectedPageNum = pageNum;
+
+          scope.onPageChange({
+            pageNum: scope.selectedPageNum,
+            itemsPerPage: scope.top,
+            firstLoad: firstLoad
+          });
+
+          $location.search('page', pageNum);
+        };
+
+        scope.topChange = function(newItemsPerPage) {
+          scope.top = newItemsPerPage;
+          $location.search('top', newItemsPerPage);
+          scope.loadPage(1);
+        };
+
+        scope.$watchGroup(['skip', 'total', 'top'], function() {
+          scope.show = false;
+          var top = $location.search()['top'] || +scope.top;
+          var total = scope.total;
+          var skip = scope.skip;
+
+          scope.top = top;
+          scope.topOptions = [20, 50, 100];
+
+          if (total !== undefined && skip !== undefined && top !== undefined) {
+            if (total > 0 && total > top) {
+              scope.totalPages = parseInt(total / top, 10);
+              scope.show = true;
+              scope.itemsPerPage = top;
+
+              scope.startPage = 1;
+
+              if (scope.maxPages < scope.totalPages) {
+                scope.endPage = scope.maxPages;
+                scope.sideBySidePages = (scope.maxPages - 1) / 2;
+                var freeSpaceLeft = (scope.selectedPageNum - 1) - scope.sideBySidePages;
+                var freeSpaceRight = scope.totalPages - (scope.selectedPageNum + scope.sideBySidePages);
+
+                if (freeSpaceLeft > 0) {
+                  scope.startPage = freeSpaceLeft + 1 + (freeSpaceRight < 0 ? freeSpaceRight : 0);
+                  scope.endPage = scope.startPage + scope.maxPages - 1;
+                } else {
+                  scope.endPage = scope.selectedPageNum + scope.sideBySidePages + (freeSpaceLeft < 0 ? freeSpaceLeft * -1 : 0);
+                }
+              } else {
+                scope.endPage = scope.totalPages;
+              }
+
+              scope.pages = [];
+              for (var i = scope.startPage; i <= scope.endPage; i++) {
+                scope.pages.push(i);
+              }
+
+              if (openPageAfterInit) {
+                scope.selectedPageNum = +openPageAfterInit;
+                scope.loadPage(openPageAfterInit, true);
+                openPageAfterInit = null;
+              }
+            }
+          }
+        });
+      }
+    };
+  }]);
