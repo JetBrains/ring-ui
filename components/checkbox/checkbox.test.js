795a0ef7226b83fee1f73afcd58c1796748b7a8b
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-09-09 18:26:47 +0300

    ES6 test WIP
    Former-commit-id: 2c995b9112e7efdf45490171ac7c99e343d620f3

--- components/checkbox/checkbox.test.js
+++ components/checkbox/checkbox.test.js
@@ -1,91 +1,90 @@
-var renderIntoDocument = require('render-into-document');
+import React from 'react';
+import $  from 'jquery';
+
+import renderIntoDocument from 'render-into-document';
+import TestUtils from 'react-addons-test-utils';
+
+import Checkbox  from './checkbox';
 
 describe('Checkbox', function () {
-  var $ = require('jquery');
-  var React = require('react/addons');
-  var TestUtils = React.addons.TestUtils;
-
-  var Checkbox = require('./checkbox');
-  var checkbox;
-
   beforeEach(function () {
-    checkbox = TestUtils.renderIntoDocument(React.createElement(Checkbox));
+    this.checkbox = renderIntoDocument(React.createElement(Checkbox));
   });
 
   it('should create component', function () {
-    checkbox.should.exist;
+    this.checkbox.should.exist;
   });
 
   it('should render checkbox', function () {
-    $(checkbox.refs.input).should.have.prop('type', 'checkbox');
+    $(this.checkbox.refs.input).should.have.prop('type', 'checkbox');
   });
 
   it('should generate id if not passed', function () {
-    $(checkbox.node).prop('id').should.exist;
+    $(this.checkbox.node).prop('id').should.exist;
   });
 
   it('should generate unique id', function () {
     var secondCheckboxId = renderIntoDocument(React.createElement(Checkbox)).node.getAttribute('id');
-    $(checkbox.node).should.not.have.id(secondCheckboxId);
+    $(this.checkbox.node).should.not.have.id(secondCheckboxId);
   });
 
   it('should set custom id', function () {
-    checkbox.rerender({
+    this. checkbox.rerender({
       id: 'test'
     });
 
-    $(checkbox.refs.input).should.have.id('test');
+    $(this.checkbox.refs.input).should.have.id('test');
   });
 
   it('should set name', function () {
-    checkbox.rerender({
+    this. checkbox.rerender({
       name: 'test'
     });
 
-    $(checkbox.refs.input).should.have.prop('name', 'test');
+    $(this.checkbox.refs.input).should.have.prop('name', 'test');
   });
 
   it('should call handler for click event', function () {
     var clickHandler = sinon.stub();
 
-    checkbox.rerender({
+    this. checkbox.rerender({
       onClick: clickHandler
     });
 
-    TestUtils.Simulate.click(checkbox.refs.input);
+    TestUtils.Simulate.click(this.checkbox.refs.input);
     clickHandler.should.have.been.called;
   });
 
   it('should not call handler on change event if disabled', function () {
     var inputChange = sinon.stub();
 
-    checkbox.rerender({
+    this. checkbox.rerender({
       disabled: true,
       inputChange: inputChange
     });
 
-    TestUtils.Simulate.click(checkbox.node);
+    TestUtils.Simulate.click(this.checkbox.node);
     inputChange.should.have.not.been.called;
   });
 
   it('should be unchecked by default', function () {
-    $(checkbox.node).should.not.be.checked;
+    $(this.checkbox.node).should.not.be.checked;
   });
 
   it('should check control', function () {
-    checkbox.rerender({
+    this. checkbox.rerender({
       checked: true
     });
 
-    $(checkbox.refs.input).should.be.checked;
+    $(this.checkbox.refs.input).should.be.checked;
   });
 
   it('should be disabled', function () {
-    checkbox.rerender({
+    this. checkbox.rerender({
       disabled: true
     });
 
-    $(checkbox.refs.input).should.be.disabled;
+    $(this.checkbox.refs.input).should.be.disabled;
   });
 
   it('should check control on change event', function () {
@@ -95,14 +94,14 @@ describe('Checkbox', function () {
       }
     };
 
-    TestUtils.Simulate.change(checkbox.refs.input, eventMock);
+    TestUtils.Simulate.change(this.checkbox.refs.input, eventMock);
 
-    $(checkbox.refs.input).should.be.checked;
+    $(this.checkbox.refs.input).should.be.checked;
   });
 
   it('should connect label with input by id', function () {
-    var inputId = checkbox.refs.input.getAttribute('id');
-    var forId = checkbox.node.getAttribute('for');
+    var inputId = this.checkbox.refs.input.getAttribute('id');
+    var forId = this.checkbox.node.getAttribute('for');
 
     expect(inputId).eq(forId);
   });
