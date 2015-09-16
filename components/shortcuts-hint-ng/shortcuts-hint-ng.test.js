7d47d0dae711ef6571f7537ef7240f960bcdd081
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-16 17:18:52 +0300

    RG-784 add input for filtering shortcuts
    Former-commit-id: a3eb17431f86a2b30407903bced406452eb24b2a

--- components/shortcuts-hint-ng/shortcuts-hint-ng.test.js
+++ components/shortcuts-hint-ng/shortcuts-hint-ng.test.js
@@ -5,15 +5,15 @@ import 'angular/angular';
 import 'angular-mocks/angular-mocks';
 
 describe('Shortcuts ng hint', function() {
-  let shortcutKeySymbolFilter;
-
   beforeEach(angular.mock.module(ShortcutsNgHint));
 
-  beforeEach(inject((_shortcutKeySymbolFilter_) => {
-    shortcutKeySymbolFilter = _shortcutKeySymbolFilter_;
-  }));
-
   describe('shortcut key symbol filter', function () {
+    let shortcutKeySymbolFilter;
+
+    beforeEach(inject((_shortcutKeySymbolFilter_) => {
+      shortcutKeySymbolFilter = _shortcutKeySymbolFilter_;
+    }));
+
     it('Should replace + with spaces on mac os', function() {
       this.sinon.stub(Sniffr.prototype, 'sniff', function() {
         this.os = {name: 'macos'}
@@ -42,6 +42,57 @@ describe('Shortcuts ng hint', function() {
       });
       shortcutKeySymbolFilter('ctrl+alt+shift+enter+up+down+left+right+backspace')
         .should.be.equal('Ctrl + Alt + Shift + ENTER + UP + DOWN + LEFT + RIGHT + BACKSPACE');
+    });
+  });
+
+  describe('shortcut search filter', function () {
+    let shortcutSearchFilter;
+    let fakeShortcuts;
+
+    beforeEach(inject((_shortcutSearchFilter_) => {
+      shortcutSearchFilter = _shortcutSearchFilter_;
+
+      fakeShortcuts = [
+        {key: 'shift+left+up', title: 'combination first'},
+        {key: 'ctrl+enter', title: 'some second'},
+        {key: 'meta+alt+down', title: 'combination third'}
+      ]
+    }));
+
+    it('Should not filter if query is empty', function() {
+      let filtered = shortcutSearchFilter(fakeShortcuts, undefined);
+
+      filtered.should.be.deep.equal(fakeShortcuts);
+    });
+
+    it('Should search by shortcut title', function() {
+      let filtered = shortcutSearchFilter(fakeShortcuts, 'second');
+
+      filtered.length.should.be.equal(1);
+      filtered[0].should.be.equal(fakeShortcuts[1]);
+    });
+
+    it('Should search by shortcut key', function() {
+      let filtered = shortcutSearchFilter(fakeShortcuts, 'down');
+
+      filtered.length.should.be.equal(1);
+      filtered[0].should.be.equal(fakeShortcuts[2]);
+    });
+
+    it('Should search by shortcut symbol on mac', function() {
+      this.sinon.stub(Sniffr.prototype, 'sniff', function() {
+        this.os = {name: 'macos'}
+      });
+      let filtered = shortcutSearchFilter(fakeShortcuts, '⌥');
+
+      filtered.length.should.be.equal(1);
+      filtered[0].should.be.equal(fakeShortcuts[2]);
+    });
+
+    it('Should find multiple results', function() {
+      let filtered = shortcutSearchFilter(fakeShortcuts, 'combination');
+
+      filtered.length.should.be.equal(2);
     });
   });
 });
