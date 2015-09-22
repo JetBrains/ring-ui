e29c079a578f023f530610af09c748bf7a266e3e
Max Maximov <max.maximov@gmail.com>
2015-09-22 21:25:58 +0300

    fixup! DOM utils
    Former-commit-id: 565eab1a2e9f95d75000cb5dfaa8b127441b5b9a

--- components/dom/dom.js
+++ components/dom/dom.js
@@ -5,8 +5,9 @@ export const isMounted = ::document.documentElement.contains;
 const rectStub = { top: 0, right: 0, bottom: 0, left: 0, width: 0, height: 0 };
 
 export function getRect(node) {
-  if (isMounted(node) || node instanceof Range) {
-    return node.getBoundingClientRect();
+  if (node instanceof Range || isMounted(node)) {
+    let { top, right, bottom, left, width, height } = node.getBoundingClientRect();
+    return { top, right, bottom, left, width, height };
   } else {
     return Object.assign({}, rectStub);
   }
