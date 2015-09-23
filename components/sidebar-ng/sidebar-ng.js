f904dc1715e6affa98dfa441025f75d6cf175017
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-09-24 00:51:29 +0300

    RG-801 Add New Ring Logos: fix lint error
    Former-commit-id: 2d755d1b77fcd81239782a60487a39d37d0d6c6e

--- components/sidebar-ng/sidebar-ng.js
+++ components/sidebar-ng/sidebar-ng.js
@@ -3,8 +3,8 @@ require('../place-under-ng/place-under-ng');
 require('../react-ng/react-ng')({
   Icon: require('../icon/icon.jsx')
 });
-var chevronLeftIcon = require('icon/source/chevron-left.svg');
-var chevronRightIcon = require('icon/source/chevron-right.svg');
+require('icon/source/chevron-left.svg');
+require('icon/source/chevron-right.svg');
 
 
 /**
