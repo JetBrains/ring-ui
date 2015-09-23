4a4c60b85f021882f15bd61aefd8447460414885
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-23 15:42:32 +0300

    RG-810 implement passing and removing tags, support React-ng's ng-model integration
    Former-commit-id: beb3ab4d0a7d296dfe6c87ed0ca4c3e3eb59fa05

--- components/tags-input/tags-input__tag.js
+++ components/tags-input/tags-input__tag.js
@@ -3,10 +3,21 @@ import RingComponent from 'ring-component/ring-component';
 import Icon from 'icon/icon';
 
 export default class Tag extends RingComponent {
+  static propTypes = {
+    onRemove: React.PropTypes.func
+  };
+
+  static defaultProps = {
+    onRemove: () => {}
+  };
+
   render() {
     return (<span className="tags-input__tag">
       <span>{this.props.children}</span>
-      <Icon className="tags-input__tag-remove ring-link" glyph="close" size={Icon.Size.Size12}/>
+      <Icon onClick={this.props.onRemove}
+            className="tags-input__tag-remove ring-link"
+            glyph="close"
+            size={Icon.Size.Size12}/>
     </span>);
   }
 }
