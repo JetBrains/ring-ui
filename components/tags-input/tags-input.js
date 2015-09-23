5b5e973df2e7e3599f30aaf80f0509bbd9d4214c
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-23 15:42:32 +0300

    RG-810 implement passing and removing tags, support React-ng's ng-model integration
    Former-commit-id: beb3ab4d0a7d296dfe6c87ed0ca4c3e3eb59fa05

--- components/tags-input/tags-input.js
+++ components/tags-input/tags-input.js
@@ -18,24 +18,95 @@ import './tags-input.scss';
     var TagsInput = require('tags-input/tags-input');
 
     var props = {
-
+      tags: [
+        {key: 'test1', label: 'test1'},
+        {key: 'test2', label: 'test2'}
+      ]
     };
 
     render(TagsInput.factory(props), document.getElementById('demo'));
    </file>
  </example>
+
+ <example name="Simple tags input via react-ng">
+  <file name="index.html">
+    <div ng-app="test-tags-app" ng-controller="testCtrl">
+      <div><span>tags = {{tagsArray}}</span></div>
+      <a href ng-click="addTag()">Add tag</a>
+      <span react="TagsInput" ng-model="tagsArray"></span>
+    </div>
+  </file>
+  <file name="index.js" webpack="true">
+    require('angular/angular.min.js');
+    require('react-ng/react-ng')({
+      TagsInput: require('tags-input/tags-input')
+    });
+
+    angular.module('test-tags-app', ['Ring.react-ng'])
+      .controller('testCtrl', function($scope) {
+        $scope.tagsArray = [
+          {key: 'test1', label: 'test1'},
+          {key: 'test2', label: 'test2'}
+        ];
+
+        $scope.addTag = function() {
+          $scope.tagsArray.push({key: Math.random().toFixed(3), label: Math.random().toFixed(3)})
+        };
+      });
+  </file>
+ </example>
 */
 
 export default class TagsInput extends RingComponentWithShortcuts {
-
-  static defaultProps = {
-    completeDataSource: null
+  static propTypes = {
+    tags: React.PropTypes.array,
+    completeDataSource: React.PropTypes.func,
+    onRemoveTags: React.PropTypes.func,
+    tagFormatter: React.PropTypes.func
   };
 
+  static defaultProps = {
+    completeDataSource: null,
+    onRemoveTags: () => {},
+    tagFormatter: tag => {
+      return {
+        key: tag.key,
+        label: tag.label,
+        tag: tag
+      };
+    }
+  };
+
+  state = {
+    tags: []
+  };
+
+  static ngModelStateField = 'tags';
+  ngModelStateField = TagsInput.ngModelStateField;
+
+  updateStateFromProps(props) {
+    if (props.tags) {
+      this.setState({tags: props.tags});
+    }
+  }
+
+  onRemoveTag(tagToRemove) {
+    let tags = this.state.tags.filter(tag => tag !== tagToRemove);
+    this.setState({tags});
+  }
+
+  willMount() {
+    this.updateStateFromProps(this.props);
+  }
+
+  willReceiveProps(props) {
+    this.updateStateFromProps(props);
+  }
 
   getTags() {
-    return ['test1', 'fooo', 'bar'].map((item) => {
-      return <Tag key={item}>{item}</Tag>
+    return this.state.tags.map((tag) => {
+      let formattedTag = this.props.tagFormatter(tag);
+      return <Tag key={formattedTag.key} onRemove={() => this.onRemoveTag(tag)}>{formattedTag.label}</Tag>
     });
   }
 
