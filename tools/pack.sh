2892ae189f64f8004f8ab514ee017c78eb8059d3
Max Maximov <max.maximov@gmail.com>
2015-09-24 21:51:22 +0300

    tar.gz -> tgz
    Former-commit-id: c0e06737e0315b0efdf64e469ed047ab10ed5737

--- tools/pack.sh
+++ tools/pack.sh
@@ -8,6 +8,6 @@ find -E . -regex \
   './((package.json)|(README.md)|(ring-upsource.js)|(webpack.config.js)|(components/.*/.*.(js|scss|png|gif|svg|ttf|woff|eof|html)))'\
   | cpio -pdm package &&
 
-tar -czf ring-ui-${version:1}.tar.gz package &&
+tar -czf ring-ui-${version:1}.tgz package &&
 rm -rf package &&
 echo
