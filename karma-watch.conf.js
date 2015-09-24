ab96da177befe2fca7a39b57479824bbf70f8d55
Max Maximov <max.maximov@gmail.com>
2015-09-24 17:23:36 +0300

    test-watch
    Former-commit-id: a00c605cddee9e8a52f9807b24300f0231916809

---
+++ karma-watch.conf.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+var merge = require('mout/object/merge');
+var generateConfig = require('./karma.conf.js');
+
+module.exports = function(karma) {
+  var config = merge(generateConfig(karma), {
+    singleRun: false,
+    //browsers: ['HeadlessNodeWebkit'],
+    reporters: ['nyan', 'osx'],
+    osxReporter: {
+      notificationMode: 'change'
+    }
+  });
+
+  karma.set(config);
+};
