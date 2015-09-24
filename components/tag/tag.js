b1364ffd70785123038590d4ce52366eab2928f0
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-24 11:21:35 +0300

    RG-810 extract tag to separate block. Remove obsolete not used ring tag styles.
    Former-commit-id: dc804f0bf7480e9bfd31ae29ee41ee773ae30c7f

--- components/tag/tag.js
+++ components/tag/tag.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RingComponent from 'ring-component/ring-component';
 import Icon from 'icon/icon';
+import './tag.scss';
 
 export default class Tag extends RingComponent {
   static propTypes = {
@@ -12,10 +13,10 @@ export default class Tag extends RingComponent {
   };
 
   render() {
-    return (<span className="tags-input__tag">
+    return (<span className="ring-tag">
       <span>{this.props.children}</span>
       <Icon onClick={this.props.onRemove}
-            className="tags-input__tag-remove ring-link"
+            className="ring-tag__remove ring-link"
             glyph="close"
             size={Icon.Size.Size12}/>
     </span>);
@@ -28,13 +29,13 @@ export class TagWithIcon extends Tag {
   };
 
   render() {
-    return (<span className="tags-input__tag">
-      {this.props.rgTagIcon ? <Icon className="tags-input__tag-icon"
+    return (<span className="ring-tag">
+      {this.props.rgTagIcon ? <Icon className="ring-tag__icon"
             glyph={this.props.rgTagIcon}
             size={Icon.Size.Size12}/> : null}
       <span>{this.props.children}</span>
       <Icon onClick={this.props.onRemove}
-            className="tags-input__tag-remove ring-link"
+            className="ring-tag__remove ring-link"
             glyph="close"
             size={Icon.Size.Size12}/>
     </span>);
