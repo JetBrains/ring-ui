9cffa255ae355ab81625f3f92a480065a99f10ae
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-09-28 14:56:28 +0300

    RG-810 after review fixes: merge Tag and TagWithIcon, drop mout
    Former-commit-id: 942bbaf416169a58e996ad28bf3d941503f53cc1

--- components/tag/tag.js
+++ components/tag/tag.js
@@ -5,27 +5,12 @@ import './tag.scss';
 
 export default class Tag extends RingComponent {
   static propTypes = {
-    onRemove: React.PropTypes.func
+    onRemove: React.PropTypes.func,
+    rgTagIcon: React.PropTypes.string
   };
 
   static defaultProps = {
     onRemove: () => {}
-  };
-
-  render() {
-    return (<span className="ring-tag">
-      <span>{this.props.children}</span>
-      <Icon onClick={this.props.onRemove}
-            className="ring-tag__remove ring-link"
-            glyph="close"
-            size={Icon.Size.Size12}/>
-    </span>);
-  }
-}
-
-export class TagWithIcon extends Tag {
-  static propTypes = {
-    rgTagIcon: React.PropTypes.string
   };
 
   render() {
