af8cf8ce951240d6af584a19b649929ed6adddc1
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-10-05 19:31:41 +0300

    RG-812 Use HubSource in HubUsersGroupsSource
    Former-commit-id: 31f91ba51235c8511478767e8a627622f28254c2

--- components/hub-users-groups-source/hub-users-groups-source.js
+++ components/hub-users-groups-source/hub-users-groups-source.js
@@ -1,13 +1,12 @@
-import List from 'list/list';
-
-export const TOP_ALL = -1;
+import HubSource from 'hub-source/hub-source';
 
 let defaultOptions =  {
   GroupsTitle: 'Groups',
   UsersTitle: 'Users',
   getPluralForUserCount: count => '',
   searchMax: 20,
-  cacheExpireTime: 60/*sec*/ * 1000
+  cacheExpireTime: 60/*sec*/ * 1000,
+  searchSideThreshold: 200
 };
 
 export default class HubUsersGroupsSource {
@@ -15,62 +14,36 @@ export default class HubUsersGroupsSource {
     this.auth = auth;
     this.options = Object.assign({}, defaultOptions, options);
 
-    this.groupsCache = {
-      content: null,
-      validUntil: 0
-    };
+    this.usersSource = new HubSource(auth, 'users', this.options);
+    this.groupsSource = new HubSource(auth, 'usergroups', this.options);
   }
 
-  makeRequest(relativeUrl, params) {
-    return this.auth.requestToken()
-      .then(token => this.auth.getApi(relativeUrl, token, params));
-  }
-
-  makeCachedRequest(cache, ...args) {
-    if (Date.now() > cache.validUntil) {
-      return this.makeRequest(...args)
-        .then(res => {
-          cache.content = res;
-          cache.validUntil = Date.now() +  + this.options.cacheExpireTime;
-          return res;
-        });
+  static prepareQuery(query) {
+    if (query && query.indexOf(' ') !== -1) {
+      return '{' + query + '}'
     }
-    return Promise.resolve(cache.content);
+    return query;
   }
 
-  static prepareFilter(filter) {
-    if (filter && filter.indexOf(' ') !== -1) {
-      return '{' + filter + '}'
-    }
-    return filter;
+  getUsers(query = '') {
+    query = HubUsersGroupsSource.prepareQuery(query);
+
+    return this.usersSource.get(query, {
+      query: query ? `nameStartsWith: ${query} or loginStartsWith: ${query}` : '',
+      fields: 'id,name,login,total,profile/avatar/url',
+      orderBy: 'name'
+    });
   }
 
-  getUsers(filter = '') {
-    filter = HubUsersGroupsSource.prepareFilter(filter);
-
-    return this.makeRequest('users', {
-      query: filter ? `nameStartsWith: ${filter} or loginStartsWith: ${filter}` : '',
-      fields: 'id,name,login,profile/avatar/url',
-      orderBy: 'name',
-      $top: this.options.searchMax
-    })
-      .then(response => response.users || [])
+  getGroups(query = '') {
+    return this.groupsSource.get(query, {
+      query: query ? `${query} or ${query}*` : '',
+      fields: 'id,name,total,userCount',
+      orderBy: 'name'
+    });
   }
 
-  getGroups(filter = '') {
-    return this.makeCachedRequest(this.groupsCache, 'usergroups', {
-      fields: 'id,name,userCount',
-      orderBy: 'name',
-      $top: TOP_ALL
-    })
-      .then(({usergroups = []}) => {
-        return usergroups.filter(group => {
-          return group.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1;
-        });
-      });
-  }
-
-  getUserAndGroups(filter) {
-    return Promise.all([this.getUsers(filter), this.getGroups(filter)]);
+  getUserAndGroups(query) {
+    return Promise.all([this.getUsers(query), this.getGroups(query)]);
   }
 }
