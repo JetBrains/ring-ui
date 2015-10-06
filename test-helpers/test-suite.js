f98311a8373101c3c6dc2789ff787ad3e9abe73a
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-10-06 17:56:47 +0300

    RING-UI-CR-609 RG-801 Add New Ring Logos: update svg-sprite-loader to fix tests #2
    Former-commit-id: d70d2578bc68ece5048ff0c5f9d1104fe2272167

--- test-helpers/test-suite.js
+++ test-helpers/test-suite.js
@@ -1,5 +1,2 @@
-// Required for svg-sprite-loader's angularBaseWorkaround
-require('angular/angular');
-
 var testsContext = require.context('../components', true, /\.test\.js$/);
 testsContext.keys().forEach(testsContext);
