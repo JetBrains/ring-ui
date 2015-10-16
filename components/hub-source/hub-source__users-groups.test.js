f567ad35ed10aaead4403613bf775ebe71163cb7
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-10-16 13:01:57 +0300

    RG-812 get rid of multiword query braces in full text search
    Former-commit-id: 5b5b27e873a81a48a3490b4e320859b5325528ad

--- components/hub-source/hub-source__users-groups.test.js
+++ components/hub-source/hub-source__users-groups.test.js
@@ -36,6 +36,13 @@ describe('HubUsersGroupsSource', function () {
       });
   });
 
+  it('Should construct multiword query for users', function () {
+    let source = new HubSourceUsersGroups(this.fakeAuth);
+
+    let formatted = source.usersSource.options.queryFormatter('two words');
+    formatted.should.equal('nameStartsWith: {two words} or loginStartsWith: {two words}');
+  });
+
   it('Should make request for groups', function () {
     let source = new HubSourceUsersGroups(this.fakeAuth);
     this.sinon.stub(source.groupsSource, 'get').returns(Promise.resolve([]));
