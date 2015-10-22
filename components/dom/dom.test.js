1a72f6d579d5f22ea80466cadfa92f5bc48833f1
Max Maximov <max.maximov@gmail.com>
2015-10-22 21:27:12 +0300

    DOM helpers tests
    Former-commit-id: 1040356be2110802bf28fb0a77594cf457ab5788

--- components/dom/dom.test.js
+++ components/dom/dom.test.js
@@ -1,12 +1,22 @@
 import { isMounted, getStyles, getRect } from './dom';
 
+let nodes = [];
+
 function create(tag = 'div') {
   return document.createElement(tag);
 }
 
-function prepend(node) {
+function attach(node) {
+  nodes.push(node);
   return document.body.insertBefore(node, document.body.firstChild);
 }
+
+afterEach(() => {
+  let node;
+  while (node = nodes.pop()) {
+    node.remove();
+  }
+});
 
 describe('DOM', () => {
   describe('isMounted', () => {
@@ -15,53 +25,44 @@ describe('DOM', () => {
     });
 
     it('should return true for an element attached to DOM', () => {
-      let element = create();
-      prepend(element);
-
+      let element = attach(create());
       isMounted(element).should.equal(true);
     });
 
     it('should return false for an element that\'s not attached to DOM', () => {
       let element = create();
-
       isMounted(element).should.equal(false);
     });
 
     it('should return true for textNode attached to DOM', () => {
-      let textNode = document.createTextNode('Lorem ipsum dolor sit amet.');
-      prepend(textNode);
-
+      let textNode = attach(document.createTextNode('Lorem ipsum dolor sit amet.'));
       isMounted(textNode).should.equal(true);
     });
 
     it('should return false for textNode that\'s not attached to DOM', () => {
       let textNode = document.createTextNode('Lorem ipsum dolor sit amet.');
-
       isMounted(textNode).should.equal(false);
     });
   });
 
   describe('getStyles', () => {
     it('should return css-property that has been set via setAttribute(\'style\')', () => {
-      let element = create();
-      prepend(element);
+      let element = attach(create());
       element.setAttribute('style', 'width: 100px;');
 
       getStyles(element).width.should.equal('100px');
     });
 
     it('should return css-property that has been set via style-attribute', () => {
-      let element = create();
-      prepend(element);
+      let element = attach(create());
       element.style.width = '100px';
 
       getStyles(element).width.should.equal('100px');
     });
 
     it('should return css-property that has been set before mounting the node', () => {
-      let element = create();
+      let element = attach(create());
       element.style.width = '100px';
-      prepend(element);
 
       getStyles(element).width.should.equal('100px');
     });
@@ -71,8 +72,7 @@ describe('DOM', () => {
     const style = 'position: absolute; width: 100px; height: 100px; top: 10px; left: 10px; padding: 3px; margin: 4px; border: 2px solid;';
 
     it('should return DOMRect-like object for an element', () => {
-      let element = create();
-      prepend(element);
+      let element = attach(create());
       element.setAttribute('style', style);
 
       getRect(element).should.deep.equal({ top: 14, right: 124, bottom: 124, left: 14, width: 110, height: 110 });
