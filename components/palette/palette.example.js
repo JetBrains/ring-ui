5867a4a554adf59c48c66bfc18ba68c7c348454e
Ekaterina.Zaikina <katriyna@gmail.com>
2015-10-29 07:57:16 +0300

    RG-841 Add palette to ring-ui
    Former-commit-id: 4fd14fecf11fbf65b1f2285c208e69b23f00427c

---
+++ components/palette/palette.example.js
@@ -0,0 +1,130 @@
+/**
+ * @ngdoc styles
+ * @name Palette
+ *
+ * @description
+ * Palette is used for tags and custom-fields
+ * @example
+ <example name="Palette">
+   <file name="index.html">
+    <h4>ColorId</h4>
+    <div class="example">
+      <div class="ring-palette_color-0">Color 0</div>
+      <div class="ring-palette_color-1">Color 1</div>
+      <div class="ring-palette_color-2">Color 2</div>
+      <div class="ring-palette_color-3">Color 3</div>
+      <div class="ring-palette_color-4">Color 4</div>
+      <div class="ring-palette_color-5">Color 5</div>
+      <div class="ring-palette_color-6">Color 6</div>
+    </div>
+    <div class="example">
+      <div class="ring-palette_color-7">Color 7</div>
+      <div class="ring-palette_color-8">Color 8</div>
+      <div class="ring-palette_color-9">Color 9</div>
+      <div class="ring-palette_color-10">Color 10</div>
+      <div class="ring-palette_color-11">Color 11</div>
+      <div class="ring-palette_color-12">Color 12</div>
+      <div class="ring-palette_color-13">Color 13</div>
+    </div>
+    <div class="example">
+      <div class="ring-palette_color-14">Color 14</div>
+      <div class="ring-palette_color-15">Color 15</div>
+      <div class="ring-palette_color-16">Color 16</div>
+      <div class="ring-palette_color-17">Color 17</div>
+      <div class="ring-palette_color-18">Color 18</div>
+      <div class="ring-palette_color-19">Color 19</div>
+      <div class="ring-palette_color-20">Color 20</div>
+    </div>
+    <div class="example">
+      <div class="ring-palette_color-21">Color 21</div>
+      <div class="ring-palette_color-22">Color 22</div>
+      <div class="ring-palette_color-23">Color 23</div>
+      <div class="ring-palette_color-24">Color 24</div>
+      <div class="ring-palette_color-25">Color 25</div>
+      <div class="ring-palette_color-26">Color 26</div>
+      <div class="ring-palette_color-27">Color 27</div>
+    </div>
+    <div class="example">
+      <div class="ring-palette_color-28">Color 28</div>
+      <div class="ring-palette_color-29">Color 29</div>
+      <div class="ring-palette_color-30">Color 30</div>
+      <div class="ring-palette_color-31">Color 31</div>
+      <div class="ring-palette_color-32">Color 32</div>
+      <div class="ring-palette_color-33">Color 33</div>
+      <div class="ring-palette_color-34">Color 34</div>
+    </div>
+
+    <h4>ToneId and BrightnessId</h4>
+    <div class="example">
+      <div><b>Tone 0:</b></div>
+      <div class="ring-palette_tone-0-0">Brightness 0</div>
+      <div class="ring-palette_tone-0-1">Brightness 1</div>
+      <div class="ring-palette_tone-0-2">Brightness 2</div>
+      <div class="ring-palette_tone-0-3">Brightness 3</div>
+      <div class="ring-palette_tone-0-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 1:</b></div>
+      <div class="ring-palette_tone-1-0">Brightness 0</div>
+      <div class="ring-palette_tone-1-1">Brightness 1</div>
+      <div class="ring-palette_tone-1-2">Brightness 2</div>
+      <div class="ring-palette_tone-1-3">Brightness 3</div>
+      <div class="ring-palette_tone-1-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 2:</b></div>
+      <div class="ring-palette_tone-2-0">Brightness 0</div>
+      <div class="ring-palette_tone-2-1">Brightness 1</div>
+      <div class="ring-palette_tone-2-2">Brightness 2</div>
+      <div class="ring-palette_tone-2-3">Brightness 3</div>
+      <div class="ring-palette_tone-2-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 3:</b></div>
+      <div class="ring-palette_tone-3-0">Brightness 0</div>
+      <div class="ring-palette_tone-3-1">Brightness 1</div>
+      <div class="ring-palette_tone-3-2">Brightness 2</div>
+      <div class="ring-palette_tone-3-3">Brightness 3</div>
+      <div class="ring-palette_tone-3-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 4:</b></div>
+      <div class="ring-palette_tone-4-0">Brightness 0</div>
+      <div class="ring-palette_tone-4-1">Brightness 1</div>
+      <div class="ring-palette_tone-4-2">Brightness 2</div>
+      <div class="ring-palette_tone-4-3">Brightness 3</div>
+      <div class="ring-palette_tone-4-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 5:</b></div>
+      <div class="ring-palette_tone-5-0">Brightness 0</div>
+      <div class="ring-palette_tone-5-1">Brightness 1</div>
+      <div class="ring-palette_tone-5-2">Brightness 2</div>
+      <div class="ring-palette_tone-5-3">Brightness 3</div>
+      <div class="ring-palette_tone-5-4">Brightness 4</div>
+    </div>
+    <div class="example">
+      <div><b>Tone 6:</b></div>
+      <div class="ring-palette_tone-6-0">Brightness 0</div>
+      <div class="ring-palette_tone-6-1">Brightness 1</div>
+      <div class="ring-palette_tone-6-2">Brightness 2</div>
+      <div class="ring-palette_tone-6-3">Brightness 3</div>
+      <div class="ring-palette_tone-6-4">Brightness 4</div>
+    </div>
+   </file>
+   <file name="index.scss" webpack="true">
+      .example {
+        margin: 4px;
+        div {
+          width: 90px;
+          display: inline-block;
+        }
+      }
+   </file>
+   <file name="index.js" webpack="true">
+      require('palette/palette.scss');
+      require('./index.scss');
+   </file>
+ </example>
+ *
+ */
