f73c3f92f8fbc6bb0837682886d37b60dec3e1d9
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-10-30 17:10:21 +0300

    RG-848 support text message in loader
    Former-commit-id: 924d14805d77b1e8fe0ad96927a3514f63c1f197

--- components/loader-screen/loader-screen.js
+++ components/loader-screen/loader-screen.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Loader from '../loader/loader';
 import RingComponent from '../ring-component/ring-component';
 import './loader-screen.scss';
+import classNames from 'classnames';
 
 /**
  * @name Loader Screen
  * @constructor
  * @extends {ReactComponent}
  * @example
-   <example name="Loader Screen">
+    <example name="Loader Screen">
      <file name="index.html">
        <div id="loader-screen"></div>
      </file>
@@ -23,12 +24,34 @@ import './loader-screen.scss';
        );
      </file>
    </example>
+   <example name="Loader Screen With Message">
+     <file name="index.html">
+       <div id="loader-screen"></div>
+     </file>
+
+     <file name="index.js" webpack="true">
+       var render = require('react-dom').render;
+       var LoaderScreen = require('ring-ui/components/loader-screen/loader-screen');
+
+       render(
+         LoaderScreen.factory({message: 'Loading...'}),
+         document.getElementById('loader-screen')
+       );
+     </file>
+   </example>
  */
 export default class LoaderScreen extends RingComponent {
   render() {
+    let classes = classNames(
+      'ring-loader-screen__loader',
+      {
+        'ring-loader-screen__loader_without-spacings': !this.props.message
+      }
+    );
+
     return (
       <div className="ring-loader-screen">
-        <Loader className="ring-loader-screen__loader"/>
+        <Loader {...this.props} className={classes}/>
       </div>
     );
   }
