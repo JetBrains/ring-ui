61e08343af83119ea85724a190dc4f54ad3db4a6
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2015-11-26 17:14:25 +0300

    rgSaveField fixes
    Former-commit-id: f748e78f2c56043f9f92b30bcb4e07bea3bae4c2

--- components/save-field-ng/save-field-ng.js
+++ components/save-field-ng/save-field-ng.js
@@ -8,6 +8,8 @@ import '../loader-inline/loader-inline';
 import MessageBundle from '../message-bundle-ng/message-bundle-ng';
 import Form from '../form-ng/form-ng';
 
+import 'dom4';
+
 const module = angular.module('Ring.save-field', [
   MessageBundle,
 
@@ -146,224 +148,239 @@ const module = angular.module('Ring.save-field', [
  </example>
  */
 
-module.directive('rgSaveField', [
-  'RingMessageBundle',
-  '$timeout',
-  '$q',
-  function (RingMessageBundle, $timeout, $q) {
-    const MULTI_LINE_SPLIT_PATTERN = /(\r\n|\n|\r)/gm;
-    const MULTI_LINE_LIST_MODE = 'list';
-    const CUSTOM_ERROR_ID = 'customError';
-    const ERROR_DESCRIPTION = 'error_description';
-    const ERROR_DEVELOPER_MSG = 'error_developer_message';
+module.directive('rgSaveField', function (RingMessageBundle, $timeout, $q) {
+  const MULTI_LINE_SPLIT_PATTERN = /(\r\n|\n|\r)/gm;
+  const MULTI_LINE_LIST_MODE = 'list';
+  const CUSTOM_ERROR_ID = 'customError';
+  const ERROR_DESCRIPTION = 'error_description';
+  const ERROR_DEVELOPER_MSG = 'error_developer_message';
 
-    return {
-      transclude: true,
-      template: require('./save-field-ng.html'),
-      scope: {
-        value: '=',
-        onSave: '&',
-        afterSave: '&?',
-        validate: '&?',
-        parseElement: '&?',
-        formatElement: '&?',
-        multiline: '@'
-      },
-      link: function (scope, iElem, iAttrs) {
-        const customError = {
-          message: ''
-        };
+  return {
+    require: 'rgSaveField',
+    transclude: true,
+    template: require('./save-field-ng.html'),
+    scope: {
+      value: '=',
+      onSave: '&',
+      afterSave: '&?',
+      validate: '&?',
+      parseElement: '&?',
+      formatElement: '&?',
+      multiline: '@'
+    },
+    link: function (scope, iElem, iAttrs, ctrl) {
+      const customError = {
+        message: ''
+      };
 
-        let blurTimeout = null;
-        let isTextarea = false;
+      let blurTimeout = null;
+      let isTextarea = false;
 
-        function submitChanges() {
-          if (scope.loading || angular.equals(scope.initial, scope.value)) {
-            return;
-          }
-
-          function success() {
-            scope.initial = angular.copy(scope.value);
-            scope.saveFieldForm.$setPristine();
-
-            scope.done = true;
-
-            $timeout(function () {
-              scope.done = false;
-            }, 1000);
-
-            if (scope.afterSave) {
-              return $q.when(scope.afterSave({
-                value: scope.value
-              }));
-            }
-          }
-
-          function error(err) {
-            let message;
-            if (typeof err === 'string') {
-              message = err;
-            } else if (typeof err === 'object') {
-              message = err[ERROR_DESCRIPTION] || err[ERROR_DEVELOPER_MSG];
-            }
-
-            customError.message = message;
-            scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, false, customError);
-          }
-
-          scope.cancelBlur();
-
-          scope.loading = true;
-          $q.when(scope.onSave({
-            value: scope.value
-          }))
-            .then(success, error)
-            .then(() => {
-              scope.loading = false;
-            });
+      function submitChanges() {
+        if (!scope.saveFieldForm.$valid || scope.loading || angular.equals(scope.initial, scope.value)) {
+          return;
         }
 
-        function resetValue() {
-          if (scope.loading) {
-            return;
-          }
+        function success() {
+          scope.initial = angular.copy(scope.value);
+          scope.saveFieldForm.$setPristine();
 
-          scope.$evalAsync(function () {
-            scope.value = scope.initial ? scope.initial : '';
-            scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, true, customError);
-            scope.saveFieldForm.$setPristine();
-          });
-        }
+          scope.done = true;
 
-        function addMultilineProcessig(controlName) {
-          const stopWatch = scope.$watch('saveFieldForm.' + controlName, function (control) {
-            if (!control || !control.$formatters || !control.$parsers) {
-              return;
-            }
-
-            control.$formatters.push(function (value) {
-              if (!value) {
-                return value;
-              }
-
-              if (iAttrs.formatElement) {
-                value = value.map(function (element) {
-                  return scope.formatElement({element: element});
-                });
-              }
-              return value.join('\n');
-            });
-
-            control.$parsers.push(function (value) {
-              let array = value && value.split(MULTI_LINE_SPLIT_PATTERN) || [];
-
-              function notEmpty(val) {
-                return val && val.trim() && val !== '\n';
-              }
-
-              array = array.filter(notEmpty);
-
-              if (iAttrs.parseElement) {
-                array = array.map(function (element) {
-                  return scope.parseElement({element: element.trim()});
-                });
-              }
-
-              return array;
-            });
-
-            stopWatch();
-          });
-        }
-
-        scope.cancelBlur = function () {
           $timeout(function () {
-            if (blurTimeout) {
-              $timeout.cancel(blurTimeout);
-              blurTimeout = null;
-            }
-          }, 10);
-        };
+            scope.done = false;
+          }, 1000);
 
-        scope.$watch('value', function (value) {
-          let promise = null;
-          if (scope.saveFieldForm.$pristine) {
-            scope.initial = value;
-          } else if (scope.initial && angular.equals(scope.initial, value)) {
-            resetValue();
-          } else if (scope.validate) {
-            promise = scope.validate({
+          if (scope.afterSave) {
+            return $q.when(scope.afterSave({
               value: scope.value
-            });
-          }
-
-          $q.when(promise)
-            .then(error => {
-              if (error) {
-                return $q.reject(error);
-              } else {
-                customError.message = '';
-                scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, true, customError);
-              }
-            }).catch(error => {
-              customError.message = error;
-              scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, false, customError);
-            });
-        }, true);
-
-        let inputNode = iElem[0].querySelector('input, .ring-save-field__input');
-
-        if (!inputNode) {
-          inputNode = iElem[0].querySelector('textarea');
-          isTextarea = !!inputNode;
-        }
-
-        if (inputNode) {
-          inputNode.addEventListener('focus', () => {
-            scope.$evalAsync(() => {
-              scope.focus = true;
-            });
-          });
-
-          inputNode.addEventListener('blur', () => {
-            scope.$evalAsync(() => {
-              scope.focus = false;
-            });
-          });
-
-          if (isTextarea && scope.multiline === MULTI_LINE_LIST_MODE) {
-            addMultilineProcessig(inputNode.name);
+            }));
           }
         }
 
-        scope.wording = {
-          save: RingMessageBundle.form_save(),
-          saved: RingMessageBundle.form_saved()
-        };
+        function error(err) {
+          let message;
+          if (typeof err === 'string') {
+            message = err;
+          } else if (typeof err === 'object') {
+            message = err[ERROR_DESCRIPTION] || err[ERROR_DEVELOPER_MSG];
+          }
 
-        scope.keyMap = {
-          comboSubmit: e => {
-            if (isTextarea) {
-              e.preventDefault();
-              submitChanges();
-            }
-          },
-          submit: e => {
-            if (!isTextarea) {
-              e.preventDefault();
-              submitChanges();
-            }
-          },
-          cancel: resetValue,
-          noop: angular.noop
-        };
+          customError.message = message;
+          scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, false, customError);
+        }
 
-        scope.submitChanges = submitChanges;
+        scope.cancelBlur();
 
-        scope.focus = false;
+        scope.loading = true;
+
+        let onsave = ctrl.getSave();
+        if (onsave) {
+          onsave = $q.when(onsave(scope.value));
+        } else {
+          onsave = $q.when(scope.onSave({
+            value: scope.value
+          }));
+        }
+
+        onsave
+          .then(success, error)
+          .then(() => {
+            scope.loading = false;
+          });
       }
-    };
-  }
-]);
+
+      function resetValue() {
+        if (scope.loading) {
+          return;
+        }
+
+        scope.$evalAsync(function () {
+          scope.value = scope.initial ? scope.initial : '';
+          scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, true, customError);
+          scope.saveFieldForm.$setPristine();
+        });
+      }
+
+      function addMultilineProcessig(controlName) {
+        const stopWatch = scope.$watch('saveFieldForm.' + controlName, function (control) {
+          if (!control || !control.$formatters || !control.$parsers) {
+            return;
+          }
+
+          control.$formatters.push(function (value) {
+            if (!value) {
+              return value;
+            }
+
+            if (iAttrs.formatElement) {
+              value = value.map(function (element) {
+                return scope.formatElement({element: element});
+              });
+            }
+            return value.join('\n');
+          });
+
+          control.$parsers.push(function (value) {
+            let array = value && value.split(MULTI_LINE_SPLIT_PATTERN) || [];
+
+            function notEmpty(val) {
+              return val && val.trim() && val !== '\n';
+            }
+
+            array = array.filter(notEmpty);
+
+            if (iAttrs.parseElement) {
+              array = array.map(function (element) {
+                return scope.parseElement({element: element.trim()});
+              });
+            }
+
+            return array;
+          });
+
+          stopWatch();
+        });
+      }
+
+      scope.cancelBlur = function () {
+        $timeout(function () {
+          if (blurTimeout) {
+            $timeout.cancel(blurTimeout);
+            blurTimeout = null;
+          }
+        }, 10);
+      };
+
+      scope.$watch('value', function (value) {
+        let promise = null;
+        if (scope.saveFieldForm.$pristine) {
+          scope.initial = value;
+        } else if (scope.initial && angular.equals(scope.initial, value)) {
+          resetValue();
+        } else if (scope.validate) {
+          promise = scope.validate({
+            value: scope.value
+          });
+        }
+
+        $q.when(promise)
+          .then(error => {
+            if (error) {
+              return $q.reject(error);
+            } else {
+              customError.message = '';
+              scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, true, customError);
+            }
+          }).catch(error => {
+            customError.message = error;
+            scope.saveFieldForm.$setValidity(CUSTOM_ERROR_ID, false, customError);
+          });
+      }, true);
+
+      let inputNode = iElem[0].querySelector('input, .ring-save-field__input');
+
+      if (!inputNode) {
+        inputNode = iElem[0].querySelector('textarea');
+        isTextarea = !!inputNode;
+      }
+
+      if (inputNode) {
+        inputNode.classList.add('ring-js-shortcuts');
+
+        inputNode.addEventListener('focus', () => {
+          scope.$evalAsync(() => {
+            scope.focus = true;
+          });
+        });
+
+        inputNode.addEventListener('blur', () => {
+          scope.$evalAsync(() => {
+            scope.focus = false;
+          });
+        });
+
+        if (isTextarea && scope.multiline === MULTI_LINE_LIST_MODE) {
+          addMultilineProcessig(inputNode.name);
+        }
+      }
+
+      scope.wording = {
+        save: RingMessageBundle.form_save(),
+        saved: RingMessageBundle.form_saved()
+      };
+
+      scope.keyMap = {
+        comboSubmit: e => {
+          if (isTextarea) {
+            e.preventDefault();
+            submitChanges();
+          }
+        },
+        submit: e => {
+          if (!isTextarea) {
+            e.preventDefault();
+            submitChanges();
+          }
+        },
+        cancel: resetValue,
+        noop: angular.noop
+      };
+
+      scope.submitChanges = submitChanges;
+
+      scope.focus = false;
+    },
+    controller: function () {
+      let onSave = null;
+
+      this.setSave = cb => {
+        onSave = cb;
+      };
+
+      this.getSave = () => onSave;
+    }
+  };
+});
 
 export default module.name;
