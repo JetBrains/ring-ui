59d881f4e8a1af54b599499ff53862707e2aef1d
Max Maximov <max.maximov@gmail.com>
2015-12-10 14:27:32 +0300

    fixup! IE9/IE10 support
    Former-commit-id: 53b281a273ef0d78488a899fc5c47773dd405abe

--- test-helpers/simulate-keypress.js
+++ test-helpers/simulate-keypress.js
@@ -4,6 +4,7 @@
 var keyEvent = require('imports?window=>{}!exports?window.KeyEvent!mousetrap/tests/libs/key-event');
 
 // Dirty hack for mousetrap/tests/libs/key-event.js and IE
+// See https://github.com/ccampbell/mousetrap/blob/1.4.6/tests/libs/key-event.js#L21
 document.createEventObject = null;
 
 module.exports = function (char, keyCode) {
