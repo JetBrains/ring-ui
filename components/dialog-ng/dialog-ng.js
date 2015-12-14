c4148ec297a803aa5465fa23304873936ebc4917
Maksim Ryzhikov <rv.maksim@gmail.com>
2015-12-14 20:34:46 +0300

    fix(components:dialog-ng): Dialog incorrect position in IE11 #RG-873 fix
    Former-commit-id: ef5152f2ab90d26ceb0dd275694959bbca2b0b78

--- components/dialog-ng/dialog-ng.js
+++ components/dialog-ng/dialog-ng.js
@@ -5,6 +5,136 @@ import shortcuts from '../shortcuts/shortcuts';
 
 import '../dialog/dialog.scss';
 
+/**
+ * @name Dialog
+ * @example
+ <example name="Dialog">
+   <file name="index.html">
+     <div ng-app="Example.dialog">
+
+     <script type="template" id="dialog-template">
+       <div ng-controller="DialogExampleCtrl as dialogExampleCtrl">
+         <div class="ring-form__group">
+           <label class="ring-form__label" for="dialog__key">Key</label>
+           <div class="ring-form__control">
+             <input id="dialog__key" class="ring-input" type="text">
+           </div>
+         </div>
+         <div class="ring-form__group">
+           <label class="ring-form__label" for="dialog__name">Name</label>
+           <div class="ring-form__control">
+             <input id="dialog__name" class="ring-input" type="text">
+             <div class="ring-input__error-bubble active">Wrong value</div>
+             <div class="ring-form__control__description">Description</div>
+           </div>
+         </div>
+         <div class="ring-form__group">
+           <label for="rg-select-demo" class="ring-form__label">Rg Select in Form</label>
+           <div class="ring-form__control">
+             <rg-select options="item.name for item in dialogExampleCtrl.arr track by item.name" id="rg-select-demo"></rg-select>
+           </div>
+         </div>
+         <div class="ring-form__group">
+           <label for="textarea-demo" class="ring-form__label">Textarea in Form</label>
+           <div class="ring-form__control">
+             <textarea id="textarea-demo" class="ring-textarea"></textarea>
+           </div>
+         </div>
+       </div>
+     </script>
+
+     <div rg-dialog=""></div>
+     <div  ng-controller="ExampleCtrl"></div>
+   </file>
+   <file name="index.js" webpack="true">
+     require('ring-ui/components/dialog/dialog.scss');
+     require('ring-ui/components/panel/panel.scss');
+     require('ring-ui/components/textarea/textarea.scss');
+
+     require('angular/angular');
+     require('ring-ui/components/select-ng/select-ng');
+
+     angular.module('Example.dialog', [
+      require('ring-ui/components/dialog-ng/dialog-ng'),
+     'Ring.select'
+     ]).controller('ExampleCtrl', function(dialog) {
+        dialog.show({
+          title: 'Test',
+          template: document.getElementById('dialog-template').innerHTML,
+          buttons: [
+            {
+              label: 'Save',
+              default: true,
+              action: angular.noop
+            },
+            {
+              label: 'Cancel',
+              close: true
+            }
+          ]
+        });
+     }).controller('DialogExampleCtrl', function(dialog) {
+        var dialogExampleCtrl = this;
+        dialogExampleCtrl.arr = [{name: 'Ada'}, {name: 'Nik'}];
+      });
+   </file>
+ </example>
+
+ <example name="Dialog with control_small">
+   <file name="index.html">
+     <div class="ring-dialog__container">
+       <div class="ring-dialog__header">
+         <span class="ring-dialog__header__title">Dialog with control_small</span>
+       </div>
+       <form class="ring-dialog__content ring-form">
+         <div class="ring-form__group">
+           <label class="ring-form__label" for="dialog__key">Small</label>
+           <div class="ring-form__control ring-form__control_small">
+             <input id="dialog__key" class="ring-input" type="text">
+             <div class="ring-input__error-bubble">I am tip</div>
+             <div class="ring-form__control__description">
+                There is a small margin before control
+             </div>
+           </div>
+          </div>
+       </form>
+       <div class="ring-dialog__footer">
+         <div class="ring-dialog__footer__spacer"></div>
+         <button class="ring-dialog__footer__item_first ring-dialog__footer__item ring-btn ring-btn_blue">Save</button>
+         <button class="ring-dialog__footer__item ring-btn">Cancel</button>
+       </div>
+     </div>
+   </file>
+   <file name="index.js" webpack="true">
+     require('ring-ui/components/dialog/dialog.scss');
+     require('ring-ui/components/input/input.scss');
+     require('ring-ui/components/form/form.scss');
+     require('ring-ui/components/panel/panel.scss');
+     require('ring-ui/components/button/button.scss');
+   </file>
+ </example>
+
+ <example name="The smalest dialog">
+   <file name="index.html">
+     <div class="ring-dialog__container">
+       <div class="ring-dialog__header">
+         <span class="ring-dialog__header__title">Small</span>
+       </div>
+       <div class="ring-dialog__content">
+          <div style="text-align: center"><b>Hello!</b></div>
+          <div style="text-align: center">I am message</div>
+       </div>
+     </div>
+   </file>
+   <file name="index.js" webpack="true">
+     require('ring-ui/components/dialog/dialog.scss');
+     require('ring-ui/components/input/input.scss');
+     require('ring-ui/components/form/form.scss');
+     require('ring-ui/components/panel/panel.scss');
+     require('ring-ui/components/button/button.scss');
+   </file>
+ </example>
+ */
 const module = angular.module('Ring.dialog', []);
 
 class DialogController {
