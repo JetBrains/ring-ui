2a2115eeaecc15120574156c101aaef1a79655a2
Max Maximov <max.maximov@gmail.com>
2015-12-16 21:29:39 +0300

    Update webpack, sass and loaders
    Former-commit-id: be821e029aebed8cb22c81c0d9154c2423c9c2f2

--- components/old-browsers-message/old-browsers-message.js
+++ components/old-browsers-message/old-browsers-message.js
@@ -1,10 +1,40 @@
+/* eslint-disable no-var */
+
 /**
  * Note: this script is a self running script. You should include it in your browser directly
  * It starts detecting unhandled errors when loaded.
  * When your app is loaded and you want to stop detecting,
  * call require('ring-ui/components/old-browsers-message/old-browsers-message__stop')();
  */
-/* eslint-disable no-var */
+
+/**
+ * @name Old Browsers Message
+ * @example
+  <example name="Old Browsers Message">
+    <file name="index.html">
+
+      <div id="ring-old-browsers-message" class="ring-old-browsers-message ring-old-browsers-message_hidden">
+        <span id="ring-old-browsers-message__smile" class="ring-old-browsers-message__smile">{{ (>_<) }}</span>
+        <br/><br/>
+        This version of your browser is not <a href="https://documentation.link">supported</a>.<br/>
+        Try upgrading to the latest stable version.
+        <br/><br/>
+        <!--[if IE 9]>
+          <span>If you use IE9.0 or higher, make sure that compatibility mode is disabled.</span>
+        <![endif]-->
+      </div>
+
+     </file>
+       <file name="index.js" webpack="true">
+          require('ring-ui/components/old-browsers-message/old-browsers-message.scss');
+          require('ring-ui/components/old-browsers-message/old-browsers-message');
+
+          //Triggering error to imitate unsupported browser
+          Object.futureMethodWhichIsNotExistInThisBrowser();
+       </file>
+   </example>
+ */
+
 (function () {
   var smileChanges = 0;
   var MAX_SMILE_CHANGES = 50;
