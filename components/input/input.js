347bfc9b55c2c49ee984703751375fd2342774f3
Ekaterina.Zaikina <katriyna@gmail.com>
2015-12-16 17:39:03 +0300

    RG-875 in progress
    Former-commit-id: 198297f82bc34db372dd5d1c38344f51228226d7

--- components/input/input.js
+++ components/input/input.js
@@ -31,6 +31,10 @@ import './input.scss';
            <div class="ring-input__error-bubble active">No modifiers (full-width)</div>
          </div>
          <div class="input-control-wrapper">
+           <input type="number" class="ring-input ring-input_short">
+           <div class="ring-input__error-bubble active">Short</div>
+         </div>
+         <div class="input-control-wrapper">
            <input type="number" class="ring-input ring-input_medium">
            <div class="ring-input__error-bubble active">Medium</div>
          </div>
@@ -45,6 +49,39 @@ import './input.scss';
           position: relative;
           margin: 16px;
         }
+     </file>
+     <file name="index.js" webpack="true">
+       require('ring-ui/components/input/input.scss');
+       require('./style.scss');
+     </file>
+   </example>
+
+   <example name="Textarea sizes">
+     <file name="index.html">
+       <div style="width: 600px">
+         <div class="input-control-wrapper">
+           <textarea class="ring-input"></textarea>
+           <div class="ring-input__error-bubble active">No modifiers (full-width)</div>
+         </div>
+         <div class="input-control-wrapper">
+           <textarea class="ring-input ring-input_short"></textarea>
+           <div class="ring-input__error-bubble active">Short</div>
+         </div>
+         <div class="input-control-wrapper">
+           <textarea class="ring-input ring-input_medium"></textarea>
+           <div class="ring-input__error-bubble active">Medium</div>
+         </div>
+         <div class="input-control-wrapper">
+           <textarea class="ring-input ring-input_long"></textarea>
+           <div class="ring-input__error-bubble active">Long</div>
+         </div>
+       </div>
+     </file>
+     <file name="style.scss">
+       .input-control-wrapper {
+         position: relative;
+         margin: 16px;
+       }
      </file>
      <file name="index.js" webpack="true">
        require('ring-ui/components/input/input.scss');
--- components/textarea/textarea.js
+++ components/textarea/textarea.js
@@ -8,53 +8,7 @@ import './textarea.scss';
  * @constructor
  * @extends {ReactComponent}
  * @example
-   <example name="React textarea">
-     <file name="index.html">
-       <div id="textarea"></div>
-     </file>
 
-     <file name="index.js" webpack="true">
-       var render = require('react-dom').render;
-       var Textarea = require('ring-ui/components/textarea/textarea');
-
-       render(
-         Textarea.factory(),
-         document.getElementById('textarea')
-       );
-     </file>
-   </example>
-
-   <example name="Textarea sizes">
-     <file name="index.html">
-       <div style="width: 600px">
-         <div class="input-control-wrapper">
-           <textarea class="ring-textarea"></textarea>
-           <div class="ring-input__error-bubble active">No modifiers (full-width)</div>
-         </div>
-         <div class="input-control-wrapper">
-           <textarea class="ring-textarea ring-textarea_medium"></textarea>
-           <div class="ring-input__error-bubble active">Medium</div>
-         </div>
-         <div class="input-control-wrapper">
-           <textarea class="ring-textarea ring-textarea_long"></textarea>
-           <div class="ring-input__error-bubble active">Long</div>
-         </div>
-       </div>
-     </file>
-     <file name="style.scss">
-       .input-control-wrapper {
-         position: relative;
-         margin: 16px;
-       }
-     </file>
-     <file name="index.js" webpack="true">
-       require('ring-ui/components/textarea/textarea.scss');
-       // For error-bubble
-       require('ring-ui/components/input/input.scss');
-
-       require('./style.scss');
-     </file>
-   </example>
  */
 export default class Textarea extends RingComponent {
   render() {
