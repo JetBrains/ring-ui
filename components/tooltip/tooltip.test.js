7be02515824126672a3fb17270391778bb194d35
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2015-12-21 19:28:00 +0300

    React Tooltip: tests
    Former-commit-id: c5367ffb39bbe4b58ed67a1d2c9ea669212d3f30

--- components/tooltip/tooltip.test.js
+++ components/tooltip/tooltip.test.js
@@ -1,13 +1,114 @@
+import React from 'react';
+import 'dom4';
+
 import {isCompositeComponentWithType, renderIntoDocument} from 'react-addons-test-utils';
 
 import Tooltip from './tooltip';
+import Popup from '../popup/popup';
 
 describe('Tooltip', function () {
   beforeEach(function () {
-    this.tooltip = renderIntoDocument(Tooltip.factory({title: 'test tooltip', children: 'test elem'}));
+    this.tooltip = renderIntoDocument(Tooltip.factory({
+      title: 'test tooltip',
+      className: 'test-class',
+      children: 'test elem'
+    }));
   });
 
   it('should create component', function () {
     isCompositeComponentWithType(this.tooltip, Tooltip).should.be.true;
+  });
+
+  describe('Children', function () {
+    it('should wrap text children', function () {
+      this.tooltip.node.should.have.text('test elem');
+      this.tooltip.node.should.match('span');
+    });
+
+    it('should pass children as is', function () {
+      const tooltip = renderIntoDocument(Tooltip.factory({
+        title: 'test tooltip',
+        children: <div>{'test div'}</div>
+      }));
+
+      tooltip.node.should.have.text('test div');
+      tooltip.node.should.match('div');
+    });
+
+    it('should pass props to children', function () {
+      this.tooltip.node.should.have.class('test-class');
+    });
+
+    it('should not pass title to children', function () {
+      this.tooltip.node.should.not.have.attribute('title');
+    });
+  });
+
+  describe('Popup', function () {
+    it('should unbind listeners when empty title is provided', function () {
+      const bindEvents = this.sinon.spy(this.tooltip.node, 'removeEventListener');
+
+      this.tooltip.rerender({
+        title: false
+      });
+
+      bindEvents.should.have.been.called.twice;
+    });
+
+    it('should render popup', function () {
+      this.tooltip.showPopup();
+
+      isCompositeComponentWithType(this.tooltip.popup, Popup).should.be.true;
+    });
+
+    it('should not render popup when empty title is provided', function () {
+      this.tooltip.rerender({
+        title: false
+      });
+
+      this.tooltip.showPopup();
+      should.not.exist(this.tooltip.popup);
+    });
+
+    it('should render with delay when provided', function () {
+      const clock = this.sinon.useFakeTimers();
+      this.tooltip.rerender({
+        delay: '100'
+      });
+
+      this.tooltip.showPopup();
+
+      should.not.exist(this.tooltip.popup);
+
+      clock.tick(200);
+      this.tooltip.popup.node.should.exist;
+
+      clock.restore();
+    });
+
+    it('should pass custom props to popup', function () {
+      this.tooltip.rerender({
+        popupProps: {
+          className: 'tooltip-test-popup'
+        }
+      });
+
+      this.tooltip.showPopup();
+      this.tooltip.popup.node.should.have.class('tooltip-test-popup');
+    });
+
+    it('should close popup on unmount', function () {
+      this.tooltip.showPopup();
+      React.unmountComponentAtNode(this.tooltip.node.parentNode);
+
+      should.not.exist(this.tooltip.popup);
+    });
+
+    it('should not close popup on click on tooltip', function () {
+      this.tooltip.showPopup();
+      this.tooltip.popup.close({target: this.tooltip.node});
+
+      this.tooltip.popup.node.should.exist;
+    });
   });
 });
