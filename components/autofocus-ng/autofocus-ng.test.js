69b44e594f15b2352ca36554592a3dceed28a268
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2015-12-29 18:12:14 +0300

    RG-883 extract Autofocus-ng to ring
    Former-commit-id: e0b6bbf7ee22f9deea2b55a0376b59ee630b4d8f

---
+++ components/autofocus-ng/autofocus-ng.test.js
@@ -0,0 +1,45 @@
+/* global inject */
+import AutoFocus from './autofocus-ng';
+import RingSelect from '../select-ng/select-ng';
+import 'angular-mocks';
+
+describe('Autofocus', function () {
+  let element;
+  let $compile;
+  let $scope;
+
+  beforeEach(window.module(AutoFocus));
+  beforeEach(window.module(RingSelect));
+
+  beforeEach(inject((_$compile_, $rootScope) => {
+    $compile = _$compile_;
+    $scope = $rootScope.$new();
+    $scope.vm = {
+      focus: false
+    };
+    element = $compile('<input rg-autofocus="vm.focus"/>')($scope);
+
+  }));
+
+  it('Should focus when flag changed', function () {
+
+    this.sinon.spy(element[0], 'focus');
+
+    $scope.vm.focus = true;
+    $scope.$digest();
+
+    element[0].focus.should.have.been.called;
+  });
+
+  it('Should focus on select button', function () {
+    element = $compile('<rg-select rg-autofocus="vm.focus" options="item in []"/>')($scope);
+    const selectButton = element[0].querySelector('.ring-select');
+
+    this.sinon.spy(selectButton, 'focus');
+
+    $scope.vm.focus = true;
+    $scope.$digest();
+
+    selectButton.focus.should.have.been.called;
+  });
+});
