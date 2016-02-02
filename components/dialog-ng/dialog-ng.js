66b856b87ff80f8f57e0ad491506106bd80d9d59
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2016-02-02 12:36:36 +0300

    RG-891 revert due to not related to the real problem
    Former-commit-id: be5448b68072679355e8cebd2ce3a398f574984b

--- components/dialog-ng/dialog-ng.js
+++ components/dialog-ng/dialog-ng.js
@@ -187,8 +187,7 @@ class DialogController {
 
     Reflect.deleteProperty(this, 'DIALOG_NAMESPACE');
 
-    //There should be a QueryAssist in a dialog that is focused but no suggest is shown
-    if (shortcuts.getScope().indexOf(this.dialogService.DIALOG_NAMESPACE) > -1) {
+    if (shortcuts.getScope().pop() === this.dialogService.DIALOG_NAMESPACE) {
       shortcuts.setScope(this.currentShortcutsScope);
     }
   }
