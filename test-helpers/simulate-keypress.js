f7590051d05f42fb134630b3dbd2c983ae0c3cd7
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-02-02 17:30:25 +0300

    RG-899 Move to Combokeys instead of Mousetrap
    Former-commit-id: 6127de421f41de1fa61e238a14e46c5334fe9460

--- test-helpers/simulate-keypress.js
+++ test-helpers/simulate-keypress.js
@@ -1,11 +1,7 @@
 /* eslint-disable no-var */
 /* eslint-disable modules/no-cjs */
 
-var keyEvent = require('imports?window=>{}!exports?window.KeyEvent!mousetrap/tests/libs/key-event');
-
-// Dirty hack for mousetrap/tests/libs/key-event.js and IE
-// See https://github.com/ccampbell/mousetrap/blob/1.4.6/tests/libs/key-event.js#L21
-document.createEventObject = null;
+var keyEvent = require('combokeys/test/lib/key-event');
 
 module.exports = function (char, keyCode) {
   var charCode = char && char.charCodeAt(0) || 0;
