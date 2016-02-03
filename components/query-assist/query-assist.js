8b93f6aea391c2b07b43102b98d1d3732c8bae3c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-02-03 18:51:15 +0300

    Open QueryAssist only to bottom
    Former-commit-id: f8369bf8dc50fef68c655cec7444fb657055fbf4

--- components/query-assist/query-assist.js
+++ components/query-assist/query-assist.js
@@ -619,7 +619,7 @@ export default class QueryAssist extends RingComponentWithShortcuts {
           anchorElement={this.node}
           autoRemove={false} // required to prevent popup unmount on Esc
           className={this.props.popupClassName}
-          directions={[PopupMenu.PopupProps.Directions.BOTTOM_RIGHT, PopupMenu.PopupProps.Directions.TOP_RIGHT]}
+          directions={[PopupMenu.PopupProps.Directions.BOTTOM_RIGHT]}
           data={renderedSuggestions}
           hint={this.props.hint}
           hintOnSelection={this.props.hintOnSelection}
