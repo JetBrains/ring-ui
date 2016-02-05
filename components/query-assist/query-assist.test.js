f1bf043810f155210fb92e6110fe46efca9069a2
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-02-05 13:53:11 +0300

    RG-896 QueryAssist: parameter that open assist suggest immediately when it's shown
    Former-commit-id: bc064a95f2b083cad14632839cfd2a1ccc7a7506

--- components/query-assist/query-assist.test.js
+++ components/query-assist/query-assist.test.js
@@ -245,6 +245,44 @@ describe('QueryAssist', function () {
       letters[3].should.have.class(LETTER_CLASS + '_operator');
     });
 
+    it('should render last text range with field-name style when not applied', function () {
+      this.renderQueryAssist({
+        query: 'a a'
+      });
+
+      this.queryAssist.setState({
+        styleRanges: [
+          {start: 0, length: 1, style: 'text'},
+          {start: 2, length: 1, style: 'text'}
+        ]
+      });
+
+      const letters = this.queryAssist.input.queryAll('.' + LETTER_CLASS);
+
+      letters[0].should.have.class(LETTER_CLASS + '_text');
+      letters[1].should.have.class(LETTER_CLASS + '_field-name');
+      letters[2].should.have.class(LETTER_CLASS + '_text');
+    });
+
+    it('should render last text range with field-name style when applied', function () {
+      this.renderQueryAssist({
+        query: 'a a'
+      });
+
+      this.queryAssist.setState({
+        dirty: true,
+        styleRanges: [
+          {start: 0, length: 1, style: 'text'},
+          {start: 2, length: 1, style: 'text'}
+        ]
+      });
+
+      const letters = this.queryAssist.input.queryAll('.' + LETTER_CLASS);
+
+      letters[0].should.have.class(LETTER_CLASS + '_text');
+      letters[1].should.have.class(LETTER_CLASS + '_field-name');
+      letters[2].should.have.class(LETTER_CLASS + '_field-name');
+    });
 
     it('should disable field when component disabled', function () {
       this.renderQueryAssist({
