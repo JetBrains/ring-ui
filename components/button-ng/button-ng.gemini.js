8646c0ad4a336bce5c60ed81b1fa0730a2513e32
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-02-09 17:37:45 +0300

    RING-UI-CR-971 drop missed console log
    Former-commit-id: 0c1c00edb5bea8e1c95b148086faa9a86f574abe

--- components/button-ng/button-ng.gemini.js
+++ components/button-ng/button-ng.gemini.js
@@ -5,7 +5,6 @@ var gemini = require('gemini');
 var ANIMATION_IGNORE_TOLERANCE = 40;
 
 gemini.suite('ButtonNg', function (suite) {
-  console.log(suite.executeJS)
   suite
     .setTolerance(ANIMATION_IGNORE_TOLERANCE)
     .setUrl('/example-button-ng')
