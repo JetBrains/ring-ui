de9eb065cf230479ea211de1418aeef294a3eac2
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-02-10 14:27:03 +0300

    RG-905 add some prerequirement tests for permisisons-ng
    Former-commit-id: 5f3dd85f1576895e9e67da9fbf7b632e6287a873

---
+++ components/permissions-ng/permissions-ng.test.js
@@ -0,0 +1,58 @@
+import PermissionsNg from './permissions-ng';
+
+describe.only('PermissionsNg', function () {
+
+  let fakeUserPermissions = {
+    check: () => {
+    }
+  };
+
+  beforeEach(window.module(PermissionsNg, function ($provide) {
+    $provide.value('userPermissions', fakeUserPermissions);
+  }));
+
+  describe('rg-permission', function () {
+    beforeEach(inject(function ($q, $rootScope, _$compile_) {
+      this.$q = $q;
+      this.scope = $rootScope.$new();
+      this.$compile = _$compile_;
+      this.sinon.stub(fakeUserPermissions, 'check').returns($q.resolve(true));
+
+      this.renderDirective = (permissionsString = 'hub-test-perm', inProject) => {
+        this.element = this.$compile(`<div rg-permission="${permissionsString}" in-project="${inProject}"></div>`)(this.scope);
+        this.ctrl = this.element.controller('rgPermission');
+        this.scope.$digest();
+      };
+
+      this.renderDirective();
+    }));
+
+    it('should init', function () {
+      this.ctrl.should.be.defined;
+    });
+
+    it('Should not hide element if permission is granted', function () {
+      this.element[0].should.not.has.class('ring-permission-hide');
+    });
+
+    it('Should hide element if has no permission', function () {
+      fakeUserPermissions.check.returns(this.$q.resolve(false));
+      this.renderDirective();
+
+      this.element[0].should.has.class('ring-permission-hide');
+    });
+
+    it('Should pass permission as string to userPermission', function () {
+      this.renderDirective('some-permission');
+
+      fakeUserPermissions.check.should.have.been.calledWith('some-permission');
+    });
+
+    it('Should pass project id as string to userPermission', function () {
+      this.scope.projectId = 'some-project';
+      this.renderDirective('some-permission', 'projectId');
+
+      fakeUserPermissions.check.should.have.been.calledWith('some-permission', 'some-project');
+    });
+  });
+});
