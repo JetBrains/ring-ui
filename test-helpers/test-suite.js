6eb5a0c8dd2688819826d992377f8edc7dd7afa9
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-03-14 13:07:26 +0300

    Avoid webpack-test configs monkey patching
    Former-commit-id: e8c4ad3b6da94e60f05674dcb5f08fd6877c4229

--- test-helpers/test-suite.js
+++ test-helpers/test-suite.js
@@ -3,7 +3,4 @@
 
 var testsContext = require.context('../components', true, /\.test\.js$/);
 
-// Always use polyfill in tests to go through Sinon's fake XHR
-window.fetch = require('!exports?self.fetch!imports?self=>{},Promise=core-js/es6/promise!whatwg-fetch');
-
 testsContext.keys().forEach(testsContext);
