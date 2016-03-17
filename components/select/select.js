ef8e572e6643b69b3ba0131af814e48114ed20a8
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-03-17 19:25:40 +0300

    Smaller example containers to fit them into browser window to make Gemini work properly with carets: revert accidental change
    Former-commit-id: db7c9551704d9b752774bb613bf231051ed58753

--- components/select/select.js
+++ components/select/select.js
@@ -507,8 +507,7 @@ export default class Select extends RingComponentWithShortcuts {
           onSelect={::this._listSelectHandler}
           onFilter={::this._filterChangeHandler}
           onLoadMore={::this.props.onLoadMore}
-        />
-      );
+        />, anchorElement);
     }
   }
 
