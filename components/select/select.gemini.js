a8406c1cddd432cb43008af623f1826ac258b24b
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-03-17 14:05:16 +0300

    Improve gemini reliability.
    Former-commit-id: e8ec466b2f509b23b81c8215c3a775a9ed7b8ff8

--- components/select/select.gemini.js
+++ components/select/select.gemini.js
@@ -6,9 +6,9 @@ var gemini = require('gemini');
 gemini.suite('Select', function () {
   gemini.suite('Input based select', function (child) {
     child
-      .skip('firefox')
       .setUrl('/example-simple-input-based-select')
       .setCaptureElements('.ring-select', '.ring-popup')
+      .ignoreElements('.ring-input')
       .capture('selectPopup', function (actions, find) {
         actions.click(find('.ring-input'));
       });
@@ -16,7 +16,6 @@ gemini.suite('Select', function () {
 
   gemini.suite('Select button', function (child) {
     child
-      .skip('firefox')
       .setUrl('/example-select-with-customized-filter-and-an-add-item-button')
       .setCaptureElements('.ring-select')
       .capture('button');
@@ -24,9 +23,9 @@ gemini.suite('Select', function () {
 
   gemini.suite('Select with filter', function (child) {
     child
-      .skip('firefox')
       .setUrl('/example-simple-select-with-default-filter-mode')
       .setCaptureElements('.ring-select', '.ring-popup')
+      .ignoreElements('.ring-input')
       .capture('selectPopup', function (actions, find) {
         actions.click(find('.ring-select'));
         actions.click(find('.ring-popup .ring-input'));
