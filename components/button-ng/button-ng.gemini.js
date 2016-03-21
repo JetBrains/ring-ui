686df3fd2597be5840b6b63ffe2abd268fdbe4be
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-03-21 15:07:10 +0300

    Gemini button suite: use ignoreElements instead of tolerance
    Former-commit-id: d603990c165ac5bba7518ecfcb9cee97c47ad5d4

--- components/button-ng/button-ng.gemini.js
+++ components/button-ng/button-ng.gemini.js
@@ -2,12 +2,18 @@
 /* eslint-disable modules/no-cjs */
 
 var gemini = require('gemini');
-var ANIMATION_IGNORE_TOLERANCE = 40;
 
 gemini.suite('ButtonNg', function (suite) {
   suite
-    .setTolerance(ANIMATION_IGNORE_TOLERANCE)
-    .setUrl('/example-button-ng')
+    .setUrl('/example-button-ng/')
     .setCaptureElements('body > div')
+    // Only first element in selector is ignored
+    .ignoreElements(
+      'p:nth-child(1) .ring-button_loader',
+      'p:nth-child(2) .ring-button_loader',
+      'p:nth-child(3) .ring-button_loader',
+      'p:nth-child(5) .ring-button_loader',
+      'p:nth-child(6) .ring-button_loader'
+    )
     .capture('buttons');
 });
