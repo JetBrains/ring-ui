f68b0d02449002c08779b4504cf5da7d017774e7
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-04-22 17:36:49 +0300

    RG-967 RG-977 restore previous body width value if was set
    Former-commit-id: fc0aa31e493b95aaba40f5547489c3f6532536d9

--- components/dialog-ng/dialog-ng.js
+++ components/dialog-ng/dialog-ng.js
@@ -177,13 +177,14 @@ class DialogController {
     const scrollWidth = scrollbarWidth();
     const bodyHasScroll = document.body.scrollHeight > window.innerHeight;
     if (bodyHasScroll && scrollWidth > 0) {
+      this.previousBodyWidh = document.body.style.width;
       document.body.style.width = 'calc(100% - ' + scrollWidth + 'px)';
     }
   }
 
   resetBodyScrollPrevention() {
     document.body.classList.remove(this.constructor.BODY_MODAL_CLASS);
-    document.body.style.width = 'auto';
+    document.body.style.width = this.previousBodyWidh || 'auto';
   }
 
   setTitle(title) {
