93dbedf14646288874d2860051aacf9c7075d6cd
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-05-11 16:42:54 +0300

    RG-985 better way: capitalize any unknown symbol in shortcut
    Former-commit-id: f5a6c54a985b14a7a2a376cceb9f1f4a7b969f33

--- components/shortcuts-hint-ng/shortcuts-hint-ng.test.js
+++ components/shortcuts-hint-ng/shortcuts-hint-ng.test.js
@@ -22,14 +22,21 @@ describe('Shortcuts ng hint', function () {
       this.sinon.stub(Sniffr.prototype, 'sniff', function () {
         this.os = {name: 'macos'};
       });
-      shortcutKeySymbolFilter('somekey1+somekey2').should.be.equal('somekey1 somekey2');
+      shortcutKeySymbolFilter('S1+S2').should.be.equal('S1 S2');
     });
 
     it('Should replace + with spaces on windows', function () {
       this.sinon.stub(Sniffr.prototype, 'sniff', function () {
         this.os = {name: 'windows'};
       });
-      shortcutKeySymbolFilter('somekey1+somekey2').should.be.equal('somekey1 + somekey2');
+      shortcutKeySymbolFilter('S1+S2').should.be.equal('S1 + S2');
+    });
+
+    it('Should capitalize key if is not in key mapping', function () {
+      this.sinon.stub(Sniffr.prototype, 'sniff', function () {
+        this.os = {name: 'macos'};
+      });
+      shortcutKeySymbolFilter('f2+e').should.be.equal('F2 E');
     });
 
     it('Should replace action keys with symbols on mac', function () {
