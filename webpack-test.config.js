4d3dcce78573c5b92ec89c748d9ae6dd1b2b59d9
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-05-19 16:01:08 +0300

    Load sourcemaps in tests and show clickable stacktrace in IDE
    Former-commit-id: 3486a0a2a239c42cb9eed58c7e3132f1480db2a3

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -9,6 +9,12 @@ config.resolve = {
   root: path.join(__dirname, 'test-helpers')
 };
 
+config.output = {
+  devtoolModuleFilenameTemplate: '/[absolute-resource-path]' // For some reason slash in the beginning is required
+};
+
+config.devtool = 'eval';
+
 config.plugins = [
   new webpack.ProvidePlugin({
     fetch: '!exports?self.fetch!imports?self=>{},Promise=core-js/es6/promise!whatwg-fetch'
