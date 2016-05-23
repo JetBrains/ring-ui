4639a2e9268a92cd876090eb581bb99331adac90
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-05-23 15:51:02 +0300

    List improvements for Toolbox
    Former-commit-id: ee54208bf21eed74d98394e15ed9cfe095fc528d

--- components/list/list__item.js
+++ components/list/list__item.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import classnames from 'classnames';
 import Icon from '../icon/icon';
 
@@ -6,16 +6,25 @@ import Icon from '../icon/icon';
  * @constructor
  * @extends {ReactComponent}
  */
+
+const RING_UNIT = 8;
+const DEFAULT_PADDING = 16;
+
 export default function ListItem(props) {
   const classes = classnames({
     'ring-list__item': true,
     'ring-list__item_action': !props.disabled,
     'ring-list__item_active': props.active && !props.disabled
   }, props.className);
+  const detailsClasses = classnames({
+    'ring-list__details': props.details,
+    'ring-list__details_padded': props.icon !== undefined || props.checkbox !== undefined || props.glyph !== undefined
+  });
 
   const style = {
-    paddingLeft: ((+props.level || 0) * 8 + 16) + 'px'
+    paddingLeft: `${(+props.level || 0) * RING_UNIT + DEFAULT_PADDING}px`
   };
+
 
   return (
     <span
@@ -23,28 +32,61 @@ export default function ListItem(props) {
       className={classes}
       style={style}
     >
-        {props.checkbox !== undefined && (
-          <Icon
-            className={classnames({
-              'ring-list__checkbox': true,
-              'ring-list__checkbox_hidden': !props.checkbox
-            })}
-            glyph={require('jetbrains-icons/check.svg')}
-            size={Icon.Size.Size18}
-          />
-        )}
-      {props.icon &&
-      <div
-        className="ring-list__icon"
-        style={{backgroundImage: 'url("' + props.icon + '")'}}
-      />}
-      {props.description &&
-      <div className="ring-list__description">{props.description}</div>}
+      {props.checkbox !== undefined && (
+        <Icon
+          className={classnames({
+            'ring-list__glyph': true,
+            'ring-list__glyph_hidden': !props.checkbox
+          })}
+          glyph={require('jetbrains-icons/check.svg')}
+          size={Icon.Size.Size18}
+        />
+      )}
+      {props.glyph && (
+        <Icon
+          className="ring-list__glyph"
+          glyph={props.glyph}
+          size={Icon.Size.Size18}
+        />
+      )}
+      {props.icon && (
+        <div
+          className="ring-list__icon"
+          style={{backgroundImage: `url("${props.icon}")`}}
+        />
+      )}
+      {props.description && (
+        <div className="ring-list__description">{props.description}</div>
+      )}
       {props.label}
+      {props.rightGlyph && (
+        <Icon
+          className="ring-list__glyph ring-list__glyph_right"
+          glyph={props.rightGlyph}
+          size={Icon.Size.Size18}
+        />
+      )}
+      {props.details && (
+        <div className={detailsClasses}>{props.details}</div>
+      )}
       </span>
   );
 }
 
 ListItem.defaultProps = {
   active: false
+};
+
+ListItem.propTypes = {
+  active: PropTypes.bool,
+  details: PropTypes.string,
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
+  checkbox: PropTypes.bool,
+  description: PropTypes.string,
+  glyph: PropTypes.string,
+  icon: PropTypes.string,
+  label: PropTypes.string,
+  level: PropTypes.number,
+  rightGlyph: PropTypes.string
 };
