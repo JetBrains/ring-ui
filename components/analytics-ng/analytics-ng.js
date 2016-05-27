9d5df80b0e15eebeefbfc9b9132c35ecf60ce40a
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-05-27 19:38:19 +0300

    RG-1001 Fix all ESLint warning and convert them to error: angular/on-watch - fix tests
    Former-commit-id: ff80b76659ba8f32ed685193c8129da979a4ac1a

--- components/analytics-ng/analytics-ng.js
+++ components/analytics-ng/analytics-ng.js
@@ -91,15 +91,14 @@ module.constant('AnalyticsCustomPlugin', AnalyticsCustomPlugin);
 /**
  * Enable page tracking
  */
-module.run(($rootScope, analytics, $scope) => {
-  /* eslint-disable angular/no-private-call */
-  const off = $rootScope.$on('$routeChangeSuccess', (evt, current) => {
+module.run(($rootScope, analytics) => {
+  $rootScope.$on('$routeChangeSuccess', (evt, current) => { // eslint-disable-line angular/on-watch
+    /* eslint-disable angular/no-private-call */
     if (current && current.$$route && current.$$route.originalPath) {
       analytics.trackPageView(current.$$route.originalPath);
     }
+    /* eslint-enable angular/no-private-call */
   });
-  $scope.$on('$destroy', off);
-  /* eslint-enable angular/no-private-call */
 });
 
 /**
