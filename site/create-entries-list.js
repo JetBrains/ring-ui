ff80f2698a51adf7ae00cf9769d58560a03dcfc8
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-08-09 12:50:29 +0300

    RG-1006 extract huge examples
    Former-commit-id: 920c7f89806745ae5498914d30fc3dc2d6886ff3

--- site/create-entries-list.js
+++ site/create-entries-list.js
@@ -8,24 +8,20 @@ const glob = require('glob');
 
 function createEntriesList(dir) {
   return glob.sync(dir).
-    filter(filePath => {
-      return fs.lstatSync(filePath).isDirectory();
-    }).
+    filter(filePath => fs.lstatSync(filePath).isDirectory()).
     map(dirpath => {
       const fileName = `${dirpath.substr(dirpath.lastIndexOf(path.sep) + 1)}.js`;
       const filePath = path.join(dirpath, fileName);
       const fileAbsPath = path.resolve(filePath);
-      var stats;
 
       try {
-        stats = fs.lstatSync(fileAbsPath);
-      } catch (e) {}
-
-      return stats && stats.isFile() ? `./${filePath}` : false;
+        const stats = fs.lstatSync(fileAbsPath);
+        return stats && stats.isFile() ? `./${filePath}` : false;
+      } catch (e) {
+        return false;
+      }
     }).
-    filter(filePath => {
-      return filePath !== false;
-    });
+    filter(filePath => filePath !== false);
 }
 
 module.exports = createEntriesList;
