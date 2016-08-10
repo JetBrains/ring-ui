110d799f8183a8a58b4d3143f86931eb4b82a497
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-08-10 12:42:31 +0300

    RG-1063 extract large examples to separate files
    Former-commit-id: 2a31f5ea430c296ee4f3d5911f9e8a927cddd60b

--- components/button-group/button-group.js
+++ components/button-group/button-group.js
@@ -8,60 +8,7 @@ import './button-group.scss';
  * @constructor
  * @description Combines several buttons into a group
  * @extends {ReactComponent}
- * @example
-  <example name="Button Group">
-    <file name="index.html">
-      <div>
-        <p>
-          <span id="button-group"></span>
-        </p>
-        <p>
-          <span id="button-group-primary"></span>
-        </p>
-        <p>
-          <span id="button-group-blue"></span>
-        </p>
-        <p>
-          <span id="button-group-with-caption"></span>
-        </p>
-      </div>
-    </file>
-
-    <file name="index.js" webpack="true">
-      var React = require('react');
-      var render = require('react-dom').render;
-      var Button = require('ring-ui/components/button/button');
-      var ButtonGroup = require('ring-ui/components/button-group/button-group');
-
-      render(ButtonGroup.factory(
-        null,
-        Button.factory(null, '1st button'),
-        Button.factory(null, '2nd button'),
-        Button.factory(null, '3rd button')
-      ), document.getElementById('button-group'));
-
-      render(ButtonGroup.factory(
-        null,
-        Button.factory({ primary: true }, '1st button'),
-        Button.factory({ primary: true }, '2nd button'),
-        Button.factory({ primary: true }, '3rd button')
-      ), document.getElementById('button-group-primary'));
-
-      render(ButtonGroup.factory(
-        null,
-        Button.factory({ blue: true }, '1st button'),
-        Button.factory({ blue: true }, '2nd button'),
-        Button.factory({ blue: true }, '3rd button')
-      ), document.getElementById('button-group-blue'));
-
-      render(ButtonGroup.factory(
-        null,
-        <span className="ring-button-group__caption">Side:</span>,
-        Button.factory(null, 'Left'),
-        Button.factory(null, 'Right'),
-      ), document.getElementById('button-group-with-caption'));
-    </file>
-  </example>
+ * @example-file ./button-group__examples.html
  */
 export default class ButtonGroup extends RingComponent {
   render() {
