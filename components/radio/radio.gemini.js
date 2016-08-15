16455e3beb18c51c2e2856258b1f176d7522920c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-08-15 17:30:24 +0300

    RG-1062 radio – implement a React component
    Former-commit-id: ef07af8f1b07f0138a25c5fe5c6bafbba4926c51

---
+++ components/radio/radio.gemini.js
@@ -0,0 +1,8 @@
+import gemini from 'gemini';
+
+gemini.suite('Radio button', suite => {
+  suite.
+    setUrl('/example-radio-button/').
+    setCaptureElements('#container').
+    capture('radio buton');
+});
---
+++ components/radio/radio__item.test.js
@@ -0,0 +1,93 @@
+/* eslint-disable func-names */
+
+import React from 'react';
+import {Simulate, renderIntoDocument} from 'react-addons-test-utils';
+
+import RadioItem from './radio__item';
+
+describe('Item', () => {
+  beforeEach(function () {
+    this.radioItem = renderIntoDocument(
+      <RadioItem value="test">
+        {'test'}
+      </RadioItem>
+    );
+  });
+
+  it('should create component', function () {
+    this.radioItem.should.exist;
+  });
+
+  it('should render radio item', function () {
+    this.radioItem.refs.input.should.have.property('type', 'radio');
+  });
+
+  it('should generate id if not passed', function () {
+    this.radioItem.refs.input.should.have.property('id');
+  });
+
+  it('should generate unique id', function () {
+    const secondRadioItem = renderIntoDocument(
+      <RadioItem>
+        {'test'}
+      </RadioItem>
+    );
+    const secondRadioId = secondRadioItem.refs.input.getAttribute('id');
+    this.radioItem.refs.input.should.not.have.id(secondRadioId);
+  });
+
+  it('should set custom id', function () {
+    this.radioItem.rerender({
+      id: 'test'
+    });
+
+    this.radioItem.refs.input.should.have.id('test');
+  });
+
+  it('should set name', function () {
+    this.radioItem.rerender({
+      name: 'test'
+    });
+
+    this.radioItem.refs.input.should.have.property('name', 'test');
+  });
+
+  it('should call handler for click event', function () {
+    const clickHandler = sinon.stub();
+
+    this.radioItem.rerender({
+      onClick: clickHandler
+    });
+
+    Simulate.click(this.radioItem.refs.input);
+    clickHandler.should.have.been.called;
+  });
+
+  it('should be unchecked by default', function () {
+    this.radioItem.refs.input.should.not.have.property('checked', true);
+  });
+
+  it('should check control', function () {
+    this.radioItem.rerender({
+      checked: true
+    });
+
+    this.radioItem.refs.input.should.have.property('checked', true);
+  });
+
+  it('should be disabled', function () {
+    this.radioItem.rerender({
+      disabled: true
+    });
+
+    this.radioItem.refs.input.should.be.disabled;
+  });
+
+
+  it('should connect labels with input by id', function () {
+    const id = this.radioItem.refs.input.getAttribute('id');
+
+    this.radioItem.refs.label.should.have.attribute('for', id);
+    this.radioItem.refs.textLabel.should.have.attribute('for', id);
+  });
+});
