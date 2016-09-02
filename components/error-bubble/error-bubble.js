1eca36b0e601246016931d577bdce0fe64916269
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-09-02 19:37:29 +0300

    Add Error Bubble component
    Former-commit-id: 684c460b23bc367652a319f00018110091385c40

---
+++ components/error-bubble/error-bubble.js
@@ -0,0 +1,68 @@
+import React, {Children, cloneElement, PropTypes} from 'react';
+import classNames from 'classnames';
+
+import RingComponent from '../ring-component/ring-component';
+
+import './error-bubble.scss';
+
+/**
+ * @name Error Bubble
+ * @category Forms
+ * @constructor
+ * @description Displays error bubble near wrapped input component when non-empty string is passed to `error` prop.
+ * Passes down to input any prop except `className`.
+ * @example
+   <example name="error-bubble">
+     <file name="index.html">
+       <div id="container"></div>
+     </file>
+
+     <file name="index.js" webpack="true">
+       import {render} from 'react-dom';
+       import React from 'react';
+
+       import ErrorBubble from 'ring-ui/components/error-bubble/error-bubble';
+       import Input from 'ring-ui/components/input/input';
+       import 'ring-ui/components/input-size/input-size.scss';
+
+       const container = document.getElementById('container');
+       const renderBubbleDemo = value => (
+         <ErrorBubble
+           error={value ? null : 'Value is required'}
+           valid={value}
+           onInput={e => render(renderBubbleDemo(e.target.value), container)}
+           placeholder="enter something"
+         >
+           <Input className="ring-input-size_md" />
+         </ErrorBubble>
+       );
+
+       render(renderBubbleDemo(), container);
+     </file>
+   </example>
+ */
+
+export default class ErrorBubble extends RingComponent {
+  static propTypes = {
+    error: PropTypes.string,
+    className: PropTypes.string,
+    children: PropTypes.node
+  }
+
+  render() {
+    const {children, className, ...restProps} = this.props;
+    const classes = classNames('ring-error-bubble active', className);
+
+    return (
+      <div className="ring-error-bubble-wrapper">
+        {Children.map(children, child => cloneElement(child, restProps))}
+        {restProps.error && (
+          <div className={classes}>
+            {restProps.error}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
---
+++ components/error-bubble/error-bubble.test.js
@@ -0,0 +1,29 @@
+import 'dom4';
+import {renderIntoDocument, isCompositeComponentWithType} from 'react-addons-test-utils';
+
+import Input from '../input/input';
+import ErrorBubble from './error-bubble';
+
+describe('Error Bubble', () => {
+  const renderComponent = params => renderIntoDocument(ErrorBubble.factory(params, Input.factory()));
+
+  it('should create component', () => {
+    isCompositeComponentWithType(renderComponent(), ErrorBubble).should.be.true;
+  });
+
+  it('should wrap children with div', () => {
+    renderComponent().node.should.match('div');
+  });
+
+  it('should not show error bubble by default', () => {
+    renderComponent().node.should.not.contain('.ring-error-bubble');
+  });
+
+  it('should show error bubble', () => {
+    renderComponent({error: 'test'}).node.should.contain('.ring-error-bubble');
+  });
+
+  it('should use passed className', () => {
+    renderComponent({error: 'test', className: 'test-class'}).node.should.contain('.test-class');
+  });
+});
