6840c8eb899c8ddbf5e39717d15a48fa9ce94389
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-09-08 15:26:58 +0300

    * Update yeoman * Refactor components generation * Fix app creation
    Former-commit-id: b0eb0be7828eb53706322649d6a88116cfe7f23a

--- packages/ring-ui-generator/jsx/index.js
+++ packages/ring-ui-generator/jsx/index.js
@@ -1,42 +1,7 @@
-var generators = require('yeoman-generator');
+const generateComponent = require('../generate-component');
 
-var convertToBemName = function (name) {
-  return name.replace(/-./g, function (symbol) {
-    return symbol.replace('-', '').toUpperCase();
-  });
-};
-
-module.exports = generators.Base.extend({
-
-  writing: function () {
-    this.prompt([{
-      type: 'input',
-      name: 'componentName',
-      message: 'What\'s your component name, ' + 'my-component'.green + ' for example',
-      default: this.appname
-    }], function (answers) {
-      var bemName = convertToBemName(answers.componentName);
-
-      var path = answers.componentName + '/' + answers.componentName;
-
-      this.fs.copyTpl(
-        this.templatePath('/component.jsx'),
-        this.destinationPath(path + '.jsx'),
-        {componentName: answers.componentName, bemName: bemName}
-      );
-
-      this.fs.copyTpl(
-        this.templatePath('/component.scss'),
-        this.destinationPath(path + '.scss'),
-        {componentName: answers.componentName, bemName: bemName}
-      );
-
-      this.fs.copyTpl(
-        this.templatePath('/component.test.jsx'),
-        this.destinationPath(path + '.test.jsx'),
-        {componentName: answers.componentName, bemName: bemName}
-      );
-
-    }.bind(this));
-  }
-});
+module.exports = generateComponent([
+  '%s.jsx',
+  '%s.scss',
+  '%s.test.jsx',
+]);
