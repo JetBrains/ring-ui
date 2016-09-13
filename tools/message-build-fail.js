798749ac14cac06f398b697df0a459d8f69a41fe
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-09-13 17:19:48 +0300

    Clean up tools
    Former-commit-id: bd3a81fdd7151b6c2e7b3089e8677518e33fafdd

--- tools/message-build-fail.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-/* eslint-disable no-console */
-
-const message = 'webpack.config.js is missing';
-console.log(`##teamcity[buildStatus status='FAILURE' text='{build.status.text},${message}']`);
-
-throw new Error(message);
