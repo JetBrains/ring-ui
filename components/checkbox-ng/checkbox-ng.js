e2290565ba3f6ad49d073d5dcf166fe3e91d9208
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2016-10-11 12:53:22 +0300

    checkboxNg: support ng-disabled instead of [x- | data-]disabled attribute. Added new "invert-value" option to invert value of the checkbox. More examples. Little refactoring.
    Former-commit-id: e8d62251f2e50647a3f420f2af749d7e1d7ae19b

--- components/checkbox-ng/checkbox-ng.js
+++ components/checkbox-ng/checkbox-ng.js
@@ -9,14 +9,17 @@ import '../checkbox/checkbox.scss';
  * @example
    <example name="Checkbox Ng">
      <file name="index.html">
-       <div ng-app="TestApp">
+       <div ng-app="TestApp" ng-controller="MainCtrl">
          <div>Checked: {{checked}}</div>
          <div>
            <rg-button id="checkButton" ng-click="checked = !checked">{{checked ? 'Uncheck' : 'Check'}} checkbox programmatically</rg-button>
            <rg-button id="disableButton" ng-click="disabled = !disabled">{{disabled ? 'Enable' : 'Disable'}} checkbox</rg-button>
          </div>
          <p style="width: 300px;">
-           <rg-checkbox x-disabled="disabled" ng-model="checked">Checkbox</rg-checkbox>
+           <rg-checkbox ng-disabled="disabled === true" ng-model="checked">Checkbox</rg-checkbox>
+           <br/>
+           <rg-checkbox ng-disabled="disabled === true" ng-model="checked" ng-change="onNgChange()" invert-value>Inverted checkbox</rg-checkbox>
+           <div>{{ changeText }}</div>
          </p>
        </div>
      </file>
@@ -25,7 +28,14 @@ import '../checkbox/checkbox.scss';
        require('ring-ui/components/checkbox-ng/checkbox-ng');
        require('ring-ui/components/button-ng/button-ng');
        angular.module('TestApp', ['Ring.button', 'Ring.checkbox'])
-     </file>
+        .controller('MainCtrl', function($scope) {
+          $scope.changeText = 'text will be changed on ng-change of inverted checkbox';
+
+          $scope.onNgChange = function(newValue) {
+            $scope.changeText = 'ngChange appear for the inverted checkbox at ' + new Date();
+          }
+        });
+ </file>
    </example>
  */
 /* global angular: false */
@@ -34,40 +44,42 @@ const angularModule = angular.module('Ring.checkbox', ['Ring.icon']);
 let idCounter = 0;
 const CHECKBOX_ID_PREFIX = 'rg-checkbox-';
 
-angularModule.directive('rgCheckbox', $parse => ({
+angularModule.directive('rgCheckbox', () => ({
   restrict: 'E',
   transclude: true,
   replace: true,
   template: require('./checkbox-ng.html'),
   require: ['?ngModel'],
 
-  link(scope, iElement, iAttrs, [ctrl]) {
+  link(scope, iElement, iAttrs, [ngModelCtrl]) {
     const input = iElement[0].query('.ring-checkbox__input');
 
     const id = CHECKBOX_ID_PREFIX + idCounter++;
     iElement[0].setAttribute('for', id);
     input.setAttribute('id', id);
 
-    if (iAttrs.disabled) {
-      const disabledGetter = $parse(iAttrs.disabled);
-
-      if (!disabledGetter.constant) {
-        scope.$watch(disabledGetter, disabled => {
-          input.disabled = disabled;
-        });
-      } else {
-        input.disabled = disabledGetter(scope);
+    function modelValue(value) {
+      if (iAttrs.invertValue !== undefined) {
+        return !value;
       }
+      return value;
     }
 
+    iAttrs.$observe('disabled', newDisabledValue => {
+      if (newDisabledValue) {
+        input.setAttribute('disabled', 'disabled');
+      } else {
+        input.removeAttribute('disabled');
+      }
+    });
 
-    if (ctrl) {
+    if (ngModelCtrl) {
       angular.element(input).on('click', ev => {
-        ctrl.$setViewValue(input.checked, ev && ev.type);
+        ngModelCtrl.$setViewValue(modelValue(input.checked), ev && ev.type);
       });
 
-      ctrl.$render = () => {
-        input.checked = ctrl.$viewValue;
+      ngModelCtrl.$render = () => {
+        input.checked = modelValue(ngModelCtrl.$viewValue);
       };
     }
   }
