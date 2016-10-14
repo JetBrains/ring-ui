685ddf2f85bc4016aaf909d83c5138388268c830
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-14 21:02:41 +0300

    Update app generator: make it build and run
    Former-commit-id: b69bc6da97fdf29a7e21d31b5251aca7b6e13816

--- packages/ring-ui-generator/app/templates/src/components/app/app.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app.js
@@ -2,13 +2,14 @@ import '!!file?name=favicon.ico!../../favicon.ico';
 import './app.scss';
 //Example of usage react components
 import React from 'react';
-import alerts from 'alert/alerts';
+import {render} from 'react-dom';
+import Alerts from 'ring-ui/components/alert/alerts';
 
 const ALERT_DELAY = 1000;
-const alertsContainer = React.renderComponent(alerts(null), document.getElementById('alerts__container'));
+const alertsContainer = render(Alerts.factory(), document.getElementById('alerts__container'));
 
 alertsContainer.add('Test message');
-alertsContainer.add('Another test message', alerts.Type.MESSAGE, ALERT_DELAY);
-alertsContainer.add('Test warning', alerts.Type.WARNING);
+alertsContainer.add('Another test message', Alerts.Type.MESSAGE, ALERT_DELAY);
+alertsContainer.add('Test warning', Alerts.Type.WARNING);
 
 export default {};
--- packages/ring-ui-generator/app/templates/webpack.config.js
+++ packages/ring-ui-generator/app/templates/webpack.config.js
@@ -1,48 +1,74 @@
 /* eslint-env node */
 /* eslint-disable modules/no-cjs */
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
 const webpackConfigMerger = require('webpack-config-merger');
+const autoprefixer = require('autoprefixer');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const argv = require('minimist')(process.argv);
+
+const pkgConfig = require('./package.json');
 const isDevelop = argv.d !== undefined;
 const serverBase = isDevelop ? 'http://localhost:8080/api' : 'api';
-const webpack = require('webpack');
-const pkgConfig = require('./package.json');
 
-const srcRegexp = /src.components.*\.js$/;
+const srcPath = [path.join(__dirname, 'src')];
 
-const webpackConfig = webpackConfigMerger(
-  require('ring-ui/webpack.config'),
+const webpackConfig = webpackConfigMerger(require('ring-ui/webpack.config'),
   {
     entry: `${pkgConfig.config.src}/components/app/app.js`,
+    resolve: {
+      alias: {
+        react: path.resolve('./node_modules/react'),
+        'react-dom': path.resolve('./node_modules/react-dom')
+      }
+    },
     output: {
-      path: pkgConfig.config.dist,
-      filename: '[name].js',
-      library: '[name]',
-      libraryTarget: 'umd'
+      path: './dist',
+      filename: '[name].js'
     },
     module: {
       loaders: [
         {
-          test: srcRegexp,
-          loaders: ['babel-loader?experimental&optional=runtime']
+          test: /\.scss$/,
+          include: srcPath,
+          loaders: [
+            'style',
+            'css',
+            'postcss?pack=<%= camelCaseName %>',
+            'sass?outputStyle=expanded'
+          ]
         },
-        {test: /.*\/app\/.*\.html$/, loader: 'html'}
+        {
+          test: /\.js$/,
+          include: srcPath,
+          loader: 'babel'
+        }
       ]
-    }
-  }, {
+    },
+    postcss: {
+      '<%= camelCaseName %>': [autoprefixer]
+    },
+    devServer: {
+      stats: {
+        assets: false,
+        children: false,
+        chunks: false,
+        hash: false,
+        version: false
+      }
+    },
     plugins: [
       new HtmlWebpackPlugin({
-        template: 'src/index.html'
+        template: 'html?interpolate!src/index.html'
       }),
       new webpack.DefinePlugin({
-        AppBuildConfig: {
-          version: JSON.stringify(process.env['version.number'] || 'dev'),
-          number: JSON.stringify(process.env.BUILD_NUMBER || 'dev'),
-          backendUrl: JSON.stringify(serverBase)
+        'process.env': {
+          // This has effect on the react lib size
+          NODE_ENV: !isDevelop ? '"production"' : '"development"'
         }
       }),
-      new webpack.optimize.DedupePlugin()
+      // new webpack.optimize.DedupePlugin()
     ]
   });
 
