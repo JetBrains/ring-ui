15f18fd7f6acf6f257975386570f64bcfa610849
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-14 12:57:13 +0300

    Update app generator: remove angular stuff
    Former-commit-id: 1598bfdbf082d357d4228c69ec52d4f881aaccab

--- packages/ring-ui-generator/app/templates/src/components/app/app.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app.js
@@ -1,6 +1,5 @@
 import '!!file?name=favicon.ico!../../favicon.ico';
 import './app.scss';
-<% if (useReact) { %>
 //Example of usage react components
 import React from 'react';
 import alerts from 'alert/alerts';
@@ -11,38 +10,5 @@ let alertsContainer = React.renderComponent(alerts(null),
 alertsContainer.add('Test message');
 alertsContainer.add('Another test message', alerts.Type.MESSAGE, 1000);
 alertsContainer.add('Test warning', alerts.Type.WARNING);
-<% } %>
-<% if (useAngular) { %>
-import 'angular';
-import 'angular-resource';
-import 'angular-route';
-import 'angular-gettext';
 
-import Footer from 'footer/footer';
-import reactNG from 'react-ng/react-ng';
-
-reactNG({Footer});
-
-angular.module('project', [
-  'ngRoute',
-  'gettext',
-  'Ring.react-ng'
-])
-.run(function ($rootScope) {
-  var renderFooter = function () {
-    $rootScope.footer = {
-      'right': [{
-        label: 'Leave Feedback',
-        url: 'https://some.link'
-      }]
-    };
-  };
-
-  renderFooter();
-})
-.config(($locationProvider) => {
-  $locationProvider.html5Mode(true);
-
-});
-<% } %>
 export default {};
