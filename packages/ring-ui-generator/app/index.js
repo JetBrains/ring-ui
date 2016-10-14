7ba0e3981f044927c0deecdf48d627001d6a09e2
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-14 21:02:41 +0300

    Update app generator: make it build and run
    Former-commit-id: ee826f042a490342e02260feeb364498058109ba

--- packages/ring-ui-generator/app/index.js
+++ packages/ring-ui-generator/app/index.js
@@ -2,8 +2,9 @@ const generators = require('yeoman-generator');
 const changeCase = require('change-case');
 
 module.exports = generators.Base.extend({
+  promt: function () {
+    const cb = this.async();
 
-  writing: function () {
     this.prompt([{
       type: 'input',
       name: 'projectName',
@@ -11,28 +12,42 @@ module.exports = generators.Base.extend({
       default: this.appname
     }]).then(answers => {
       const projectName = changeCase.paramCase(answers.projectName);
-
-      this.fs.copyTpl(
-        this.templatePath('src/**/*'),
-        this.destinationPath('src/'),
-        {projectName}
-      );
-
-      this.fs.copyTpl(
-        this.templatePath('*.{json,js}'),
-        this.destinationPath(''),
-        {projectName}
-      );
-
-      this._copyUtilFiles();
+      const camelCaseName = changeCase.camelCase(answers.projectName);
+      this.props = {projectName, camelCaseName};
+      cb();
     });
   },
 
-  _copyUtilFiles: function () {
+  files: function () {
+    this.fs.copyTpl(
+      this.templatePath('src/**/*'),
+      this.destinationPath('src/'),
+      this.props
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('*.{json,js}'),
+      this.destinationPath(''),
+      this.props
+    );
+
     this.template('npmrc', '.npmrc');
     this.template('editorconfig', '.editorconfig');
     this.template('gitignore', '.gitignore');
     this.template('eslintignore', '.eslintignore');
     this.template('eslintrc', '.eslintrc');
+  },
+
+  install: function () {
+    this.npmInstall([
+      'ring-ui',
+      'jetbrains-logos',
+      'jetbrains-icons',
+    ], { 'save': true });
+
+    this.installDependencies({
+      bower: false,
+      skipInstall: this.options['skip-install']
+    });
   }
 });
