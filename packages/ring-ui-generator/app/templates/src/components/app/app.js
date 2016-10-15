9c0e96e8f6ea316a02aab7728ec34f58bcc7c56c
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-15 20:12:29 +0300

    Update app generator: app rendering with Ring UI
    Former-commit-id: e339ae8a53f5a7a447204951f95ce342482c4c18

--- packages/ring-ui-generator/app/templates/src/components/app/app.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app.js
@@ -1,15 +1,42 @@
+import 'babel-polyfill'
+import React from 'react'
+import {render} from 'react-dom'
+
 import './app.scss';
 
-//Example of usage react components
-import React from 'react';
-import {render} from 'react-dom';
-import Alerts from 'ring-ui/components/alert/alerts';
+const rootEl = document.getElementById('app__root')
 
-const ALERT_DELAY = 1000;
-// const alertsContainer = render(Alerts.factory(), document.getElementById('alerts__container'));
-//
-// alertsContainer.add('Test message');
-// alertsContainer.add('Another test message', Alerts.Type.MESSAGE, ALERT_DELAY);
-// alertsContainer.add('Test warning', Alerts.Type.WARNING);
+let renderApp = () => {
+  const AppRoot = require('./app__root');
+  render(
+    <AppRoot />,
+    rootEl
+  )
+}
 
-export default {};
+/* Hot Replacement support, won't be bundled to production */
+/* eslint-disable modules/no-exports-typo */
+if (module.hot) {
+  const renderError = error => {
+    const RedBox = require('redbox-react').default;
+
+    render(
+      <RedBox error={error} />,
+      rootEl
+    )
+  }
+
+  const renderHot = () => {
+    try {
+      renderApp();
+    } catch (error) {
+      renderError(error);
+    }
+  }
+
+  module.hot.accept('./app__root', () => {
+    setTimeout(renderHot);
+  })
+}
+
+renderApp();
