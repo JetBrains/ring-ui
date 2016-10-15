bf09e8c1e8a67f91cdb09fb34a43061ab810041e
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-15 21:17:11 +0300

    Update app generator: lint templates
    Former-commit-id: 8749a5582eb4eaa46dc4053e5f95fb8189217e12

--- packages/ring-ui-generator/app/templates/karma.conf.js
+++ packages/ring-ui-generator/app/templates/karma.conf.js
@@ -46,7 +46,7 @@ module.exports = config => {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: argv.teamcity ? ['progress', 'teamcity'] : ['progress'] ,
+    reporters: argv.teamcity ? ['progress', 'teamcity'] : ['progress'],
 
 
     // web server port
--- packages/ring-ui-generator/app/templates/src/components/app/app.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app.js
@@ -1,18 +1,18 @@
-import 'babel-polyfill'
-import React from 'react'
-import {render} from 'react-dom'
+import 'babel-polyfill';
+import React from 'react';
+import {render} from 'react-dom';
 
 import './app.scss';
 
-const rootEl = document.getElementById('app__root')
+const rootEl = document.getElementById('app__root');
 
-let renderApp = () => {
+const renderApp = () => {
   const AppRoot = require('./app__root');
   render(
     <AppRoot />,
     rootEl
-  )
-}
+  );
+};
 
 /* Hot Replacement support, won't be bundled to production */
 /* eslint-disable modules/no-exports-typo */
@@ -21,10 +21,10 @@ if (module.hot) {
     const RedBox = require('redbox-react').default;
 
     render(
-      <RedBox error={error} />,
+      <RedBox error={error}/>,
       rootEl
-    )
-  }
+    );
+  };
 
   const renderHot = () => {
     try {
@@ -32,11 +32,11 @@ if (module.hot) {
     } catch (error) {
       renderError(error);
     }
-  }
+  };
 
   module.hot.accept('./app__root', () => {
     setTimeout(renderHot);
-  })
+  });
 }
 
 renderApp();
