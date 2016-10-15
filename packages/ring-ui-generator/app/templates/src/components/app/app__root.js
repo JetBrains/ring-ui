710f8f870637b27a6a231bb3af26b2a6d0fa24e9
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-15 21:17:11 +0300

    Update app generator: lint templates
    Former-commit-id: b004fc440a7f28594191d749f7aecedebad3546a

--- packages/ring-ui-generator/app/templates/src/components/app/app__root.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app__root.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from 'react';
 import RingComponent from 'ring-ui/components/ring-component/ring-component';
 
 import Header from 'ring-ui/components/header/header';
 import Footer from 'ring-ui/components/footer/footer';
-import jetbrainsLogo from 'jetbrains-logos/jetbrains/jetbrains.svg'
+import jetbrainsLogo from 'jetbrains-logos/jetbrains/jetbrains.svg';
 
 export default class AppRoot extends RingComponent {
   render() {
     return (
       <div>
-        <Header logo={jetbrainsLogo}></Header>
+        <Header logo={jetbrainsLogo} />
         <div className="app__content">
           {'App content'}
         </div>
-        <Footer></Footer>
+        <Footer />
       </div>
     );
   }
