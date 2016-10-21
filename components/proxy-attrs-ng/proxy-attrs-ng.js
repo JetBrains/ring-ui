c818f0fd49c99822974f3f47700d1dcc5d1f25dc
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2016-10-21 20:33:28 +0300

    Proxy attrs: tests + improvement to support HUB cases
    Former-commit-id: 253c031f9f261ed3d9f0a8217450c06816cfde50

--- components/proxy-attrs-ng/proxy-attrs-ng.js
+++ components/proxy-attrs-ng/proxy-attrs-ng.js
@@ -3,16 +3,39 @@ const angularModule = angular.module('Ring.proxy-attrs', []);
 
 function proxyAttrs(template, attrsWhitelist) {
   return function copyAtrrs(iElement, iAttrs) {
-    const PREFIX = 'data-proxy-';
+    const ATTR_PREFIX = 'data-proxy-';
+    const VALUE_PREFIX = 'data-proxyvalue-';
+
     const attrsList = attrsWhitelist || Object.keys(iAttrs.$attr);
     let resultTemplate = template;
+
+    const hasProxyAttrs = template.indexOf(ATTR_PREFIX) !== -1;
+    const hasProxyValues = template.indexOf(VALUE_PREFIX) !== -1;
 
     attrsList.forEach(attrName => {
       if (iAttrs[attrName] !== undefined) {
         const attrLower = iAttrs.$attr[attrName];
-        resultTemplate = resultTemplate.replace(`${PREFIX}${attrLower}`, `${attrLower}="${iAttrs[attrName]}"`);
+        const attrValue = iAttrs[attrName];
+
+        // proxy full attr
+        if (hasProxyAttrs) {
+          const attrFind = `${ATTR_PREFIX}${attrLower}`.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+          const attrReplace = `${attrLower}="${attrValue}"`;
+          resultTemplate = resultTemplate.replace(RegExp(`${attrFind}(="")*`, 'g'), attrReplace);
+        }
+
+        // proxy only values only
+        if (hasProxyValues) {
+          const valueFind = `${VALUE_PREFIX}${attrLower}`.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+          resultTemplate = resultTemplate.replace(RegExp(valueFind, 'g'), attrValue);
+        }
       }
     });
+
+    // remove attributes with empty proxy values
+    if (hasProxyValues) {
+      resultTemplate = resultTemplate.replace(RegExp(`([^\\s]+?)="${VALUE_PREFIX}(.+?)"`, 'g'), '');
+    }
 
     return resultTemplate;
   };
