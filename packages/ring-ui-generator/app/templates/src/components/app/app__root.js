af2719a575268368a4794288fbcc3ecba301317e
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-10-21 16:56:35 +0300

    Use HeaderAuth
    Former-commit-id: 1e2b19e074b17b776ddaefae5d206f7b6854b905

--- packages/ring-ui-generator/app/templates/src/components/app/app__root.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app__root.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import RingComponent from 'ring-ui/components/ring-component/ring-component';
 
-import Header from 'ring-ui/components/header/header';
+import HeaderAuth from 'ring-ui/components/header-auth/header-auth';
+import Auth from 'ring-ui/components/auth/auth';
 import Footer from 'ring-ui/components/footer/footer';
 import jetbrainsLogo from 'jetbrains-logos/jetbrains/jetbrains.svg';
 
 export default class AppRoot extends RingComponent {
+  willMount() {
+    this.auth = new Auth({
+      serverUri: '***REMOVED***/'
+    });
+    this.auth.init();
+  }
+
   render() {
     return (
       <div>
-        <Header logo={jetbrainsLogo} />
+        <HeaderAuth
+          auth={this.auth}
+          logo={jetbrainsLogo}
+        />
         <div className="app__content">
           {'App content'}
         </div>
