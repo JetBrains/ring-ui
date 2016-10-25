afa40db5f1b8f2873806f5475b5a477ab077c0e2
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-10-25 16:55:48 +0300

    RG-1076 add react wrapper, tests and example for "Badge" component
    Former-commit-id: 509ff74bd56d8c8228db8b364fc070fc2d7bb256

---
+++ components/badge/badge.js
@@ -0,0 +1,56 @@
+import React, {PropTypes} from 'react';
+import classNames from 'classnames';
+import RingComponent from '../ring-component/ring-component';
+import './badge.scss';
+
+/**
+ * @name Badge
+ * @category Forms
+ * @constructor
+ * @description Provides simple component for badges
+ * @extends {RingComponent}
+ * @example-file ./badge__examples.html
+ */
+export default class Badge extends RingComponent {
+  static propTypes = {
+    gray: PropTypes.bool,
+    valid: PropTypes.bool,
+    invalid: PropTypes.bool,
+    disabled: PropTypes.bool,
+    className: PropTypes.string
+  }
+
+  render() {
+    const {
+      // Modifiers
+      gray,
+      valid,
+      invalid,
+      disabled,
+
+      // Props
+      className,
+      children,
+      ...props
+    } = this.props;
+
+    const classes = classNames(
+      'ring-badge',
+      className, {
+        'ring-badge_gray': gray,
+        'ring-badge_valid': valid,
+        'ring-badge_invalid': invalid,
+        'ring-badge_disabled': disabled
+      }
+    );
+
+    return (
+      <span
+        {...props}
+        className={classes}
+      >
+        {children}
+      </span>
+    );
+  }
+}
