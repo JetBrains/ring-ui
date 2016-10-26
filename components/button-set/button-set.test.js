2703b9325bb263777dcd14058370fc5ae8b2a63d
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2016-10-26 15:53:28 +0300

    ButtonSet component created
    Former-commit-id: b6153ff87f980a1dc94be5b5cfbb1cb55f446678

---
+++ components/button-set/button-set.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import ButtonSet from './button-set';
+
+describe('Button Set', () => {
+  let component;
+
+  beforeEach(() => {
+    component = TestUtils.renderIntoDocument(React.createElement(ButtonSet));
+  });
+
+  it('should create component', () => {
+    TestUtils.isCompositeComponentWithType(component, ButtonSet).should.equal(true);
+  });
+});
