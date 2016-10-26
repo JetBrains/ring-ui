0f104f486c11f9a8620e17841819d5092bf2af7a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-10-26 19:49:37 +0300

    RG-1074 beatify search for main file in component
    Former-commit-id: 474ddd0ba69b8843b53a2faff4f3dcbeab253214

--- site/create-entries-list.js
+++ site/create-entries-list.js
@@ -6,32 +6,27 @@ const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
 
-function tryReadMainScssFile(dirpath) {
-  const fileName = `${dirpath.substr(dirpath.lastIndexOf(path.sep) + 1)}.scss`;
+function readMainFileWithExtension(extension, dirpath) {
+  const fileName = `${dirpath.substr(dirpath.lastIndexOf(path.sep) + 1)}.${extension}`;
   const filePath = path.join(dirpath, fileName);
   const fileAbsPath = path.resolve(filePath);
 
-  try {
-    const stats = fs.lstatSync(fileAbsPath);
-    return stats && stats.isFile() ? `./${filePath}` : false;
-  } catch (e) {
-    return false;
-  }
+  const stats = fs.lstatSync(fileAbsPath);
+  return stats && stats.isFile() ? `./${filePath}` : false;
 }
 
 function createEntriesList(dir) {
   return glob.sync(dir).
     filter(filePath => fs.lstatSync(filePath).isDirectory()).
     map(dirpath => {
-      const fileName = `${dirpath.substr(dirpath.lastIndexOf(path.sep) + 1)}.js`;
-      const filePath = path.join(dirpath, fileName);
-      const fileAbsPath = path.resolve(filePath);
-
       try {
-        const stats = fs.lstatSync(fileAbsPath);
-        return stats && stats.isFile() ? `./${filePath}` : false;
-      } catch (e) {
-        return tryReadMainScssFile(dirpath);
+        return readMainFileWithExtension('js', dirpath);
+      } catch (jsReadErr) {
+        try {
+          return readMainFileWithExtension('scss', dirpath);
+        } catch (e) {
+          return false;
+        }
       }
     }).
     filter(filePath => filePath !== false);
