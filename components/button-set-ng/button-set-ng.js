a4443f6fca1b126720f6a36e5d8354c2fb73e54a
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2016-10-27 12:25:29 +0300

    RG-1116: Fixed. Create NG component for ButtonSet
    Former-commit-id: be30707742eb3933df7896707e5a9636069290ff

---
+++ components/button-set-ng/button-set-ng.js
@@ -0,0 +1,42 @@
+import '../button-set/button-set.scss';
+
+/**
+ * @name Button Set Ng
+ * @category Angular Components
+ * @description Provides an Angular wrapper for Button Set.
+ * @example
+   <example name="Button Set Ng">
+    <file name="index.html">
+      <div ng-app="TestApp">
+        <rg-button-set>
+          <rg-button>Button 1</rg-button>
+          <rg-button>Button 2</rg-button>
+          <rg-button>Button 3</rg-button>
+        </rg-button-set>
+      </div>
+    </file>
+    <file name="index.js" webpack="true">
+     require('angular');
+     require('ring-ui/components/button-set-ng/button-set-ng');
+     require('ring-ui/components/button-ng/button-ng');
+     angular.module('TestApp', ['Ring.button', 'Ring.button-set']);
+    </file>
+   </example>
+ */
+/* global angular: false */
+
+const angularModule = angular.module('Ring.button-set', []);
+
+function rgButtonSet() {
+  return {
+    restrict: 'E',
+    replace: true,
+    scope: false,
+    transclude: true,
+    template: require('./button-set-ng.html')
+  };
+}
+
+angularModule.directive('rgButtonSet', rgButtonSet);
+
+export default angularModule.name;
