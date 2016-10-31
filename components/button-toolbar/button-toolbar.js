60e19f7bda5efa33f7f9292e915b3cf4ae573c0e
Max Maximov <max.maximov@gmail.com>
2016-10-31 19:43:33 +0300

    button-toolbar
    Former-commit-id: b1cc6a36cc057d41fd57f55f62c91ee4eb79105a

---
+++ components/button-toolbar/button-toolbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import classNames from 'classnames';
+import RingComponent from '../ring-component/ring-component';
+import './button-toolbar.scss';
+
+/**
+ * @name Button Toolbar
+ * @category Components
+ * @framework React
+ * @constructor
+ * @extends {ReactComponent}
+ * @description Displays a toolbar with several buttons.
+ * @example
+  <example name="Button Toolbar">
+    <file name="index.html">
+      <div id="button-toolbar"></div>
+    </file>
+    <file name="index.js" webpack="true">
+      import React from 'react';
+      import {render} from 'react-dom';
+
+      import Button from 'ring-ui/components/button/button';
+      import ButtonGroup from 'ring-ui/components/button-group/button-group';
+      import ButtonToolbar from 'ring-ui/components/button-toolbar/button-toolbar';
+
+      const container = document.getElementById('button-toolbar');
+
+      const buttonToolbarDemo = (
+        <ButtonToolbar>
+          <Button primary={true} delayed={true}>Do it</Button>
+          <ButtonGroup>
+            <Button>Just do it</Button>
+            <Button>Yes you can</Button>
+            <Button disabled="disabled">Do it</Button>
+          </ButtonGroup>
+          <Button>Just do it</Button>
+        </ButtonToolbar>
+      );
+
+      render(buttonToolbarDemo, container);
+    </file>
+  </example>
+*/
+
+export default class ButtonToolbar extends RingComponent {
+  render() {
+    const classes = classNames('ring-button-toolbar', this.props.className);
+    return (
+      <div
+        {...this.props}
+        className={classes}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
