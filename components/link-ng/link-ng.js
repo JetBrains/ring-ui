66bd2c6dccb11076a90ec2b532052a25e13ebce7
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-10-31 19:01:23 +0300

    RG-1098 implement angular link component
    Former-commit-id: 7b1b46d9dcad86bdb72fbb0d5d8ec6cf7fc984aa

---
+++ components/link-ng/link-ng.js
@@ -0,0 +1,39 @@
+import '../link/link.scss';
+
+/**
+ * @name Link Ng
+ * @category Angular Components
+ * @framework Angular
+ * @constructor
+ * @description Displays a link.
+ * @example
+    <example name="link-ng">
+      <file name="index.html">
+        <div id="link" ng-app="TestApp">
+          <rg-link href="http://google.com" class="test-class">Open google</rg-link>
+        </div>
+      </file>
+      <file name="index.js">
+        import angular from 'angular';
+        import Link from  'ring-ui/components/link-ng/link-ng';
+
+        angular.module('TestApp', [Link]);
+      </file>
+  </example>
+ */
+
+/* global angular: false */
+const angularModule = angular.module('Ring.link', []);
+
+function rgLinkDirective() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    replace: true,
+    template: '<a class="ring-link" ng-transclude></a>'
+  };
+}
+
+angularModule.directive('rgLink', rgLinkDirective);
+
+export default angularModule.name;
