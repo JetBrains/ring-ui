a69bfe99266ed26cc86bb81127171f2670108ba3
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-11-01 19:31:21 +0300

    RG-1123 first implementation of content-layout component
    Former-commit-id: 4f2541e84ba16a8c0419ad12ccdcff3118633113

---
+++ components/content-layout/content-layout.test.js
@@ -0,0 +1,41 @@
+import 'dom4';
+import React from 'react';
+import {renderIntoDocument, isCompositeComponentWithType} from 'react-addons-test-utils';
+
+import ContentLayout from './content-layout';
+import ContentLayoutSidebar from './content-layout__sidebar';
+import styles from './content-layout.css';
+
+describe('Content Layout', () => {
+  const renderComponent = params => renderIntoDocument(ContentLayout.factory(params));
+
+  it('should create component', () => {
+    isCompositeComponentWithType(renderComponent(), ContentLayout).should.be.true;
+  });
+
+  it('should wrap children with div', () => {
+    renderComponent().node.should.match('div');
+  });
+
+  it('should use passed className', () => {
+    renderComponent({className: 'test-class'}).node.should.match('.test-class');
+  });
+
+  it('should render sidebar', () => {
+    const component = renderIntoDocument(<ContentLayout>
+      <ContentLayoutSidebar>{'In sidebar'}</ContentLayoutSidebar>
+      <div>{'Foo'}</div>
+    </ContentLayout>);
+
+    component.node.should.contain(`div.${styles.sidebar}`);
+  });
+
+  it('should render sidebar on the right', () => {
+    const component = renderIntoDocument(<ContentLayout>
+      <ContentLayoutSidebar right={true}>{'In sidebar'}</ContentLayoutSidebar>
+      <div>{'Foo'}</div>
+    </ContentLayout>);
+
+    component.node.should.contain(`div.${styles.sidebarRight}`);
+  });
+});
