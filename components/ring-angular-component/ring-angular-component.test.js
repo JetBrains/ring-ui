ec766716d5f83e0a98935e2be74ba0abdb3844e5
Max Maximov <max.maximov@gmail.com>
2016-11-02 04:57:08 +0300

    ring-angular-component
    Former-commit-id: dd4b2a65e94e86b0a69e4cdc90096628f2f8889e

---
+++ components/ring-angular-component/ring-angular-component.test.js
@@ -0,0 +1,32 @@
+/* global angular: false */
+/* global inject: false */
+import 'angular';
+import 'angular-mocks';
+
+import RingAngularComponent from './ring-angular-component';
+
+class TestComponent extends RingAngularComponent {
+  static $inject = ['$scope'];
+}
+
+const testModule = angular.module('Test', []).component('testComponent', TestComponent).name;
+
+describe('RingAngularComponent', () => {
+  let $componentController;
+
+  beforeEach(window.module(testModule));
+
+  beforeEach(inject(_$componentController_ => {
+    $componentController = _$componentController_;
+  }));
+
+  it('should return a controller as a static property', () => {
+    const ctrl = $componentController('testComponent');
+    ctrl.should.not.be.undefined;
+  });
+
+  it('should copy dependencies to class instances', () => {
+    const ctrl = $componentController('testComponent');
+    ctrl.should.have.deep.property('$inject.$scope');
+  });
+});
