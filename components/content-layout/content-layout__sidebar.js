c94209854a7789931906d1e428911282bfe1d90d
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-11-07 16:37:59 +0300

    RG-1126 first implementation of nicely fixing sidebar
    Former-commit-id: 92fc269ea8d2e872967fd95ecf2054bd4fa98139

--- components/content-layout/content-layout__sidebar.js
+++ components/content-layout/content-layout__sidebar.js
@@ -1,8 +1,12 @@
 import React, {PropTypes} from 'react';
 import classNames from 'classnames';
 import styles from './content-layout.css';
+import Waypoint from 'react-waypoint';
 
 import RingComponent from '../ring-component/ring-component';
+
+const ABOVE = 'above';
+const INSIDE = 'inside';
 
 export default class ContentLayoutSidebar extends RingComponent {
   static propTypes = {
@@ -10,19 +14,59 @@ export default class ContentLayoutSidebar extends RingComponent {
     className: PropTypes.string
   };
 
+  state = {
+    topIsOutside: true,
+    bottomIsOutside: true,
+    sidebarHeight: null
+  };
+
+  handleTopWaypoint({currentPosition}) {
+    this.setState({topIsOutside: currentPosition === ABOVE});
+  }
+
+  handleBottomWaypoint({currentPosition, waypointTop}) {
+    this.setState({
+      sidebarHeight: waypointTop,
+      bottomIsOutside: currentPosition !== INSIDE
+    });
+  }
+
   render() {
     const {right, children, className, ...restProps} = this.props;
 
+    const shouldFixateBottom = !this.state.bottomIsOutside && this.state.topIsOutside;
+
     const classes = classNames(styles.sidebar, className, {
-      [styles.sidebarRight]: right
+      [styles.sidebarRight]: right,
+      [styles.sidebarFixedTop]: this.state.bottomIsOutside && this.state.topIsOutside,
+      [styles.sidebarFixedBottom]: shouldFixateBottom
     });
 
+    const style = {
+      maxHeight: shouldFixateBottom && this.state.sidebarHeight ? `${this.state.sidebarHeight}px` : null
+    };
+
     return (
-      <div
-        {...restProps}
-        className={classes}
-      >
-        {children}
+      <div className={styles.sidebarContainer}>
+        <Waypoint
+          onEnter={data => this.handleTopWaypoint(data)}
+          onLeave={data => this.handleTopWaypoint(data)}
+        />
+
+        <div
+          {...restProps}
+          style={style}
+          className={classes}
+        >
+          {children}
+        </div>
+
+        <div className={styles.bottomMarker}>
+          <Waypoint
+            onEnter={data => this.handleBottomWaypoint(data)}
+            onLeave={data => this.handleBottomWaypoint(data)}
+          />
+        </div>
       </div>
     );
   }
