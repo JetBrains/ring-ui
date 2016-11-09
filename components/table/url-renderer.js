ab4da02b2401297e03ed574ad3ad6cbce3e5ed6e
Max Maximov <max.maximov@gmail.com>
2016-11-09 17:45:06 +0300

    ring-table: renderers
    Former-commit-id: df90c8b2a4397255bac40e9314c34f5ed04f1d29

---
+++ components/table/url-renderer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import DefaultRenderer from './default-renderer';
+import Cell from './cell';
+import Link from '../link/link';
+
+export default class URLRenderer extends DefaultRenderer {
+  render() {
+    const {item, column} = this.props;
+    const value = item[column.id];
+
+    let url;
+    let title;
+
+    if (value.url && value.title) {
+      url = value.url;
+      title = value.title;
+    } else if (value.url) {
+      url = title = value.url;
+    } else {
+      url = title = value;
+    }
+
+    return <Cell><Link href={url}>{title}</Link></Cell>;
+  }
+}
