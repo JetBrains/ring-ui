806e84a55a7ff40d626a42b972d96288fb7f2225
Max Maximov <max.maximov@gmail.com>
2016-11-10 03:27:57 +0300

    ring-table: getValue
    Former-commit-id: 10a6cf3d863f75250b5ffed667f734d90981f630

--- components/table/renderer-default.js
+++ components/table/renderer-default.js
@@ -4,12 +4,10 @@ import Cell from './cell';
 
 export default class DefaultRenderer extends RingComponent {
   static propTypes = {
-    item: PropTypes.object.isRequired,
-    column: PropTypes.object.isRequired
+    children: PropTypes.any.isRequired
   }
 
   render() {
-    const {item, column} = this.props;
-    return <Cell>{item[column.id]}</Cell>;
+    return <Cell>{this.props.children}</Cell>;
   }
 }
