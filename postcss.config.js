1c0a1dbb118a7a588aa930b04575767df7d6a776
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-11-15 21:48:14 +0300

    RG-1114 Setup CSS modules for new components: replace values!
    Former-commit-id: 205c0f63f78020bbcc99dbf3c36df2b81308c476

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,28 @@
 /* eslint-disable modules/no-cjs */
 
-const autoprefixer = require('autoprefixer');
-const cssnext = require('postcss-cssnext');
-const replaceValues = require('postcss-modules-values-replace');
-
 const scssRE = /\.scss$/;
 
 module.exports = ctx => {
+  const cssModules = {
+    'postcss-modules-values-replace': {
+      fs: ctx.webpack._compiler.inputFileSystem
+    },
+    'postcss-cssnext': {
+      features: {
+        calc: {
+          mediaQueries: true
+        }
+      }
+    }
+  };
+
+  const autoprefixer = {
+    autoprefixer: {}
+  };
+
   const plugins = scssRE.test(ctx.webpack.resourcePath)
-    ? [autoprefixer]
-    : [replaceValues({fs: ctx.webpack._compiler.inputFileSystem}), cssnext];
+    ? autoprefixer
+    : cssModules;
 
   return {plugins};
 };
