6a4c32c824e4f88062aaf78c710ba8faeb1ad60a
hypnos <Filipp.Riabchun@jetbrains.com>
2016-11-16 15:13:52 +0300

    RG-418 Date Picker
    Former-commit-id: c45da8570cc59fff4425a00f650b583c1003312b

--- components/date-picker/date-picker.gemini.js
+++ components/date-picker/date-picker.gemini.js
@@ -6,7 +6,9 @@ gemini.suite('Date Picker', () => {
       setUrl('/example-date-picker-single-date').
       setCaptureElements('#date-picker', '.ring-popup').
       capture('datePickerPopup', (actions, find) => {
+        const button = find('button');
         actions.click(find('button'));
+        actions.mouseMove(find('body'), [0, 0]);
       });
   });
 
@@ -15,7 +17,9 @@ gemini.suite('Date Picker', () => {
       setUrl('/example-date-picker-range').
       setCaptureElements('#date-picker', '.ring-popup').
       capture('datePickerPopup', (actions, find) => {
+        const button = find('button');
         actions.click(find('button'));
+        actions.mouseMove(find('body'), [0, 0]);
       });
   });
 
