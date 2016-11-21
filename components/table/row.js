f3d5fac5b917777566e40cfbcee7fbd0c4cb2a7f
Max Maximov <max.maximov@gmail.com>
2016-11-21 03:26:42 +0300

    ring-table: hover
    Former-commit-id: 964f6ffeb2f2a456a2481a5e9f5355a73c0189d9

--- components/table/row.js
+++ components/table/row.js
@@ -1,8 +1,11 @@
 import 'core-js/modules/es6.number.is-finite';
 import React, {PropTypes} from 'react';
 import RingComponent from '../ring-component/ring-component';
+
 import DefaultRenderer from './renderer-default';
 import NumberRenderer from './renderer-number';
+
+import style from './table.css';
 
 export default class Row extends RingComponent {
   static propTypes = {
@@ -12,8 +15,9 @@ export default class Row extends RingComponent {
 
   render() {
     const {item, columns} = this.props;
+
     return (
-      <tr>{
+      <tr className={style.row}>{
         columns.map((column, key) => {
           const getValue = column.getValue || (() => item[column.id]);
           const value = getValue(item, column);
