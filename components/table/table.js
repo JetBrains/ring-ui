e090b8895199ef935aa50beee6f74ac21f0d6a05
Max Maximov <max.maximov@gmail.com>
2016-11-24 05:34:29 +0300

    ring-table: shortcuts
    Former-commit-id: 92c8ecafd5cb6e460c4791b521e6c6a20cebf033

--- components/table/table.js
+++ components/table/table.js
@@ -12,7 +12,7 @@
 import 'core-js/modules/es6.array.find';
 
 import React, {PropTypes} from 'react';
-import RingComponent from '../ring-component/ring-component';
+import RingComponentWithShortcuts from '../ring-component/ring-component_with-shortcuts';
 import classNames from 'classnames';
 
 import HeaderCell from './header-cell';
@@ -21,7 +21,7 @@ import style from './table.css';
 
 import LoaderInline from '../loader-inline/loader-inline';
 
-export default class Table extends RingComponent {
+export default class Table extends RingComponentWithShortcuts {
   static propTypes = {
     data: PropTypes.array.isRequired,
     columns: PropTypes.array.isRequired,
@@ -39,34 +39,23 @@ export default class Table extends RingComponent {
   state = {
     focusedRow: undefined,
     selectedRows: new Set(),
+    shortcuts: true,
     userSelectNone: false
   }
 
-  onRowFocus = row => {
-    this.setState({focusedRow: row}, () => {
-      const {selectedRows, focusedRow} = this.state;
-      if (!selectedRows.size) {
-        this.props.onSelect({selection: new Set([focusedRow])});
-      }
-    });
-  }
-
-  onRowSelect = row => {
-    this.setState(({selectedRows}) => {
-      if (selectedRows.has(row)) {
-        selectedRows.delete(row);
-      } else {
-        selectedRows.add(row);
-      }
-      return {selectedRows};
-    }, () => {
-      const {selectedRows, focusedRow} = this.state;
-      if (selectedRows.size) {
-        this.props.onSelect({selection: selectedRows});
-      } else if (focusedRow) {
-        this.props.onSelect({selection: new Set([focusedRow])});
-      }
-    });
+  getShortcutsProps() {
+    return {
+      map: {
+        up: this.onUpPress,
+        down: this.onDownPress,
+        shift: this.onShiftPress,
+        'shift+up': this.onShiftUpPress,
+        'shift+down': this.onShiftDownPress,
+        space: this.onSpacePress,
+        esc: this.onEscPress
+      },
+      scope: ::this.constructor.getUID('ring-table-')
+    };
   }
 
   onMouseDown = e => {
@@ -77,6 +66,108 @@ export default class Table extends RingComponent {
 
   onMouseUp = () => {
     this.setState({userSelectNone: false});
+  }
+
+  onRowFocus = row => {
+    this.setState({focusedRow: row});
+  }
+
+  onRowSelect = row => {
+    const selectedRows = new Set(this.state.selectedRows);
+    if (selectedRows.has(row)) {
+      selectedRows.delete(row);
+    } else {
+      selectedRows.add(row);
+    }
+    this.setState({selectedRows});
+  }
+
+  getPrevRow = () => {
+    const {state: {focusedRow}, props: {data}} = this;
+    const i = data.indexOf(focusedRow) - 1;
+    if (i > -1) {
+      return data[i];
+    } else {
+      return data[data.length - 1];
+    }
+  }
+
+  getNextRow = () => {
+    const {state: {focusedRow}, props: {data}} = this;
+    const i = data.indexOf(focusedRow) + 1;
+    if (i < data.length) {
+      return data[i];
+    } else {
+      return data[0];
+    }
+  }
+
+  shiftSelectRow = () => {
+    const {focusedRow} = this.state;
+    const selectedRows = new Set(this.state.selectedRows);
+
+    if (!this.shiftSelectionMode) {
+      if (selectedRows.has(focusedRow)) {
+        this.shiftSelectionMode = 'deleting';
+      } else {
+        this.shiftSelectionMode = 'adding';
+      }
+    }
+
+    if (this.shiftSelectionMode === 'deleting') {
+      selectedRows.delete(focusedRow);
+    } else if (this.shiftSelectionMode === 'adding') {
+      selectedRows.add(focusedRow);
+    }
+
+    return selectedRows;
+  }
+
+  onUpPress = () => {
+    this.setState({focusedRow: this.getPrevRow()});
+  }
+
+  onDownPress = () => {
+    this.setState({focusedRow: this.getNextRow()});
+  }
+
+  onShiftPress = () => {
+    Reflect.deleteProperty(this, 'shiftSelectionMode');
+  }
+
+  onShiftUpPress = () => {
+    const focusedRow = this.getPrevRow();
+    const selectedRows = this.shiftSelectRow();
+    this.setState({focusedRow, selectedRows});
+  }
+
+  onShiftDownPress = () => {
+    const focusedRow = this.getNextRow();
+    const selectedRows = this.shiftSelectRow();
+    this.setState({focusedRow, selectedRows});
+  }
+
+  onSpacePress = () => {
+    const {focusedRow} = this.state;
+    if (focusedRow) {
+      this.onRowSelect(focusedRow);
+    }
+  }
+
+  onEscPress = () => {
+    this.setState({focusedRow: undefined, selectedRows: new Set()});
+  }
+
+  didUpdate(prevProps, prevState) {
+    const {selectedRows, focusedRow} = this.state;
+    if (selectedRows.size) {
+      const union = new Set([...selectedRows, ...prevState.selectedRows]);
+      if (selectedRows.size !== union.size || prevState.selectedRows.size !== union.size) {
+        this.props.onSelect({selection: new Set(selectedRows)});
+      }
+    } else if (focusedRow && focusedRow !== prevState.focusedRow) {
+      this.props.onSelect({selection: new Set([focusedRow])});
+    }
   }
 
   didMount() {
