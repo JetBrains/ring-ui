bacf8b8585d5ef84d6ee2ddd34cec99ca8ce53a3
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-11-30 20:58:32 +0300

    RG-1157 improve island API using reexport
    Former-commit-id: 15e3241a08782ef365743644b46c878674213349

--- components/island/header.js
+++ components/island/header.js
@@ -3,33 +3,24 @@ import classNames from 'classnames';
 import RingComponent from '../ring-component/ring-component';
 import './island.scss';
 
-/**
- * @name Island
- * @name Island
- * @category Components
- * @description Displays an island.
- * @example-file ./island.examples.html
- */
-
-export default class Island extends RingComponent {
+export default class Header extends RingComponent {
   static propTypes = {
     className: PropTypes.string
   };
 
   render() {
     const {children, className, ...restProps} = this.props;
-    const classes = classNames('ring-island', className);
+    const classes = classNames('ring-island__header', className);
 
     return (
       <div
         {...restProps}
         className={classes}
       >
-        {children}
+        <div className="ring-island__title">
+          {children}
+        </div>
       </div>
     );
   }
 }
-
-export {default as Header} from './header';
-export {default as Content} from './content';
