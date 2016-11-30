769659eca17ba1802a5936719f19d6bf7dd38c35
hypnos <Filipp.Riabchun@jetbrains.com>
2016-11-30 23:07:20 +0300

    RG-1159 UI Language: link
    Former-commit-id: b197d207f9d05c87b35c059a4d9343b31357ef7a

--- components/link/link.js
+++ components/link/link.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import classNames from 'classnames';
 import RingComponent from '../ring-component/ring-component';
-import './link.scss';
-
+import styles from './link.css';
+console.log(styles);
 /**
  * @name Link
  * @category Components
@@ -11,6 +11,32 @@ import './link.scss';
  * @extends {ReactComponent}
  * @example
    <example name="Link">
+     <file type="scss">
+       .links > a {
+         margin: 8px;
+       }
+     </file>
+
+     <file name="index.html">
+       <div id="links"></div>
+     </file>
+
+     <file name="index.js" webpack="true">
+       import {render} from 'react-dom';
+       import React from 'react';
+       import Link from 'ring-ui/components/link/link';
+
+       const links = (
+         <div className="links">
+           <Link href="#hash">Link text</Link>
+           <Link href="#hash" focus={true}>Focused link</Link>
+         </div>
+       );
+
+       render(links, document.getElementById('links'));
+     </file>
+   </example>
+   <example name="Link focus">
      <file name="index.html">
        <div id="link"></div>
      </file>
@@ -20,18 +46,28 @@ import './link.scss';
        var Link = require('ring-ui/components/link/link');
 
        render(
-         Link.factory({href: "#hash"}, 'Link text'),
+         Link.factory({
+           href: "#hash",
+           focus: true
+         }, 'Link text'),
          document.getElementById('link')
        );
      </file>
    </example>
  */
 export default class Link extends RingComponent {
+  static propTypes = {
+    focus: PropTypes.bool
+  }
+
   render() {
-    const classes = classNames('ring-link', this.props.className);
+    const {focus, ...props} = this.props;
+    const classes = classNames(styles.link, this.props.className, {
+      [styles.focus]: focus
+    });
     return (
       <a
-        {...this.props}
+        {...props}
         className={classes}
       >{this.props.children}</a>
     );
