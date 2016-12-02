db211bdee852f6161d41649c0df239aef5683e39
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-12-02 21:41:01 +0300

    RG-1161 refine alerts API
    Former-commit-id: b25932753ad3d363302d701bdbed1ee7750a9c5e

---
+++ components/alert-service/alert-service.test.js
@@ -0,0 +1,77 @@
+import Alert from '../alert/alert';
+import {showError, showMessage, showWarning, remove, removeWithoutAnimation, getShowingAlerts} from './alert-service';
+
+describe('Alert Service', () => {
+  let alertKey;
+  let clock;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clock.restore();
+    removeWithoutAnimation(alertKey);
+  });
+
+  it('Should show alert', () => {
+    alertKey = showMessage('foo');
+    const alert = getShowingAlerts()[0];
+
+    alert.message.should.equal('foo');
+    alert.type.should.equal(Alert.Type.MESSAGE);
+    alert.timeout.should.equal(0);
+    alert.isClosing.should.equal(false);
+    alert.count.should.equal(1);
+  });
+
+  it('Should show message', () => {
+    alertKey = showMessage('foo');
+    getShowingAlerts()[0].type.should.equal(Alert.Type.MESSAGE);
+  });
+
+  it('Should show error', () => {
+    alertKey = showError('foo');
+    getShowingAlerts()[0].type.should.equal(Alert.Type.ERROR);
+  });
+
+  it('Should show warning', () => {
+    alertKey = showWarning('foo');
+    getShowingAlerts()[0].type.should.equal(Alert.Type.WARNING);
+  });
+
+  it('Should join same alerts', () => {
+    alertKey = showMessage('foo');
+    alertKey = showMessage('foo');
+
+    getShowingAlerts().length.should.equal(1);
+    getShowingAlerts()[0].count.should.equal(2);
+  });
+
+  it('Should remove alert after timeout', () => {
+    alertKey = showMessage('foo', 100);
+    clock.tick(1000);
+    getShowingAlerts().length.should.equal(0);
+  });
+
+  it('Should remove alert after timeout', () => {
+    alertKey = showMessage('foo', 100);
+    clock.tick(1000);
+    getShowingAlerts().length.should.equal(0);
+  });
+
+  it('should remove alert without animation', () => {
+    alertKey = showMessage('foo');
+    removeWithoutAnimation(alertKey);
+    getShowingAlerts().length.should.equal(0);
+  });
+
+  it('should remove alert after animation', () => {
+    alertKey = showMessage('foo');
+    remove(alertKey);
+    getShowingAlerts().filter(it => it.key === alertKey)[0].isClosing.should.be.true;
+    clock.tick(1000);
+
+    getShowingAlerts().length.should.equal(0);
+  });
+});
