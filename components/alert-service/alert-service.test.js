fad387a1663453fe6c79ddb9639533f4b28654a9
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-12-07 15:10:36 +0300

    RG-1161 use object-with-methods api instead of particular functions export
    Former-commit-id: 72e51a7f44709a756e5dcfc60b4ba9aceb1e2060

--- components/alert-service/alert-service.test.js
+++ components/alert-service/alert-service.test.js
@@ -1,13 +1,5 @@
 import Alert from '../alert/alert';
-import {
-  showError,
-  showMessage,
-  showWarning,
-  remove,
-  removeWithoutAnimation,
-  getShowingAlerts,
-  setDefaultTimeout
-} from './alert-service';
+import alert from './alert-service';
 
 describe('Alert Service', () => {
   let alertKey;
@@ -19,75 +11,75 @@ describe('Alert Service', () => {
 
   afterEach(() => {
     clock.restore();
-    setDefaultTimeout(0);
-    removeWithoutAnimation(alertKey);
+    alert.setDefaultTimeout(0);
+    alert.removeWithoutAnimation(alertKey);
   });
 
   it('Should show alert', () => {
-    alertKey = showMessage('foo');
-    const alert = getShowingAlerts()[0];
+    alertKey = alert.message('foo');
+    const alertItem = alert._getShowingAlerts()[0];
 
-    alert.message.should.equal('foo');
-    alert.type.should.equal(Alert.Type.MESSAGE);
-    alert.timeout.should.equal(0);
-    alert.isClosing.should.equal(false);
-    alert.count.should.equal(1);
+    alertItem.message.should.equal('foo');
+    alertItem.type.should.equal(Alert.Type.MESSAGE);
+    alertItem.timeout.should.equal(0);
+    alertItem.isClosing.should.equal(false);
+    alertItem.count.should.equal(1);
   });
 
   it('Should show message', () => {
-    alertKey = showMessage('foo');
-    getShowingAlerts()[0].type.should.equal(Alert.Type.MESSAGE);
+    alertKey = alert.message('foo');
+    alert._getShowingAlerts()[0].type.should.equal(Alert.Type.MESSAGE);
   });
 
   it('Should show error', () => {
-    alertKey = showError('foo');
-    getShowingAlerts()[0].type.should.equal(Alert.Type.ERROR);
+    alertKey = alert.error('foo');
+    alert._getShowingAlerts()[0].type.should.equal(Alert.Type.ERROR);
   });
 
   it('Should show warning', () => {
-    alertKey = showWarning('foo');
-    getShowingAlerts()[0].type.should.equal(Alert.Type.WARNING);
+    alertKey = alert.warning('foo');
+    alert._getShowingAlerts()[0].type.should.equal(Alert.Type.WARNING);
   });
 
   it('Should join same alerts', () => {
-    alertKey = showMessage('foo');
-    alertKey = showMessage('foo');
+    alertKey = alert.message('foo');
+    alertKey = alert.message('foo');
 
-    getShowingAlerts().length.should.equal(1);
-    getShowingAlerts()[0].count.should.equal(2);
+    alert._getShowingAlerts().length.should.equal(1);
+    alert._getShowingAlerts()[0].count.should.equal(2);
   });
 
   it('Should remove alert after timeout', () => {
-    alertKey = showMessage('foo', 100);
+    alertKey = alert.message('foo', 100);
     clock.tick(1000);
-    getShowingAlerts().length.should.equal(0);
+    alert._getShowingAlerts().length.should.equal(0);
   });
 
   it('Should remove alert after timeout', () => {
-    alertKey = showMessage('foo', 100);
+    alertKey = alert.message('foo', 100);
     clock.tick(1000);
-    getShowingAlerts().length.should.equal(0);
+    alert._getShowingAlerts().length.should.equal(0);
   });
 
   it('should remove alert without animation', () => {
-    alertKey = showMessage('foo');
-    removeWithoutAnimation(alertKey);
-    getShowingAlerts().length.should.equal(0);
+    alertKey = alert.message('foo');
+    alert.removeWithoutAnimation(alertKey);
+    alert._getShowingAlerts().length.should.equal(0);
   });
 
   it('should remove alert after animation', () => {
-    alertKey = showMessage('foo');
-    remove(alertKey);
-    getShowingAlerts().filter(it => it.key === alertKey)[0].isClosing.should.be.true;
+    alertKey = alert.message('foo');
+    alert.remove(alertKey);
+    alert._getShowingAlerts().filter(it => it.key === alertKey)[0].isClosing.should.be.true;
     clock.tick(1000);
 
-    getShowingAlerts().length.should.equal(0);
+    alert._getShowingAlerts().length.should.equal(0);
   });
 
   it('should allow configuting default timeout', () => {
-    setDefaultTimeout(1000);
-    alertKey = showMessage('foo');
+    alert.setDefaultTimeout(1000);
+    alertKey = alert.message('foo');
     clock.tick(2000);
-    getShowingAlerts().length.should.equal(0);
+    alert._getShowingAlerts().length.should.equal(0);
   });
 });
