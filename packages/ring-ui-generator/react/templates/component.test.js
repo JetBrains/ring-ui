6ad62d1ce8f52581297d3d4fe077b13ed2e09d60
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-12-19 18:33:47 +0300

    ring-ui:react improvements: * Remove RingComponent dependency * Use component instead of function in demo
    Former-commit-id: 300cfee993fdac6ff36adc7133a333d2472f1558

--- packages/ring-ui-generator/react/templates/component.test.js
+++ packages/ring-ui-generator/react/templates/component.test.js
@@ -1,21 +1,23 @@
 import 'dom4';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
 import {renderIntoDocument, isCompositeComponentWithType} from 'react-addons-test-utils';
 
 import <%= pascalCaseName %> from './<%= paramCaseName %>';
 
 describe('<%= titleCaseName %>', () => {
-  const renderComponent = params => renderIntoDocument(<%= pascalCaseName %>.factory(params));
+  const renderComponent = props => renderIntoDocument(<<%= pascalCaseName %> {...props} />);
 
   it('should create component', () => {
     isCompositeComponentWithType(renderComponent(), <%= pascalCaseName %>).should.be.true;
   });
 
   it('should wrap children with div', () => {
-    renderComponent().node.should.match('div');
+    findDOMNode(renderComponent()).should.match('div');
   });
 
   it('should use passed className', () => {
-    renderComponent({className: 'test-class'}).node.should.match('.test-class');
+    findDOMNode(renderComponent({className: 'test-class'})).should.match('.test-class');
   });
 
   // TODO Add more tests
