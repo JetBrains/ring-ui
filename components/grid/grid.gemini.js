ffa58f837bd5d648886ef06799b2a634b6a5faee
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2016-12-20 12:13:07 +0300

    RG-1124 add simple xs-size gemini example
    Former-commit-id: d6cfc57ff862b5a5540c8b2dd33c996ea2648ad3

--- components/grid/grid.gemini.js
+++ components/grid/grid.gemini.js
@@ -10,10 +10,16 @@ const middle = {
   height: 500
 };
 
+const small = {
+  width: 500,
+  height: 300
+};
+
 gemini.suite('Grid', suite => {
   suite.
     setUrl('/example-simple-grid/').
     setCaptureElements('#grid-simple').
+    capture('grid-simple-small', actions => actions.setWindowSize(small.width, small.height)).
     capture('grid-simple-middle', actions => actions.setWindowSize(middle.width, middle.height)).
     capture('grid-simple-large', actions => actions.setWindowSize(large.width, large.height));
 });
