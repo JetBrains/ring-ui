7f4993881f8494577a289de45127ede4ac56899d
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2016-12-22 16:30:33 +0300

    proxyAttrs & CheckboxNg: replace angular component with pure js component and use it in checkboxNg
    Former-commit-id: 86281ac5d59643d80d8708a2429cccf0b1188739

--- components/proxy-attrs/proxy-attrs.js
+++ components/proxy-attrs/proxy-attrs.js
@@ -1,14 +1,9 @@
-/* global angular: false */
-const angularModule = angular.module('Ring.proxy-attrs', []);
-
 function proxyAttrs(template, attrsWhitelist) {
   return function copyAtrrs(iElement, iAttrs) {
     const PREFIX = 'data-proxy-';
 
     const attrsList = attrsWhitelist || Object.keys(iAttrs.$attr);
     let resultTemplate = template;
-
-    const hasProxyValues = /%([^\s]+?)%/.test(resultTemplate);
 
     attrsList.forEach(attrName => {
       if (iAttrs[attrName] !== undefined) {
@@ -19,22 +14,11 @@ function proxyAttrs(template, attrsWhitelist) {
         const attrReplace = `${attrLower}="${attrValue}"`;
 
         resultTemplate = resultTemplate.replace(RegExp(`${attrFind}(="")*`, 'g'), attrReplace);
-
-        if (hasProxyValues) {
-          resultTemplate = resultTemplate.replace(new RegExp(`%${attrLower.replace('-', '\\-')}%`, 'g'), attrValue);
-        }
       }
     });
-
-    if (hasProxyValues) {
-      resultTemplate = resultTemplate.replace(/([^\s]+?)="%([^\s]+?)%"/g, '');
-      resultTemplate = resultTemplate.replace(/%([^\s]+?)%/g, '');
-    }
 
     return resultTemplate;
   };
 }
 
-angularModule.value('proxyAttrs', proxyAttrs);
-
-export default angularModule.name;
+export default proxyAttrs;
