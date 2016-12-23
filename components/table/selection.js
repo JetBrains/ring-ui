1a8942f378dfec231ebe76d1ba00bb452515f666
Max Maximov <max.maximov@gmail.com>
2016-12-23 04:49:37 +0300

    ring-table: the immutable Selection
    Former-commit-id: 57af5c37c398b17ef6d30b2d7475a9c8701946fb

--- components/table/selection.js
+++ components/table/selection.js
@@ -1,21 +1,69 @@
 export default class Selection {
-  selected = new Set()
-  focused = undefined
+  _selected = new Set()
+  _focused = undefined
 
   constructor({selected, focused} = {}) {
     if (selected) {
-      this.selected = new Set(selected);
+      this._selected = new Set(selected);
     }
+
     if (focused) {
-      this.focused = focused;
+      this._focused = focused;
     }
   }
 
+  getFocus() {
+    return this._focused;
+  }
+
+  setFocus(value) {
+    return new this.constructor({selected: this._selected, focused: value});
+  }
+
+  get size() {
+    return this._selected.size;
+  }
+
+  has(value) {
+    return this._selected.has(value);
+  }
+
+  add(value) {
+    const focused = this._focused;
+    const selected = new Set(this._selected);
+
+    selected.add(value);
+
+    return new this.constructor({selected, focused});
+  }
+
+  delete(value) {
+    const focused = this._focused;
+    const selected = new Set(this._selected);
+
+    selected.delete(value);
+
+    return new this.constructor({selected, focused});
+  }
+
+  toggle(value) {
+    const focused = this._focused;
+    const selected = new Set(this._selected);
+
+    if (selected.has(value)) {
+      selected.delete(value);
+    } else {
+      selected.add(value);
+    }
+
+    return new this.constructor({selected, focused});
+  }
+
   getActive() {
-    if (this.selected.size) {
-      return new Set(this.selected);
-    } else if (this.focused) {
-      return new Set([this.focused]);
+    if (this._selected.size) {
+      return new Set(this._selected);
+    } else if (this._focused) {
+      return new Set([this._focused]);
     } else {
       return new Set();
     }
