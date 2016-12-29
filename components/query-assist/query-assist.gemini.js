169498a2bf2f4f68f4ba9c8a651c4c609fc7200d
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2016-12-29 17:00:34 +0300

    Revert "Revert "RG-991 Gemini suite for every visual component with example: hide caret in query assist""
    This reverts commit 5601fb27ae0c670292d6d614dc604fa82e8f1f5e.
    
    
    Former-commit-id: f7d5777dd3d2b68f542a061ca44e693318e72a60

--- components/query-assist/query-assist.gemini.js
+++ components/query-assist/query-assist.gemini.js
@@ -11,10 +11,18 @@ gemini.suite('Query Assist', suite => {
       setCaptureElements(['#query-assist', '.ring-popup']).
       capture('focused', (actions, find) => {
         actions.click(find('.ring-query-assist__input'));
+        // eslint-disable-next-line prefer-arrow-callback
+        actions.executeJS(function hideCaret() {
+          document.querySelector('[data-test=ring-query-assist-input]').style = 'color: transparent;';
+        });
         actions.waitForElementToShow('.ring-popup');
       }).
       capture('input', (actions, find) => {
-        actions.sendKeys(find('.ring-query-assist__input'), 'test.');
+        actions.sendKeys(find('.ring-query-assist__input'), 'test ');
+        // eslint-disable-next-line prefer-arrow-callback
+        actions.executeJS(function hideCaret() {
+          document.querySelector('[data-test=ring-query-assist-last-letter]').style = 'color: transparent;';
+        });
         actions.waitForElementToShow('.ring-popup');
       });
   });
