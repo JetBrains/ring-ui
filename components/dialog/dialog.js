5a4f6df16a50e98993f6a422d296859cb5fc8cd7
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-10 23:59:01 +0300

    RG-1179 Use react-portal in popup and deprecate renderPopup
    Former-commit-id: 6cd0fcb1b82be6f7c3b5911fe7822495a5dd2795

--- components/dialog/dialog.js
+++ components/dialog/dialog.js
@@ -66,7 +66,7 @@ export default class Dialog extends RingComponent {
 
     return (
       <Portal
-        isOpened={show}
+        isOpen={show}
         onOpen={() => ScrollPreventer.prevent()}
         onClose={() => ScrollPreventer.reset()}
       >
--- components/popup/popup.gemini.js
+++ components/popup/popup.gemini.js
@@ -4,14 +4,14 @@ gemini.suite('Popup', () => {
   gemini.suite('Popups', child => {
     child.
       setUrl('/popup/popup.html').
-      setCaptureElements(['#top__left', '#top__right', '#bottom__left', '#bottom__right']).
+      setCaptureElements('.topLeft', '.topRight', '.bottomLeft', '.bottomRight').
       capture('popup-different-directions');
   });
 
   gemini.suite('Autoposition', child => {
     child.
       setUrl('/popup/auto-positioning-a-popup.html').
-      setCaptureElements(['#leftSide', '#rightSide', '#downSide', '#topSide']).
+      setCaptureElements('.left', '.right', '.bottom', '.top').
       capture('popup-autoposition');
   });
 });
