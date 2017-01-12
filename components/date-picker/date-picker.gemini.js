bfe268bdc4e1186cea3fbe709d7e3a07a0c3a4f6
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-12 15:27:44 +0300

    use data-test attribute for popup
    Former-commit-id: b97085ec70f37d32aba5103ce98b91d516451bb0

--- components/date-picker/date-picker.gemini.js
+++ components/date-picker/date-picker.gemini.js
@@ -6,7 +6,7 @@ gemini.suite('Date Picker', () => {
   gemini.suite('Single date', child => {
     child.
       setUrl('/date-picker/date-picker-single-date.html').
-      setCaptureElements('#date-picker', '.test-popup').
+      setCaptureElements('#date-picker', '[data-test=ring-popup]').
       capture('datePickerPopup', (actions, find) => {
         actions.click(find('button'));
         actions.mouseMove(find('body'), [0, 0]);
@@ -17,7 +17,7 @@ gemini.suite('Date Picker', () => {
   gemini.suite('Range', child => {
     child.
       setUrl('/date-picker/date-picker-range.html').
-      setCaptureElements('#date-picker', '.test-popup').
+      setCaptureElements('#date-picker', '[data-test=ring-popup]').
       capture('datePickerPopup', (actions, find) => {
         actions.click(find('button'));
         actions.mouseMove(find('body'), [0, 0]);
@@ -28,7 +28,7 @@ gemini.suite('Date Picker', () => {
   gemini.suite('Clearable', child => {
     child.
       setUrl('/date-picker/date-picker-clearable.html').
-      setCaptureElements('#date-picker', '.test-popup').
+      setCaptureElements('#date-picker', '[data-test=ring-popup]').
       capture('datePickerPopup', (actions, find) => {
         actions.click(find('button'));
         actions.mouseMove(find('body'), [0, 0]);
