a603ddedce492f4eb4f9e36b99a629d4807496f4
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-17 20:54:25 +0300

    RG-1198 Dropdown component: gemini
    Former-commit-id: b1beefcffa62161ef96cf550aada64d86bb897ec

--- components/dropdown/dropdown.gemini.js
+++ components/dropdown/dropdown.gemini.js
@@ -1,19 +1,21 @@
 /* global gemini: false */
 
-gemini.suite('Dropdown', suite => {
-  suite.
-    setUrl('/dropdown/dropdown.html').
-    setCaptureElements('#dropdown', '[data-test=ring-popup]').
-    capture('dropdown', (actions, find) => {
-      actions.click(find('[data-test=ring-dropdown]'));
-    });
-});
+gemini.suite('Dropdown', () => {
+  gemini.suite('Simple', suite => {
+    suite.
+      setUrl('/dropdown/dropdown.html').
+      setCaptureElements('#dropdown', '[data-test=ring-popup]').
+      capture('dropdown', (actions, find) => {
+        actions.click(find('[data-test=ring-dropdown]'));
+      });
+  });
 
-gemini.suite('Custom', suite => {
-  suite.
-    setUrl('/dropdown/dropdown-with-custom-anchor-and-popup.html').
-    setCaptureElements('#dropdown', '[data-test=ring-popup]').
-    capture('dropdown', (actions, find) => {
-      actions.click(find('[data-test=ring-dropdown]'));
-    });
+  gemini.suite('Custom', suite => {
+    suite.
+      setUrl('/dropdown/dropdown-with-custom-anchor-and-popup.html').
+      setCaptureElements('#dropdown', '[data-test=ring-popup]').
+      capture('dropdown', (actions, find) => {
+        actions.click(find('[data-test=ring-dropdown]'));
+      });
+  });
 });
