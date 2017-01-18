dff541b4cb31ca788b4f02d1ac2935986f2372b2
hypnos <Filipp.Riabchun@jetbrains.com>
2017-01-18 04:08:01 +0300

    RG-1198 Dropdown component: fix gemini
    Former-commit-id: c2c031cb381dd0409f27bf7986cb22874d4146af

--- components/dropdown/dropdown.gemini.js
+++ components/dropdown/dropdown.gemini.js
@@ -1,5 +1,7 @@
 /* global gemini: false */
 
+const UNHOVER_DELAY = 500;
+
 gemini.suite('Dropdown', () => {
   gemini.suite('Simple', suite => {
     suite.
@@ -7,6 +9,8 @@ gemini.suite('Dropdown', () => {
       setCaptureElements('#dropdown', '[data-test=ring-popup]').
       capture('dropdown', (actions, find) => {
         actions.click(find('[data-test=ring-dropdown]'));
+        actions.mouseMove(find('body'), [0, 0]);
+        actions.wait(UNHOVER_DELAY);
       });
   });
 
@@ -16,6 +20,8 @@ gemini.suite('Dropdown', () => {
       setCaptureElements('#dropdown', '[data-test=ring-popup]').
       capture('dropdown', (actions, find) => {
         actions.click(find('[data-test=ring-dropdown]'));
+        actions.mouseMove(find('body'), [0, 0]);
+        actions.wait(UNHOVER_DELAY);
       });
   });
 });
