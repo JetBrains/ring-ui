237b61d60860a118bb8e41be07c6c655878cf128
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-19 15:03:15 +0300

    RG-1202 correct isparta loader setting up
    Former-commit-id: 2e7eb61a326f319d21ca3546461265f48e28d40e

--- webpack-test-coverage.config.js
+++ webpack-test-coverage.config.js
@@ -3,11 +3,12 @@
 
 const config = require('./webpack-test.config');
 
-config.module.preLoaders = [{
+config.module.loaders.push({
   test: /\.js$/,
   exclude: /\.test\.js$/,
   include: config.componentsPath,
+  enforce: 'pre',
   loader: 'isparta-instrumenter'
-}];
+});
 
 module.exports = config;
