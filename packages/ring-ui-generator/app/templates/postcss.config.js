35220e3a9e4f34cea2525462c8bf7adcccec9dbb
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-01-20 19:20:17 +0300

    Update to webpack 2
    Former-commit-id: 58156a0c11da3fb5c5ef0d51e4f6ec98402ce2bd

---
+++ packages/ring-ui-generator/app/templates/postcss.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable modules/no-cjs */
+
+module.exports = ctx => ({
+  plugins: [
+    require('postcss-modules-values-replace')({
+      fs: ctx.webpack._compiler.inputFileSystem
+    }),
+    require('postcss-cssnext')({
+      features: {
+        calc: {
+          mediaQueries: true
+        }
+      }
+    })
+  ]
+});
