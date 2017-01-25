08341c7304465231b92d5cabd2281a6d3413df08
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-25 19:26:36 +0300

    RG-1163 add unit tests for adaptive island
    Former-commit-id: 3d494eced94cc7682b3c1064ef97c4501bfbe14a

--- components/island/island.test.js
+++ components/island/island.test.js
@@ -2,13 +2,14 @@ import 'dom4';
 import {findDOMNode} from 'react-dom';
 import {renderIntoDocument, isCompositeComponentWithType} from 'react-addons-test-utils';
 import React from 'react';
-import Island, {Content} from './island';
+import TestUtils from 'react-addons-test-utils';
+import Island, {AdaptiveIsland, Content, Header} from './island';
 import styles from './island.css';
 
-describe('Island', () => {
+describe.only('Island', () => {
   const renderComponent = params => renderIntoDocument(<Island {...params}/>);
 
-  it('should create component', () => {
+  it('should create Island component', () => {
     isCompositeComponentWithType(renderComponent(), Island).should.be.true;
   });
 
@@ -25,5 +26,50 @@ describe('Island', () => {
 
     node.should.contain(`.${styles.fadeTop}`);
     node.should.contain(`.${styles.fadeBottom}`);
+  });
+
+  describe('AdaptiveIsland', () => {
+    it('should render AdaptiveIsland', () => {
+      isCompositeComponentWithType(renderIntoDocument(<AdaptiveIsland/>), AdaptiveIsland).should.be.true;
+    });
+
+    it('should change header size if content is scrolled', () => {
+      const instance = renderIntoDocument(<AdaptiveIsland>
+        <Header></Header>
+        <Content></Content>
+      </AdaptiveIsland>);
+
+      const headerNode = findDOMNode(instance).querySelector('[data-test="ring-island-header"]');
+
+      instance.onContentScroll({scrollTop: 8});
+      headerNode.style.minHeight.should.equal('28px');
+    });
+  });
+
+  describe('Header', () => {
+    it('should render header', () => {
+      isCompositeComponentWithType(renderIntoDocument(<Header/>), Header).should.be.true;
+    });
+
+    it('should change header size', () => {
+      const size = 22;
+      const node = findDOMNode(renderIntoDocument(<Header size={size}/>));
+      node.style.minHeight.should.equal(`${size}px`);
+      node.style.lineHeight.should.equal(`${size}px`);
+    });
+
+    it('should change header size', () => {
+      const size = 22;
+      const node = findDOMNode(renderIntoDocument(<Header size={size}/>));
+      node.style.minHeight.should.equal(`${size}px`);
+      node.style.lineHeight.should.equal(`${size}px`);
+    });
+
+    it('should change header title font size', () => {
+      const size = 24;
+      const fontSize = size - 8;
+      const titleNode = findDOMNode(renderIntoDocument(<Header size={size}/>)).querySelector(`.${styles.title}`);
+      titleNode.style.fontSize.should.equal(`${fontSize}px`);
+    });
   });
 });
