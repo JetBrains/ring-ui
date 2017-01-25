fa2b44f588df445884fa189cf1166f0578a2e4b2
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-25 17:56:22 +0300

    RG-1163 initial implementation of adaptive island header
    Former-commit-id: 3ec456bcf4e3038e5fe84457fef94de9b348af93

--- components/island/content.js
+++ components/island/content.js
@@ -1,17 +1,23 @@
-import React, {PropTypes} from 'react';
+import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
-import RingComponent from '../ring-component/ring-component';
 import styles from './island.css';
 
-export default class Content extends RingComponent {
+export default class Content extends Component {
   static propTypes = {
+    children: PropTypes.node,
     className: PropTypes.string,
-    fade: PropTypes.bool
+    fade: PropTypes.bool,
+    onScroll: PropTypes.func
   };
 
   static defaultProps = {
     fade: false
   };
+
+  onScroll = event => {
+    const {scrollTop, scrollHeight} = event.nativeEvent.target;
+    this.props.onScroll({scrollTop, scrollHeight});
+  }
 
   render() {
     const {children, className, fade, ...restProps} = this.props;
@@ -22,6 +28,7 @@ export default class Content extends RingComponent {
         {...restProps}
         data-test="ring-island-content"
         className={classes}
+        onScroll={this.onScroll}
       >
         {fade && <div className={styles.fadeTop} />}
         {children}
