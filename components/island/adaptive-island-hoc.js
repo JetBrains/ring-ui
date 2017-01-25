7890722d9fbd4172dd1ce7b39f6f1489ad3c7c05
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-25 17:56:22 +0300

    RG-1163 initial implementation of adaptive island header
    Former-commit-id: 3ec456bcf4e3038e5fe84457fef94de9b348af93

---
+++ components/island/adaptive-island-hoc.js
@@ -0,0 +1,56 @@
+import React, {Component, Children, cloneElement} from 'react';
+import Header from './header';
+import Content from './content';
+
+const DEFAULT_SIZE = 32;
+const MIN_SIZE = 21;
+const BORDER_APPEAR_SIZE = 25;
+const TITLE_RESIZE_SPEED = 0.5;
+
+/* eslint-disable react/prop-types */
+
+export default function adaptiveIslandHOC(ComposedComponent) {
+
+  return class ResizingIsland extends Component {
+    state = {
+      border: false,
+      size: DEFAULT_SIZE
+    };
+
+    onContentScroll = ({scrollTop}) => {
+      let size = DEFAULT_SIZE - (scrollTop * TITLE_RESIZE_SPEED);
+      size = size < MIN_SIZE ? MIN_SIZE : size;
+      const border = size <= BORDER_APPEAR_SIZE;
+
+      this.setState({size, border});
+    }
+
+    addResizingProps(children) {
+      return Children.map(children, child => {
+        let props;
+        const {size, border} = this.state;
+
+        if (child.type === Content) {
+          props = {onScroll: this.onContentScroll};
+        }
+
+        if (child.type === Header) {
+          props = {size, border};
+        }
+
+        return props ? cloneElement(child, props) : child;
+      });
+    }
+
+    render() {
+      const {children, ...restProps} = this.props;
+
+      return (
+        <ComposedComponent
+          {...restProps}
+          children={this.addResizingProps(children)}
+        />
+      );
+    }
+  };
+}
