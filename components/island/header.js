0fb408a38d32ccab3a3450b64aad021fc670f6d1
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-01-30 12:06:54 +0300

    RG-1163 precise design tuning
    Former-commit-id: 649a27b5d1bb1f6cb46b13e058396db6dfe93670

--- components/island/header.js
+++ components/island/header.js
@@ -3,7 +3,9 @@ import classNames from 'classnames';
 import styles from './island.css';
 
 const SIZE_FONT_DIFF = 16;
-const ADDITIONAL_PADDING_DIFF = 32;
+const ADDITIONAL_PADDING_DIFF = 35;
+export const MAX_SIZE = 40;
+export const MIN_SIZE = 30;
 
 export default class Header extends Component {
   static propTypes = {
@@ -13,6 +15,16 @@ export default class Header extends Component {
     size: PropTypes.number
   };
 
+  getFontSize(size) {
+    const fontDiff = SIZE_FONT_DIFF + (1 - (size - MIN_SIZE) / (MAX_SIZE - MIN_SIZE));
+    return size - fontDiff;
+  }
+
+  getTopPadding(size) {
+    const padding = size - ADDITIONAL_PADDING_DIFF;
+    return padding <= 0 ? 0 : padding;
+  }
+
   render() {
     const {children, className, border, size, ...restProps} = this.props;
     const classes = classNames(styles.header, className, {
@@ -21,10 +33,10 @@ export default class Header extends Component {
 
     const headerStyle = size ? {
       lineHeight: `${size}px`,
-      paddingTop: `${size - ADDITIONAL_PADDING_DIFF}px`
+      paddingTop: this.getTopPadding(size)
     } : null;
 
-    const fontSize = size ? (size - SIZE_FONT_DIFF) : null;
+    const fontSize = size ? this.getFontSize(size) : null;
 
     return (
       <div
