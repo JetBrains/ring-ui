3662d7854b65e742148cd6240e7d72d97bd51ce4
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-02-01 15:30:26 +0300

    Fix app output path
    Former-commit-id: e2b5e2bedb64f260d96a42fbde9d4e016a39e35b

--- packages/ring-ui-generator/app/templates/webpack.config.js
+++ packages/ring-ui-generator/app/templates/webpack.config.js
@@ -1,13 +1,13 @@
 /* eslint-env node */
 /* eslint-disable modules/no-cjs */
 
-const path = require('path');
+const {join, resolve} = require('path');
 const webpack = require('webpack');
 const webpackConfigMerger = require('webpack-config-merger');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const pkgConfig = require('./package.json');
-const componentsPath = [path.join(__dirname, pkgConfig.config.components)];
+const pkgConfig = require('./package.json').config;
+const componentsPath = join(__dirname, pkgConfig.components);
 const ringUiWebpackConfig = require('ring-ui');
 
 // Patch ring-ui svg-sprite-loader config
@@ -18,12 +18,12 @@ const webpackConfig = webpackConfigMerger(ringUiWebpackConfig.config,
     entry: `${componentsPath}/app/app.js`,
     resolve: {
       alias: {
-        react: path.resolve('./node_modules/react'),
-        'react-dom': path.resolve('./node_modules/react-dom')
+        react: resolve('./node_modules/react'),
+        'react-dom': resolve('./node_modules/react-dom')
       }
     },
     output: {
-      path: pkgConfig.dist,
+      path: resolve(__dirname, pkgConfig.dist),
       filename: '[name].js',
       publicPath: '/',
       devtoolModuleFilenameTemplate: '/[absolute-resource-path]'
@@ -36,7 +36,7 @@ const webpackConfig = webpackConfigMerger(ringUiWebpackConfig.config,
           loaders: [
             'style-loader',
             'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:7]',
-            `postcss-loader`
+            'postcss-loader'
           ]
         },
         {
