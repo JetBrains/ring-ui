89f82d57ff275d939fc1e209fc01d488976c5754
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-02-02 15:20:01 +0300

    JPS-5064 better check if controller contains property
    Former-commit-id: c68b073465eb855b2bd6f22c28726c91ccda9cb5

--- components/global/angular-component-factory.js
+++ components/global/angular-component-factory.js
@@ -75,7 +75,7 @@ function angularComponentFactory(Component, name) {
 
       const props = {};
       propKeys.forEach(key => {
-        if (this[key]) {
+        if (this.hasOwnProperty(key)) {
           if (funcTypes.includes(propTypes[key])) {
             props[key] = (...rest) => {
               const ret = this[key](...rest);
