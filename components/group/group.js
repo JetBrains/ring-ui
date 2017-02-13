c6650cd1910630a61d48cb5b97ca119f19f6d0db
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-02-13 17:42:18 +0300

    RG-1238 implement group component
    Former-commit-id: 9e5d741ff5d5507d49108227a52ff035e8b89a7f

---
+++ components/group/group.js
@@ -0,0 +1,63 @@
+import React, {Component, PropTypes} from 'react';
+import classNames from 'classnames';
+
+import styles from './group.css';
+
+/**
+ * @name Group
+ * @category Components
+ * @framework React
+ * @constructor
+ * @description Places inner components with fixed spacing between
+ * @example
+   <example name="group">
+     <file name="index.html">
+       <div id="group"></div>
+     </file>
+
+     <file name="index.js">
+       import React, {Component} from 'react';
+       import {render} from 'react-dom';
+
+       import Link from 'ring-ui/components/link/link';
+       import Badge from 'ring-ui/components/badge/badge';
+       import Group from 'ring-ui/components/group/group';
+
+       const container = document.getElementById('group');
+       class GroupDemo extends Component {
+         render() {
+           return (
+             <Group>
+               <Badge valid>Badge</Badge>
+               <span>Text</span>
+               <Link>Link</Link>
+             </Group>
+           );
+         }
+       }
+
+       render(<GroupDemo />, container);
+     </file>
+   </example>
+ */
+
+export default class Group extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    className: PropTypes.string
+  };
+
+  render() {
+    const {children, className, ...restProps} = this.props;
+    const classes = classNames(styles.group, className);
+
+    return (
+      <div
+        {...restProps}
+        className={classes}
+      >
+        {children}
+      </div>
+    );
+  }
+}
