72c55323439bd9dbb93eef3070598f85996cba23
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-02-21 15:47:49 +0300

    Support webpack2 and remove config merger
    Former-commit-id: 553f7e2045c351cb529ecad3dd76bbe8f8dc529d

--- packages/ring-ui-generator/app/templates/webpack.config.js
+++ packages/ring-ui-generator/app/templates/webpack.config.js
@@ -1,9 +1,8 @@
 /* eslint-env node */
 /* eslint-disable modules/no-cjs */
 
-const {join, resolve} = require('path');
+const { join, resolve } = require('path');
 const webpack = require('webpack');
-const webpackConfigMerger = require('webpack-config-merger');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const pkgConfig = require('./package.json').config;
@@ -13,53 +12,53 @@ const ringUiWebpackConfig = require('ring-ui');
 // Patch ring-ui svg-sprite-loader config
 ringUiWebpackConfig.loaders.svgSpriteLoader.include.push(require('jetbrains-logos'), require('jetbrains-icons'));
 
-const webpackConfig = webpackConfigMerger(ringUiWebpackConfig.config,
-  {
-    entry: `${componentsPath}/app/app.js`,
-    resolve: {
-      alias: {
-        react: resolve('./node_modules/react'),
-        'react-dom': resolve('./node_modules/react-dom')
+const webpackConfig = () => ({
+  entry: `${componentsPath}/app/app.js`,
+  resolve: {
+    alias: {
+      react: resolve('./node_modules/react'),
+      'react-dom': resolve('./node_modules/react-dom')
+    }
+  },
+  output: {
+    path: resolve(__dirname, pkgConfig.dist),
+    filename: '[name].js',
+    publicPath: '/',
+    devtoolModuleFilenameTemplate: '/[absolute-resource-path]'
+  },
+  module: {
+    rules: [
+      ...ringUiWebpackConfig.config.module.rules,
+      {
+        test: /\.css$/,
+        include: componentsPath,
+        loaders: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:7]',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        include: componentsPath,
+        loader: 'babel-loader'
       }
-    },
-    output: {
-      path: resolve(__dirname, pkgConfig.dist),
-      filename: '[name].js',
-      publicPath: '/',
-      devtoolModuleFilenameTemplate: '/[absolute-resource-path]'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.css$/,
-          include: componentsPath,
-          loaders: [
-            'style-loader',
-            'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:7]',
-            'postcss-loader'
-          ]
-        },
-        {
-          test: /\.js$/,
-          include: componentsPath,
-          loader: 'babel-loader'
-        }
-      ]
-    },
-    devServer: {
-      stats: {
-        assets: false,
-        children: false,
-        chunks: false,
-        hash: false,
-        version: false
-      }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'html-loader?interpolate!src/index.html'
-      })
     ]
-  });
+  },
+  devServer: {
+    stats: {
+      assets: false,
+      children: false,
+      chunks: false,
+      hash: false,
+      version: false
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'html-loader?interpolate!src/index.html'
+    })
+  ]
+});
 
 module.exports = webpackConfig;
