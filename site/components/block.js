440c253f247350c1e42fb254150726893229494c
hypnos <Filipp.Riabchun@jetbrains.com>
2017-02-27 20:15:14 +0300

    fix memory issues
    Former-commit-id: 5cae23f862ab6691b3842201a2ae18943ff7e6c6

--- site/components/block.js
+++ site/components/block.js
@@ -3,24 +3,22 @@ import React, {PropTypes} from 'react';
 import Example from './example';
 import Markdown from './markdown';
 
-const Block = ({attrs, examples}) => (
+const Block = ({description, examples}) => (
   <div>
-    {attrs.description &&
-      <Markdown source={attrs.description} />
-    }
+    {description && (
+      <Markdown source={description} />
+    )}
     {examples.map(example =>
       <Example
         {...example}
-        key={example.attrs.name}
+        key={example.name}
       />
     )}
   </div>
 );
 
 Block.propTypes = {
-  attrs: PropTypes.shape({
-    description: PropTypes.string
-  }),
+  description: PropTypes.string,
   examples: PropTypes.arrayOf(PropTypes.shape(Example.propTypes))
 };
 
