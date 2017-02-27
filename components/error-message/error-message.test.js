f9fa0a717b0f98d47796230b8e92c0a9d21d8645
Edgar Klimov <edgar.klimov@jetbrains.com>
2017-02-27 16:18:27 +0300

    initial implementation of error-message React component (WIP)
    Former-commit-id: 8edf29d0a6b696335967f99ebfc4c3ea8f4a7b47

---
+++ components/error-message/error-message.test.js
@@ -0,0 +1,24 @@
+import 'dom4';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import {renderIntoDocument, isCompositeComponentWithType} from 'react-addons-test-utils';
+
+import ErrorMessage from './error-message';
+
+describe('Error Message', () => {
+  const renderComponent = props => renderIntoDocument(<ErrorMessage {...props} />);
+
+  it('should create component', () => {
+    isCompositeComponentWithType(renderComponent(), ErrorMessage).should.be.true;
+  });
+
+  it('should wrap children with div', () => {
+    findDOMNode(renderComponent()).should.match('div');
+  });
+
+  it('should use passed className', () => {
+    findDOMNode(renderComponent({className: 'test-class'})).should.match('.test-class');
+  });
+
+  // TODO Add more tests
+});
