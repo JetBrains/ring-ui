3e6a37cc5b04278cc75402c76601ce3d31e9f9b3
Stas.Kurilov <stas.kurilov@jetbrains.com>
2017-02-28 19:46:19 +0300

    RG-1260 Rewrite showcase page with react
    Former-commit-id: 4eb148fdbe3e1414b028c1e9e74f46e5ab191fcb

--- site/index.js
+++ site/index.js
@@ -1,6 +1,7 @@
 import styles from './index.css';
 import 'github-markdown-css/github-markdown.css';
 import 'file-loader?name=favicon.ico!jetbrains-logos/hub/favicon.ico';
+import 'whatwg-fetch';
 
 import 'dom4';
 import React from 'react';
@@ -12,21 +13,33 @@ import Header from './components/header';
 import Nav from './components/nav';
 import Content from './components/content';
 
-const {nav, content} = window;
+const currentURL = window.location.href;
+const currentPath = currentURL.substr(currentURL.lastIndexOf('/') + 1);
 
-const App = () => (
-  <div className={styles.app}>
-    <Header />
-    <ContentLayout className={styles.main}>
-      <Sidebar>
-        <Nav {...nav} />
-      </Sidebar>
-      <Content {...content} />
-    </ContentLayout>
-  </div>
-);
+fetch('data.json').
+  then(response => response.json()).
+  then(data => {
+    const nav = {
+      categories: data.sourcesByCategory,
+      version: data.version,
+      url: currentPath
+    };
+    const source = data.sources.find(s => s.url === currentPath);
 
-render(
-  <App />,
-  document.query('#app')
-);
+    const App = () => (
+      <div className={styles.app}>
+        <Header />
+        <ContentLayout className={styles.main}>
+          <Sidebar>
+            <Nav {...nav} />
+          </Sidebar>
+          <Content {...source} />
+        </ContentLayout>
+      </div>
+    );
+
+    render(
+      <App />,
+      document.query('#app')
+    );
+  });
