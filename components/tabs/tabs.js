cb7c952213da1c93311492a61d7f4526ba654ed3
hypnos <Filipp.Riabchun@jetbrains.com>
2017-02-28 15:23:34 +0300

    move jsdoc to tabs.js
    Former-commit-id: d7b39770086c46367abbe3eea4e36fb4566f83b9

--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -1,3 +1,13 @@
+/**
+ * @name Tabs
+ * @category Components
+ * @tags 3.0
+ * @framework React
+ * @constructor
+ * @description Displays a tab set
+ * @example-file ./tabs.examples.html
+ */
+
 export {default as Tabs} from './dumb-tabs';
 export {default as SmartTabs} from './smart-tabs';
 export {default as Tab} from './tab';
