b474ee21cf022048eb381bab9cf917bf4aa16d86
hypnos <Filipp.Riabchun@jetbrains.com>
2017-03-03 16:58:30 +0300

    RG-1259 UI Language: header. Gemini
    Former-commit-id: 32a9c86a62da14722e2dfecd9fecf32e6928443c

--- components/header/header.gemini.js
+++ components/header/header.gemini.js
@@ -1,15 +1,17 @@
 /* global gemini: false */
 
-gemini.suite('Header', suite => {
-  suite.
-    setUrl('/header/header.html').
-    setCaptureElements('#header').
-    capture('header');
-});
+gemini.suite('Header', () => {
+  gemini.suite('Header', suite => {
+    suite.
+      setUrl('/header/header.html').
+      setCaptureElements('#header').
+      capture('header');
+  });
 
-gemini.suite('Low header', suite => {
-  suite.
-    setUrl('/header/low-header.html').
-    setCaptureElements('#header').
-    capture('header');
-});
+  gemini.suite('Low header', suite => {
+    suite.
+      setUrl('/header/low-header.html').
+      setCaptureElements('#header').
+      capture('header');
+  });
+})
