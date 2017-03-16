ef23d9473de76dcb27bd965c9466e61f4dda34a7
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-03-16 13:57:40 +0300

    RG-1299 use refs instead of findDOMNode. Review-related fixups
    Former-commit-id: 67f98a793c6c107e023546431d6a54e353072f15

--- components/global/react-dom-renderer.js
+++ components/global/react-dom-renderer.js
@@ -1,5 +1,4 @@
 import React, {PropTypes, Component} from 'react';
-import {findDOMNode} from 'react-dom';
 
 export default class Renderer extends Component {
   static propTypes = {
@@ -8,18 +7,21 @@ export default class Renderer extends Component {
 
   componentDidMount() {
     const {nodes} = this.props;
-    if (!nodes || !nodes.length) {
+    if (!this.node || !nodes || !nodes.length) {
       return;
     }
-    const componentNode = findDOMNode(this);
-
     const fragment = document.createDocumentFragment();
     nodes.forEach(nodeToRender => fragment.appendChild(nodeToRender));
 
-    componentNode.parentNode.replaceChild(fragment, componentNode);
+    this.node.parentNode.replaceChild(fragment, this.node);
   }
 
   render() {
-    return <span/>;
+    return (
+      <span ref={node => {
+        this.node = node;
+      }}
+      />
+    );
   }
 }
