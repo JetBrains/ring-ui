b9ab32759e08a789ceb1fae38a5275434a791a2f
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-03-20 17:24:01 +0300

    RG-1301 use element-resize-detector to detect if island content changes size
    Former-commit-id: 62f96bf4fc10d45602a4e56aa68380c83c1a955a

--- components/island/content.js
+++ components/island/content.js
@@ -2,9 +2,11 @@ import React, {Component, PropTypes} from 'react';
 import classNames from 'classnames';
 import throttle from 'mout/function/throttle';
 import styles from './island.css';
+import createResizeDetector from 'element-resize-detector';
 
 const noop = () => {};
 const FADE_SHOW_TROTTLING = 50;
+const resizeDetector = createResizeDetector();
 
 export default class Content extends Component {
   static propTypes = {
@@ -26,7 +28,17 @@ export default class Content extends Component {
     scrolledToBottom: false
   }
 
-  calculateScrollPosition = throttle(scrollableNode => {
+  componentWillUnmount() {
+    resizeDetector.removeAllListeners(this.wrapperNode);
+  }
+
+  setWrapper = node => {
+    this.wrapperNode = node;
+    resizeDetector.listenTo(node, this.calculateScrollPosition);
+  }
+
+  calculateScrollPosition = throttle(() => {
+    const {scrollableNode} = this;
     if (!scrollableNode) {
       return;
     }
@@ -36,10 +48,15 @@ export default class Content extends Component {
     this.setState({scrolledToTop, scrolledToBottom});
   }, FADE_SHOW_TROTTLING)
 
-  onScroll = event => {
-    const {scrollTop, scrollHeight} = event.nativeEvent.target;
+  onScroll = () => {
+    const {scrollTop, scrollHeight} = this.scrollableNode;
     this.props.onScroll({scrollTop, scrollHeight});
-    this.calculateScrollPosition(event.nativeEvent.target);
+    this.calculateScrollPosition();
+  }
+
+  setScrollableNodeAndCalculatePosition = node => {
+    this.scrollableNode = node;
+    this.calculateScrollPosition();
   }
 
   render() {
@@ -61,10 +78,12 @@ export default class Content extends Component {
       >
         <div
           className={styles.scrollableWrapper}
-          ref={this.calculateScrollPosition}
+          ref={this.setScrollableNodeAndCalculatePosition}
           onScroll={fade ? this.onScroll : noop}
         >
-          {children}
+          <div ref={this.setWrapper}>
+            {children}
+          </div>
         </div>
       </div>
     );
