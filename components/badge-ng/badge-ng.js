e1d2803eff4c4619db2c4c4e91c4681803a6482e
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-03-27 16:34:33 +0300

    RG-1323 use group-ng to space badge example
    Former-commit-id: f804b494f2fe0a8bac531a682d65ace85e369bab

--- components/badge-ng/badge-ng.js
+++ components/badge-ng/badge-ng.js
@@ -7,19 +7,22 @@
   <example name="rgBadge">
     <file type="html">
       <div ng-app="Example.badge" ng-controller="ExampleCtrl as ctrl">
-        <rg-badge>simple</rg-badge>
-        <rg-badge gray="true">gray</rg-badge>
-        <rg-badge valid="true">valid</rg-badge>
-        <rg-badge invalid="true">invalid</rg-badge>
-        <rg-badge disabled="true">disabled</rg-badge>
+        <rg-group>
+          <rg-badge>simple</rg-badge>
+          <rg-badge gray="true">gray</rg-badge>
+          <rg-badge valid="true">valid</rg-badge>
+          <rg-badge invalid="true">invalid</rg-badge>
+          <rg-badge disabled="true">disabled</rg-badge>
+        </rg-group>
       </div>
     </file>
 
     <file type="js">
       import 'angular/angular';
       import BadgeNg from 'ring-ui/components/badge-ng/badge-ng';
+      import GroupNg from 'ring-ui/components/group-ng/group-ng';
 
-      angular.module('Example.badge', [BadgeNg]).
+      angular.module('Example.badge', [BadgeNg, GroupNg]).
         controller('ExampleCtrl', angular.noop);
     </file>
   </example>
