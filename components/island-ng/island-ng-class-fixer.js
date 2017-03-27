7c64fd45742eb721f7071b7ab637a23aca7f3886
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-03-27 12:23:49 +0300

    RG-1321 additionaly check that ngClass exists before touching it
    Former-commit-id: 2804000800ffe3d4f9338be284749656fad785cf

--- components/island-ng/island-ng-class-fixer.js
+++ components/island-ng/island-ng-class-fixer.js
@@ -1,5 +1,7 @@
 // Workaround fo Angular bug with ng-class & replace: true support
 // See https://github.com/angular/angular.js/issues/5695#issuecomment-31898355
 export default function compile(tElement, tAttrs) {
-  tAttrs.ngClass = tAttrs.ngClass.replace(/}\s*{/g, ', ');
+  if (tAttrs.ngClass) {
+    tAttrs.ngClass = tAttrs.ngClass.replace(/}\s*{/g, ', ');
+  }
 }
