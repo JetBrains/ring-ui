75a440f4ce378c89788a6e738a5d3d280ff89c6e
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-03-27 16:27:35 +0300

    RG-1323 implement BadgeNg wrapper
    Former-commit-id: 696a29af9920bbebb0f18141a6d1a54f9b699629

---
+++ components/badge-ng/badge-ng.js
@@ -0,0 +1,30 @@
+/**
+ * @name Badge Ng
+ * @category Angular Components
+ * @framework Angular
+ * @description Provides an Angular wrapper for Badge.
+ * @example
+  <example name="rgIsland">
+    <file type="html">
+      <div ng-app="Example.badge" ng-controller="ExampleCtrl as ctrl">
+        <rg-badge>simple</rg-badge>
+        <rg-badge gray="true">gray</rg-badge>
+        <rg-badge valid="true">valid</rg-badge>
+        <rg-badge invalid="true">invalid</rg-badge>
+        <rg-badge disabled="true">disabled</rg-badge>
+      </div>
+    </file>
+
+    <file type="js">
+      import 'angular/angular';
+      import BadgeNg from 'ring-ui/components/badge-ng/badge-ng';
+
+      angular.module('Example.badge', [BadgeNg]).
+        controller('ExampleCtrl', angular.noop);
+    </file>
+  </example>
+*/
+
+import angularComponentFactory from '../global/angular-component-factory';
+import BadgeNg from '../badge/badge';
+export default angularComponentFactory(BadgeNg, 'Badge').name;
