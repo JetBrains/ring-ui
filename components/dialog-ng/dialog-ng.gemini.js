a689e407e2872a169b4814a86abf97ba110e2dca
Ekaterina.Zaikina <katriyna@gmail.com>
2017-03-29 14:56:54 +0300

    RG-1310 added gemini test
    Former-commit-id: 0db31facc5694285593356dbfbb207e20d2c7334

--- components/dialog-ng/dialog-ng.gemini.js
+++ components/dialog-ng/dialog-ng.gemini.js
@@ -1,8 +1,17 @@
 /* global gemini: false */
 
 gemini.suite('Dialog Ng', suite => {
-  suite.
-    setUrl('/dialog-ng/dialog-ng.html').
-    setCaptureElements(['*[data-test=ring-dialog]', '.ring-error-bubble']).
-    capture('dialog');
+  gemini.suite('dialog', child => {
+    child.
+      setUrl('/dialog-ng/dialog-ng.html').
+      setCaptureElements(['*[data-test=ring-dialog]', '.ring-error-bubble']).
+      capture('dialog');
+  });
+
+  gemini.suite('dialog with custom fooler', child => {
+    child.
+      setUrl('/dialog-ng/dialog-ng-with-overriden-footer.html').
+      setCaptureElements(['*[data-test=ring-dialog]']).
+      capture('dialog with custom footer');
+  });
 });
