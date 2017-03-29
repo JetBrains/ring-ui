fc646f2b5c8902682402e8950a7efd8040d59b31
hypnos <Filipp.Riabchun@jetbrains.com>
2017-03-29 18:01:45 +0300

    fix gemini
    Former-commit-id: 4de9a54ac78f4c23e92e349d0aef7b7ed2b7dcce

--- components/shortcuts-hint-ng/shortcuts-hint-ng.gemini.js
+++ components/shortcuts-hint-ng/shortcuts-hint-ng.gemini.js
@@ -4,6 +4,8 @@ gemini.suite('Shortcuts hint popup', suite => {
   suite.
     skip('firefox').
     setUrl('/shortcuts-hint-ng/shortcuts-ng-hint-popup.html').
-    setCaptureElements('*[data-test="ring-dialog"]').
-    capture('dialog');
+    setCaptureElements('[data-test=ring-dialog]').
+    capture('dialog', actions => {
+      actions.waitForElementToShow('[data-test=ring-dialog]');
+    });
 });
