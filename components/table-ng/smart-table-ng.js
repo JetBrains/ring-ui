4f0337cac7a7a14d52e82fdd18b8bf6c301b3a4e
Max Maximov <max.maximov@gmail.com>
2017-03-30 22:39:57 +0300

    Create the smart table component with selection control #RG-1337 Fixed
    Former-commit-id: cc1ed10fb647cc03461d72bc158c26eabbbc6274

---
+++ components/table-ng/smart-table-ng.js
@@ -0,0 +1,9 @@
+/**
+ * @name SmartTable Ng
+ * @category Angular Components
+ * @framework Angular
+ * @description Provides an Angular wrapper for SmartTable.
+ */
+import angularComponentFactory from '../global/angular-component-factory';
+import SmartTable from '../table/smart-table';
+export default angularComponentFactory(SmartTable, 'SmartTable').name;
