94d805ad8f6ee79dd42aece3fc5f476b2f3662a7
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-03 14:51:27 +0300

    RG-1343 add data-tests to list items
    Former-commit-id: 7d9aaf1ab337073659d414c2aded3e51b9225e7d

--- components/list/list__title.js
+++ components/list/list__title.js
@@ -15,7 +15,10 @@ export default class ListTitle extends PureComponent {
   render() {
     const {description, label} = this.props;
     return (
-      <span className="ring-list__title ring-list__text">
+      <span
+        className="ring-list__title ring-list__text"
+        data-test="ring-list-title"
+      >
         <span
           className="ring-list__item__label ring-list__title-text"
         >{label}</span>
