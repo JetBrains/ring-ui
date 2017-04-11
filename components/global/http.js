1a576966c66a0cc794e6f170458e0630f11cf761
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-11 17:46:30 +0300

    RG-1351 implement HTTP module for network requests
    Former-commit-id: 4337b1b3f090133a1ecb344038f0e7dbd68e62bf

---
+++ components/global/http.js
@@ -0,0 +1,99 @@
+import 'whatwg-fetch';
+
+/**
+ * @name Http
+ * @category Utilities
+ * @description Provides a way to perform authorized network requests
+ * @example-file ./http.examples.html
+ */
+
+const TOKEN_TYPE = 'Bearer';
+const STATUS_OK_IF_MORE_THAN = 200;
+const STATUS_BAD_IF_MORE_THAN = 300;
+
+export const DEFAULT_HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json'
+};
+
+class Http {
+  setAuth(auth) {
+    this.requestToken = () => auth.requestToken();
+    this.shouldRefreshToken = errorType => auth.constructor.shouldRefreshToken(errorType);
+    this.forceTokenUpdate = () => auth.forceTokenUpdate();
+  }
+
+  _fetch(...args) {
+    return fetch(...args);
+  }
+
+  async _authorizedFetch(url, params = {}) {
+    if (!this.requestToken) {
+      throw new Error('RingUI Http: setAuth should have been called before performing authorized requests');
+    }
+    const token = await this.requestToken();
+
+    const body = params.body ? JSON.stringify(params.body) : params.body;
+
+    return this._fetch(url, {
+      headers: {
+        ...DEFAULT_HEADERS,
+        Authorization: `${TOKEN_TYPE} ${token}`
+      },
+      ...params,
+      body
+    });
+  }
+
+  async _checkIfShouldRefreshToken(response) {
+    try {
+      const res = await response.json();
+      return this.shouldRefreshToken(res.data.error);
+    } catch (err) {
+      return false;
+    }
+  }
+
+  _isErrorStatus(status) {
+    return status < STATUS_OK_IF_MORE_THAN || status >= STATUS_BAD_IF_MORE_THAN;
+  }
+
+  async performRequest(url, params) {
+    let response = await this._authorizedFetch(url, params);
+
+    if (this._isErrorStatus(response.status)) {
+      const shouldRefreshToken = await this._checkIfShouldRefreshToken(response);
+      if (shouldRefreshToken) {
+        await this.forceTokenUpdate();
+        response = await this._authorizedFetch(url, params);
+      }
+    }
+
+    if (this._isErrorStatus(response.status)) {
+      try {
+        const resJson = await response.json();
+        throw resJson;
+      } catch (err) {
+        throw response;
+      }
+    }
+
+    return await response.json();
+  }
+
+  get(url, params) {
+    return this.performRequest(url, {
+      method: 'GET',
+      ...params
+    });
+  }
+
+  post(url, params) {
+    return this.performRequest(url, {
+      method: 'POST',
+      ...params
+    });
+  }
+}
+
+export default new Http();
