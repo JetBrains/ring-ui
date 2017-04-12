f4feec62ee90bba4eb564a1c06b6e31c79c41f88
hypnos <Filipp.Riabchun@jetbrains.com>
2017-04-12 18:16:05 +0300

    Move to React 15.5; update packages; bring app generator back to life; adjust indentation in inline examples
    Former-commit-id: 5a77cdf25ee274e76e75d3b2f62a782cf1bef16b

--- packages/ring-ui-generator/app/templates/src/components/app/app-root.js
+++ packages/ring-ui-generator/app/templates/src/components/app/app-root.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import RingComponent from 'ring-ui/components/ring-component/ring-component';
+import React, {Component} from 'react';
 
-import HeaderAuth from 'ring-ui/components/header-auth/header-auth';
+import Link from 'ring-ui/components/link/link';
+import Header, {Logo, Tray, SmartProfile, SmartServices} from 'ring-ui/components/header/header';
 import Auth from 'ring-ui/components/auth/auth';
 import Footer from 'ring-ui/components/footer/footer';
 import jetbrainsLogo from 'jetbrains-logos/jetbrains/jetbrains.svg';
 
 import styles from './app.css';
 
-export default class AppRoot extends RingComponent {
-  willMount() {
-    this.auth = new Auth({
-      serverUri: '***REMOVED***/'
-    });
+export default class AppRoot extends Component {
+  auth = new Auth({
+    serverUri: '***REMOVED***/'
+  });
+
+  didMount() {
     this.auth.init();
   }
 
   render() {
     return (
       <div>
-        <HeaderAuth
-          auth={this.auth}
-          logo={jetbrainsLogo}
-        />
+        <Header>
+          <Link href="/" >
+            <Logo
+              glyph={jetbrainsLogo}
+              size={Logo.Size.Size96}
+            />
+          </Link>
+          <Tray>
+            <SmartServices auth={this.auth}/>
+            <SmartProfile auth={this.auth}/>
+          </Tray>
+        </Header>
         <div className={styles.content}>
           {'App content'}
         </div>
