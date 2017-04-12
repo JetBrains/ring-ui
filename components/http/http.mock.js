da08775107ba78e616af775e99a1ffae0a78f2ce
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-12 17:37:19 +0300

    RG-1355 add mock for Http module
    Former-commit-id: 5f47469bc70019283ebb4b829eec4a619fec7c45

---
+++ components/http/http.mock.js
@@ -0,0 +1,55 @@
+
+import Http from './http';
+
+const authMock = {
+  requestToken: async () => 'mock token',
+  shouldRefreshToken: () => false,
+  forceTokenUpdate: async () => {}
+};
+
+export default class HttpMock extends Http {
+  constructor() {
+    super(authMock);
+
+    this.defaultResponse = null;
+    this.requests = [];
+    this.responsesByUrlMap = new Map();
+  }
+
+  async _fetch(url, params) {
+    this.requests = [...this.requests, {url, params}];
+
+    return {
+      status: 200,
+      json: async () => (this._getResponseForUrl(url) || this.defaultResponse)
+    };
+  }
+
+  respondDefault(response) {
+    this.defaultResponse = response;
+  }
+
+  respondForUrl(url, response) {
+    this.responsesByUrlMap.set(url, response);
+  }
+
+  _getResponseForUrl(urlToMatch) {
+    const urls = this.responsesByUrlMap.keys();
+
+    for (const url of urls) {
+      if (url === urlToMatch) {
+        return this.responsesByUrlMap.get(url);
+      }
+
+      if (url.test && url.test(urlToMatch)) {
+        return this.responsesByUrlMap.get(url);
+      }
+    }
+
+    return null;
+  }
+
+  getRequestsByUrlPart(url) {
+    return this.requests.filter(it => it.url.indexOf(url) !== -1);
+  }
+}
