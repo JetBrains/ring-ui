c2e38c22ff8c8dac851e818e376ac2832f9d9546
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-04-14 16:13:15 +0300

    Use consistent imports for TestUtils
    Former-commit-id: 24dd51bcaef027c4a55a0d4a6081ebad97605495

--- components/loader-inline/loader-inline.test.js
+++ components/loader-inline/loader-inline.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import TestUtils from 'react-dom/test-utils';
+import {isCompositeComponentWithType, renderIntoDocument} from 'react-dom/test-utils';
 import LoaderInline from './loader-inline';
 
 describe('Loader Inline', () => {
   let loader;
 
   beforeEach(() => {
-    loader = TestUtils.renderIntoDocument(React.createElement(LoaderInline));
+    loader = renderIntoDocument(React.createElement(LoaderInline));
   });
 
   it('should create component', () => {
-    TestUtils.isCompositeComponentWithType(loader, LoaderInline).should.be.true;
+    isCompositeComponentWithType(loader, LoaderInline).should.be.true;
   });
 
   it('should add custom class', () => {
