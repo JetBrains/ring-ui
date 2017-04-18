4652b32b49919726e14b4146bb2965b2970dce76
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-18 11:51:16 +0300

    Add AvatarNg component
    Former-commit-id: ac781b2d10ac48a8cf6ca2b2ef2aa1cf584a1f06

---
+++ components/avatar-ng/avatar-ng.js
@@ -0,0 +1,30 @@
+/**
+ * @name Avatar Ng
+ * @category Angular Components
+ * @framework Angular
+ * @description Provides an Angular wrapper for Avatar
+ * @example
+ <example name="AvatarNg">
+   <file name="index.html">
+      <div ng-app="test" ng-controller="testCtrl as $ctrl">
+        <rg-avatar size="$ctrl.AvatarSize.Size32" url="$ctrl.avatarUrl"></rg-avatar>
+      </div>
+   </file>
+
+   <file name="index.js" webpack="true">
+    import angular from 'angular';
+    import hubConfig from 'ring-ui/site/hub-config';
+    import {Size as AvatarSize} from 'ring-ui/components/avatar/avatar';
+    import AvatarNg from 'ring-ui/components/avatar-ng/avatar-ng';
+
+    angular.module('test', [AvatarNg]).controller('testCtrl', function () {
+      this.AvatarSize = AvatarSize;
+      this.avatarUrl = `${hubConfig.serverUri}/api/rest/avatar/default?username=Jet%20Brains`;
+    });
+   </file>
+ </example>
+ */
+import angularComponentFactory from '../global/angular-component-factory';
+import Avatar from '../avatar/avatar';
+
+export default angularComponentFactory(Avatar, 'Avatar').name;
