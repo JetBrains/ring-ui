dbe6acb064935bb0b75694f902c5609e911f5c60
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-04-20 15:44:52 +0300

    RG-1361 extract ie-compatible HOC
    Former-commit-id: 002f66a72541aaaf8d7524382c57d0f50d4fa8ea

--- components/input/input.test.js
+++ components/input/input.test.js
@@ -1,24 +1,26 @@
 import 'dom4';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
 import {isCompositeComponentWithType, renderIntoDocument} from 'react-dom/test-utils';
 
 import Input from './input';
 
 describe('Input', () => {
-  const renderComponent = params => renderIntoDocument(Input.factory(params));
+  const renderComponent = params => renderIntoDocument(<Input {...params} />);
 
   it('should create component', () => {
     isCompositeComponentWithType(renderComponent(), Input).should.be.true;
   });
 
   it('should create input by default', () => {
-    renderComponent().node.should.match('input');
+    findDOMNode(renderComponent()).should.match('input');
   });
 
   it('should create textarea with multiline option', () => {
-    renderComponent({multiline: true}).node.should.match('textarea');
+    findDOMNode(renderComponent({multiline: true})).should.match('textarea');
   });
 
   it('should use passed className', () => {
-    renderComponent({className: 'test-class'}).node.should.have.class('test-class');
+    findDOMNode(renderComponent({className: 'test-class'})).should.have.class('test-class');
   });
 });
