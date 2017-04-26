e042ef6b56ebcf9abb05fe0d99bcac8bafd2f6bd
Max Maximov <max.maximov@gmail.com>
2017-04-26 17:28:12 +0300

    Create the data-list component #RG-1334 WIP
    Former-commit-id: 4badfe35777ed2dc598338a2eba587d007c00cb2

---
+++ components/data-list/data-list.test.js
@@ -0,0 +1,24 @@
+import 'dom4';
+import React from 'react';
+import {findDOMNode} from 'react-dom';
+import {renderIntoDocument, isCompositeComponentWithType} from 'react-dom/test-utils';
+
+import DataList from './data-list';
+
+describe('Data List', () => {
+  const renderComponent = props => renderIntoDocument(<DataList {...props} />);
+
+  it('should create component', () => {
+    isCompositeComponentWithType(renderComponent(), DataList).should.be.true;
+  });
+
+  it('should wrap children with div', () => {
+    findDOMNode(renderComponent()).should.match('div');
+  });
+
+  it('should use passed className', () => {
+    findDOMNode(renderComponent({className: 'test-class'})).should.match('.test-class');
+  });
+
+  // TODO Add more tests
+});
