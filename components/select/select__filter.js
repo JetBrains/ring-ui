aaca856b4e6df9fe3b1e6012ece67933cb22aac3
hypnos <Filipp.Riabchun@jetbrains.com>
2017-04-27 19:58:27 +0300

    RG-1362 Select's input is not focused after dropdown open
    Former-commit-id: 548c5c100be1481b1f7e9eecb990a961ab7dc2ad

---
+++ components/select/select__filter.js
@@ -0,0 +1,52 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Input from '../input/input';
+
+function noop() {}
+
+export default class SelectFilter extends Component {
+  static propTypes = {
+    placeholder: PropTypes.string,
+    className: PropTypes.string,
+    inputRef: PropTypes.func
+  };
+
+  static defaultProps = {
+    placeholder: '',
+    inputRef: noop
+  }
+
+  componentDidMount() {
+    this.focus();
+  }
+
+  componentDidUpdate() {
+    this.focus();
+  }
+
+  focus() {
+    if (this.input) {
+      this.input.focus();
+    }
+  }
+
+  inputRef = el => {
+    this.input = el;
+    this.props.inputRef(el);
+  };
+
+  render() {
+    const {className, ...restProps} = this.props;
+    const classes = classNames('ring-input_filter-popup', className);
+
+    return (
+      <Input
+        {...restProps}
+        inputRef={this.inputRef}
+        className={classes}
+      />
+    );
+  }
+}
