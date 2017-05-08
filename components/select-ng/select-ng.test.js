5e378fa49dc7355bd960e2adf05dbdf1f560d49f
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2017-05-08 13:50:41 +0200

    RG-1381 digest call restored
    Former-commit-id: 00138dc3dbb323c5fd07f2b7f8589dc71c0aba62

--- components/select-ng/select-ng.test.js
+++ components/select-ng/select-ng.test.js
@@ -503,6 +503,7 @@ describe('Select Ng', () => {
     it('should clear last query on close', () => {
       ctrl.query = 'query';
       ctrl.config.onClose();
+      scope.$digest();
 
       expect(ctrl.query).to.be.null;
     });
