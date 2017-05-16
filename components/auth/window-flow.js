63dd8cb4fe68c2c316f9b2773d5867d2082e0c36
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-05-16 17:03:28 +0300

    RG-913 Auth: login in window
    Former-commit-id: e6be092c61c8fe8334f5d425fd696f7977c775b4

---
+++ components/auth/window-flow.js
@@ -0,0 +1,106 @@
+/* eslint-disable camelcase */
+
+import AuthResponseParser from './response-parser';
+
+const NAVBAR_HEIGHT = 50;
+
+export default class WindowFlow {
+  constructor(requestBuilder, storage) {
+    this._requestBuilder = requestBuilder;
+    this._storage = storage;
+    this._reset();
+  }
+
+  /**
+   * Redirects the given iframe to the given URL
+   * @param {HTMLIFrameElement} iframe
+   * @param {string} url
+   * @private
+   */
+  _openWindow(url) {
+    const height = 700;
+    const width = 750;
+    const screenHalves = 2;
+    const top = (window.screen.height - height - NAVBAR_HEIGHT) / screenHalves;
+    const left = (window.screen.width - width) / screenHalves;
+
+    return window.open(
+      url,
+      'HubLoginWindow',
+      `height=${height}, width=${width}, left=${left}, top=${top}`
+    );
+  }
+
+  /**
+   * Refreshes the access token in an iframe.
+   *
+   * @return {Promise.<string>} promise that is resolved to access the token when it is loaded in a background iframe. The
+   * promise is rejected if no token was received after {@link BackgroundToken.BACKGROUND_TIMEOUT} ms.
+   */
+  async _load() {
+    const authRequest = await this._requestBuilder.prepareAuthRequest({nonRedirect: true});
+
+    return new Promise((resolve, reject) => {
+      let cleanRun;
+
+      const cleanUp = () => {
+        if (cleanRun) {
+          return;
+        }
+        cleanRun = true;
+        /* eslint-disable no-use-before-define */
+        removeStateListener();
+        removeTokenListener();
+        /* eslint-enable no-use-before-define */
+
+        this._loginWindow.close();
+      };
+
+      const removeTokenListener = this._storage.onTokenChange(token => {
+        if (token !== null) {
+          cleanUp();
+          resolve(token.access_token);
+        }
+      });
+
+      const removeStateListener = this._storage.onStateChange(authRequest.stateId, state => {
+        if (state && state.error) {
+          cleanUp();
+          reject(new AuthResponseParser.AuthError(state));
+        }
+      });
+
+      if (this._loginWindow === null || this._loginWindow.closed === true) {
+        this._loginWindow = this._openWindow(authRequest.url);
+      } else {
+        this._loginWindow.location = authRequest.url;
+      }
+    });
+  }
+
+  _reset = () => {
+    this._promise = null;
+    this._loginWindow = null;
+  }
+
+  stop() {
+    if (this._loginWindow !== null) {
+      this._loginWindow.close();
+    }
+    this._reset();
+  }
+
+  authorize() {
+    if (this._promise !== null && this._loginWindow !== null && this._loginWindow.closed !== true) {
+      this._loginWindow.focus();
+
+      return this._promise;
+    }
+
+    this._promise = this._load();
+
+    this._promise.then(this._reset, this._reset);
+
+    return this._promise;
+  }
+}
