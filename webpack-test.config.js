49e6657b84a9692d68c5aba4afbc7b96d590a13f
hypnos <Filipp.Riabchun@jetbrains.com>
2017-06-21 22:38:59 +0300

    RING-UI-CR-2033 move externals to webpack-test.config
    Former-commit-id: 696823b3aee56709841d02a4e49978b796412047

--- webpack-test.config.js
+++ webpack-test.config.js
@@ -34,5 +34,12 @@ config.plugins = [
   })
 ];
 
+config.externals = {
+  'react/addons': 'react',
+  'react/lib/ExecutionEnvironment': 'react',
+  'react/lib/ReactContext': 'react',
+  'react-addons-test-utils': 'window'
+};
+
 module.exports = config;
 
