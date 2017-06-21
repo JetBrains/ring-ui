0ee8857cd93231099346ff31ee76e04bbe373976
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2017-06-21 14:48:26 +0200

    RG-1416 test fixed
    Former-commit-id: f6111b9393cf7be6ce6f034bc0b8bcbcc6e0aa81

--- components/tags-list/tags-list.test.js
+++ components/tags-list/tags-list.test.js
@@ -1,61 +1,55 @@
 /* eslint-disable func-names */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import {findDOMNode} from 'react-dom';
 import {renderIntoDocument} from 'react-dom/test-utils';
-
-import RingComponent from '../ring-component/ring-component';
 
 import TagsList from './tags-list';
 
 describe('Tags List', () => {
+  let tagsList;
   const tagsMock = [{key: 1, label: 'test1'}];
+  const renderTagsList = props => renderIntoDocument(React.createElement(TagsList, props));
 
-  beforeEach(function () {
-    this.tagsList = renderIntoDocument(React.createElement(TagsList, {tags: tagsMock}));
+  beforeEach(() => {
+    tagsList = renderTagsList({tags: tagsMock});
   });
 
   describe('DOM', () => {
-    it('should render tags', function () {
-      this.tagsList.node.should.contain('.ring-tag');
+    it('should render tags list', () => {
+      findDOMNode(tagsList).should.match('[data-test="ring-tags-list"]');
     });
 
-    it('should render passed label inside tags', function () {
-      this.tagsList.node.querySelector('.ring-tag').
+    it('should render passed label inside tags', () => {
+      findDOMNode(tagsList).querySelector('.ring-tag').
         textContent.
         should.
         be.
         equal('test1');
     });
 
-    it('should render tag', function () {
-      const renderedTag = this.tagsList.renderTag(tagsMock[0]);
-      const containerEl = document.createElement('div');
-      ReactDOM.render(renderedTag, containerEl);
-
-      containerEl.textContent.should.be.equal(tagsMock[0].label);
-    });
-
-    it('should render custom tag', function () {
-      class CustomTag extends RingComponent {
+    it('should render custom tag', () => {
+      class CustomTag extends Component {
         render() {
-          return (<span className="custom-tag">{this.props.children}</span>);
+          return (<span data-test="custom-tag" className="custom-tag"/>);
         }
       }
 
-      this.tagsList.rerender({
-        tags: [{key: 1, label: 'test3', rgTagIcon: 'bug'}],
+      tagsList = renderTagsList({
+        tags: tagsMock,
         customTagComponent: CustomTag
       });
 
-      this.tagsList.node.should.contain('.custom-tag');
+      findDOMNode(tagsList).querySelector('.custom-tag').should.match('[data-test="custom-tag"]');
     });
 
-    it('Should use passed className', function () {
-      this.tagsList.rerender({
+    it('Should use passed className', () => {
+      tagsList = renderTagsList({
+        tags: tagsMock,
         className: 'test-class'
       });
-      this.tagsList.node.should.have.class('test-class');
+
+      findDOMNode(tagsList).should.match('.test-class');
     });
   });
 });
