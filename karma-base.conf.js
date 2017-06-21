7e7c06ebf9c618d9911f78b494541cdac5e24a4e
hypnos <Filipp.Riabchun@jetbrains.com>
2017-06-21 22:38:59 +0300

    RING-UI-CR-2033 move externals to webpack-test.config
    Former-commit-id: 0035094cdad564efe6a4618d2c9109a542c67ce4

--- karma-base.conf.js
+++ karma-base.conf.js
@@ -40,19 +40,7 @@ module.exports = config => {
       'test-helpers/*.js': ['webpack', 'sourcemap']
     },
 
-    webpack: Object.assign(
-      {},
-      require('./webpack-test.config'),
-      {
-        devtool: 'inline-source-map',
-        externals: {
-          'react/addons': 'react',
-          'react/lib/ExecutionEnvironment': 'react',
-          'react/lib/ReactContext': 'react',
-          'react-addons-test-utils': 'window'
-        }
-      }
-    ),
+    webpack: require('./webpack-test.config'),
 
     webpackServer: {
       stats: {
