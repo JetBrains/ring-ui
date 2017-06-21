a6ba213143e7c417f3acd09b5dc4719ebcc3e1bd
Maksim Ryzhikov <rv.maksim@gmail.com>
2017-06-21 15:03:39 +0300

    chore(components:permissions): allow set permission cache manually without request #JT-41791 relates
    Former-commit-id: fd30a587c704f96979f3ecd41b22d80c1f117bc1

---
+++ components/permissions/permissions__cache.test.js
@@ -0,0 +1,75 @@
+import PermissionCache from './permissions__cache';
+
+describe('PermissionCache', () => {
+  function createProject(id) {
+    return {
+      id
+    };
+  }
+
+  function createPermission(key, projects, isGlobal) {
+    return {
+      permission: {key}, projects, global: isGlobal
+    };
+  }
+
+  it('should create permission cache', () => {
+    const permissions = [createPermission('A')];
+    const permissionCache = new PermissionCache(permissions);
+
+    expect(permissionCache).to.be.ok;
+  });
+
+
+  it('should not throw exception if we do not pass permissions', () => {
+    expect(() => (new PermissionCache())).to.not.throw();
+  });
+
+
+  it('should allow set permissions', () => {
+    const permissions = [createPermission('A')];
+    const permissionCache = new PermissionCache(null);
+
+    permissionCache.set(permissions);
+
+    expect(permissionCache.has('A')).to.be.true;
+  });
+
+
+  it('should allow get permissions', () => {
+    const permissions = [createPermission('A')];
+    const permissionCache = new PermissionCache(permissions);
+
+    expect(permissionCache.get()).to.be.equal(permissions);
+  });
+
+
+  it('should check permission', () => {
+    const permissions = [createPermission('A')];
+    const permissionCache = new PermissionCache(permissions);
+
+    expect(permissionCache.has('A')).to.be.true;
+  });
+
+
+  it('should check permission in project', () => {
+    const projectA = createProject('A');
+    const projectB = createProject('B');
+    const permissions = [createPermission('A', [projectA])];
+
+    const permissionCache = new PermissionCache(permissions);
+
+    expect(permissionCache.has('A', projectA.id)).to.be.true;
+    expect(permissionCache.has('A', projectB.id)).to.be.false;
+  });
+
+
+  it('should allow pass name converter', () => {
+    const permissions = [createPermission('A')];
+    const nameConverter = key => `#${key}`;
+
+    const permissionCache = new PermissionCache(permissions, nameConverter);
+
+    expect(permissionCache.has('#A')).to.be.true;
+  });
+});
