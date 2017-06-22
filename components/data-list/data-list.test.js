4a3023a840b6b3bcd717bf9a4a229c4188c8f9da
hypnos <Filipp.Riabchun@jetbrains.com>
2017-06-22 17:08:58 +0300

    RING-UI-CR-2033 uze enzyme for some more tests
    Former-commit-id: e3daa9ca789d7fa278c83cf1f2c5af0bed8cd5f8

--- components/data-list/data-list.test.js
+++ components/data-list/data-list.test.js
@@ -1,28 +1,36 @@
-import 'dom4';
 import React from 'react';
-import {findDOMNode} from 'react-dom';
-import {renderIntoDocument, isCompositeComponentWithType} from 'react-dom/test-utils';
+import {shallow, mount, render} from 'enzyme';
 
 import DataList from './data-list';
 import Selection from './selection';
 
 describe('Data List', () => {
-  const renderComponent = (props = {}) => {
+  const shallowDataList = (props = {}) => {
     const data = props.data || [];
     const selection = new Selection({data, isItemSelectable: item => item.selectable});
-    return renderIntoDocument(<DataList {...{...{data, selection}, ...props}}/>);
+    return shallow(<DataList {...{...{data, selection}, ...props}}/>);
+  };
+  const mountDataList = (props = {}) => {
+    const data = props.data || [];
+    const selection = new Selection({data, isItemSelectable: item => item.selectable});
+    return mount(<DataList {...{...{data, selection}, ...props}}/>);
+  };
+  const renderDataList = (props = {}) => {
+    const data = props.data || [];
+    const selection = new Selection({data, isItemSelectable: item => item.selectable});
+    return render(<DataList {...{...{data, selection}, ...props}}/>);
   };
 
   it('should create component', () => {
-    isCompositeComponentWithType(renderComponent(), DataList).should.be.true;
+    mountDataList().should.have.type(DataList);
   });
 
   it('should wrap children with div', () => {
-    findDOMNode(renderComponent()).should.match('div');
+    shallowDataList().should.have.tagName('div');
   });
 
   it('should use passed className', () => {
-    findDOMNode(renderComponent({className: 'test-class'})).querySelector('ul').should.match('.test-class');
+    renderDataList({className: 'test-class'}).find('ul').should.have.className('test-class');
   });
 
   // TODO Add more tests
