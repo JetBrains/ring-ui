a95a972dfad3e54e427c97ac4be64befd24b7bf5
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-06-22 15:57:14 +0300

    RG-1001 drop more redundant eslint-ignore comments
    Former-commit-id: d83109c795f9d6f9ca232c2ed70ac9f8362e3d2b

--- components/list/list__users-groups-source.test.js
+++ components/list/list__users-groups-source.test.js
@@ -99,7 +99,6 @@ describe('List Users Groups Source', () => {
       returns(Promise.resolve([{id: 1, name: 'test group'}]));
 
     const dataForList = await source.getForList();
-    // eslint-disable-next-line no-magic-numbers
     dataForList[2].description.should.equal('No users');
   });
 
