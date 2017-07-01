620ae13e67f82d14abdd02a544ef739057d7a273
hypnos <Filipp.Riabchun@jetbrains.com>
2017-07-01 03:10:13 +0300

    merge master
    Former-commit-id: dc9dad955100dac39cf301b90e339a8c4c27c1bc

--- kotlin-libs.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  libraries: [
-    '@hypnosphi/kotlin-extensions',
-    '@hypnosphi/kotlin-react',
-    '@hypnosphi/kotlin-react-dom',
-    '@hypnosphi/kotlinx-html-js'
-  ].map(pkg => require.resolve(pkg))
-};
