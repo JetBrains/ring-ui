c7a30b6b51dd61d6699e96d174c62f93c3571ff7
Eugene Datsky <Eugene.Datsky@jetbrains.com>
2017-07-04 22:56:16 +0300

    RG-1395 Rewrite some commits in jetbrains-eslint-config and publish it to GitHub: make it build, review fixes
    Former-commit-id: a388a0ade174f4dcd92a1b5b554b0710d199e2ec

--- components/auth-dialog-service/auth-dialog-service.js
+++ components/auth-dialog-service/auth-dialog-service.js
@@ -25,7 +25,7 @@ import AuthDialog from '../auth-dialog/auth-dialog';
  import Button from 'ring-ui/components/button/button';
  import showAuthDialog from 'ring-ui/components/auth-dialog-service/auth-dialog-service';
 
- import hubConfig from 'ring-ui/hub-config';
+ import hubConfig from 'ring-ui-docs/components/hub-config';
 
  const auth = new Auth(hubConfig);
  const http = new HTTP(auth, auth.getAPIPath());
