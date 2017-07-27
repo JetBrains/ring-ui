9d2318246154c9b238df6455392341d5a11a1a38
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-07-27 14:20:25 +0300

    Turn off renderOptimization for select-ng dropdowns by default
    Former-commit-id: 62be71dcb7fc694961400d146d1c23df29006919

--- components/list/list.js
+++ components/list/list.js
@@ -593,7 +593,7 @@ export default class List extends RingComponentWithShortcuts {
       >
         <div
           style={maxHeight
-            ? {maxHeight: this.getVisibleListHeight()}
+            ? {maxHeight: this.getVisibleListHeight(this.props)}
             : null
           }
         >
