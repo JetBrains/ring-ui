2a1b72004e4624d007f70d4aa3a3e1fd8e08d694
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-01 22:00:44 +0300

    Fix docsite auth and temporarily turn it off
    Former-commit-id: 9c7ac4c92c2463fe3275907da5165a5775f1ff8f

--- packages/ring-ui-docs/components/utils.js
+++ packages/ring-ui-docs/components/utils.js
@@ -3,6 +3,10 @@ export function currentPath() {
   return chunks[chunks.length - 1];
 }
 
-export function getIndexDoc(docs) {
-  return docs.find(({order}) => order === 0).url;
+export function getDocs() {
+  return window.navData.categories.find(({name}) => name === 'Docs');
+}
+
+export function getIndexDoc() {
+  return getDocs().items.find(({order}) => order === 0).url;
 }
