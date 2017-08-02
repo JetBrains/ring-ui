b10002cb3d15f57eafba7a36c74f82a82f04bf67
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-02 19:08:56 +0300

    Handle errors in generator test

--- generator_test.sh
+++ generator_test.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 function test {
-  mkdir test_gen
-  cd test_gen
-  npm link ../packages/ring-ui-generator
-  ../node_modules/.bin/yo @jetbrains/ring-ui:$1 my-app --widget-name=widget --widget-description=description --widget-author=author
-  npm link ../packages/ring-ui-generator
-  npm link ../.
-  npm run create-component my-component
-  npm run ci-test
-  npm run build
-  cd ..
-  rm -rf test_gen
+  npm link ../packages/ring-ui-generator || exit 1
+  ../node_modules/.bin/yo @jetbrains/ring-ui:$1 my-app --widget-name=widget --widget-description=description --widget-author=author || exit 1
+  npm link ../packages/ring-ui-generator || exit 1
+  npm link ../. || exit 1
+  npm run create-component my-component || exit 1
+  npm run ci-test || exit 1
+  npm run build || exit 1
 }
 
-test app
-test hub-widget
+function setup {
+  mkdir test_gen
+  cd test_gen
+  test $1
+  code = $?
+  cd ..
+  rm -rf test_gen
+  exit code
+}
+
+setup app && setup hub-widget
