afca335204713b71a5a7e7847a7bce96e6bc46cd
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-02 03:33:59 +0300

    RG-1443 Include hub-widget into integration test for generator
    Former-commit-id: 8d998e07dc6c039f3461160bbfff335d263b2f4a

--- generator_test.sh
+++ generator_test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-mkdir test_gen
-cd test_gen
-npm link ../packages/ring-ui-generator
-yo @jetbrains/ring-ui my-app
-npm link ../packages/ring-ui-generator
-npm link ../.
-npm run create-component my-component
-npm run ci-test
-npm run build
-cd ..
-rm -rf test_gen
+function test {
+  mkdir test_gen
+  cd test_gen
+  npm link ../packages/ring-ui-generator
+  yo @jetbrains/ring-ui:$1 my-app --widget-name=widget --widget-description=description --widget-author=author
+  npm link ../packages/ring-ui-generator
+  npm link ../.
+  npm run create-component my-component
+  npm run ci-test
+  npm run build
+  cd ..
+  rm -rf test_gen
+}
+
+test app
+test hub-widget
