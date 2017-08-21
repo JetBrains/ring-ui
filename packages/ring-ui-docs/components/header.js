dfe2a6032d10cab7c5adfb56fd9fceadcb5f7fac
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-21 18:03:07 +0300

    Bring branch selector back

--- packages/ring-ui-docs/components/header.js
+++ packages/ring-ui-docs/components/header.js
@@ -22,7 +22,7 @@ import teamcityLogo from '!file-loader?publicPath=./!@jetbrains/logos/teamcity/t
 import styles from './index.css';
 import hubConfig from './hub-config';
 import Item from './item';
-// import Version from './version';
+import Version from './version';
 import {getIndexDoc} from './utils';
 
 class SiteHeader extends PureComponent {
@@ -50,7 +50,10 @@ class SiteHeader extends PureComponent {
             size={JetBrainsLogo.Size.Size128}
           />
         </Link>
-        <span className={styles.headerItem}>{`Ring UI library ${version}`}</span>
+        <span className={styles.headerItem}>
+          {'Ring UI library '}
+          <Version version={version}/>
+        </span>
         {docsItems.map(item => (
           <Item
             key={item.title}
