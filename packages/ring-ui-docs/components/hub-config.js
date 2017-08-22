a7e1500ee0339af9491a9cd537f059feb2938d52
hypnos <Filipp.Riabchun@jetbrains.com>
2017-08-22 03:35:16 +0300

    Adjust redirectUri

--- packages/ring-ui-docs/components/hub-config.js
+++ packages/ring-ui-docs/components/hub-config.js
@@ -2,11 +2,18 @@
 
 const {serverUri, clientId} = hubConfig;
 
+const RING_UI_BASE = '/ring-ui/';
+
+const {origin, pathname, hash} = window.location;
+const base = pathname.startsWith(RING_UI_BASE)
+  ? RING_UI_BASE
+  : '/';
+
 export default {
   reloadOnUserChange: false,
   windowLogin: true,
   serverUri,
   clientId,
   requestCredentials: 'skip',
-  redirectUri: window.location.href.replace(/\/[^\/]+.html(?=(#|$))/, '/')
+  redirectUri: origin + base + hash
 };
