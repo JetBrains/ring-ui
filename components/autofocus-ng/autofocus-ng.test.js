eb8004a90e10da39e7e9263b7dfa6e4abf9c3046
katriyna <katriyna@gmail.com>
2017-08-22 21:16:15 +0300

    RG-1531 fixed selector in unit-test

--- components/autofocus-ng/autofocus-ng.test.js
+++ components/autofocus-ng/autofocus-ng.test.js
@@ -37,7 +37,7 @@ describe('Autofocus Ng', () => {
 
   it('Should focus on select button', () => {
     element = $compile('<rg-select rg-autofocus="vm.focus" options="item in []"/>')($scope);
-    const selectButton = element[0].querySelector('.ring-select');
+    const selectButton = element[0].querySelector('.ring-select button');
 
     sandbox.spy(selectButton, 'focus');
 
