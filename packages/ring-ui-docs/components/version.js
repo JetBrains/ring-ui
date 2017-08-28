946c7c9f49e484bdb2855cca0892ef945e010a52
Igor Shevchenko <cyberskunk@gmail.com>
2017-08-28 12:27:01 +0300

    docsite: do not lead users to http by default

--- packages/ring-ui-docs/components/version.js
+++ packages/ring-ui-docs/components/version.js
@@ -8,7 +8,7 @@ import branches from '../branches.json';
 import {currentPath} from './utils';
 import styles from './index.css';
 
-const HOST = 'http://jetbrains.org/ring-ui';
+const HOST = '//jetbrains.org/ring-ui';
 
 const rgItemType = PopupMenu.ListProps.Type.LINK;
 
