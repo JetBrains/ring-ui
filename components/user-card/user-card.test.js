413ddfaaf0e5ecc13b1ea41ae8cdc4a49f2f281f
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-09-19 12:18:17 +0300

    RG-1554 add dummy tests for user card

--- components/user-card/user-card.test.js
+++ components/user-card/user-card.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
 
-import {UserCard} from './user-card';
+import {HubUserCardTooltip, UserCard, UserCardTooltip} from './user-card';
 
 describe('UserCard', () => {
   const fakeUser = {
@@ -17,24 +17,65 @@ describe('UserCard', () => {
 
   const getHref = () => 'http://foo';
 
-  const shallowCard = props => shallow(
-    <UserCard user={fakeUser} getHref={getHref} {...props}/>
-  );
-  const mountCard = props => mount(<UserCard user={fakeUser} getHref={getHref} {...props}/>);
+  describe('Card', () => {
+    const shallowCard = props => shallow(
+      <UserCard user={fakeUser} getHref={getHref} {...props}/>
+    );
+    const mountCard = props => mount(<UserCard user={fakeUser} getHref={getHref} {...props}/>);
 
-  it('should create component', () => {
-    mountCard().should.have.type(UserCard);
+    it('should create component', () => {
+      mountCard().should.have.type(UserCard);
+    });
+
+    it('should wrap children with span', () => {
+      shallowCard().should.have.tagName('div');
+    });
+
+    it('should use passed className', () => {
+      shallowCard({className: 'test-class'}).should.have.className('test-class');
+    });
+
+    it('should use pass rest props to dom node', () => {
+      shallowCard({'data-test': 'foo'}).should.have.data('test', 'foo');
+    });
   });
 
-  it('should wrap children with span', () => {
-    shallowCard().should.have.tagName('div');
+  describe('UserCardTooltip', () => {
+    const anchor = <span data-test="anchor">{'foo'}</span>;
+
+    const mountTooltip = props => mount(
+      <UserCardTooltip user={fakeUser} getHref={getHref} {...props}>
+        {anchor}
+      </UserCardTooltip>
+    );
+
+    it('should render anchor', () => {
+      const wrapper = mountTooltip();
+
+      wrapper.should.have.descendants('[data-test="anchor"]');
+    });
   });
 
-  it('should use passed className', () => {
-    shallowCard({className: 'test-class'}).should.have.className('test-class');
-  });
+  describe('HubUserCardTooltip', () => {
+    const anchor = <span>{'foo'}</span>;
 
-  it('should use pass rest props to dom node', () => {
-    shallowCard({'data-test': 'foo'}).should.have.data('test', 'foo');
+    const mountTooltip = props => mount(
+      <HubUserCardTooltip userId="test-id" {...props}>
+        {anchor}
+      </HubUserCardTooltip>
+    );
+
+    it('should load user on hover', () => {
+      const wrapper = mountTooltip();
+      sandbox.stub(wrapper.instance(), 'loadUser').callsFake(() => {});
+
+      // Force the component and wrapper to update so that the stub is used https://github.com/airbnb/enzyme/issues/586
+      wrapper.instance().forceUpdate();
+      wrapper.update();
+
+      wrapper.simulate('mouseenter');
+
+      wrapper.instance().loadUser.should.have.been.called;
+    });
   });
 });
