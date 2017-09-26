b50bf6ec547832c031838081c84a0bcdb497f3ba
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-09-26 12:21:25 +0300

    Chore: extract "convertUserForCard" utility function

--- components/hub-source/hub-source__user.js
+++ components/hub-source/hub-source__user.js
@@ -2,6 +2,16 @@ import HTTP from '../http/http';
 
 const DEFAULT_FIELDS = 'id,name,login,profile(email/email,avatar/url)';
 
+export function convertUserForCard(hubUser, serverUri) {
+  return {
+    name: hubUser.name,
+    login: hubUser.login,
+    email: hubUser.profile.email && hubUser.profile.email.email,
+    avatarUrl: hubUser.profile.avatar.url,
+    href: `${serverUri}users/${hubUser.id}`
+  };
+}
+
 export function createHubUserCardSource(auth, userId, fields = DEFAULT_FIELDS) {
   const http = new HTTP(auth);
 
@@ -10,12 +20,6 @@ export function createHubUserCardSource(auth, userId, fields = DEFAULT_FIELDS) {
       `${auth.config.serverUri}api/rest/users/${userId}?fields=${fields}`
     );
 
-    return {
-      name: hubUser.name,
-      login: hubUser.login,
-      email: hubUser.profile.email && hubUser.profile.email.email,
-      avatarUrl: hubUser.profile.avatar.url,
-      href: `${auth.config.serverUri}users/${hubUser.id}`
-    };
+    return convertUserForCard(hubUser, auth.config.serverUri);
   };
 }
