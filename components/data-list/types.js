4f7485610f87e1cbf42b39361a6d6c8351b7317e
Max Maximov <max.maximov@gmail.com>
2017-10-03 03:08:55 +0300

    "group" -> "item" #RG-1580

--- components/data-list/types.js
+++ components/data-list/types.js
@@ -1,23 +1,7 @@
 /* @flow */
-export type SubitemType = {
-  type: 'subitem',
-  id: number,
-  title: any
-};
-
 export type ItemType = {
-  type: 'item',
   id: number,
   title: any,
-  selectable?: boolean,
-  subitems?: SubitemType[]
-};
-
-export type GroupType = {
-  type: 'group',
-  id: number,
-  title: any,
-  size: number,
   selectable?: boolean,
   items: ItemType[]
 };
