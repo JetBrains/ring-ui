d8fa9315f877fe58b67249dafdb89bedfe47fc07
hypnos <Filipp.Riabchun@jetbrains.com>
2017-11-22 01:07:48 +0300

    RG-1576 Make navigation categories collapsible [publish]

--- packages/docs/components/nav.js
+++ packages/docs/components/nav.js
@@ -1,64 +1,29 @@
-import React, {PureComponent} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import List from '@jetbrains/ring-ui/components/list/list';
 
+import Category from './category';
 import {currentPath} from './utils';
-import styles from './index.css';
 
-const groupListItem = name => ({
-  rgItemType: List.ListProps.Type.TITLE,
-  label: name === 'Docs' ? 'Ring UI' : name,
-  key: name,
-  className: styles.item
-});
-
-const linkListItem = ({url, title, legacy}) => {
-  const active = url === currentPath();
-
-  return {
-    rgItemType: List.ListProps.Type.LINK,
-    href: url,
-    active
-    ,
-    label: title,
-    key: url,
-    className: classNames(styles.item, {
-      [styles.legacy]: legacy,
-      [styles.active]: active
-    })
-  };
-};
-
-const Nav = ({categories}) => {
-  // [].concat(...arrs) === arrs.flatten()
-  const data = [].concat(
-    ...categories.map(({name, items}) => [
-      groupListItem(name),
-      ...items.map(linkListItem)
-    ])
-  );
+const category = ({name, items}) => {
+  const isDocs = name === 'Docs';
 
   return (
-    <div className={styles.nav}>
-      <List
-        renderOptimization={false}
-        className={styles.list}
-        shortcuts={true}
-        compact={true}
-        {...{data}}
-      />
-    </div>
+    <Category
+      key={name}
+      collapsible={!isDocs}
+      initExpanded={items.some(({url}) => url === currentPath())}
+      name={isDocs ? 'Ring UI' : name}
+      items={items}
+    />
   );
 };
+
+const Nav = ({categories}) => categories.map(category);
 
 Nav.propTypes = {
   categories: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string,
-    items: PropTypes.arrayOf(PropTypes.shape({
-      url: PropTypes.string,
-      title: PropTypes.string
-    }))
+    items: PropTypes.arrayOf(PropTypes.shape({}))
   }))
 };
 
