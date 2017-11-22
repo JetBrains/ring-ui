06f3d5970704ac848259fe24db70651f42ca8f33
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-11-22 14:49:15 +0300

    RG-1628 RG-1627 explicitly enable trapFocus for authDialog, Confirm and for dialog example

--- components/confirm/confirm.js
+++ components/confirm/confirm.js
@@ -72,6 +72,7 @@ export default class Confirm extends PureComponent {
         className={className}
         onEscPress={this.onEscPress}
         show={show}
+        trapFocus
       >
         {text && <Header>{text}</Header>}
         <Content>
