ae1a59aad3285fd85ae60e15b110684a44128d40
hypnos <Filipp.Riabchun@jetbrains.com>
2017-11-23 18:30:25 +0300

    RG-1576 Example link, iframe, code

--- packages/docs/components/iframe.js
+++ packages/docs/components/iframe.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '@jetbrains/ring-ui/components/link/link';
 import className from 'classnames';
 
 import styles from './index.css';
 
 const Iframe = ({src, disableAutoSize}) => (
-  <div className={styles.iframe}>
-    <iframe
-      id={src}
-      width="100%"
-      height="auto"
-      src={src}
-      data-resize={disableAutoSize ? 'disabled' : 'enabled' }
-      className={className({
-        [styles.frame]: true,
-        [styles.frameFixedHeight]: disableAutoSize
-      })}
-    />
-    <Link
-      href={src}
-      className={styles.exampleLink}
-      target="_blank"
-      title="Open in new tab"
-    >{'Example'}</Link>
-  </div>
+  <iframe
+    id={src}
+    width="100%"
+    height="auto"
+    src={src}
+    data-resize={disableAutoSize ? 'disabled' : 'enabled' }
+    className={className({
+      [styles.frame]: true,
+      [styles.frameFixedHeight]: disableAutoSize
+    })}
+  />
 );
 
 Iframe.propTypes = {
