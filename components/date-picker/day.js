a473d7331a010be273d9db83fb68c7e529110dde
hypnos <Filipp.Riabchun@jetbrains.com>
2017-12-02 21:51:30 +0300

    Fix new lint errors

--- components/date-picker/day.js
+++ components/date-picker/day.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import moment from 'moment';
@@ -8,67 +8,65 @@ import {dateType, weekdays} from './consts';
 import styles from './date-picker.css';
 
 let hoverTO;
-export default function Day(props) {
-  const {
-    day,
-    from,
-    currentRange,
-    activeRange,
-    empty,
-    onSelect,
-    onHover
-  } = props;
+export default class Day extends Component {
+  handleClick = () => this.props.onSelect(this.props.day);
 
-  function isDay(date) {
-    return day.isSame(date, 'day');
+  handleMouseOver = () => {
+    if (hoverTO) {
+      window.clearTimeout(hoverTO);
+      hoverTO = null;
+    }
+
+    this.props.onHover(this.props.day);
+  };
+
+  handleMouseOut = () => {
+    hoverTO = window.setTimeout(this.props.onHover, 0);
+  };
+
+  isDay = date => this.props.day.isSame(date, 'day');
+
+  is = name => this.props[name] && this.isDay(this.props[name]);
+
+  inRange = range => range && this.props.day.isBetween(...range, 'days');
+
+  render() {
+    const {
+      day,
+      from,
+      currentRange,
+      activeRange,
+      empty
+    } = this.props;
+
+    const reverse = activeRange && activeRange[1] === from;
+
+    return (
+      <div
+        className={classNames(
+          styles.day,
+          {
+            [styles.current]: ['date', 'from', 'to'].some(this.is),
+            [styles.today]: day.isSame(moment(), 'day'),
+            [styles.active]: this.is('activeDate'),
+            [styles.weekend]: [weekdays.SA, weekdays.SU].includes(day.day()),
+            [styles.empty]: empty,
+            [styles.from]: (currentRange && this.isDay(currentRange[0]) && !reverse ||
+              activeRange && this.isDay(activeRange[0])),
+            [styles.to]: (currentRange && this.isDay(currentRange[1])) ||
+            activeRange && this.isDay(activeRange[1]),
+            [styles.between]: this.inRange(currentRange),
+            [styles.activeBetween]: this.inRange(activeRange)
+          },
+        )}
+        onClick={this.handleClick}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+      >
+        {empty || day.format('D')}
+      </div>
+    );
   }
-
-  function is(name) {
-    return props[name] && isDay(props[name]);
-  }
-
-  function inRange(range) {
-    return range && day.isBetween(...range, 'days');
-  }
-
-  const reverse = activeRange && activeRange[1] === from;
-
-  return (
-    <div
-      className={classNames(
-        styles.day,
-        {
-          [styles.current]: ['date', 'from', 'to'].some(is),
-          [styles.today]: day.isSame(moment(), 'day'),
-          [styles.active]: is('activeDate'),
-          [styles.weekend]: [weekdays.SA, weekdays.SU].includes(day.day()),
-          [styles.empty]: empty,
-          [styles.from]: (currentRange && isDay(currentRange[0]) && !reverse ||
-            activeRange && isDay(activeRange[0])),
-          [styles.to]: (currentRange && isDay(currentRange[1])) ||
-            activeRange && isDay(activeRange[1]),
-          [styles.between]: inRange(currentRange),
-          [styles.activeBetween]: inRange(activeRange)
-        },
-      )}
-      onClick={function handleClick() {
-        onSelect(day);
-      }}
-      onMouseOver={function handleMouseOver() {
-        if (hoverTO) {
-          window.clearTimeout(hoverTO);
-          hoverTO = null;
-        }
-
-        onHover(day);
-      }}
-      onMouseOut={function handleMouseOut() {
-        hoverTO = window.setTimeout(onHover, 0);
-      }}
-    >
-      {empty || day.format('D')}
-    </div>
-  );
 }
 
 Day.propTypes = {
