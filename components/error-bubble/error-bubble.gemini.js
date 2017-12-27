19fbab85e02b95f293915d11eca6e105510a3500
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2017-12-27 16:21:58 +0300

    RG-1662 reimplement ErrorBubble using Popup

--- components/error-bubble/error-bubble.gemini.js
+++ components/error-bubble/error-bubble.gemini.js
@@ -1,11 +1,20 @@
 /* global gemini: false */
 
-gemini.suite('Error Bubble', suite => {
-  suite.
-    setUrl('error-bubble/error-bubble.html').
-    setCaptureElements('#container').
-    capture('error bubble').
-    capture('error bubble hidden', (actions, find) => {
-      actions.sendKeys(find('.ring-input'), 'something');
-    });
+gemini.suite('Error Bubble', () => {
+  gemini.suite('Error Bubble', child => {
+    child.
+      setUrl('error-bubble/error-bubble.html').
+      setCaptureElements('#container').
+      capture('error bubble').
+      capture('error bubble hidden', (actions, find) => {
+        actions.sendKeys(find('.ring-input'), 'something');
+      });
+  });
+
+  gemini.suite('Error Bubble in dialog form', child => {
+    child.
+      setUrl('error-bubble/error-bubble-in-dialog-form.html').
+      setCaptureElements('[data-test="ring-dialog"]').
+      capture('error bubble');
+  });
 });
--- components/error-bubble/error-bubble.js
+++ components/error-bubble/error-bubble.js
@@ -2,42 +2,17 @@ import React, {Children, cloneElement, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import './error-bubble.scss';
+import Popup from '../popup/popup';
+import {Directions} from '../popup/position';
 
+import styles from './error-bubble.css';
 /**
  * @name Error Bubble
  * @category Components
  * @constructor
  * @description Displays an error bubble near the wrapped input component when a non-empty string is passed to the `error` prop.
  * Passes any prop except `className` down to input.
- * @example
-   <example name="Error Bubble">
-     <file name="index.html">
-       <div id="container"></div>
-     </file>
-
-     <file name="index.js" webpack="true">
-       import {render} from 'react-dom';
-       import React from 'react';
-
-       import ErrorBubble from '@jetbrains/ring-ui/components/error-bubble/error-bubble';
-       import Input from '@jetbrains/ring-ui/components/input/input';
-       import '@jetbrains/ring-ui/components/input-size/input-size.scss';
-
-       const container = document.getElementById('container');
-       const renderBubbleDemo = value => (
-         <ErrorBubble
-           error={value ? null : 'Value is required'}
-           onInput={e => render(renderBubbleDemo(e.target.value), container)}
-           placeholder="enter something"
-         >
-           <Input className="ring-input-size_md" />
-         </ErrorBubble>
-       );
-
-       render(renderBubbleDemo(), container);
-     </file>
-   </example>
+ * @example-file ./error-bubble.examples.html
  */
 
 export default class ErrorBubble extends PureComponent {
@@ -50,15 +25,27 @@ export default class ErrorBubble extends PureComponent {
   render() {
     // eslint-disable-next-line no-unused-vars
     const {children, className, ...restProps} = this.props;
-    const classes = classNames('ring-error-bubble active', className);
+
+    const errorBubbleClasses = classNames(styles.errorBubble, className);
 
     return (
-      <div className="ring-error-bubble-wrapper">
+      <div className={styles.errorBubbleWrapper}>
         {Children.map(children, child => cloneElement(child, restProps))}
+
         {restProps.error && (
-          <div className={classes}>
-            {restProps.error}
-          </div>
+          <Popup
+            className={styles.errorBubblePopup}
+            hidden={false}
+            attached={false}
+            directions={[Directions.RIGHT_CENTER, Directions.RIGHT_BOTTOM, Directions.RIGHT_TOP]}
+          >
+            <div
+              className={errorBubbleClasses}
+              data-test="ring-error-bubble"
+            >
+              {restProps.error}
+            </div>
+          </Popup>
         )}
       </div>
     );
