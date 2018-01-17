d4d4032a5d9882fe5ba295c7535c20e0f40b3425
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-01-17 16:00:26 +0300

    RG-1671 ignore buttons with active loader to avoid gemini false-failures

--- components/button-ng/button-ng.gemini.js
+++ components/button-ng/button-ng.gemini.js
@@ -4,15 +4,6 @@ gemini.suite('Button Ng', suite => {
   suite.
     setUrl('button-ng/button-ng.html').
     setCaptureElements('body > div').
-    capture('buttons', actions => {
-      actions.executeJS(function disableLoadingButtons() {
-        // ignore is too slow on IE
-        Array.prototype.forEach.call(
-          document.querySelectorAll('.ring-button_loader'),
-          function removeClass(node) {
-            node.classList.remove('ring-button_loader');
-          }
-        );
-      });
-    });
+    ignoreElements({every: '[loader="true"]'}).
+    capture('buttons');
 });
