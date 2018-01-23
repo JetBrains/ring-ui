0df8dc16578fcb5ab56d0922109f4c3ce41f295c
Kateryna Shlyakhovetska <shkate@jetbrains.com>
2018-01-23 14:08:52 +0100

    test page selector appearence

--- components/pager/pager.gemini.js
+++ components/pager/pager.gemini.js
@@ -2,7 +2,7 @@
 
 gemini.suite('Pager', suite => {
   suite.
-    setUrl('pager/pager.html').
+    setUrl('pager/pager-with-a-page-size-selector.html').
     setCaptureElements('#pager').
     capture('pager');
 });
