186b8b40fcdb575d3c4c8a22f7698d2aa9e5d959
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-01-24 01:38:23 +0300

    App generator: update tests setup

--- packages/generator/app/templates/test-bundler.js
+++ packages/generator/app/templates/test-bundler.js
@@ -7,6 +7,10 @@ import chaiDOM from 'chai-dom';
 import sinonChai from 'sinon-chai';
 import chaiEnzyme from 'chai-enzyme';
 import sinon from 'sinon';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter()});
 
 chai.use(chaiAsPromised);
 chai.use(chaiDOM);
