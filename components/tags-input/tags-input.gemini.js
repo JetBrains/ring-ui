4c7fb082cac2373aadacc578ef6a63465a9199e9
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-02-20 12:54:55 +0300

    RG-1675 add simple gemini test for tags-input

---
+++ components/tags-input/tags-input.gemini.js
@@ -0,0 +1,8 @@
+/* global gemini: false */
+
+gemini.suite('TagsInput', suite => {
+  suite.
+    setUrl('tags-input/tags-input.html').
+    setCaptureElements('body').
+    capture('tags-input');
+});
