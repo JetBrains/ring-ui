2dd6200ce1502a559b51917df3aa3e1fa63e4e1b
Max Maximov <max.maximov@gmail.com>
2018-02-27 17:56:26 +0300

    toggle-ng

---
+++ components/toggle-ng/toggle-ng.js
@@ -0,0 +1,13 @@
+/**
+ * @name Toggle Ng
+ * @category Legacy Angular
+ * @tags Ring UI Language
+ * @framework Angular
+ * @description Provides an Angular wrapper for Toggle.
+ * @example-file ./toggle-ng.examples.html
+ */
+import angularComponentFactory from '../global/angular-component-factory';
+import Toggle from '../toggle/toggle';
+
+export default angularComponentFactory(Toggle, 'Toggle').name;
+
