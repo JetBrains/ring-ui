87a66941757414bf9c1a3176f6a1a13b44c14dbb
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-03-15 00:05:38 +0300

    [publish] Fix tests

--- components/link/link.test.js
+++ components/link/link.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 
 import Link, {linkHOC} from './link';
+import ClickableLink from './clickableLink';
 import styles from './link.css';
 
 function noop() {}
@@ -14,8 +15,8 @@ describe('Link', () => {
     mountLink().should.have.type(Link);
   });
 
-  it('should wrap children with a', () => {
-    shallowLink().should.have.tagName('a');
+  it('should wrap children with ClickableLink', () => {
+    shallowLink({href: '/'}).should.have.type(ClickableLink);
   });
 
   it('should use passed className', () => {
@@ -26,8 +27,9 @@ describe('Link', () => {
     shallowLink({active: true}).should.have.className(styles.active);
   });
 
-  it('should render span for pseudo links', () => {
-    shallowLink({pseudo: true}).should.have.tagName('span');
+  it('should render button for pseudo links', () => {
+    shallowLink().should.have.tagName('button');
+    shallowLink({href: '/', pseudo: true}).should.have.tagName('button');
   });
 
   describe('linkHOC', () => {
@@ -59,6 +61,98 @@ describe('Link', () => {
       const CustomLink = linkHOC(CustomComponent);
 
       shallow(<CustomLink/>).should.not.have.prop('activeClassName');
+    });
+  });
+
+  describe('ClickableLink', () => {
+    it('should render "a" tag', () => {
+      shallow(<ClickableLink href="/"/>).should.containMatchingElement(<a href="/"/>);
+    });
+
+    describe('events', () => {
+      const Buttons = {
+        LEFT: 0,
+        MIDDLE: 1,
+        RIGHT: 2
+      };
+
+      let onClick;
+      let onPlainLeftClick;
+      let wrapper;
+
+      const makeEvent = e => ({
+        ...e,
+        preventDefault: sandbox.spy()
+      });
+
+      beforeEach(() => {
+        onClick = sandbox.spy();
+        onPlainLeftClick = sandbox.spy();
+        wrapper = shallow(<ClickableLink onClick={onClick} onPlainLeftClick={onPlainLeftClick}/>);
+      });
+
+      it('should call onClick and onPlainLeftClick on a plain left click', () => {
+        const e = makeEvent({button: Buttons.LEFT});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.have.been.calledWith(e);
+        e.preventDefault.should.have.been.calledOnce;
+      });
+
+      it('should call only onClick on a middle click', () => {
+        const e = makeEvent({button: Buttons.MIDDLE});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
+
+      it('should call only onClick on a right click', () => {
+        const e = makeEvent({button: Buttons.RIGHT});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
+
+      it('should call only onClick on alt+click', () => {
+        const e = makeEvent({button: Buttons.LEFT, altKey: true});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
+
+      it('should call only onClick on ctrl+click', () => {
+        const e = makeEvent({button: Buttons.LEFT, ctrlKey: true});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
+
+      it('should call only onClick on cmd+click / win+click', () => {
+        const e = makeEvent({button: Buttons.LEFT, metaKey: true});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
+
+      it('should call only onClick on shift+click', () => {
+        const e = makeEvent({button: Buttons.LEFT, shiftKey: true});
+        wrapper.simulate('click', e);
+
+        onClick.should.have.been.calledWith(e);
+        onPlainLeftClick.should.not.have.been.called;
+        e.preventDefault.should.not.have.been.called;
+      });
     });
   });
 });
