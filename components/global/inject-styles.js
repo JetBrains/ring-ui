738655dabc9872ff29434bd2b0d02763d28bb23a
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-03-21 23:43:59 +0300

    Extract `conic-gradient` helper to use conic gradient polyfill without prefixfree

---
+++ components/global/inject-styles.js
@@ -0,0 +1,12 @@
+export const injectStyleSheet = styles => {
+  const styleTag = document.createElement('style');
+  styleTag.setAttribute('type', 'text/css');
+  styleTag.textContent = styles;
+  document.head.appendChild(styleTag);
+};
+
+export const injectRuleSet = (selector, declarations) =>
+  injectStyleSheet(`
+${selector} {${Object.entries(declarations).map(([property, value]) => `
+  ${property}: ${value};`)}
+}`);
