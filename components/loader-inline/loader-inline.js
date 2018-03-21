8e9d7953f0f9d130e343ad950e4aed3cabf4d7c3
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-03-21 23:43:59 +0300

    Extract `conic-gradient` helper to use conic gradient polyfill without prefixfree

--- components/loader-inline/loader-inline.js
+++ components/loader-inline/loader-inline.js
@@ -1,10 +1,25 @@
-import 'prefixfree';
-import 'conic-gradient';
 import React, {PureComponent} from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
+import conicGradient from '../global/conic-gradient';
+import {injectRuleSet} from '../global/inject-styles';
+
 import styles from './loader-inline.css';
+
+injectRuleSet(`.${styles.loader}`, {
+  'background-image': conicGradient([
+    '#000',
+    '#e32581',
+    '#d73cea',
+    '#9135e0',
+    '#5848f4',
+    '#25b7ff',
+    '#59bd00',
+    '#fbac02',
+    '#000'
+  ])
+});
 
 /**
  * @name Loader Inline
