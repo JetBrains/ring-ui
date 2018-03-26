7961682add4729433753a99d9df522138c9681ac
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-03-26 15:03:20 +0200

    RG-1741 more elegant gemini tests code

--- components/query-assist/query-assist.gemini.js
+++ components/query-assist/query-assist.gemini.js
@@ -1,5 +1,17 @@
 /* global gemini: false */
 
+function hideInput() {
+  document.querySelector(
+    '[data-test=ring-query-assist-input]'
+  ).style = 'color: transparent;';
+}
+
+function hideCaret() {
+  document.querySelector(
+    '[data-test=ring-query-assist-last-letter]'
+  ).style = 'color: transparent;';
+}
+
 gemini.suite('Query Assist', suite => {
   suite.
     setUrl('query-assist/query-assist-no-auth.html').
@@ -11,20 +23,12 @@ gemini.suite('Query Assist', suite => {
       setCaptureElements(['#query-assist', '[data-test~=ring-query-assist-popup]']).
       capture('focused', (actions, find) => {
         actions.click(find('[data-test=ring-query-assist-input]'));
-        actions.executeJS(function hideCaret() {
-          document.querySelector(
-            '[data-test=ring-query-assist-input]'
-          ).style = 'color: transparent;';
-        });
+        actions.executeJS(hideInput);
         actions.waitForElementToShow('[data-test~=ring-query-assist-popup]');
       }).
       capture('input', (actions, find) => {
         actions.sendKeys(find('[data-test=ring-query-assist-input]'), 'test ');
-        actions.executeJS(function hideCaret() {
-          document.querySelector(
-            '[data-test=ring-query-assist-last-letter]'
-          ).style = 'color: transparent;';
-        });
+        actions.executeJS(hideCaret);
         actions.waitForElementToShow('[data-test~=ring-query-assist-popup]');
       });
   });
@@ -36,20 +40,12 @@ gemini.suite('Query Assist', suite => {
       capture('default').
       capture('focused', (actions, find) => {
         actions.click(find('[data-test=ring-query-assist-input]'));
-        actions.executeJS(function hideCaret() {
-          document.querySelector(
-            '[data-test=ring-query-assist-input]'
-          ).style = 'color: transparent;';
-        });
+        actions.executeJS(hideInput);
         actions.waitForElementToShow('[data-test~=ring-query-assist-popup]');
       }).
       capture('input', (actions, find) => {
         actions.sendKeys(find('[data-test=ring-query-assist-input]'), 'test ');
-        actions.executeJS(function hideCaret() {
-          document.querySelector(
-            '[data-test=ring-query-assist-last-letter]'
-          ).style = 'color: transparent;';
-        });
+        actions.executeJS(hideCaret);
         actions.waitForElementToShow('[data-test~=ring-query-assist-popup]');
       });
   });
