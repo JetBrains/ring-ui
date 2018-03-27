9b941eabf96fee5f44d6a41820049489ddd06fdb
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-03-27 05:15:44 +0300

    [publish] prevent animations in dialog gemini screenshots

--- components/dialog-ng/dialog-ng.gemini.js
+++ components/dialog-ng/dialog-ng.gemini.js
@@ -3,14 +3,14 @@
 gemini.suite('Dialog Ng', () => {
   gemini.suite('dialog', child => {
     child.
-      setUrl('dialog-ng/dialog-ng.html').
+      setUrl('dialog-ng/dialog-ng.html?block-animations').
       setCaptureElements(['*[data-test~=ring-dialog]']).
       capture('dialog');
   });
 
   gemini.suite('dialog with custom styles', child => {
     child.
-      setUrl('dialog-ng/dialog-ng-with-overridden-styles.html').
+      setUrl('dialog-ng/dialog-ng-with-overridden-styles.html?block-animations').
       setCaptureElements(['*[data-test~=ring-dialog]']).
       capture('dialog with custom styles');
   });
