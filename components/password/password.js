e74583e5ea382aeab4fd53ba29afaf4098eda70a
Max Erekhinskiy <maxim.erehinskiy@jetbrains.com>
2018-03-30 19:03:53 +0300

    RG-1750 React component for password strength  display
    (cherry picked from commit 2f855d3)

---
+++ components/password/password.js
@@ -0,0 +1,96 @@
+import React, {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import Input from '../input/input';
+import './password.scss';
+
+/**
+ * @name Password
+ * @category Components
+ * @tags Ring UI Language
+ * @framework React
+ * @constructor
+ * @description Input for password with ability to show password strength.
+ * @example-file ./password.examples.html
+ */
+
+const ifStringEmpty = value => {
+  const valueType = typeof value;
+
+  switch (valueType) {
+    case 'string':
+      return value === '';
+    case 'number':
+      return false;
+    default:
+      return true;
+  }
+};
+
+export default class Password extends PureComponent {
+  static propTypes = {
+    value: PropTypes.string,
+    className: PropTypes.string,
+    message: PropTypes.string,
+    description: PropTypes.string,
+    requiredStrength: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    currentStrength: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    forceValid: PropTypes.bool
+  };
+
+  static defaultProps = {
+    requiredStrength: 0,
+    currentStrength: 0
+  };
+
+  render() {
+    const {
+      value,
+      className,
+      requiredStrength, currentStrength,
+      message,
+      description,
+      forceValid,
+      ...restProps
+    } = this.props;
+
+    const required = +requiredStrength;
+    const current = +currentStrength;
+
+    const valid = current > required;
+    const markerColorize = current >= required;
+
+    const getValidClasses = (baseClass, applyColors = true) => classNames(baseClass, {
+      [`${baseClass}_valid`]: ((valid && required) || forceValid) && applyColors,
+      [`${baseClass}_invalid`]: !valid && required && applyColors
+    });
+
+    return (
+      <div
+        data-test="ring-password"
+        className={className}
+      >
+        <Input
+          type={'password'}
+          rows={1}
+          value={value}
+          {...restProps}
+        />
+
+        <div className={'password'}>
+          {!!required && <div className={getValidClasses('password__marker', markerColorize)} style={{left: `${required}%`}}/>}
+          <div className={getValidClasses('password__progress')} style={{width: `${current}%`}}/>
+        </div>
+
+        {Boolean(message) && !ifStringEmpty(value) &&
+          <div className={getValidClasses('password__message')}>{message}</div>
+        }
+
+        {Boolean(description) &&
+          <div className={getValidClasses('password__description')}>{description}</div>
+        }
+      </div>
+    );
+  }
+}
