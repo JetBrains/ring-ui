6ff53821118e043667e7ef6fd5ba83ce0dc34e48
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-05-24 15:09:40 +0300

    RG-1797 reimplement user agreement dialog machinery to make it closer to normal dialogs implementation and UI

--- components/user-agreement/user-agreement.js
+++ components/user-agreement/user-agreement.js
@@ -8,18 +8,15 @@
  */
 
 import React, {PureComponent} from 'react';
-import {findDOMNode} from 'react-dom';
 import PropTypes from 'prop-types';
 
 import Dialog from '../dialog/dialog';
-import {Content} from '../island/island';
+import {Content, Header} from '../island/island';
 import Panel from '../panel/panel';
 import Button from '../button/button';
 import Markdown from '../markdown/markdown';
 
 import style from './user-agreement.css';
-
-const SCROLL_TOLERANCE = 10;
 
 export default class UserAgreement extends PureComponent {
   static propTypes = {
@@ -46,23 +43,7 @@ export default class UserAgreement extends PureComponent {
     scrolledDown: false
   };
 
-  onTextRef = component => {
-    // eslint-disable-next-line react/no-find-dom-node
-    const textRef = this.textRef = findDOMNode(component);
-    if (textRef) {
-      textRef.addEventListener('scroll', this.onScroll);
-      this.onScroll();
-    }
-  };
-
-  onScroll = () => {
-    const textRef = this.textRef;
-    const delta = Math.abs(textRef.scrollTop - (textRef.scrollHeight - textRef.offsetHeight));
-
-    if (delta < SCROLL_TOLERANCE) {
-      this.setState({scrolledDown: true});
-    }
-  };
+  onScrollToBottom = () => this.setState({scrolledDown: true});
 
   render() {
     const {scrolledDown} = this.state;
@@ -76,14 +57,21 @@ export default class UserAgreement extends PureComponent {
         autoFocusFirst={false}
         data-test="user-agreement-dialog-container"
       >
-        <Content>
-          <Markdown source={text} className={style.text} ref={this.onTextRef} tabindex={-1}/>
+        <Header>&nbsp;</Header>
+        <Content
+          scrollableWrapperClassName={style.scrollableWrapper}
+          fade
+          onScrollToBottom={this.onScrollToBottom}
+        >
+          <Markdown source={text} tabindex={-1}/>
         </Content>
         {!preview && (
           <Panel>
             <Button blue disabled={!scrolledDown} onClick={onAccept}>{translations.accept}</Button>
             <Button onClick={onDecline} autoFocus>{translations.decline}</Button>
-            {!scrolledDown && translations.scrollToAccept}
+            {!scrolledDown && (
+              <span className={style.suggestion}>{translations.scrollToAccept}</span>
+            )}
           </Panel>
         )}
         {preview && (
