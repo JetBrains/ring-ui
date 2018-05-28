5b298aa5a85c6790e90d01b7b7a634e97102ca88
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-05-28 16:06:31 +0300

    RG-1801 Move Icon NG to css-modules. Also fixes RG-1799

--- components/icon-ng/icon-ng.gemini.js
+++ components/icon-ng/icon-ng.gemini.js
@@ -2,17 +2,7 @@
 
 gemini.suite('Icon Ng', suite => {
   suite.
-    setUrl('icon-ng/icon-ng.html').
+    setUrl('icon-ng/icon-ng.html?block-animations').
     setCaptureElements('body > div').
-    capture('icons', actions => {
-      actions.executeJS(function disableLoadingButtons() {
-        // ignore is too slow on IE
-        Array.prototype.forEach.call(
-          document.querySelectorAll('.ring-icon_loading'),
-          function removeClass(node) {
-            node.classList.remove('ring-icon_loading');
-          }
-        );
-      });
-    });
+    capture('icons');
 });
