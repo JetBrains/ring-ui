c0754b94b217feb18c374f6215a1bbe98968253b
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-06-08 19:14:38 +0300

    JPS-8689 experimental implementation of IFrame login
    (cherry picked from commit 7e73fd560f21cd5a886b0546750149beb7f82568)

--- components/login-dialog/login-dialog.js
+++ components/login-dialog/login-dialog.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import Dialog from '../dialog/dialog';
+import {Content} from '../island/island';
+import LoaderScreen from '../loader-screen/loader-screen';
 
 import styles from './login-dialog.css';
 
 const HEIGHT = 700;
-const WIDTH = 350;
+const WIDTH = 500;
 /**
  * @name Login Dialog
  * @category Components
@@ -16,11 +17,12 @@ const WIDTH = 350;
  * @description A component that shows an authentication form.
  */
 
-export default class AuthDialog extends Component {
+export default class LoginDialog extends Component {
   static propTypes = {
     show: PropTypes.bool,
     className: PropTypes.string,
     url: PropTypes.string,
+    loader: PropTypes.bool,
     onCancel: PropTypes.func
   };
 
@@ -36,29 +38,28 @@ export default class AuthDialog extends Component {
   };
 
   render() {
-    const {
-      show,
-      className,
-      url
-    } = this.props;
+    const {show, className, url, loader} = this.props;
 
     return (
       <Dialog
         data-test="ring-login-dialog"
         className={className}
-        contentClassName={classNames(className)}
+        contentClassName={styles.dialogContent}
         onEscPress={this.onEscPress}
         trapFocus={false}
         show={show}
       >
-        <div>
+
+        {loader && <LoaderScreen/>}
+
+        <Content>
           <iframe
             height={HEIGHT}
             width={WIDTH}
             src={url}
             className={styles.iFrame}
           />
-        </div>
+        </Content>
       </Dialog>
     );
   }
