172dac22904f5ae6d72b31eef784a1d00f5bb27b
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-07-13 13:47:53 +0300

    DataTests utility improvement
    It now supports passing object as classNames does:
    dataTests('foo', {
      bar: true,
      test: false
    });

--- components/global/data-tests.js
+++ components/global/data-tests.js
@@ -1,5 +1,17 @@
+
+function expandMap(attrsMap) {
+  return Object.entries(attrsMap).
+    reduce((result, [key, value]) => (value ? [...result, key] : result), []);
+}
+
 export default function joinDataTestAttributes(...attrs) {
   return attrs.
     filter(attr => !!attr).
+    reduce((result, attr) => {
+      if (typeof attr === 'object') {
+        return [...result, ...expandMap(attr)];
+      }
+      return [...result, attr];
+    }, []).
     join(' ');
 }
