afe1faf156c02bdfa93a206b81572a9ebb1d66ca
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-07-13 13:47:53 +0300

    DataTests utility improvement
    It now supports passing object as classNames does:
    dataTests('foo', {
      bar: true,
      test: false
    });

---
+++ components/global/data-tests.test.js
@@ -0,0 +1,18 @@
+import dataTests from './data-tests';
+
+describe('dataTests', () => {
+  it('should join strings', () => {
+    dataTests('foo', 'bar').should.be.equal('foo bar');
+  });
+
+  it('should ignore nulls', () => {
+    dataTests('foo', null, undefined, '', 'bar').should.be.equal('foo bar');
+  });
+
+  it('should support map-like arguments', () => {
+    dataTests('foo', {
+      bar: true,
+      notBar: false
+    }).should.be.equal('foo bar');
+  });
+});
