4704b0f2763a4388222fb0135c28fb9ee2af1f42
Maksim Ryzhikov <rv.maksim@gmail.com>
2018-07-16 12:55:02 +0300

    chore: setup wallaby

--- wallaby.config.js
+++ wallaby.config.js
@@ -5,68 +5,20 @@ const wallabyWebpack = require('wallaby-webpack');
 const webpackConfig = require('./webpack.config');
 const webpackTestConfig = require('./webpack-test.config');
 
-const postcssLoaderPath = require.resolve('postcss-loader');
-const addConfig = rule => {
-  rule.use.forEach((loader, index) => {
-    if (loader === postcssLoaderPath) {
-      rule.use[index] = {
-        loader,
-        options: {
-          config: __dirname
-        }
-      };
-    }
-  });
-};
-
 module.exports = wallaby => {
   webpackConfig.componentsPath.
     push(path.join(wallaby.projectCacheDir, 'components'));
   webpackTestConfig.entryPatterns = [
     'test-helpers/mocha-globals.js',
+    'test-helpers/enzyme-configuration.js',
     'components/**/*.test.js'
   ];
 
-  addConfig(webpackConfig.loaders.scssLoader);
-  addConfig(webpackConfig.loaders.cssLoader);
-
   return {
     files: [
-      // test helpers
-      {
-        pattern: 'test-helpers/mocha-globals.js',
-        instrument: false,
-        load: false
-      },
-
-      // test frameworks
-      {
-        pattern: 'node_modules/sinon/pkg/sinon.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/chai/chai.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/chai-as-promised/lib/chai-as-promised.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/chai-dom/chai-dom.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/sinon-chai/lib/sinon-chai.js',
-        instrument: false
-      },
-      {
-        pattern: 'node_modules/karma-chai-plugins/chai-adapter.js',
-        instrument: false
-      },
-
-      // files
       {pattern: 'components/**/*.*', load: false},
+      {pattern: 'test-helpers/**/*.js', load: false},
+      {pattern: 'postcss.config.js', instrument: false, load: false},
       {pattern: 'components/**/*.test.js', ignore: true}
     ],
 
