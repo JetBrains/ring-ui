156ff687b4f6ab8485abf312a9a3ffc6c5703231
Alexander Anisimov <Alexander.Anisimov@jetbrains.com>
2018-07-19 22:53:51 +0200

    Ported RG-1842 Support custom list item in Query Assist from `master` branch [publish]

---
+++ components/query-assist/query-assist__suggestions.js
@@ -0,0 +1,94 @@
+import React from 'react';
+
+import List from '../list/list';
+
+const ICON_ID_LENGTH = 44;
+
+
+export default class QueryAssistSuggestions {
+
+  static createKey(suggestion) {
+    const {
+      description,
+      group,
+      icon,
+      option,
+      prefix = '',
+      suffix = ''
+    } = suggestion;
+
+    return prefix + option + suffix + group + description +
+      (icon ? icon.substring(icon.length - ICON_ID_LENGTH) : '');
+  }
+
+  static renderLabel(suggestion) {
+    const {
+      className,
+      matchingStart,
+      matchingEnd,
+      option,
+      prefix = '',
+      suffix = ''
+    } = suggestion;
+
+    let wrappedOption;
+    let before = '';
+    let after = '';
+
+    if (matchingStart !== matchingEnd) {
+      before = option.substring(0, matchingStart);
+      wrappedOption = (
+        <span className={'ring-list__highlight'}>
+          {option.substring(matchingStart, matchingEnd)}
+        </span>
+      );
+      after = option.substring(matchingEnd);
+    } else {
+      wrappedOption = option;
+    }
+
+    const wrappedPrefix = prefix && <span className={'ring-list__service'}>{prefix}</span>;
+    const wrappedSuffix = suffix && <span className={'ring-list__service'}>{suffix}</span>;
+
+    return (
+      <span className={className}>
+        {wrappedPrefix}{before}{wrappedOption}{after}{wrappedSuffix}
+      </span>
+    );
+  }
+
+  static renderGroupSeparator(suggestion, prevSuggestion) {
+    const {group, option} = suggestion;
+    const {SEPARATOR} = List.ListProps.Type;
+
+    if (prevSuggestion !== group) {
+      return {
+        key: option + group + SEPARATOR,
+        description: group,
+        rgItemType: SEPARATOR
+      };
+    }
+
+    return null;
+  }
+
+  static renderList(suggestions, suggestionRenderer) {
+    const renderedSuggestions = [];
+
+    suggestions.forEach((suggestion, index, arr) => {
+      const prevSuggestionGroup = arr[index - 1] && arr[index - 1].group;
+      const groupSeparator = QueryAssistSuggestions.renderGroupSeparator(
+        suggestion, prevSuggestionGroup
+      );
+
+      if (groupSeparator) {
+        renderedSuggestions.push(groupSeparator);
+      }
+
+      renderedSuggestions.push(suggestionRenderer(suggestion));
+    });
+
+    return renderedSuggestions;
+  }
+
+}
