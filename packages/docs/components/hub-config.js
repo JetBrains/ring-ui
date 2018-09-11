174f2f2455580edb347c052f068fb46b1eea103c
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2018-09-11 21:16:54 +0200

    Update redirect URL

--- packages/docs/components/hub-config.js
+++ packages/docs/components/hub-config.js
@@ -4,10 +4,11 @@ import 'core-js/modules/es6.string.starts-with';
 const {serverUri, clientId} = hubConfig;
 
 const RING_UI_BASE = '/ring-ui/';
+const branch = 'master';
 
 const {origin, pathname} = window.location;
 const base = pathname.startsWith(RING_UI_BASE)
-  ? RING_UI_BASE
+  ? `${RING_UI_BASE}/${branch}/index.html`
   : '/';
 
 export default {
