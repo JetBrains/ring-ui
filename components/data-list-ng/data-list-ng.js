4c0a4cf57b42696b35fd6cc28ae77b42a3e79bce
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-09-14 16:38:37 +0300

    Mark migrated Angular components as Ring UI 1.0 ready

--- components/data-list-ng/data-list-ng.js
+++ components/data-list-ng/data-list-ng.js
@@ -1,6 +1,7 @@
 /**
  * @name DataList Ng
  * @category Legacy Angular
+ * @tags Ring UI Language
  * @framework Angular
  * @description Provides an Angular wrapper for DataList.
  * @example-file ./data-list-ng.examples.html
