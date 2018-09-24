8f135ee63f84e5a359b1f75af71c7d1736ab6fd5
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-09-24 16:58:37 +0300

    Fix Error Bubble example

--- components/error-bubble/error-bubble.js
+++ components/error-bubble/error-bubble.js
@@ -9,6 +9,7 @@ import styles from './error-bubble.css';
 /**
  * @name Error Bubble
  * @category Components
+ * @tags Ring UI Language
  * @constructor
  * @description Displays an error bubble near the wrapped input component when a non-empty string is passed to the `error` prop.
  * Passes any prop except `className` down to input.
