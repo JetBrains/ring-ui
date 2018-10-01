8a6fafb14590ed95fa60b6cc2313fa595f23001a
Andrey.Skladchikov <andrey.skladchikov@jetbrains.com>
2018-10-01 21:45:47 +0300

    RG-1900 Revert font changes back to initial state but without Helvetica Neue
    Sergoe UI (system-ui on Win) is removed due to user feedback that it too small. Now it falls back to Arial.
    
    Helvetica Neue is removed because San Francisco is default font on Macs for more than 5 years now, and `-apple-system` seems to be enough.

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,8 @@ module.exports = ctx => {
         customProperties: {
           preserve: true,
           variables: ctx.options.variables
-        }
+        },
+        fontFamilySystemUi: false
       }
     },
     'postcss-flexbugs-fixes': {}
