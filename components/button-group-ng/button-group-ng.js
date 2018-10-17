beaa34c763d901c9cb3e447db2544ee2cb418d49
Denis Lesnik <denis.lesnik@jetbrains.com>
2018-10-17 12:44:41 +0200

    1. Updated button group. Move the font-size:0 hack for HTML template to a separate angular-specific css. Remove that rule for React Component
    2. Removed autoprefixer from package.lock. Updated dependencies.

--- components/button-group-ng/button-group-ng.js
+++ components/button-group-ng/button-group-ng.js
@@ -1,7 +1,10 @@
 import angular from 'angular';
+import classNames from 'classnames';
 import 'dom4';
 
 import styles from '../button-group/button-group.css';
+
+import {buttonGroup} from './button-group-ng.css';
 
 /**
  * @name Button Group Ng
@@ -30,13 +33,14 @@ import styles from '../button-group/button-group.css';
  */
 
 const angularModule = angular.module('Ring.button-group', []);
+const buttonGroupClasses = classNames(styles.buttonGroup, buttonGroup);
 
 function rgButtonGroup() {
   return {
     restrict: 'A',
     link: function link($scope, iElement) {
       const element = iElement[0];
-      element.classList.add(...styles.buttonGroup.split(' '));
+      element.classList.add(...buttonGroupClasses.split(' '));
     }
   };
 }
