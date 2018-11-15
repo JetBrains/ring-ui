f964b01c84ca956f9d3cda689da29d2d17febbdf
Andrey <andrey.skladchikov@gmail.com>
2018-11-15 17:58:43 +0300

    RG-1917 pass dark theme to iFrames

--- packages/docs/components/example-common.js
+++ packages/docs/components/example-common.js
@@ -1,6 +1,8 @@
 import 'iframe-resizer/js/iframeResizer.contentWindow';
 import classNames from 'classnames';
 import URLSearchParams from 'url-search-params'
+import darkVariables from '@jetbrains/ring-ui/components/global/variables_dark';
+import {setRootStyleProperties, resetRootStyleProperties} from '@jetbrains/ring-ui/components/global/dom';
 
 import styles from './example-common.css';
 
@@ -15,3 +17,18 @@ if (window.parent !== window) {
   } catch (error) {
   }
 }
+
+function toggleDarkTheme(isOn) {
+  isOn ? setRootStyleProperties(darkVariables) : resetRootStyleProperties(darkVariables);
+}
+
+window.addEventListener('message', event => {
+  try {
+    const message = JSON.parse(event.data);
+    if (message.type === 'RING_DARK_THEME') {
+      toggleDarkTheme(message.value);
+    }
+  } catch (e) {
+
+  }
+});
--- packages/docs/components/theme-toggle.js
+++ packages/docs/components/theme-toggle.js
@@ -5,16 +5,28 @@ import styles from './index.css';
 import darkVariables from '@jetbrains/ring-ui/components/global/variables_dark';
 import {setRootStyleProperties, resetRootStyleProperties} from '@jetbrains/ring-ui/components/global/dom';
 
-export function toggleDarkTheme(isOn) {
+function toggleDarkTheme(isOn) {
   isOn ? setRootStyleProperties(darkVariables) : resetRootStyleProperties(darkVariables);
+}
+
+function messageToIFrames(isOn) {
+  const message = JSON.stringify({
+    type: 'RING_DARK_THEME',
+    value: isOn
+  });
+
+  [...document.querySelectorAll('iframe')].
+    forEach(iFrame => iFrame.contentWindow.postMessage(message, '*'))
 }
 
 export default class ThemeToggle extends PureComponent {
   state = {dark: false};
 
   onToggle = () => {
-    this.setState({dark: !this.state.dark});
-    toggleDarkTheme(!this.state.dark);
+    const dark = !this.state.dark;
+    this.setState({dark});
+    toggleDarkTheme(dark);
+    messageToIFrames(dark);
   };
 
   render() {
