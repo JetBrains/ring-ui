f0d7d06cb8e05e252639e0fe1106be20932d4827
Andrey <andrey.skladchikov@gmail.com>
2018-11-15 17:25:56 +0300

    RG-1917 create CSS variables overrides for Dark theme

---
+++ components/global/variables_dark.js
@@ -0,0 +1,35 @@
+export default {
+  '--ring-line-color': '#475159',
+  '--ring-borders-color': '#406380',
+  '--ring-icon-color': '#80929d',
+  '--ring-icon-secondary-color': '#80929d',
+  '--ring-border-disabled-color': '#475159',
+  '--ring-border-hover-color': '#70b1e6',
+  '--ring-icon-error-color': '#c22731',
+  '--ring-icon-warning-color': '#cc8b00',
+  '--ring-icon-success-color': '#1c8c32',
+  '--ring-popup-border-components': '0, 42, 76',
+  '--ring-popup-border-color': 'rgba(var(--ring-popup-border-components), 0.1)',
+  '--ring-popup-shadow-color': 'rgba(var(--ring-popup-border-components), 0.15)',
+  '--ring-message-shadow-color': 'rgba(var(--ring-popup-border-components), 0.3)',
+
+  /* Text */
+  '--ring-hint-color': '#80929d',
+  '--ring-link-color': '#70b1e6',
+  '--ring-error-color': '#db5860',
+  '--ring-warning-color': '#eda200',
+  '--ring-success-color': '#59a869',
+  '--ring-text-color': '#bbb',
+  '--ring-heading-color': '#fff',
+  '--ring-secondary-color': '#80929d',
+  '--ring-disabled-color': '#80929d',
+
+  /* Background */
+  '--ring-content-background-color': '#23272b',
+  '--ring-popup-background-color': '#111314',
+  '--ring-sidebar-background-color': '#28343d',
+  '--ring-selected-background-color': '#062640',
+  '--ring-hover-background-color': '#062640',
+  '--ring-message-background-color': 'rgba(0, 21, 38, 0.9)',
+  '--ring-navigation-background-color': '#111314'
+};
