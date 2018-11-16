ca0cb5a6fec31625fd838534622fcd8243b2c508
Andrey <andrey.skladchikov@gmail.com>
2018-11-16 13:55:26 +0300

    RG-1917 extract theme code

---
+++ packages/docs/components/theme.js
@@ -0,0 +1,34 @@
+import {resetRootStyleProperties, setRootStyleProperties} from '../../../components/global/dom';
+import darkVariables from '../../../components/global/variables_dark';
+import Storage from '../../../components/storage/storage';
+
+export const RING_DARK_THEME_MESSAGE = 'RING_DARK_THEME_MESSAGE';
+const STORAGE_KEY = 'ring-ui-dark-mode';
+const storage = new Storage();
+
+function toggleDarkTheme(isDark) {
+  isDark ? setRootStyleProperties(darkVariables) : resetRootStyleProperties(darkVariables);
+}
+
+function messageToIFrames(isDark) {
+  const message = JSON.stringify({
+    type: RING_DARK_THEME_MESSAGE,
+    value: isDark
+  });
+
+  [...document.querySelectorAll('iframe')].
+    forEach(iFrame => iFrame.contentWindow.postMessage(message, '*'))
+}
+
+export async function loadStoredTheme() {
+  const isDark = await storage.get(STORAGE_KEY);
+  toggleDarkTheme(isDark);
+  messageToIFrames(isDark);
+  return isDark;
+}
+
+export async function setTheme(isDark) {
+  toggleDarkTheme(isDark);
+  messageToIFrames(isDark);
+  await this.storage.set(STORAGE_KEY, isDark);
+}
