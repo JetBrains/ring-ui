270c06722d71d93b61e6d0bb45d706841cb62ff0
Andrey <andrey.skladchikov@gmail.com>
2018-11-16 13:55:26 +0300

    RG-1917 extract theme code

--- packages/docs/components/theme-toggle.js
+++ packages/docs/components/theme-toggle.js
@@ -2,29 +2,11 @@ import React, {PureComponent} from 'react';
 
 import Toggle from '../../../components/toggle/toggle';
 import styles from './index.css';
-import darkVariables from '@jetbrains/ring-ui/components/global/variables_dark';
-import Storage from '@jetbrains/ring-ui/components/storage/storage';
-import {setRootStyleProperties, resetRootStyleProperties} from '@jetbrains/ring-ui/components/global/dom';
 import Theme from '@jetbrains/ring-ui/components/global/theme';
+import {loadStoredTheme, setTheme} from './theme';
 
-function toggleDarkTheme(isOn) {
-  isOn ? setRootStyleProperties(darkVariables) : resetRootStyleProperties(darkVariables);
-}
-
-function messageToIFrames(isOn) {
-  const message = JSON.stringify({
-    type: 'RING_DARK_THEME',
-    value: isOn
-  });
-
-  [...document.querySelectorAll('iframe')].
-    forEach(iFrame => iFrame.contentWindow.postMessage(message, '*'))
-}
-
-const STORAGE_KEY = 'ring-ui-dark-mode';
 
 export default class ThemeToggle extends PureComponent {
-  storage = new Storage();
   state = {dark: false};
 
   componentDidMount() {
@@ -32,18 +14,14 @@ export default class ThemeToggle extends PureComponent {
   }
 
   async loadDarkModeState() {
-    const dark = await this.storage.get(STORAGE_KEY);
+    const dark = await loadStoredTheme();
     this.setState({dark});
-    toggleDarkTheme(dark);
-    messageToIFrames(dark);
   }
 
   onToggle = () => {
     const dark = !this.state.dark;
+    setTheme(dark);
     this.setState({dark});
-    toggleDarkTheme(dark);
-    messageToIFrames(dark);
-    this.storage.set(STORAGE_KEY, dark);
   };
 
   render() {
