a4073dd2ba63445664fa133486a85e9185d34fda
Andrey <andrey.skladchikov@gmail.com>
2019-02-08 12:43:26 +0300

    RG-1646 fix tests

--- components/icon/icon.test.js
+++ components/icon/icon.test.js
@@ -20,23 +20,22 @@ describe('Icon', () => {
 
   it('should set size 16', () => {
     const icon = renderIcon({glyph: expandIcon, size: Icon.Size.Size16});
-
-    icon.find('svg').should.have.style('width', '16px');
-    icon.find('svg').should.have.style('height', '16px');
+    icon.should.have.style('width', '16px');
+    icon.should.have.style('height', '16px');
   });
 
   it('should set one custom dimension', () => {
     const icon = renderIcon({glyph: expandIcon, width: 100});
 
-    icon.find('svg').should.have.style('width', '100px');
-    icon.find('svg').should.not.have.style('height');
+    icon.should.have.style('width', '100px');
+    icon.should.not.have.style('height');
   });
 
   it('should set two custom dimensions', () => {
     const icon = renderIcon({glyph: expandIcon, width: 99, height: 66});
 
-    icon.find('svg').should.have.style('width', '99px');
-    icon.find('svg').should.have.style('height', '66px');
+    icon.should.have.style('width', '99px');
+    icon.should.have.style('height', '66px');
   });
 
   it('should set custom class', () => {
