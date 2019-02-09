501c655464bddce39bf653476427e93060cffc76
Andrey <andrey.skladchikov@gmail.com>
2019-02-09 20:20:38 +0300

    reorganize Auth code – bring component documentatino back to main file

--- components/auth/auth.js
+++ components/auth/auth.js
@@ -3,6 +3,40 @@ import defaultOnBackendDown from './down-notification';
 import Auth from './auth__core';
 
 /**
+ * @name Auth
+ * @category Utilities
+ * @tags Ring UI Language
+ * @description Authenticates a user in [Hub](https://www.jetbrains.com/hub/).
+ *
+ * @prop {object} config
+ * @prop {string} config.serverUri
+ * @prop {string} config.redirectUri
+ * @prop {string} config.clientId
+ * @prop {boolean=false} config.redirect — use redirects instead of loading the token in the background.
+ * @prop {string[]} config.scope
+ * @prop {string[]} config.optionalScopes
+ * @prop {boolean} config.cleanHash - whether or not location.hash will be cleaned after authorization is completed.
+ * Should be set to false in angular > 1.2.26 apps to prevent infinite redirect in Firefox
+ * @prop {User?} user
+ * @prop {string[]} config.userFields List of user data fields to be returned by auth.requestUser (default list is used in Header.HeaderHelper)
+ * @prop {string[]} config.fetchCredentials
+ *
+ * @param {{
+ *   serverUri: string,
+ *   redirectUri: string?,
+ *   requestCredentials: string?,
+ *   clientId: string?,
+ *   scope: string[]?,
+ *   optionalScopes: string[]?,
+ *   cleanHash: boolean?,
+ *   fetchCredentials: string?,
+ *   userFields: string[]?
+ * }} config
+ *
+ * @example-file ./auth.examples.html
+ */
+
+/**
  * Extend Auth config with non-required and not pure-JS stuff
  */
 Auth.DEFAULT_CONFIG = {
