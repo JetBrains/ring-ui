99a66968e0f39070cce5de1956560344f43f1618
Andrey <andrey.skladchikov@gmail.com>
2019-03-04 12:43:20 +0300

    RG-1958 increase header icons size

--- components/header/tray-icon.js
+++ components/header/tray-icon.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-import Button from '../button/button';
+import Button, {IconSize} from '../button/button';
 
 import styles from './header.css';
 
@@ -15,6 +15,7 @@ export default class TrayIcon extends Component {
 
   static defaultProps = {
     ...Button.defaultProps,
+    iconSize: IconSize.Size20,
     theme: Button.Theme.DARK
   };
 
