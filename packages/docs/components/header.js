38795421ebbeb33fa829e8e2f9c6fcfa30882692
Andrey <andrey.skladchikov@gmail.com>
2019-03-05 19:30:21 +0300

    RG-1958 update icons to get fixed header sized icons

--- packages/docs/components/header.js
+++ packages/docs/components/header.js
@@ -68,7 +68,7 @@ class SiteHeader extends PureComponent {
           <Search categories={categories}/>
           <TrayIcon
             icon={gitHubLogo}
-            iconSize={22}
+            iconSize={24}
             href="https://github.com/JetBrains/ring-ui"
             target="_blank"
             title="GitHub repo"
