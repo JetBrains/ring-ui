8aa5a764473bd70802537f17544e4364b1c3a9aa
Andrey <andrey.skladchikov@gmail.com>
2019-04-12 14:15:57 +0300

    RG-1864 switch to postcss-preset-env

--- extract-css-vars.js
+++ extract-css-vars.js
@@ -4,6 +4,22 @@
  *
  * Inspired by https://github.com/jonathantneal/postcss-export-custom-variables
  */
+
+// eslint-disable-next-line no-console
+console.warn(`
+  **** WARNING: Ring UI extract-css-vars.js is deprecated since 2.0 in favor of "importFrom" option of postcss-preset-env ****
+  **** Consider using the following way of including variables: ****
+  'postcss-preset-env': {
+    importFrom: require.resolve('@jetbrains/ring-ui/components/global/variables.css'),
+    features: {
+      'postcss-custom-properties': {
+        preserve: true
+      }
+    }
+  }
+  **** More details https://github.com/postcss/postcss-custom-properties#importfrom https://github.com/csstools/postcss-preset-env#importfrom ***
+`);
+
 const fs = require('fs');
 
 const postcss = require('postcss');
