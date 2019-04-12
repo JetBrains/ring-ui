ef3298f60a43888d0fa7c16383ce0089c3269a51
Andrey <andrey.skladchikov@gmail.com>
2019-04-12 16:08:21 +0300

    fixup! RG-1864 switch to postcss-preset-env

--- packages/generator/app/templates/postcss.config.js
+++ packages/generator/app/templates/postcss.config.js
@@ -1,10 +1,10 @@
-module.exports = ctx => ({
+module.exports = () => ({
   plugins: [
     require('postcss-import'),
     require('postcss-preset-env')({
       features: {
         stage: 3, // See https://cssdb.org/#staging-process
-        importFrom: require.resolve(__dirname, '@jetbrains/ring-ui/components/global/variables.css'),
+        importFrom: require.resolve('@jetbrains/ring-ui/components/global/variables.css'),
         features: {
           'nesting-rules': true,
           'custom-properties': {
