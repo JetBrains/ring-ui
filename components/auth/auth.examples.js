62fdaa6e9f5c11763f6a1458efe8fd8598d74ac1
Andrey <andrey.skladchikov@gmail.com>
2019-04-23 22:47:53 +0300

    RG-1894 implement auth example in storybook

---
+++ components/auth/auth.examples.js
@@ -0,0 +1,42 @@
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import Auth from '../auth/auth';
+import authDialogService from '../auth-dialog-service/auth-dialog-service';
+import hubConfig from '../../packages/docs/components/hub-config';
+import '../link/link__legacy.css';
+
+storiesOf('Auth', module).
+  add('simple', () => {
+    const log = action('auth-log');
+
+    const auth = new Auth(hubConfig);
+    auth.setAuthDialogService(authDialogService);
+
+    const node = document.createElement('div');
+    node.innerHTML = '<a href="javascript:void(0)" id="force-update" class="ring-link">Force token update</a>';
+
+    (async () => {
+      try {
+        const location = await auth.init();
+        log('Location to restore:', location);
+        const token = await auth.requestToken();
+        log('Token:', token);
+        const data = await auth.requestUser();
+        log('User profile data:', data);
+      } catch (e) {
+        log('error', e.toString());
+        // eslint-disable-next-line no-console
+        console.error('Auth error', e);
+      }
+
+      const forceUpdateLink = node.querySelector('#force-update');
+
+      forceUpdateLink.addEventListener('click', async () => {
+        const newToken = await auth.forceTokenUpdate();
+        log('Token has been refreshed:', newToken);
+      });
+    })();
+
+    return node;
+  });
