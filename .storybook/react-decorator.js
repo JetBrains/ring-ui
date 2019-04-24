458e0f8e012bf4e81eb0ba959a1dbaf05d1f5fbc
Andrey <andrey.skladchikov@gmail.com>
2019-04-24 14:22:37 +0300

    RG-1894 add experimental react-decorator to AlertService example

---
+++ .storybook/react-decorator.js
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom';
+import addons from '@storybook/addons';
+import {REGISTER_SUBSCRIPTION, STORY_CHANGED} from '@storybook/core-events'
+
+const reactDecorator = () => {
+  const node = document.createElement('div');
+
+  const channel = addons.getChannel();
+  const unmount = () => ReactDOM.unmountComponentAtNode(node);
+  const subscription = () => {
+    channel.on(STORY_CHANGED, unmount);
+    return () => channel.removeListener(STORY_CHANGED, unmount);
+  };
+
+  channel.emit(REGISTER_SUBSCRIPTION, subscription);
+
+  return story => {
+    ReactDOM.render(story(), node);
+    return node
+  };
+};
+
+export default reactDecorator;
