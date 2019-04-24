85e03903effade8c677ce29cae07f841d9a0ce86
Andrey <andrey.skladchikov@gmail.com>
2019-04-24 20:31:18 +0300

    RG-1894 move anlytics examples to storybook

---
+++ components/analytics/analytics.examples.js
@@ -0,0 +1,38 @@
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import analytics from '../analytics/analytics';
+import linkStyles from '../link/link.css';
+import AnalyticsCustomPlugin from '../analytics/analytics__custom-plugin';
+
+storiesOf('Components|Analytics', module).
+  add('analytics', () => {
+
+    const node = document.createElement('div');
+
+    node.innerHTML = `
+      <div>
+        <p>Click the link below and check the console output:</p>
+        <div>
+          <a href id="click-me" class="${linkStyles.link}">
+            Track click event
+          </a>
+        </div>
+      </div>
+    `;
+
+    const FLUSH_INTERVAL = 100;
+
+    const customPlugin = new AnalyticsCustomPlugin(events => (
+      action('analytics')('Custom plugin receives:', events[0].category, events[0].action)
+    ), false, FLUSH_INTERVAL);
+
+    analytics.config([customPlugin]);
+
+    node.querySelector('#click-me').addEventListener('click', event => {
+      analytics.trackEvent('test-category', 'test-action');
+      event.preventDefault();
+    });
+
+    return node;
+  });
