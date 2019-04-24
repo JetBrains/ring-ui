e5e75a95bc5c306c07282d67a4731ffe555230ef
Andrey <andrey.skladchikov@gmail.com>
2019-04-24 12:56:29 +0300

    RG-1894 add Welcome story to display README, changelog, contributing

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,8 @@ import './preview.css';
 const req = require.context('../components', true, /\.examples\.js$/);
 
 function loadStories() {
+  // Make welcome stories default
+  require('../components/welcome.examples');
   req.keys().forEach(filename => req(filename));
 }
 
