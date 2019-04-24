64bcb671aa94fe3523e0766a68d4448eb52f48b5
Andrey <andrey.skladchikov@gmail.com>
2019-04-24 20:14:34 +0300

    RG-1894 use react-decorator for Welcome pages

--- components/welcome.examples.js
+++ components/welcome.examples.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {render} from 'react-dom';
 
 import {storiesOf} from '@storybook/html';
 
@@ -7,20 +6,10 @@ import ReadMe from '../README.md';
 import Contributing from '../CONTRIBUTING.md';
 import Changelog from '../CHANGELOG.md';
 import Markdown from '../components/markdown/markdown';
+import reactDecorator from '../.storybook/react-decorator';
 
 storiesOf('Ring UI|Welcome', module).
-  add('Getting Started', () => {
-    const node = document.createElement('div');
-    render(<Markdown source={ReadMe}/>, node);
-    return node;
-  }).
-  add('Contributing', () => {
-    const node = document.createElement('div');
-    render(<Markdown source={Contributing}/>, node);
-    return node;
-  }).
-  add('Changelog', () => {
-    const node = document.createElement('div');
-    render(<Markdown source={Changelog}/>, node);
-    return node;
-  });
+  addDecorator(reactDecorator()).
+  add('Getting Started', () => <Markdown source={ReadMe}/>).
+  add('Contributing', () => <Markdown source={Contributing}/>).
+  add('Changelog', () => <Markdown source={Changelog}/>);
