71e3bf1683a0df00d83592d399e119af365c5d54
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2019-04-25 14:55:20 +0300

    HTTP: Ability to remove

---
+++ components/link/link.examples.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import classNames from 'classnames';
+import hubLogo from '@jetbrains/logos/hub/hub.svg';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+
+import Icon from '../icon';
+import Toggle from '../toggle/toggle';
+
+import Link, {linkHOC, setCompatibilityMode} from './link';
+
+
+storiesOf('Components|Link', module).
+  addDecorator(reactDecorator()).
+  add('all variants', () => {
+    class CustomComponent extends React.Component {
+      render() {
+        // eslint-disable-next-line react/prop-types
+        const {active, activeClassName, className, ...props} = this.props;
+        const classes = classNames(className, {
+          [activeClassName]: active
+        });
+        return (
+          <a
+            {...props}
+            className={classes}
+          />
+        );
+      }
+    }
+
+    const CustomLink = linkHOC(CustomComponent);
+
+    class LinkDemo extends React.Component {
+      state = {compatibility: false};
+
+      changeCompatibility = () => {
+        // eslint-disable-next-line react/no-access-state-in-setstate
+        const newCompat = !this.state.compatibility;
+        setCompatibilityMode(newCompat);
+
+        this.setState({compatibility: newCompat});
+      };
+
+      render() {
+        return (
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+
+            <Link href="#hash">Ordinary link</Link>
+
+            <Link href="#hash" active>Active link (inherits color)</Link>
+
+            <Link href="#hash" pseudo>Pseudo link (no underline on hover)</Link>
+
+            <Link href="#hash">Link with a very long text, wrapping over lines</Link>
+
+            <Link href="#hash">Link with a very long text, wrapping over lines</Link>
+
+            <Link href="#hash" className="hub-link">
+              {WrapText => [
+                <Icon key="icon" glyph={hubLogo} className="hub-icon"/>,
+                <div key="text"><WrapText>Link with non-text content</WrapText></div>
+              ]}
+            </Link>
+
+            <CustomLink href="#hash">Custom link component</CustomLink>
+
+            <CustomLink href="#hash" active>Active custom link component</CustomLink>
+
+            <Toggle onChange={this.changeCompatibility}>Compatibility mode</Toggle>
+          </div>
+        );
+      }
+    }
+
+    return <LinkDemo/>;
+  });
