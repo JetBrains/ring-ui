c6f09a55ae9ac8cd356b9c1b75e0e8df17cbafbe
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 00:33:58 +0300

    RG-1894 add ErrorMessage examples

---
+++ components/error-message-ng/error-message-ng.examples.js
@@ -0,0 +1,26 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import {FrownIcon} from '../icon';
+
+import ErrorMessageNG from './error-message-ng';
+
+storiesOf('Legacy Angular|Error Message Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [ErrorMessageNG]).
+      controller('DemoCtrl', function ctrl() {
+        this.errorIcon = FrownIcon;
+      });
+
+    return `
+      <div ng-controller="DemoCtrl as ctrl">
+        <rg-error-message code="Disconnected" message="No, no one\\'s there." icon="{{ctrl.errorIcon}}" links="[{href:'.',text:'home'}]">
+          Service backend isn't available
+        </rg-error-message>
+      </div>
+    `;
+  });
