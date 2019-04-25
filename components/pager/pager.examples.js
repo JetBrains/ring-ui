169d5851d112e4fa609250311dcc8422cc5a07fa
Max Maximov <max.maximov@gmail.com>
2019-04-26 01:01:40 +0300

    pager examples #RG-1975

--- components/pager/pager.examples.js
+++ components/pager/pager.examples.js
@@ -6,6 +6,21 @@ import reactDecorator from '../../.storybook/react-decorator';
 
 import Pager from './pager';
 
+function getDataFromUrl(name, defaultValue) {
+  const params = new URLSearchParams(location.search);
+  const value = params.get(name);
+  return value ? parseInt(value, 10) : defaultValue;
+}
+
+function hrefGenerator(linkPageNumber, pageSize) {
+  const params = new URLSearchParams(location.search);
+  params.set('page', linkPageNumber);
+  if (pageSize) {
+    params.set('pageSize', pageSize);
+  }
+  return `${location.pathname}?${params}`;
+}
+
 storiesOf('Components|Pager', module).
   addDecorator(reactDecorator()).
   add('basic', () => {
@@ -22,7 +37,7 @@ storiesOf('Components|Pager', module).
             total={total}
             currentPage={currentPage}
             disablePageSizeSelector
-            onPageChange={newCurrentPage => this.setState({currentPage: newCurrentPage})}
+            onPageChange={cp => this.setState({currentPage: cp})}
           />
         );
       }
@@ -45,7 +60,7 @@ storiesOf('Components|Pager', module).
             currentPage={currentPage}
             visiblePagesLimit={3}
             disablePageSizeSelector
-            onPageChange={newCurrentPage => this.setState({currentPage: newCurrentPage})}
+            onPageChange={cp => this.setState({currentPage: cp})}
           />
         );
       }
@@ -68,7 +83,7 @@ storiesOf('Components|Pager', module).
             currentPage={currentPage}
             visiblePagesLimit={5}
             disablePageSizeSelector
-            onPageChange={newCurrentPage => this.setState({currentPage: newCurrentPage})}
+            onPageChange={cp => this.setState({currentPage: cp})}
           />
         );
       }
@@ -91,7 +106,7 @@ storiesOf('Components|Pager', module).
             currentPage={currentPage}
             visiblePagesLimit={5}
             disablePageSizeSelector
-            onPageChange={newCurrentPage => this.setState({currentPage: newCurrentPage})}
+            onPageChange={cp => this.setState({currentPage: cp})}
           />
         );
       }
@@ -114,8 +129,109 @@ storiesOf('Components|Pager', module).
             total={total}
             currentPage={currentPage}
             pageSize={pageSize}
-            onPageChange={newCurrentPage => this.setState({currentPage: newCurrentPage})}
-            onPageSizeChange={newPageSize => this.setState({pageSize: newPageSize})}
+            onPageChange={cp => this.setState({currentPage: cp})}
+            onPageSizeChange={ps => this.setState({pageSize: ps})}
+          />
+        );
+      }
+    }
+
+    return <PagerDemo/>;
+  }).
+  add('open total', () => {
+    class PagerDemo extends Component {
+      state = {
+        total: 10,
+        currentPage: 1,
+        pageSize: 10
+      }
+
+      render() {
+        const {total, currentPage, pageSize} = this.state;
+        return (
+          <Pager
+            total={total}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            openTotal={total < 100}
+            onPageChange={cp => this.setState({currentPage: cp})}
+            onLoadPage={cp => this.setState({currentPage: cp, total: total + 10})}
+            visiblePagesLimit={5}
+            disablePageSizeSelector
+          />
+        );
+      }
+    }
+
+    return <PagerDemo/>;
+  }).
+  add('open total and history support', () => {
+    class PagerDemo extends Component {
+      state = {
+        total: 999,
+        currentPage: getDataFromUrl('page', 1),
+        pageSize: getDataFromUrl('pageSize', 20)
+      };
+
+      componentDidMount() {
+        history.replaceState(this.state, '', hrefGenerator(this.state.currentPage, this.state.pageSize));
+        window.addEventListener('popstate', this.onPopstate);
+      }
+
+      componentWillUnmount() {
+        window.removeEventListener('popstate', this.onPopstate);
+      }
+
+      onPopstate = () => {
+        if (window.history.state && window.history.state.currentPage && window.history.state.pageSize) {
+          this.setState({pageSize: window.history.state.pageSize, currentPage: window.history.state.currentPage});
+        }
+      }
+
+      handlePageChange(update) {
+        this.setState(
+          update,
+          () => history.pushState(this.state, '', hrefGenerator(this.state.currentPage, this.state.pageSize))
+        );
+      }
+
+      render() {
+        const {total, currentPage, pageSize} = this.state;
+        return (
+          <Pager
+            total={total}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            hrefFunc={hrefGenerator}
+            visiblePagesLimit={5}
+            openTotal={total < 2000}
+            onPageChange={cp => this.handlePageChange({currentPage: cp})}
+            onPageSizeChange={ps => this.handlePageChange({currentPage: 1, pageSize: ps})}
+            onLoadPage={cp => this.handlePageChange(prevState => ({currentPage: cp, total: prevState.total + 500}))}
+          />
+        );
+      }
+    }
+
+    return <PagerDemo/>;
+  }).
+  add('reload on current page change', () => {
+    class PagerDemo extends Component {
+      state = {
+        total: 120,
+        currentPage: getDataFromUrl('page', 1),
+        pageSize: getDataFromUrl('pageSize', 20)
+      }
+
+      render() {
+        const {total, currentPage, pageSize} = this.state;
+        return (
+          <Pager
+            total={total}
+            currentPage={currentPage}
+            pageSize={pageSize}
+            hrefFunc={hrefGenerator}
+            visiblePagesLimit={5}
           />
         );
       }
