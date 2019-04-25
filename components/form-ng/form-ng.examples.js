5f9c0e202bdbfa2c083e2f143d67a8e789279383
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 00:50:25 +0300

    RG-1894 add form examples

---
+++ components/form-ng/form-ng.examples.js
@@ -0,0 +1,64 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import '../form/form.scss';
+
+import ButtonNG from '../button-ng/button-ng';
+import SelectNG from '../select-ng/select-ng';
+import CheckboxNG from '../checkbox-ng/checkbox-ng';
+import FormNG from '../form-ng/form-ng';
+import InputNG from '../input-ng/input-ng';
+
+
+storiesOf('Legacy Angular|Form Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [FormNG, CheckboxNG, SelectNG, ButtonNG, InputNG]).
+      controller('FormExampleCtrl', function ctrl() {
+        this.checkedModel = true;
+        this.data = [{name: 'Ada'}, {name: 'Nik'}];
+      });
+
+    return `
+      <div style="width: 700px"
+        ng-controller="FormExampleCtrl as formExampleCtrl">
+  
+        <form class="ring-form" name="ExampleForm" novalidate>
+          <span class="ring-form__title">Form Title</span>
+  
+          <div class="ring-form__group">
+            <rg-input size="M" required ng-model="formExampleCtrl.inputValue" label="Medium Input" name="inputMD" type="text"></rg-input>
+            <div rg-error-bubble="ExampleForm.inputMD"></div>
+          </div>
+  
+          <div class="ring-form__group">
+            <rg-select options="item.name for item in formExampleCtrl.data track by item.name"></rg-select>
+            <div class="ring-form__control__description">I am a simple description</div>
+          </div>
+  
+          <div class="ring-form__group">
+            <div>
+              <rg-checkbox ng-model="formExampleCtrl.checkedModel">Checkbox 1</rg-checkbox>
+            </div>
+            <div>
+              <rg-checkbox ng-model="formExampleCtrl.checkedModel">Checkbox 2</rg-checkbox>
+            </div>
+            <div>
+              <rg-checkbox>Checkbox 3</rg-checkbox>
+            </div>
+            <div>
+              <rg-checkbox>Checkbox 4</rg-checkbox>
+            </div>
+          </div>
+  
+          <div class="ring-form__footer">
+            <rg-button mode="primary">Save</rg-button>
+            <rg-button>Cancel</rg-button>
+          </div>
+        </form>
+      </div>
+    `;
+  });
