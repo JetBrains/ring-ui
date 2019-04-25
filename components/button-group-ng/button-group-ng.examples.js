7ca1d6dcebfeae1903f1872fc0e597c2a9ee29f9
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 17:27:36 +0300

    RG-1975 add button group storybook examples

---
+++ components/button-group-ng/button-group-ng.examples.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import ButtonNG from '../button-ng/button-ng';
+import ButtonGroupNG from '../button-group-ng/button-group-ng';
+
+storiesOf('Legacy Angular|Button Group Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [ButtonNG, ButtonGroupNG]);
+
+    return `
+      <div rg-button-group>
+        <span rg-button-group-caption>Side</span>
+        <rg-button>Left</rg-button>
+        <rg-button>Right</rg-button>
+      </div>
+    `;
+  });
