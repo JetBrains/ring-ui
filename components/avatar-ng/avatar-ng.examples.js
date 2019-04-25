c623912b6ff0ac61452735208e1b8cb4fe70193a
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 19:21:01 +0300

    RG-1975 add avatar examples

---
+++ components/avatar-ng/avatar-ng.examples.js
@@ -0,0 +1,24 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+import hubConfig from '../../packages/docs/components/hub-config';
+import {Size as AvatarSize} from '../avatar/avatar';
+import AvatarNG from '../avatar-ng/avatar-ng';
+
+storiesOf('Legacy Angular|Avatar Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [AvatarNG]).
+      controller('testCtrl', function controller() {
+        this.AvatarSize = AvatarSize;
+        this.avatarUrl = `${hubConfig.serverUri}/api/rest/avatar/default?username=Jet%20Brains`;
+      });
+
+    return `
+      <div ng-controller="testCtrl as $ctrl">
+        <rg-avatar size="$ctrl.AvatarSize.Size32" url="$ctrl.avatarUrl"></rg-avatar>
+      </div>
+    `;
+  });
