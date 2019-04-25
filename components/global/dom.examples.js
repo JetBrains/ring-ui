e75057405ab5a024e129fb735539c8aacfb43bef
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 00:11:50 +0300

    RG-1894 add DOM examples

---
+++ components/global/dom.examples.js
@@ -0,0 +1,31 @@
+import {storiesOf} from '@storybook/html';
+
+import {getStyles, getRect, getPixelRatio, getWindowHeight} from '../global/dom';
+
+storiesOf('Utilities|DOM', module).
+  add('basic', () => {
+    const node = document.createElement('div');
+
+    node.innerHTML = `
+      <div id="rect-target" style="min-width: 200px"></div>
+      <div id="report"></div>
+    `;
+
+    function calc() {
+      const rectTarget = node.querySelector('#rect-target');
+      rectTarget.innerHTML = `
+        Element min-width = ${getStyles(rectTarget).minWidth} <br/>
+        Element rect = ${JSON.stringify(getRect(rectTarget))} <br/>
+      `;
+
+      const report = node.querySelector('#report');
+      report.innerHTML = `
+       Pixel ratio = ${getPixelRatio()} <br/>
+       Window height = ${getWindowHeight()} <br/>
+      `;
+    }
+
+    setTimeout(calc);
+
+    return node;
+  });
