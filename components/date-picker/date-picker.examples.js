627e9a81f1825205e4d73a6699ca9108710b20e5
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 23:36:21 +0300

    RG-1894 add DatePicker examples

---
+++ components/date-picker/date-picker.examples.js
@@ -0,0 +1,79 @@
+import React, {Component} from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import DatePicker from '../date-picker/date-picker';
+
+storiesOf('Components|Date Picker', module).
+  addDecorator(reactDecorator()).
+  add('single date', () => {
+    class DatePickerExample extends Component {
+      state = {date: '01.01.18'};
+
+      setDate = date => {
+        this.setState({date});
+      };
+
+      render() {
+        return (
+          <div>
+            <DatePicker
+              date={this.state.date}
+              onChange={this.setDate}
+            />
+          </div>
+        );
+      }
+    }
+
+    return <DatePickerExample/>;
+  }).
+  add('range', () => {
+    class DatePickerExample extends Component {
+      state = {
+        from: '1 January 2018',
+        to: '15 February 2018'
+      };
+
+      setRange = ({from, to}) => {
+        this.setState({from, to});
+      };
+
+      render() {
+        return (
+          <div>
+            <DatePicker
+              from={this.state.from}
+              to={this.state.to}
+              onChange={this.setRange}
+              range
+            />
+          </div>
+        );
+      }
+    }
+    return <DatePickerExample/>;
+  }).
+  add('clearable', () => {
+    class DatePickerExample extends Component {
+      state = {date: '01.01.18'};
+
+      setDate = date => {
+        this.setState({date});
+      };
+
+      render() {
+        return (
+          <div>
+            <DatePicker
+              date={this.state.date}
+              onChange={this.setDate}
+              clear
+            />
+          </div>
+        );
+      }
+    }
+
+    return <DatePickerExample/>;
+  });
