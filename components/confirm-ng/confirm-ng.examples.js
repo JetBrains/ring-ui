cb88ee72b322acdebfe5690cb3137d8dd538e71b
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 20:11:24 +0300

    RG-1975 add confirm examples

--- components/confirm-ng/confirm-ng.examples.js
+++ components/confirm-ng/confirm-ng.examples.js
@@ -5,15 +5,18 @@ import {storiesOf} from '@storybook/html';
 
 import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
 import ConfirmNG from '../confirm-ng/confirm-ng';
+import {hideConfirm} from '../confirm-service/confirm-service';
 
 storiesOf('Legacy Angular|Confirm Ng', module).
   addDecorator(angularDecorator()).
   add('basic', () => {
     angular.module(APP_NAME, [ConfirmNG]).
-      controller('TestCtrl', function controller(confirm) {
+      controller('TestCtrl', function controller($scope, confirm) {
         confirm('Do you really wish to proceed?', 'A description of an action that is about to take place.').
           then(action('confirmed')).
           catch(action('declined'));
+
+        $scope.$on('$destroy', hideConfirm);
       });
 
     return `
---
+++ components/confirm-service/confirm-service.examples.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import confirm, {hideConfirm} from '../confirm-service/confirm-service';
+import Button from '../button/button';
+
+storiesOf('Components|Confirm Service', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => {
+    class ConfirmDemo extends React.Component {
+      componentDidMount() {
+        this.showConfirm();
+      }
+
+      componentWillUnmount() {
+        hideConfirm();
+      }
+
+      showConfirm = () => confirm({text: 'Do you really wish to proceed?'}).
+        then(action('Confirmed')).
+        catch(action('Rejected'));
+
+      showWithAnotherText = () => confirm({
+        text: 'There is another confirmation',
+        description: 'Confirmation description',
+        confirmLabel: 'OK',
+        rejectLabel: 'Cancel',
+        cancelIsDefault: true,
+        onBeforeConfirm: () => new Promise(resolve => setTimeout(resolve, 1000))
+      }).
+        then(action('Confirmed')).
+        catch(action('Rejected'));
+
+      render() {
+        return (
+          <div>
+            <Button onClick={this.showConfirm}>Show confirm</Button>
+            <Button onClick={this.showWithAnotherText}>Show another message</Button>
+          </div>
+        );
+      }
+    }
+
+    return <ConfirmDemo/>;
+  });
