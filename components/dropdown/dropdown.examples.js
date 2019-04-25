a180fa81f60b0d4ff2bc289c19064459b1d0a5f5
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 00:19:20 +0300

    RG-1894 add Dropdown examples

---
+++ components/dropdown/dropdown.examples.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import {ChevronDownIcon} from '../icon';
+import Dropdown from '../dropdown/dropdown';
+import Popup from '../popup/popup';
+import PopupMenu from '../popup-menu/popup-menu';
+import Button from '../button/button';
+import Link from '../link/link';
+
+storiesOf('Components|Dropdown', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <Dropdown
+      anchor="Click me"
+    >
+      <Popup>Popup content</Popup>
+    </Dropdown>
+  )).
+  add('with custom anchor and popup', () => (
+    <Dropdown
+      anchor={<Button delayed>Edit</Button>}
+    >
+      <PopupMenu
+        closeOnSelect
+        data={['Cut', 'Copy', 'Paste'].map(label => ({label}))}
+      />
+    </Dropdown>
+  )).
+  add('with activeClassName', () => (
+    <Dropdown
+      className="chevron"
+      activeClassName="rotated"
+      anchor={<Button icon={ChevronDownIcon}/>}
+    >
+      <Popup>Popup content</Popup>
+    </Dropdown>
+  ), {
+    cssresources: [{
+      id: 'example-styles',
+      picked: true,
+      code: `
+<style>
+  .chevron svg {
+    transition: transform 0.3s ease-out;
+    transform-origin: 50% 40%;
+    transform: rotate(0deg);
+  }
+  
+  .rotated svg {
+    transform: rotate(180deg);
+  }
+</style>
+      `
+    }]
+  }).
+  add('with hover mode', () => (
+    <Dropdown
+      anchor="Hover over me"
+      hoverMode
+    >
+      <Popup>
+        <div>Outer popup</div>
+        <Popup>Inner popup</Popup>
+      </Popup>
+    </Dropdown>
+  )).
+  add('with hover mode and disabled click mode', () => (
+    <Dropdown
+      anchor={<Link href="">Hover over me</Link>}
+      clickMode={false}
+      hoverMode
+    >
+      <Popup>Popup content</Popup>
+    </Dropdown>
+  ));
