aa58b3e147da627a01572ab9975b3cfb5a73b961
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 23:26:47 +0300

    RG-1894 add contenteditable example

---
+++ components/contenteditable/contenteditable.examples.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+
+import ContentEditable from './contenteditable';
+
+storiesOf('Components|ContentEditable', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <div>
+      <ContentEditable className="my-input">
+        <span>text <b>bold text</b> text</span>
+      </ContentEditable>
+      <ContentEditable className="my-input" disabled>
+        <span>text <b>bold text</b> text</span>
+      </ContentEditable>
+    </div>
+  ), {
+    cssresources: [{
+      id: 'example-styles',
+      picked: true,
+      code: `
+<style>
+    .my-input {
+      padding-left: 4px;
+    }
+
+    .my-input[disabled] {
+      border: 1px solid var(--ring-border-disabled-color);
+      background-color: var(--ring-disabled-color);
+    }
+</style>
+      `
+    }]
+  });
