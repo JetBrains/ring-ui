13398eb53cb177559801827a33260fdf2b01b588
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 00:33:58 +0300

    RG-1894 add ErrorMessage examples

---
+++ components/error-message/error-message.examples.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import {FrownIcon} from '../icon';
+import Link from '../link/link';
+
+import ErrorMessage from './error-message';
+
+storiesOf('Components|Error Message', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <div style={{height: '300px'}}>
+      <ErrorMessage
+        icon={FrownIcon}
+        code="Disconnected"
+        message="no answer from server."
+        description="Please try again soon."
+      >
+        <Link href="/">Go to the home page</Link>
+      </ErrorMessage>
+    </div>
+  ));
