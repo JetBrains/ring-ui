09a62234b45a82d131fb9307f76b161018f11937
Andrey <andrey.skladchikov@gmail.com>
2019-04-25 16:03:46 +0300

    RG-1975 add button toolbar storybook examples

---
+++ components/button-toolbar-ng/button-toolbar-ng.examples.js
@@ -0,0 +1,28 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import ButtonNG from '../button-ng/button-ng';
+import ButtonGroupNg from '../button-group-ng/button-group-ng';
+
+import ButtonToolbarNG from './button-toolbar-ng';
+
+storiesOf('Legacy Angular|Button Toolbar Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [ButtonNG, ButtonToolbarNG, ButtonGroupNg]);
+
+    return `
+      <div rg-button-toolbar>
+        <rg-button mode="primary" delayed="true">Run</rg-button>
+        <div rg-button-group>
+          <rg-button>Button one</rg-button>
+          <rg-button>Button two</rg-button>
+          <rg-button disabled>Button three</rg-button>
+        </div>
+        <rg-button>Another action</rg-button>
+      </div>
+    `;
+  });
