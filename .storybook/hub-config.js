385e8da4611022aa44379403ae15e569d46a6e36
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 21:47:12 +0300

    RG-1894 use auth example as a landing page

--- .storybook/hub-config.js
+++ .storybook/hub-config.js
@@ -1,11 +1,19 @@
 /* global hubConfig */
 const {serverUri, clientId} = hubConfig;
 
+const RING_UI_BASE = '/ring-ui/';
+const branch = 'master';
+
+const {origin, pathname} = window.location;
+const base = pathname.startsWith(RING_UI_BASE)
+  ? `${RING_UI_BASE}/${branch}/`
+  : '/';
+
 export default {
   reloadOnUserChange: false,
   embeddedLogin: true,
   serverUri,
   clientId,
   requestCredentials: 'skip',
-  redirectUri: window.location.href
+  redirectUri: `${origin + base}iframe.html?id=utilities-auth--authorization`
 };
