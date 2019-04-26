711f56ce243bd30cb8059bcb72493d59c6f763a5
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2019-04-26 17:24:57 +0300

    RG-1975 Move all examples to storybook: Loader examples

---
+++ components/loader/loader.examples.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+
+import Loader from './loader';
+
+
+storiesOf('Components|Loader', module).
+  addDecorator(reactDecorator()).
+  add('Loader', () => {
+    class Example extends React.Component {
+      render() {
+        return (
+          <Loader message="Loading..."/>
+        );
+      }
+    }
+
+    return <Example/>;
+  });
