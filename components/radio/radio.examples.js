756e6ca656eb2870790a6684a47f073fbbac174b
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 19:24:57 +0300

    RG-1975 add radio examples

---
+++ components/radio/radio.examples.js
@@ -0,0 +1,37 @@
+import React, {Fragment} from 'react';
+import {storiesOf} from '@storybook/html';
+import {withState} from 'recompose';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import Radio from '../radio/radio';
+
+storiesOf('Components|Radio', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => {
+    const RadioExample = withState('value', 'onChange', 'one')(props => (
+      <Fragment>
+        Selected: {props.value}
+        <Radio {...props}>
+          <Radio.Item value="one">One</Radio.Item>
+          <Radio.Item value="two">Two</Radio.Item>
+          <Radio.Item value="three">Three</Radio.Item>
+        </Radio>
+      </Fragment>
+    ));
+
+    return <RadioExample/>;
+  }).
+  add('uncontrolled', () => (
+    <Radio>
+      <Radio.Item value="one" defaultChecked>One</Radio.Item>
+      <Radio.Item value="two">Two</Radio.Item>
+      <Radio.Item value="three">Three</Radio.Item>
+    </Radio>
+  )).
+  add('disabled', () => (
+    <Radio disabled>
+      <Radio.Item value="one" defaultChecked>One</Radio.Item>
+      <Radio.Item value="two">Two</Radio.Item>
+      <Radio.Item value="three">Three</Radio.Item>
+    </Radio>
+  ));
