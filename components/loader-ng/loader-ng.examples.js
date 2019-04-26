2cb277306482b8bd19a0bd84fff6962e5a6c8cae
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2019-04-26 17:29:45 +0300

    RG-1975 Move all examples to storybook: Loader Ng examples

---
+++ components/loader-ng/loader-ng.examples.js
@@ -0,0 +1,19 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import LoaderNg from './loader-ng';
+
+storiesOf('Legacy Angular|Loader Ng', module).
+  addDecorator(angularDecorator()).
+  add('simple', () => {
+    angular.module(APP_NAME, [LoaderNg]);
+
+    return `
+      <div>
+        <rg-loader/>
+      </div>
+    `;
+  });
