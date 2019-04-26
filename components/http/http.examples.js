e881762b83946e5930dea29327e237822c6c6870
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:56:29 +0300

    RG-1975 add HTTP example

---
+++ components/http/http.examples.js
@@ -0,0 +1,25 @@
+import {storiesOf} from '@storybook/html';
+
+import hubConfig from '../../.storybook/hub-config';
+import Auth from '../auth/auth';
+import HTTP from '../http/http';
+
+
+storiesOf('Utilities|HTTP service', module).
+  add('basic', () => {
+    const node = document.createElement('div');
+    node.innerHTML = '<div id="output">Fetching user using HTTP service...</div>';
+
+    const auth = new Auth(hubConfig);
+    const http = new HTTP(auth);
+
+    (async function initializeExample() {
+      await auth.init();
+
+      const user = await http.get(`${hubConfig.serverUri}/api/rest/users/me?fields=name,login`);
+
+      node.querySelector('#output').innerText = JSON.stringify(user);
+    }());
+
+    return node;
+  });
