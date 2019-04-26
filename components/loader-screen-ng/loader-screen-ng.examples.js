0d391eae4f530b710df113657e8c646cd21fadee
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2019-04-26 17:42:44 +0300

    RG-1975 Move all examples to storybook: Loader Screen Ng examples

---
+++ components/loader-screen-ng/loader-screen-ng.examples.js
@@ -0,0 +1,22 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import LoaderScreen from './loader-screen-ng';
+
+storiesOf('Legacy Angular|Loader Screen Ng', module).
+  addDecorator(angularDecorator()).
+  add('simple', () => {
+    angular.module(APP_NAME, [LoaderScreen]).run(loaderScreen => {
+      loaderScreen.setVisible(true);
+      loaderScreen.startInitialLoading();
+    });
+
+    return `
+      <div>
+        <div rg-loader-screen="Loading..."></div>
+      </div>
+    `;
+  });
