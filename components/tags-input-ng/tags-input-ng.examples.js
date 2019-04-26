d40cfe2d33b55941ebedb2ba83e85a214bde3a2d
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 22:00:47 +0300

    RG-1975 add tags input examples

---
+++ components/tags-input-ng/tags-input-ng.examples.js
@@ -0,0 +1,40 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import TagsInputNG from './tags-input-ng';
+
+storiesOf('Legacy Angular|Tags Input Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [TagsInputNG]).
+      controller('ExampleCtrl', function ctrl($q) {
+        this.tags = [
+          {key: 'test1', label: 'test1'},
+          {key: 'test2', label: 'test2'}
+        ];
+
+        this.dataSource = () => $q.when([
+          {key: 'test3', label: 'test3'},
+          {key: 'test4', label: 'test4'}
+        ]);
+
+        this.onAddTag = action('onAddTag');
+
+        this.removeTag = action('removeTag');
+      });
+    return `
+      <div ng-controller="ExampleCtrl as ctrl">
+        <rg-tags-input
+          tags="ctrl.tags"
+          x-data-source="ctrl.dataSource"
+          placeholder="'Type something'"
+          on-add-tag="ctrl.onAddTag"
+          on-remove-tag="ctrl.removeTag"
+        ></rg-tags-input>
+      </div>
+    `;
+  });
