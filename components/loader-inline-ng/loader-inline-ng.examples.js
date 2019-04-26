2d79fd8df50325e0de8e0c2e63afd0dccfb3d089
Max Erekhinskiy <Maxim.Erehinskiy@jetbrains.com>
2019-04-26 17:16:08 +0300

    RG-1975 Move all examples to storybook: Inline loader ng examples

---
+++ components/loader-inline-ng/loader-inline-ng.examples.js
@@ -0,0 +1,21 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import LoaderInlineNg from './loader-inline-ng';
+
+storiesOf('Legacy Angular|Loader Inline Ng', module).
+  addDecorator(angularDecorator()).
+  add('simple', () => {
+    angular.module(APP_NAME, [LoaderInlineNg]);
+
+    return `
+      <div>
+        <span>some text on top</span>
+        <div>before <rg-loader-inline/> Some text after</div>
+        <div>some text under loader</div>
+      </div>
+    `;
+  });
