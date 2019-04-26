0750535a3a07509c46905cc876b216857426d7ba
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:36:47 +0300

    RG-1975 add markdown example

---
+++ components/markdown/markdown.examples.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import Markdown, {md} from '../markdown/markdown';
+
+storiesOf('Components|Markdown', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <Markdown
+      source={`
+          # Header
+
+          _Various_ types of **highlighting**
+
+          [Link](/)
+
+          [Link with definition][definition]
+
+          [definition]: /
+
+          > Blockquote
+          >
+          > Second line
+
+          Unordered list:
+
+          * List
+          * List
+
+          Ordered list:
+
+          1. One
+          2. Two
+
+          Horizontal line
+
+          ---
+          Some \`inline(code)\` inside text
+
+          ## Block code
+          \`\`\`js
+          import React, {Component} from 'react';
+          import ChildComponent from './child-component';
+
+          const MyComponent = () => (
+            <div className="class">
+              <ChildComponent prop="value" />
+            </div>
+          );
+          \`\`\`
+        `}
+    />
+  )).
+  add('tagged template', () => (
+    md`
+      # Header
+
+      _Various_ types of **highlighting**
+
+      [Link](/)
+
+      [Link with definition][definition]
+
+      [definition]: /
+
+      > Blockquote
+      >
+      > Second line
+
+      Unordered list:
+
+      * List
+      * List
+
+      Ordered list:
+
+      1. One
+      2. Two
+
+      Horizontal line
+
+      ---
+      Some \`inline(code)\` inside text
+
+      ## Block code
+      \`\`\`js
+      import React from 'react';
+
+      import {md} from '@jetbrains/ring-ui/components/markdown/markdown';
+
+      const MarkdownHeader = ({children}) => md\`#\${children}\`;
+      \`\`\`
+    `
+  ));
