e0783789dbf58545b311ac0fa9f41e73b87ac25c
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:30:15 +0300

    RG-1975 add island examples

---
+++ components/island-legacy/island-legacy.examples.js
@@ -0,0 +1,23 @@
+import {storiesOf} from '@storybook/html';
+
+import '../island-legacy/island-legacy.scss';
+
+storiesOf('Style-only|Island', module).
+  add('basic', () => `
+    <div class="ring-island">
+      <div class="ring-island__header">
+        <span class="ring-island__title">Title</span>
+      </div>
+      <div class="ring-island__content">Content</div>
+    </div>
+    `).
+  add('with a header and buttons', () => `
+    <div class="ring-island">
+      <div class="ring-island__header">
+        <span class="ring-island__title">Title</span>
+        <span class="ring-island__header-button">Button1</span>
+        <span class="ring-island__header-button">Button2</span>
+      </div>
+      <div class="ring-island__content">Content</div>
+    </div>
+  `);
