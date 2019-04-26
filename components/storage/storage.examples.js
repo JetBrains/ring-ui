fb6015a36ffa4040ea08997cc9a80361ebece1ff
Max Maximov <max.maximov@gmail.com>
2019-04-26 20:42:22 +0300

    storage example #RG-1975

---
+++ components/storage/storage.examples.js
@@ -0,0 +1,29 @@
+import {storiesOf} from '@storybook/html';
+
+import Storage from './storage';
+
+storiesOf('Components|Storage', module).
+  add('basic', () => {
+    const STORAGE_KEY = 'storage-example-key';
+    const storage = new Storage();
+    const node = document.createElement('div');
+
+    let value;
+
+    async function init() {
+      const storedValue = await storage.get(STORAGE_KEY);
+      if (!storedValue) {
+        const generatedValue = Math.random().toString();
+        await storage.set(STORAGE_KEY, generatedValue);
+        value = generatedValue;
+      } else {
+        value = storedValue;
+      }
+
+      node.innerText = `Stored value = ${value}`;
+    }
+
+    init();
+
+    return node;
+  });
