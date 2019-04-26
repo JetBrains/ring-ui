ddd24830e4a95fec20c09e721b992cdf77d92f64
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 22:08:52 +0300

    RG-1975 add template ng examples

---
+++ components/template-ng/template-ng.examples.js
@@ -0,0 +1,23 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import TemplateNG from './template-ng';
+
+storiesOf('Legacy Angular|Template Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [TemplateNG]);
+
+    return '<rg-template template="\'<input/>\'"></rg-template>';
+  }).
+  add('with controller', () => {
+    angular.module(APP_NAME, [TemplateNG]).
+      controller('ExampleCtrl', function ctrl() {
+        this.template = '<button>button</button>';
+      });
+
+    return '<rg-template template="ctrl.template" ng-controller="ExampleCtrl as ctrl"></rg-template>';
+  });
