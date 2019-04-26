ad8fcdfa39e397226b0c35680232337d3dce3737
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:32:22 +0300

    RG-1975 add link-ng example

---
+++ components/link-ng/link-ng.examples.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import LinkNG from '../link-ng/link-ng';
+
+storiesOf('Legacy Angular|Link Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [LinkNG]);
+
+    return '<rg-link href="http://example.com" class="test-class">Open example</rg-link>';
+  });
