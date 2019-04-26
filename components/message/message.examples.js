0add495b0b92b6daa7e167c9461e5dca089cf546
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:46:36 +0300

    RG-1975 add message examples

---
+++ components/message/message.examples.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import Message from '../message/message';
+import Popup from '../popup/popup';
+
+const {Directions} = Popup.PopupProps;
+
+storiesOf('Components|Message', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <div style={{padding: 200}}>
+      <span>
+        Anchor
+        <Message
+          title="This is title"
+          onClose={action('got it')}
+          direction={Directions.TOP_RIGHT}
+          tailOffset={32}
+        >
+          This is long long long long long long long long
+          long long long long long long long long long long description
+        </Message>
+      </span>
+    </div>
+  )).
+  add('with onDissmiss', () => (
+    <div style={{padding: 200}}>
+      <span>
+        Anchor
+        <Message
+          title="This is title"
+          onClose={action('got it')}
+          onDismiss={action('dismiss')}
+          direction={Directions.TOP_RIGHT}
+          tailOffset={32}
+        >
+          This is long long long long long long long long
+          long long long long long long long long long long description
+        </Message>
+      </span>
+    </div>
+  ));
