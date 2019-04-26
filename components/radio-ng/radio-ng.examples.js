e16808dcd356929d9c4bcef35f431d2576845455
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 19:24:57 +0300

    RG-1975 add radio examples

---
+++ components/radio-ng/radio-ng.examples.js
@@ -0,0 +1,25 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import RadioNG from '../radio-ng/radio-ng';
+
+storiesOf('Legacy Angular|Radio Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [RadioNG]).
+      controller('ExampleCtrl', function ctrl() {
+        this.selected = 'two';
+      });
+
+    return `
+      <div ng-controller="ExampleCtrl as ctrl">
+        <rg-radio ng-model="ctrl.selected" value="one" name="first" id="one-option">One</rg-radio>
+        <rg-radio ng-model="ctrl.selected" value="two" name="second">Two</rg-radio>
+  
+        <div>Selected value is "{{ctrl.selected}}"</div>
+      </div>
+    `;
+  });
