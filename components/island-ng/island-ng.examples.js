1f90ddf4cc3aaa8cb4a3e17dbbb7846d4d453ac2
Andrey <andrey.skladchikov@gmail.com>
2019-04-26 14:30:15 +0300

    RG-1975 add island examples

---
+++ components/island-ng/island-ng.examples.js
@@ -0,0 +1,43 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import IslandNg from '../island-ng/island-ng';
+
+storiesOf('Legacy Angular|Island Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [IslandNg]);
+
+    return `
+      <rg-island>
+        <rg-island-header border="true">
+          This is header
+        </rg-island-header>
+        <rg-island-content>
+          This is content
+        </rg-island-content>
+      </rg-island>
+    `;
+  }).
+  add('scrollable', () => {
+    angular.module(APP_NAME, [IslandNg]);
+
+    return `
+      <rg-island narrow="true" style="height: 200px; width: 200px;">
+        <rg-island-header border="true">
+          Title
+        </rg-island-header>
+        <rg-island-content fade="true">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining
+          essentially unchanged.
+        </rg-island-content>
+      </rg-island>
+    `;
+  });
---
+++ components/island/island.examples.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import Island, {AdaptiveIsland, Header, Content} from '../island/island';
+
+storiesOf('Components|Island', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => (
+    <Island>
+      <Header border>Title</Header>
+      <Content>Content</Content>
+    </Island>
+  )).
+  add('with scroll', () => (
+    <Island className="limited-island" narrow>
+      <Header border>Title</Header>
+      <Content fade>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Content>
+    </Island>
+  ), {
+    cssresources: [{
+      id: 'example-styles',
+      picked: true,
+      code: `
+<style>
+  .limited-island {
+    height: 200px;
+    width: 200px;
+  }
+</style>
+      `
+    }]
+  }).
+  add('with resizeable header', () => (
+    <AdaptiveIsland className="limited-island" narrow>
+      <Header>Title</Header>
+      <Content fade>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged.
+      </Content>
+    </AdaptiveIsland>
+  ), {
+    cssresources: [{
+      id: 'example-styles',
+      picked: true,
+      code: `
+<style>
+  .limited-island {
+    height: 200px;
+    width: 200px;
+  }
+</style>
+      `
+    }]
+  });
