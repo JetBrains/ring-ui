cfd47f72f8022b573c43067ca1599fdc3b5d39d3
Max Maximov <max.maximov@gmail.com>
2019-04-26 03:16:09 +0300

    tabs examples #RG-1975

---
+++ components/tabs/tabs.examples.js
@@ -0,0 +1,204 @@
+/* eslint-disable max-len */
+import React, {Component} from 'react';
+import {storiesOf} from '@storybook/html';
+
+import reactDecorator from '../../.storybook/react-decorator';
+
+import {SearchIcon, WarningIcon} from '../icon';
+import Button from '../button/button';
+
+import {Tabs, Tab, SmartTabs, CustomItem} from './tabs';
+
+storiesOf('Components|Tabs', module).
+  addDecorator(reactDecorator()).
+  add('basic', () => {
+    class TabsDemo extends Component {
+      state = {selected: 'first'};
+
+      render() {
+        return (
+          <Tabs
+            selected={this.state.selected}
+            onSelect={selected => this.setState({selected})}
+          >
+            <Tab
+              id="first"
+              title="First tab"
+            >
+              First tab content
+            </Tab>
+            <Tab
+              id="second"
+              title="Second tab"
+            >
+              Second tab content
+            </Tab>
+            <Tab
+              id="third"
+              title="Third tab"
+            >
+              Third tab content
+            </Tab>
+            <Tab
+              id="fourth"
+              title="Fourth tab (Link)"
+              href="#href"
+            >
+              Fourth tab content
+            </Tab>
+            <Tab
+              disabled
+              id="disabled"
+              title="Disabled tab"
+            >
+              Disabled tab content
+            </Tab>
+          </Tabs>
+        );
+      }
+    }
+
+    return <TabsDemo/>;
+  }).
+  add('dark', () => {
+    class TabsDemo extends Component {
+      state = {selected: 'first'};
+
+      render() {
+        return (
+          <Tabs
+            selected={this.state.selected}
+            onSelect={selected => this.setState({selected})}
+            theme={Tabs.Theme.DARK}
+          >
+            <Tab
+              id="first"
+              title="First tab"
+            >
+              First tab content
+            </Tab>
+            <Tab
+              id="second"
+              title="Second tab"
+            >
+              Second tab content
+            </Tab>
+            <Tab
+              id="third"
+              title="Third tab"
+            >
+              Third tab content
+            </Tab>
+            <Tab
+              id="fourth"
+              title="Fourth tab (Link)"
+              href="#href"
+            >
+              Fourth tab content
+            </Tab>
+            <Tab
+              disabled
+              id="disabled"
+              title="Disabled tab"
+            >
+              Disabled tab content
+            </Tab>
+          </Tabs>
+        );
+      }
+    }
+
+    return <TabsDemo/>;
+  }).
+  add('smart', () => {
+    class TabsDemo extends Component {
+      render() {
+        return (
+          <SmartTabs>
+            <Tab title="First tab">
+              First tab content
+            </Tab>
+            <Tab title="Second tab">
+              Second tab content
+            </Tab>
+            <Tab title="Third tab">
+              Third tab content
+            </Tab>
+            <Tab title="Fourth  tab (Link)" href="#href">
+              Fourth tab content
+            </Tab>
+            <Tab
+              disabled
+              title="Disabled tab"
+            >
+              Disabled tab content
+            </Tab>
+          </SmartTabs>
+        );
+      }
+    }
+
+    return <TabsDemo/>;
+  }).
+  add('custom titles', () => {
+    class TabsDemo extends Component {
+      render() {
+        function Title({Icon, children}) {
+          return (
+            <span>
+              <Icon className="icon"/>
+              {children}
+            </span>
+          );
+        }
+
+        return (
+          <SmartTabs>
+            <Tab title={<Title Icon={SearchIcon}>First tab</Title>}>
+              First tab content
+            </Tab>
+            <Tab title={<Title Icon={WarningIcon}>Second tab</Title>}>
+              Second tab content
+            </Tab>
+            <Tab title={<Title Icon={WarningIcon}>Third tab (Link)</Title>} href="#href">
+              Third tab content
+            </Tab>
+            <CustomItem>
+              <Button text>Action</Button>
+            </CustomItem>
+          </SmartTabs>
+        );
+      }
+    }
+
+    return <TabsDemo/>;
+  }).
+  add('custom titles (function)', () => {
+    class TabsDemo extends Component {
+      render() {
+        function Title({Icon, children}) {
+          return (
+            <span className={children ? 'has-content' : ''}>
+              <Icon
+                className="icon"
+              />
+              {children}
+            </span>
+          );
+        }
+
+        return (
+          <SmartTabs>
+            <Tab title={isSelected => <Title Icon={SearchIcon}>{isSelected && 'First tab'}</Title>}>
+              First tab content
+            </Tab>
+            <Tab title={isSelected => <Title Icon={WarningIcon}>{isSelected && 'Second tab (Link)'}</Title>} href="#href">
+              Second tab content
+            </Tab>
+          </SmartTabs>
+        );
+      }
+    }
+
+    return <TabsDemo/>;
+  });
