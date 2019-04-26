5a6f685b148ce9a1888bce078d0c69a77a681158
Max Maximov <max.maximov@gmail.com>
2019-04-26 19:46:37 +0300

    sidebar-ng example #RG-1975

---
+++ components/sidebar-ng/sidebar-ng.examples.js
@@ -0,0 +1,82 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import '../link/link__legacy.css';
+import SelectNG from '../select-ng/select-ng';
+
+import SidebarNG from './sidebar-ng';
+
+storiesOf('Legacy Angular|Sidebar Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [SidebarNG, SelectNG]);
+
+    return `
+      <div ng-init="isShowSideBar = true" style="position: relative;">
+        <rg-sidebar
+          show="isShowSideBar"
+          place-under-sibling=".some-toolbar"
+          top-offset="1">
+          <div>
+
+            <img class="ring-sidebar__avatar" ng-src="http://via.placeholder.com/64x64"/>
+
+            <h3 class="ring-sidebar__title">
+              <a class="ring-link" href="#">User Link</a>
+            </h3>
+
+            <div class="ring-sidebar__section">
+              <span class="ring-sidebar__section-title" translate>Full Name</span>
+              <div class="ring-sidebar__section-text">Value</div>
+            </div>
+          </div>
+        </rg-sidebar>
+        <div class="some-toolbar">
+          Toolbar to place before sidebar
+          <rg-sidebar-toggle-button model="isShowSideBar">Toggle sidebar
+          </rg-sidebar-toggle-button>
+        </div>
+      </div>
+    `;
+  }).
+  add('a lot of content', () => {
+    angular.module(APP_NAME, [SidebarNG, SelectNG]);
+
+    let bigContent = '';
+    let after = '';
+
+    for (let i = 0; i < 100; i++) {
+      bigContent += ` ${Math.random()}<br/>`;
+      after += ` ${Math.random()}<br/>`;
+    }
+
+    bigContent += '===== The end of sidebar =====';
+
+    return `
+      <div>
+        <div>
+          <div>Lorem</div>
+          <div>Ipsum</div>
+          <div>Lorem</div>
+          <div>Lorem</div>
+          <div>Lorem</div>
+        </div>
+        <div ng-init="isShowSideBar = true" style="position: relative;">
+          <rg-sidebar show="isShowSideBar" place-under-sibling=".some-toolbar" top-offset="1">
+            <div>===== The start of sidebar =====
+              <rg-select options="item in []"></rg-select>
+              <br/>${bigContent}</div>
+          </rg-sidebar>
+          <div class="some-toolbar">
+            Toolbar to place before sidebar
+            <rg-sidebar-toggle-button model="isShowSideBar">Toggle sidebar
+            </rg-sidebar-toggle-button>
+          </div>
+        </div>
+        <div>${after}</div>
+      </div>
+    `;
+  });
