096957abd1cf8901f8323efcc170e006bca638ac
Max Maximov <max.maximov@gmail.com>
2019-04-27 03:50:09 +0300

    progress-bar-ng example #RG-1975

---
+++ components/progress-bar-ng/progress-bar-ng.examples.js
@@ -0,0 +1,34 @@
+import angular from 'angular';
+
+import {storiesOf} from '@storybook/html';
+
+import angularDecorator, {APP_NAME} from '../../.storybook/angular-decorator';
+
+import RingProgressBar from './progress-bar-ng';
+
+storiesOf('Legacy Angular|Progress Bar Ng', module).
+  addDecorator(angularDecorator()).
+  add('basic', () => {
+    angular.module(APP_NAME, [RingProgressBar]).
+      controller('ExampleCtrl', function controller($interval) {
+        this.value = 0;
+
+        $interval(() => {
+          const currentValue = this.value;
+          this.value = currentValue >= 1 ? 0 : currentValue + 0.1;
+        }, 500);
+      });
+    return `
+      <div ng-controller="ExampleCtrl as ctrl">
+        <div style="height: 25px; padding-top: 25px;">
+          <rg-progress-bar value="ctrl.value"></rg-progress-bar>
+        </div>
+        <div style="height: 25px; background: #000; padding-top: 25px;">
+          <rg-progress-bar value="ctrl.value" theme="'dark'"></rg-progress-bar>
+        </div>
+        <div style="height: 25px; background: #F0F0F0; padding-top: 25px;">
+          <rg-progress-bar value="ctrl.value"></rg-progress-bar>
+        </div>
+      </div>
+    `;
+  });
