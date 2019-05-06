b21b144dcc80a0483adf29e93437498c121bd70c
Andrey <andrey.skladchikov@gmail.com>
2019-05-06 15:50:24 +0300

    RG-1894 add missing Auth Dialog Service example

---
+++ components/auth-dialog-service/auth-dialog-service.examples.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {storiesOf} from '@storybook/html';
+import {action} from '@storybook/addon-actions';
+
+import reactDecorator from '../../.storybook/react-decorator';
+
+import Auth from '../auth/auth';
+import HTTP from '../http/http';
+import Button from '../button/button';
+import hubConfig from '../../.storybook/hub-config';
+
+import showAuthDialog from './auth-dialog-service';
+
+
+storiesOf('Services|Auth Dialog Service', module).
+  addParameters({
+    notes: 'A wrapper for the AuthDialog component. Allows showing the auth dialog without mounting the AuthDialog component first. Can be used outside React.',
+    hermione: {skip: true}
+  }).
+  addDecorator(reactDecorator()).
+  add('basic', () => {
+    const auth = new Auth(hubConfig);
+    const http = new HTTP(auth, auth.getAPIPath());
+
+    class AuthDialogDemo extends React.Component {
+      componentDidMount() {
+        auth.init();
+        http.get('services/0-0-0-0-0?fields=name,iconUrl').then(serviceDetails => {
+          this.setState({serviceDetails});
+          this.showAuthDialog();
+        });
+      }
+
+      showAuthDialog = () => {
+        const {serviceDetails} = this.state;
+
+        showAuthDialog({
+          serviceDetails,
+          errorMessage: 'Error message',
+          onConfirm: action('onConfirm'),
+          onCancel: action('onCancel')
+        });
+      };
+
+      render() {
+        return (
+          <div>
+            <Button onClick={this.showAuthDialog}>Show auth dialog</Button>
+          </div>
+        );
+      }
+    }
+
+    return <AuthDialogDemo/>;
+  });
