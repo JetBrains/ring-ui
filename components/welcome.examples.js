68f48461819b2dd39d2c8e2c069cb06863aab8c2
Andrey <andrey.skladchikov@gmail.com>
2019-05-07 16:10:29 +0300

    RG-1894 add License page to "Welcome" group

--- components/welcome.examples.js
+++ components/welcome.examples.js
@@ -9,9 +9,12 @@ import Changelog from '../CHANGELOG.md';
 import Markdown from '../components/markdown/markdown';
 import reactDecorator from '../.storybook/react-decorator';
 
+import License from 'raw-loader!../LICENSE.txt';
+
 storiesOf(`Ring UI [${packageInfo.version}]|Welcome`, module).
   addParameters({hermione: {skip: true}}).
   addDecorator(reactDecorator()).
   add('Getting Started', () => <Markdown source={ReadMe}/>).
   add('Contributing', () => <Markdown source={Contributing}/>).
-  add('Changelog', () => <Markdown source={Changelog}/>);
+  add('Changelog', () => <Markdown source={Changelog}/>).
+  add('License', () => <pre>{License}</pre>);
