99ec1a8e942b39169e015ce950e1b02e28f35150
Andrey <andrey.skladchikov@gmail.com>
2019-05-07 14:35:47 +0300

    RG-1894 display package version in Ring UI sidebar section for now

--- components/welcome.examples.js
+++ components/welcome.examples.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import {storiesOf} from '@storybook/html';
 
+import packageInfo from '../package.json';
 import ReadMe from '../README.md';
 import Contributing from '../CONTRIBUTING.md';
 import Changelog from '../CHANGELOG.md';
 import Markdown from '../components/markdown/markdown';
 import reactDecorator from '../.storybook/react-decorator';
 
-storiesOf('Ring UI|Welcome', module).
+storiesOf(`Ring UI [${packageInfo.version}]|Welcome`, module).
   addParameters({hermione: {skip: true}}).
   addDecorator(reactDecorator()).
   add('Getting Started', () => <Markdown source={ReadMe}/>).
