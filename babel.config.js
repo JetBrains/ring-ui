796167614bb0d3c2229542996d721222b5d086a2
Andrey <andrey.skladchikov@gmail.com>
2019-08-08 14:23:29 +0300

    RG-1982 parametrize @jetbrains/babel preset's corejs version with one that installed

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,22 @@
 const browserslist = require('browserslist');
 
+const coreJsVersion = process.env.RING_UI_COREJS_VERSION ||
+  require('core-js/package.json').version;
+
 module.exports = function config(api) {
+  if (api.env() === 'development') {
+    // eslint-disable-next-line no-console
+    console.info(`Compiling Ring UI with Core JS version "${coreJsVersion}"`);
+  }
+
   api.cache(true);
 
   return {
     presets: [
       ['@jetbrains/jetbrains', {
         angular: true,
-        useBuiltIns: 'usage'
+        useBuiltIns: 'usage',
+        corejs: coreJsVersion.startsWith('3') ? '3' : '2'
       }]
     ],
     plugins: [
