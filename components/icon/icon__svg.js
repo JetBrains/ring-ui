af7b8a1b2f529a37598eaa6305b60ab621856c71
Andrey <andrey.skladchikov@gmail.com>
2019-09-04 12:49:28 +0300

    RG-2014 reuse `svg-inline-react` implementation with SVG node patching
     https://github.com/sairion/svg-inline-react/blob/master/src/index.js

---
+++ components/icon/icon__svg.js
@@ -0,0 +1,73 @@
+// Inspired by https://github.com/sairion/svg-inline-react
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import {pure} from 'recompose';
+
+import styles from './icon.css';
+
+function convertReactSVGDOMProperty(str) {
+  return str.replace(/[-|:]([a-z])/g, g => g[1].toUpperCase());
+}
+
+function serializeAttrs(map) {
+  const res = {};
+  for (let i = 0; i < map.length; i++) {
+    const key = map[i].name;
+    let prop = key;
+    if (key === 'class') {
+      prop = 'className';
+    } else if (!key.startsWith('data-')) {
+      prop = convertReactSVGDOMProperty(key);
+    }
+
+    res[prop] = map[i].value;
+  }
+  return res;
+}
+
+function getSVGFromSource(src) {
+  const svgContainer = document.createElement('div');
+  svgContainer.innerHTML = src;
+  const svg = svgContainer.firstElementChild;
+  svg.remove ? svg.remove() : svgContainer.removeChild(svg);
+  return svg;
+}
+
+function extractSVGProps(svgNode) {
+  const map = svgNode.attributes;
+  return (map.length > 0) ? serializeAttrs(map) : null;
+}
+
+function isCompatibilityMode(iconSrc) {
+  const hasWidth = /width="[\d\.]+"/ig.test(iconSrc);
+  const hasHeight = /height="[\d\.]+"/ig.test(iconSrc);
+  return !hasWidth || !hasHeight;
+}
+
+function IconSVG({src, className, ...rest}) {
+  const glyphClasses = classNames(styles.glyph, {
+    [styles.compatibilityMode]: isCompatibilityMode(src)
+  }, className);
+
+  const svgNode = getSVGFromSource(src);
+
+  return (
+    <svg
+      {...extractSVGProps(svgNode)}
+      {...rest}
+      className={glyphClasses}
+      dangerouslySetInnerHTML={{
+        __html: svgNode.innerHTML
+      }}
+    />
+  );
+}
+
+IconSVG.propTypes = {
+  className: PropTypes.string,
+  src: PropTypes.string.isRequired,
+  style: PropTypes.object
+};
+
+export default pure(IconSVG);
