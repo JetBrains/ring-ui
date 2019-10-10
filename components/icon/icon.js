98c98bb5189cbb168351e688243947de7ac263b5
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2019-10-10 23:23:50 +0200

    Fix icon proptypes [publish]

--- components/icon/icon.js
+++ components/icon/icon.js
@@ -25,7 +25,7 @@ export default class Icon extends PureComponent {
   static propTypes = {
     className: PropTypes.string,
     color: PropTypes.string,
-    glyph: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
+    glyph: PropTypes.oneOfType([PropTypes.string, PropTypes.elementType]),
     height: PropTypes.number,
     size: PropTypes.number,
     width: PropTypes.number,
