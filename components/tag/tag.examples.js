a65482d513172781c383fb38a64aaa11af2f4587
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2019-10-18 23:29:52 +0300

    chore: use Base64 URI decoded avatar to avoid flaky UI tests if image loads slow

--- components/tag/tag.examples.js
+++ components/tag/tag.examples.js
@@ -4,8 +4,6 @@ import reactDecorator from '../../.storybook/react-decorator';
 
 import {CheckmarkIcon} from '../../components/icon/icons';
 
-import hubConfig from '../../.storybook/hub-config';
-
 import Tag from './tag';
 
 export default {
@@ -18,7 +16,8 @@ export default {
 };
 
 export const demo = () => {
-  const url = `${hubConfig.serverUri}/api/rest/avatar/default?username=Jet%20Brains`;
+  const avatarUrl =
+    'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNDAgNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+ICAgIDxkZWZzPiAgICAgICAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkaWVudCIgeDE9IjAiIHkxPSIwIiB4Mj0iMCIgeTI9IjEiPiAgICAgICAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiNENTBGNkIiIG9mZnNldD0iMCIvPiAgICAgICAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiNFNzNBRTgiIG9mZnNldD0iMSIvPiAgICAgICAgPC9saW5lYXJHcmFkaWVudD4gICAgPC9kZWZzPiAgICA8Zz4gICAgICAgIDxyZWN0IGZpbGw9InVybCgjZ3JhZGllbnQpIiAgICAgICAgICAgICAgeD0iMCIgeT0iMCIgd2lkdGg9IjQwIiBoZWlnaHQ9IjQwIiAgICAgICAgICAgICAgcng9IjMiIHJ5PSIzIi8+ICAgICAgICA8dGV4dCB4PSI1IiB5PSIxOSIgICAgICAgICAgICAgIGZvbnQtZmFtaWx5PSJBcmlhbCwgSGVsdmV0aWNhLCBzYW5zLXNlcmlmIiAgICAgICAgICAgICAgZm9udC1zaXplPSIxNXB4IiAgICAgICAgICAgICAgbGV0dGVyLXNwYWNpbmc9IjEiICAgICAgICAgICAgICBmaWxsPSIjRkZGRkZGIj4gICAgICAgICAgICA8dHNwYW4+SkI8L3RzcGFuPiAgICAgICAgICAgIDx0c3BhbiB4PSI2IiB5PSIyOCI+XzwvdHNwYW4+ICAgICAgICA8L3RleHQ+ICAgIDwvZz48L3N2Zz4=';
 
   class TagDemo extends React.Component {
     render() {
@@ -34,7 +33,7 @@ export const demo = () => {
           <Tag rgTagIcon={CheckmarkIcon} rgTagTitle="I am an icon title">
             With icon title
           </Tag>
-          <Tag avatar={url} readOnly={false}>
+          <Tag avatar={avatarUrl} readOnly={false}>
             With avatar
           </Tag>
         </div>
