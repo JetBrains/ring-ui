38bd5160767025beb6c1d6fc54a7ccde80e101e6
Denis Lesnik <denis.lesnik@jetbrains.com>
2019-11-21 14:19:17 +0100

    Autoscrolling to the previous scroll position for the case, when <HTML> has a `overflow: scroll`. Whenever Dialog Opens
    Added param to disabled / enable scrollBarWidth emulation

--- components/dialog/dialog__body-scroll-preventer.js
+++ components/dialog/dialog__body-scroll-preventer.js
@@ -4,20 +4,24 @@ import {bodyWithoutScroll} from './dialog.css';
 
 let isPrevented = false;
 let previousBodyWidth = null;
+let initialScrollPosition = 0;
 
 export default {
-  prevent() {
+  prevent(emulateScrollBarSpace) {
     if (isPrevented) {
       return;
     }
     isPrevented = true;
+    initialScrollPosition = document.body.parentNode.scrollTop;
+
     document.body.classList.add(bodyWithoutScroll);
 
     const scrollWidth = scrollbarWidth();
-
     const bodyHasScroll = document.body.scrollHeight > window.innerHeight;
 
-    if (bodyHasScroll && scrollWidth > 0) {
+    document.body.scroll(0, initialScrollPosition);
+
+    if (emulateScrollBarSpace && bodyHasScroll && scrollWidth > 0) {
       previousBodyWidth = document.body.style.width;
       document.body.style.width = `calc(100% - ${scrollWidth}px)`;
     }
@@ -30,6 +34,9 @@ export default {
     isPrevented = false;
 
     document.body.classList.remove(bodyWithoutScroll);
+    document.body.parentNode.scroll(0, initialScrollPosition);
+    initialScrollPosition = 0;
+
 
     if (previousBodyWidth !== null) {
       document.body.style.width = previousBodyWidth;
