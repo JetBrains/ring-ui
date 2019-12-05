b64eb345afe64c8380e523c1e652810ecdb1aae2
filipp.riabchun <filipp.riabchun@TeamCity>
2019-12-05 15:16:49 +0000

    TeamCity change in 'JetBrains Public Projects / JetBrains UI / Ring UI' project: Cleanup rule was added for project

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -18,4 +18,17 @@ changeProject(DslContext.projectId) {
             text("env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD", "true", allowEmpty = true)
         }
     }
+
+    features {
+        add {
+            feature {
+                type = "keepRules"
+                id = "KEEP_RULE_2"
+                param("limit.type", "all")
+                param("keepData.1.type", "everything")
+                param("ruleDisabled", "false")
+                param("preserveArtifacts", "true")
+            }
+        }
+    }
 }
