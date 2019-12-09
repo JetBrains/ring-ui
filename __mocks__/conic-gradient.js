82b053ea89fb4896b13efcbbc5132f94047a7879
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2019-12-09 18:55:33 +0100

    Report console errors in examples

---
+++ __mocks__/conic-gradient.js
@@ -0,0 +1,1 @@
+global.ConicGradient = function ConicGradient() {};
--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,9 @@ process.env.IS_JEST = true;
 
 module.exports = {
   moduleNameMapper: {
-    '\\.(svg|html|gif|ico|md|txt)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
+    '\\.(html|gif|ico|md|txt)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
   setupFiles: [
     '<rootDir>/test-helpers/inject-hub-config.js',
