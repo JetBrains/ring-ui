b3b700c4b4fdaf4ba52137b182b39a3c706c8ec9
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2019-12-09 20:33:19 +0100

    Date picker: repeat dimensions in const.js to avoid NaN in tests

--- components/date-picker/consts.js
+++ components/date-picker/consts.js
@@ -3,13 +3,15 @@ import moment from 'moment';
 
 import formats from './formats';
 
-import styles from './date-picker.css';
-
-const units = ['unit', 'cellSize', 'calHeight', 'yearHeight'].
-  reduce((obj, key) => {
-    obj[key] = parseInt(styles[key], 10);
-    return obj;
-  }, {});
+const unit = 8; // px;
+const units = {
+  unit,
+  /* eslint-disable no-magic-numbers */
+  cellSize: unit * 3,
+  calHeight: unit * 36,
+  yearHeight: unit * 4
+  /* eslint-enable */
+};
 
 export default units;
 
