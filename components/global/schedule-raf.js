8bbe6d189f8bca4aa8c4da51609013e996c9ee0d
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2019-12-14 00:38:26 +0100

    Popup: ensure top-down redraw order

--- components/global/schedule-raf.js
+++ components/global/schedule-raf.js
@@ -1,9 +1,10 @@
-export default function scheduleRAF() {
+export default function scheduleRAF(trailingCall) {
   let scheduledCb;
   let RAF;
   return function schedule(cb) {
     scheduledCb = cb;
-    if (!RAF) {
+    if (!RAF || trailingCall) {
+      cancelAnimationFrame(RAF);
       RAF = window.requestAnimationFrame(() => {
         scheduledCb();
         RAF = null;
