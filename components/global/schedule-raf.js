4180f386484a5b252f757e5f8bc5129e9178e6de
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2019-12-14 01:25:44 +0100

    Fix tests

--- components/global/schedule-raf.js
+++ components/global/schedule-raf.js
@@ -1,15 +1,27 @@
 export default function scheduleRAF(trailingCall) {
   let scheduledCb;
   let RAF;
-  return function schedule(cb) {
-    scheduledCb = cb;
-    if (!RAF || trailingCall) {
-      cancelAnimationFrame(RAF);
-      RAF = window.requestAnimationFrame(() => {
-        scheduledCb();
+  let trailingCallScheduled = false;
+
+  function doSchedule() {
+    RAF = window.requestAnimationFrame(() => {
+      scheduledCb();
+      if (trailingCallScheduled) {
+        trailingCallScheduled = false;
+        doSchedule();
+      } else {
         RAF = null;
         scheduledCb = null;
-      });
+      }
+    });
+  }
+
+  return function schedule(cb) {
+    scheduledCb = cb;
+    if (!RAF) {
+      doSchedule();
+    } else if (trailingCall) {
+      trailingCallScheduled = true;
     }
   };
 }
