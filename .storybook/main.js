e99b7729e315cfc4bcd9526d54eec9d9839b8d8c
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2020-01-08 17:00:25 +0100

    Move storybook webpack config to `main.js`

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,8 @@
+const webpack = require('webpack');
+
+const ringConfig = require('../webpack.config');
+const pkgConfig = require('../package.json').config;
+
 module.exports = {
   stories: [
     // Make welcome stories default
@@ -12,5 +17,40 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y'
-  ]
+  ],
+  webpackFinal(config) {
+    ringConfig.loaders.cssLoader.include.push(/\.storybook/);
+    ringConfig.loaders.svgInlineLoader.include.push(/octicons/);
+    ringConfig.loaders.svgInlineLoader.include.push(/@jetbrains\/logos/);
+    ringConfig.loaders.babelLoader.options.plugins = [[
+      'babel-plugin-react-docgen',
+      {
+        DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES'
+      }
+    ]];
+
+    config.module.rules = [
+      ...ringConfig.config.module.rules,
+      config.module.rules.find(rule =>
+        rule.include instanceof RegExp &&
+        rule.include.test('node_modules/acorn-jsx')),
+      {
+        test: /\.md$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.examples\.js$/,
+        loaders: [require.resolve('@storybook/source-loader')],
+        enforce: 'pre'
+      }
+    ];
+
+    const serverUri = pkgConfig.hub;
+    const clientId = pkgConfig.clientId;
+    const hubConfig = JSON.stringify({serverUri, clientId});
+
+    config.plugins.push(new webpack.DefinePlugin({hubConfig}));
+
+    return config;
+  }
 };
