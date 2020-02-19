f043d683d531ccf8d3d84c2795b2ac82f29abb01
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2020-02-19 12:59:50 +0300

    use "html-loader-jest" to avoid angular components crashes when accessing DOM

--- jest.config.js
+++ jest.config.js
@@ -3,14 +3,18 @@ process.env.IS_JEST = true;
 module.exports = {
   moduleNameMapper: {
     '\\.(gif|ico|md|txt)$': '<rootDir>/__mocks__/fileMock.js',
-    '\\.html$': '<rootDir>/__mocks__/htmlMock.js',
     '\\.svg$': '<rootDir>/__mocks__/svgMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy'
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '!!file-loader.+': '<rootDir>/__mocks__/fileMock.js'
   },
   setupFiles: [
     '<rootDir>/test-helpers/inject-hub-config.js',
     '<rootDir>/test-helpers/register-context.js'
   ],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.html?$': 'html-loader-jest'
+  },
   transformIgnorePatterns: [
     'node_modules/(?!(?:@hypnosphi/)?react-virtualized)'
   ],
