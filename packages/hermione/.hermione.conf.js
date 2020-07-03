3ac9e7969f1b0a9ec1f36fba1c2e4dc6984e01be
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2020-07-03 19:47:29 +0300

    Update FF to 79.0 to avoid "old browser" message in hermione tests

--- packages/hermione/.hermione.conf.js
+++ packages/hermione/.hermione.conf.js
@@ -64,7 +64,7 @@ module.exports = {
       desiredCapabilities: {
         browser: 'Firefox',
         pageLoadStrategy: 'normal',
-        browser_version: '74.0',
+        browser_version: '78.0',
         os: 'OS X',
         os_version: 'Catalina',
         maxDuration
