6a928d849ea4c861ad5e30684a250717ac5afe9d
Maksim Ryzhikov <rv.maksim@gmail.com>
2020-09-15 22:19:00 +0300

    chore: debug karam ci

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -12,6 +12,7 @@ module.exports = config => {
         {type: 'teamcity'}
       ]
     },
+    logLevel: config.LOG_DEBUG,
     reporters: ['teamcity', 'coverage', 'coverage-html-index'],
     webpack: require('./webpack-test.config'),
     client: {
