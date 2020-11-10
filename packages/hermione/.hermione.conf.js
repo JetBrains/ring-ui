a9b27f52959622c5f7cb00410d4dd54f3e7113af
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2020-11-10 14:40:50 +0100

    Update screenshot browsers

--- packages/hermione/.hermione.conf.js
+++ packages/hermione/.hermione.conf.js
@@ -48,7 +48,7 @@ module.exports = {
       desiredCapabilities: {
         browser: 'Chrome',
         pageLoadStrategy: 'normal',
-        browser_version: '85.0',
+        browser_version: '86.0',
         chromeOptions: {
           excludeSwitches: ['enable-automation']
         },
@@ -64,7 +64,7 @@ module.exports = {
       desiredCapabilities: {
         browser: 'Firefox',
         pageLoadStrategy: 'normal',
-        browser_version: '80.0',
+        browser_version: '82.0',
         os: 'OS X',
         os_version: 'Catalina',
         maxDuration
