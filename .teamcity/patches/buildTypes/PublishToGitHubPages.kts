6ac31c07a447187d6b9ca22b0b75360b2785a7e1
andrey.skladchikov <andrey.skladchikov@TeamCity>
2020-11-24 09:13:44 +0000

    TeamCity change in 'JetBrains Public Projects / JetBrains UI / Ring UI' project: runners of 'Deploy to GitHub Pages' build configuration were updated

--- .teamcity/patches/buildTypes/PublishToGitHubPages.kts
+++ .teamcity/patches/buildTypes/PublishToGitHubPages.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
 
 /*
@@ -9,6 +11,40 @@ To apply the patch, change the buildType with id = 'PublishToGitHubPages'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("PublishToGitHubPages")) {
+    expectSteps {
+        script {
+            name = "Publish"
+            scriptContent = """
+                #!/bin/bash
+                set -e -x
+                
+                mkdir -p ~/.ssh/
+                touch ~/.ssh/config
+                cat << EOT >> ~/.ssh/config
+                Host github.com
+                    StrictHostKeyChecking no
+                    UserKnownHostsFile /dev/null
+                EOT
+                
+                chmod 644 ~/.ssh/config
+                
+                # GitHub authorization
+                git config user.email "%github.com.builduser.email%"
+                git config user.name "%github.com.builduser.name%"
+                
+                npx gh-pages --dist dist --dest %teamcity.build.branch% --message "Deploy %teamcity.build.branch%"
+            """.trimIndent()
+            dockerImage = "node:latest"
+            dockerRunParameters = "-v %teamcity.build.workingDir%/npmlogs:/root/.npm/_logs"
+        }
+    }
+    steps {
+        update<ScriptBuildStep>(0) {
+            clearConditions()
+            dockerImage = "node:14"
+        }
+    }
+
     requirements {
         remove {
             contains("system.agent.name", "ubuntu-16.04")
