12b759d21fc5afaaf87809c83dda8a2839a6a6fc
dmitry.mashkov <dmitry.mashkov@jetbrains.com>
2021-01-15 14:24:46 +0100

    A render prop was introduced for date-popup component

--- components/date-picker/date-popup.js
+++ components/date-picker/date-popup.js
@@ -33,6 +33,7 @@ export default class DatePopup extends Component {
     time: PropTypes.string,
     from: dateType,
     to: dateType,
+    render: PropTypes.func,
     displayFormat: PropTypes.func,
     parseDateInput: PropTypes.func,
     onChange: PropTypes.func,
@@ -392,6 +393,8 @@ export default class DatePopup extends Component {
           />
           <Years {...calendarProps}/>
         </div>
+
+        {this.props.render && this.props.render(this.state)}
       </div>
     );
   }
