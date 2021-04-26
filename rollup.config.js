5941c1f5c720fefa7c00e4358338948385062fc1
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2021-04-26 15:24:11 +0200

    [prebuild]: build all components by wildcard

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,30 @@
+import path from 'path';
+
 import {babel} from '@rollup/plugin-babel';
 import styles from 'rollup-plugin-styles';
 import globals from 'rollup-plugin-node-globals';
 import replace from '@rollup/plugin-replace';
 import clear from 'rollup-plugin-clear';
 import browserslist from 'browserslist';
+import glob from 'glob';
 
+
+const files = glob.sync(
+  'components/**/*.js',
+  {
+    ignore: [
+      '**/__mocks__/**',
+      'components/*-ng/*',
+      'components/**/*.test.js',
+      'components/**/*.examples.js',
+
+      // highlight.js import does not allow to build these files:
+      'components/code/**',
+      'components/markdown/**',
+      'components/user-agreement/**'
+    ]
+  }
+);
 
 const TARGET_DIR = 'dist';
 
@@ -14,89 +34,16 @@ export default {
     return !isInternal;
   },
 
-  input: [
-    'components/alert/alert.js',
-    'components/alert-service/alert-service.js',
-    'components/analytics/analytics.js',
-    'components/auth/auth.js',
-    'components/auth-dialog/auth-dialog.js',
-    'components/auth-dialog-service/auth-dialog-service.js',
-    'components/avatar/avatar.js',
-    'components/badge/badge.js',
-    'components/button/button.js',
-    'components/button-group/button-group.js',
-    'components/button-set/button-set.js',
-    'components/button-toolbar/button-toolbar.js',
-    'components/caret/caret.js',
-    'components/checkbox/checkbox.js',
-
-    // 'components/code/code.js', // highlight.js import
-
-    'components/confirm/confirm.js',
-    'components/confirm-service/confirm-service.js',
-    'components/content-layout/content-layout.js',
-    'components/contenteditable/contenteditable.js',
-    'components/data-list/data-list.js',
-    'components/date-picker/date-picker.js',
-    'components/dialog/dialog.js',
-    'components/dropdown/dropdown.js',
-    'components/error-bubble/error-bubble.js',
-    'components/error-message/error-message.js',
-    'components/footer/footer.js',
-    'components/grid/grid.js',
-    'components/group/group.js',
-    'components/header/header.js',
-    'components/header/logo.js',
-    'components/heading/heading.js',
-    'components/http/http.js',
-    'components/hub-source/hub-source.js',
-    'components/icon/icon.js',
-    'components/input/input.js',
-    'components/island/island.js',
-    'components/link/link.js',
-    'components/list/list.js',
-    'components/loader/loader.js',
-    'components/loader/loader.js',
-    'components/loader-inline/loader-inline.js',
-    'components/loader-screen/loader-screen.js',
-    'components/login-dialog/login-dialog.js',
-
-    // 'components/markdown/markdown.js', // uses code.js that fails
-
-    'components/message/message.js',
-    'components/old-browsers-message/old-browsers-message.js',
-    'components/pager/pager.js',
-    'components/panel/panel.js',
-    'components/permissions/permissions.js',
-    'components/popup/popup.js',
-    'components/popup-menu/popup-menu.js',
-    'components/progress-bar/progress-bar.js',
-    'components/proxy-attrs/proxy-attrs.js',
-    'components/query-assist/query-assist.js',
-    'components/radio/radio.js',
-    'components/select/select.js',
-    'components/shortcuts/shortcuts.js',
-    'components/storage/storage.js',
-    'components/tab-trap/tab-trap.js',
-    'components/table/table.js',
-    'components/table/selection.js',
-    'components/tabs/tabs.js',
-    'components/tag/tag.js',
-    'components/tags-input/tags-input.js',
-    'components/tags-list/tags-list.js',
-    'components/text/text.js',
-    'components/toggle/toggle.js',
-    'components/tooltip/tooltip.js',
-
-    // 'components/user-agreement/user-agreement.js', // uses markdown that uses code that fails
-
-    'components/user-card/user-card.js'
-  ],
+  input: files,
 
   output: {
     dir: TARGET_DIR,
     format: 'esm',
-    entryFileNames: '[name].js', // exposed components
+    // exposed components
+    entryFileNames: chunkInfo => {
+      const dirName = path.basename(path.dirname(chunkInfo.facadeModuleId));
+      return `${dirName}/[name].js`;
+    },
     chunkFileNames: '_helpers/[name].js',
     assetFileNames: '[name][extname]' // effective for css styles
   },
