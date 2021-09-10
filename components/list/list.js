4a460489ad299aeebf01395c773eb84a247bb432
katriyna <katriyna@gmail.com>
2021-09-10 15:07:15 +0300

    Revert "RG-2163 Make popup menu more friendly for accessibility tools (e.g. "Mac Voice over")"
    This reverts commit e0a87596786a83f1a96408753d66cca98e3f4d93.

--- components/list/list.js
+++ components/list/list.js
@@ -612,7 +612,6 @@ export default class List extends Component {
           <div ref={registerChild}>
             <VirtualizedList
               aria-label={this.props.ariaLabel}
-              role={this.props.role}
               ref={this.virtualizedListRef}
               className="ring-list__i"
               autoHeight={autoHeight}
@@ -682,7 +681,6 @@ export default class List extends Component {
       >
         <div
           aria-label={this.props.ariaLabel}
-          role={this.props.role}
           style={maxHeight
             ? {maxHeight: this.getVisibleListHeight(this.props)}
             : null
@@ -728,6 +726,7 @@ export default class List extends Component {
         <div
           id={this.props.id}
           ref={this.containerRef}
+          role={this.props.role}
           tabIndex={-1}
           className={classes}
           onMouseOut={this.props.onMouseOut}
