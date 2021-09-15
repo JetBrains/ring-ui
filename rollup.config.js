3cc5f63e6e46604640675108d9097d7be993bbec
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2021-09-15 14:19:58 +0200

    Temporarily exclude "error-page" components to fix built version

--- rollup.config.js
+++ rollup.config.js
@@ -12,6 +12,8 @@ const files = glob.sync(
   {
     ignore: [
       '**/__mocks__/**',
+      'components/error-page/*', // TODO Error page does not work because of importing GIF file
+      'components/error-page-ng/*',
       'components/**/*.test.js',
       'components/**/*.examples.js'
     ]
