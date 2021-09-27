538bc482b18d432f8a9af0da3e8eaa9322ab7f52
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-09-27 20:50:40 +0200

    Typescript migration: link

---
+++ components/global/memoize.ts
@@ -0,0 +1,23 @@
+interface MapLike<K, V> {
+  delete(key: K): boolean;
+  get(key: K): V | undefined;
+  has(key: K): boolean;
+  set(key: K, value: V): this;
+}
+
+export default function memoize<A = void, T = void>(fn: (arg: A) => T): (arg: A) => T {
+  const primitiveCache = new Map();
+  const objectCache = new WeakMap();
+  return function memoized(arg) {
+    const key = arg ?? '__singleValue__';
+    const cache: MapLike<unknown, T> = key instanceof Object ? objectCache : primitiveCache;
+    const cached = cache.get(key);
+    if (cached != null) {
+      return cached;
+    }
+
+    const value = fn(arg);
+    cache.set(key, value);
+    return value;
+  };
+}
