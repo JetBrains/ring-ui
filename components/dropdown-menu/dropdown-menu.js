9da5760c76b273f3078471ab34fef0f2a0ef8436
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2021-10-14 12:57:36 +0200

    Revert "RG-2169 turn off "activateFirstItem" because it not always needed"
    This reverts commit a4bab074bcd38f8a0acad6ac301b1414f0f6a839.

--- components/dropdown-menu/dropdown-menu.js
+++ components/dropdown-menu/dropdown-menu.js
@@ -93,6 +93,7 @@ const DropdownMenu = React.forwardRef(function DropdownMenu(
           id={listId}
           ariaLabel={ariaLabel || defaultAriaLabel}
           closeOnSelect
+          activateFirstItem
           data={data}
           onSelect={onSelect}
           {...menuProps}
