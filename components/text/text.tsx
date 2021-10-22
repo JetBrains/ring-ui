20ac1b40fdbe8c445632c7e4dedf185c27575242
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-10-22 19:25:23 +0200

    Typescript migration: text

--- components/text/text.tsx
+++ components/text/text.tsx
@@ -1,14 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component, HTMLAttributes} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import styles from './text.css';
 
+export interface TextProps extends HTMLAttributes<HTMLElement> {
+  info?: boolean | null | undefined
+  'data-test'?: string | null | undefined
+}
+
 /**
  * @name Text
  */
 
-export default class Text extends Component {
+export default class Text extends Component<TextProps> {
   static propTypes = {
     children: PropTypes.node,
     info: PropTypes.bool,
