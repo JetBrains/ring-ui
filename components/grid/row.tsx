58adc102ab088cbc9fb282630c22527291b438d3
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-11-11 20:27:40 +0100

    Typescript migration: grid

--- components/grid/row.tsx
+++ components/grid/row.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, ReactNode} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -10,15 +10,34 @@ const modifierKeys = [
   'around', 'between', // justify-content
   'top', 'middle', 'baseline', 'bottom', // align-items
   'first', 'last' // order
-];
+] as const;
+
+type ModifierType = 'xs' | 'sm' | 'md' | 'lg'
+
+export interface RowProps {
+  children?: ReactNode
+  reverse?: boolean | null | undefined
+  start?: ModifierType | null | undefined
+  center?: ModifierType | null | undefined
+  end?: ModifierType | null | undefined
+  top?: ModifierType | null | undefined
+  middle?: ModifierType | null | undefined
+  baseline?: ModifierType | null | undefined
+  bottom?: ModifierType | null | undefined
+  around?: ModifierType | null | undefined
+  between?: ModifierType | null | undefined
+  first?: ModifierType | null | undefined
+  last?: ModifierType | null | undefined
+  className?: string | null | undefined
+}
 
 /**
  * Converts xs="middle" to class "middle-xs"
  * @param {Object} props incoming props
  * @returns {Array} result modifier classes
  */
-function getModifierClassNames(props) {
-  return modifierKeys.reduce((result, key) => {
+function getModifierClassNames(props: RowProps) {
+  return modifierKeys.reduce((result: string[], key) => {
     if (props[key]) {
       return result.concat(styles[`${key}-${props[key]}`]);
     }
@@ -26,7 +45,7 @@ function getModifierClassNames(props) {
   }, []);
 }
 
-export default class Row extends Component {
+export default class Row extends Component<RowProps> {
   static propTypes = {
     reverse: PropTypes.bool,
     start: ModifierType,
