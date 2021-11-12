5498e6a4e620ab6e8d74eac89ebc50f14e9fd28a
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-11-13 00:45:18 +0100

    Typescript migration: island-legacy

--- components/island-legacy/island-legacy.tsx
+++ components/island-legacy/island-legacy.tsx
@@ -1,10 +1,10 @@
-import React, {PureComponent} from 'react';
+import React, {HTMLAttributes, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import './island-legacy.css';
 
-export default class Header extends PureComponent {
+export default class Island extends PureComponent<HTMLAttributes<HTMLElement>> {
   static propTypes = {
     className: PropTypes.string,
     children: PropTypes.node
@@ -12,17 +12,18 @@ export default class Header extends PureComponent {
 
   render() {
     const {children, className, ...restProps} = this.props;
-    const classes = classNames('ring-island__header', className);
+    const classes = classNames('ring-island', className);
 
     return (
       <div
         {...restProps}
         className={classes}
       >
-        <div className="ring-island__title">
-          {children}
-        </div>
+        {children}
       </div>
     );
   }
 }
+
+export {default as Header} from './header-legacy';
+export {default as Content} from './content-legacy';
