5e24f24936c040a507d91232a673070e02044151
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-11-13 01:01:42 +0100

    Typescript migration: login-dialog

--- components/login-dialog/service.tsx
+++ components/login-dialog/service.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import {render, unmountComponentAtNode} from 'react-dom';
 
-import LoginDialog from './login-dialog';
+import LoginDialog, {LoginDialogAttrs} from './login-dialog';
 
 const containerElement = document.createElement('div');
 
 /**
  * Renders LoginDialog into virtual node to skip maintaining container
  */
-function renderLoginDialog(props) {
+function renderLoginDialog(props: LoginDialogAttrs) {
   render(<LoginDialog {...props}/>, containerElement);
 }
 
-export default function showAuthDialog(props = {}) {
+function noop() {}
+export default function showAuthDialog(props: LoginDialogAttrs = {onCancel: noop}) {
   renderLoginDialog({
     ...props,
     show: true
