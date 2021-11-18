9144baa943105852bd6670a733caacd4479cdb8f
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-11-18 20:41:39 +0100

    Typescript migration: progress-bar

--- components/progress-bar/progress-bar.tsx
+++ components/progress-bar/progress-bar.tsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from 'react';
+import React, {HTMLAttributes, PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -6,17 +6,25 @@ import Theme from '../global/theme';
 
 import styles from './progress-bar.css';
 
+export interface ProgressBarProps extends HTMLAttributes<HTMLElement>{
+  max: number
+  value: number
+  theme: Theme
+  label: string
+  global?: boolean | null | undefined
+}
+
 /**
  * @name Progress Bar
  */
-export default class ProgressBar extends PureComponent {
+export default class ProgressBar extends PureComponent<ProgressBarProps> {
   /**
    * @param {number} value The progress task value
    * @param {number} max The maximum value
    * @return {number} The progress task value in percents
    * @private
    */
-  static toPercent(value, max) {
+  static toPercent(value: number, max: number) {
     const HUNDRED_PERCENT = 100;
     const percents = (value * HUNDRED_PERCENT) / max;
     return percents > HUNDRED_PERCENT ? HUNDRED_PERCENT : percents;
@@ -63,18 +71,20 @@ export default class ProgressBar extends PureComponent {
     label: 'Progress'
   };
 
-  progressbarWrapperRef = el => {
+  progressbarWrapper?: (HTMLElement | null);
+  progressbarWrapperRef = (el: HTMLElement | null) => {
     this.progressbarWrapper = el;
   };
 
-  progressbarRef = el => {
+  progressbar?: (HTMLElement | null);
+  progressbarRef = (el: HTMLElement | null) => {
     this.progressbar = el;
   };
 
   render() {
     const {theme, className, global, max, value, label, ...otherProps} = this.props;
 
-    const width = value ? `${ProgressBar.toPercent(value, max)}%` : null;
+    const width = value ? `${ProgressBar.toPercent(value, max)}%` : undefined;
     const classes = classNames(styles.progressBar, className, {
       [styles.light]: theme === Theme.LIGHT,
       [styles.dark]: theme === Theme.DARK,
@@ -101,3 +111,5 @@ export default class ProgressBar extends PureComponent {
     );
   }
 }
+
+export type ProgressBarAttrs = JSX.LibraryManagedAttributes<typeof ProgressBar, ProgressBarProps>
