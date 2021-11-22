1703e0fee2444c91d16faac7d02b6e90031fc003
andrey.skladchikov <andrey.skladchikov@TeamCity>
2021-11-22 12:51:09 +0000

    TeamCity change in 'JetBrains Public Projects / JetBrains UI / Ring UI' project: runners of 'Publish @latest (master)' build configuration were updated

--- .teamcity/patches/buildTypes/Publish.kts
+++ .teamcity/patches/buildTypes/Publish.kts
@@ -1,6 +1,8 @@
 package patches.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.ScriptBuildStep
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.RetryBuildTrigger
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.retryBuild
 import jetbrains.buildServer.configs.kotlin.v2018_2.ui.*
@@ -11,6 +13,124 @@ To apply the patch, change the buildType with id = 'Publish'
 accordingly, and delete the patch script.
 */
 changeBuildType(RelativeId("Publish")) {
+    expectSteps {
+        script {
+            name = "Publish"
+            id = "RUNNER_1461"
+            scriptContent = """
+                #!/bin/bash
+                set -e -x
+                
+                # Required for docker
+                mkdir -p ~/.ssh/
+                touch ~/.ssh/config
+                cat << EOT >> ~/.ssh/config
+                Host github.com
+                    StrictHostKeyChecking no
+                    UserKnownHostsFile /dev/null
+                EOT
+                
+                chmod 644 ~/.ssh/config
+                
+                # GitHub and NPM authorization
+                git config user.email "%github.com.builduser.email%"
+                git config user.name "%github.com.builduser.name%"
+                
+                echo "//registry.npmjs.org/:_authToken=%npmjs.com.auth.key%" > ~/.npmrc
+                
+                node -v
+                npm -v
+                npm whoami
+                
+                # Temporary until docker is not updated
+                npm config set unsafe-perm true
+                
+                if [ -n "${'$'}(git status --porcelain)" ]; then
+                  echo "Your git status is not clean. Aborting.";
+                  exit 1;
+                fi
+                
+                npm install
+                npm run bootstrap
+                # Reset possibly changed lock to avoid "git status is not clear" error
+                git checkout package.json package-lock.json packages/*/package-lock.json
+                npm whoami
+                npm run release-ci -- %lerna.publish.options%
+                
+                cat package.json
+                
+                function publishBuildNumber {
+                    local VERSION=${'$'}(node -p 'require("./package.json").version')
+                    echo "##teamcity[buildNumber '${'$'}VERSION']"
+                }
+                
+                publishBuildNumber
+                
+                #chmod 777 ~/.ssh/config
+            """.trimIndent()
+            dockerImage = "node:14"
+            dockerRunParameters = "-v %teamcity.build.workingDir%/npmlogs:/root/.npm/_logs"
+        }
+    }
+    steps {
+        update<ScriptBuildStep>(0) {
+            id = "RUNNER_1461"
+            clearConditions()
+            scriptContent = """
+                #!/bin/bash
+                set -e -x
+                
+                # Required for docker
+                mkdir -p ~/.ssh/
+                touch ~/.ssh/config
+                cat << EOT >> ~/.ssh/config
+                Host github.com
+                    StrictHostKeyChecking no
+                    UserKnownHostsFile /dev/null
+                EOT
+                
+                chmod 644 ~/.ssh/config
+                
+                # GitHub and NPM authorization
+                git config user.email "%github.com.builduser.email%"
+                git config user.name "%github.com.builduser.name%"
+                
+                echo "//registry.npmjs.org/:_authToken=%npmjs.com.auth.key%" > ~/.npmrc
+                
+                node -v
+                npm -v
+                npm whoami
+                
+                # Temporary until docker is not updated
+                npm config set unsafe-perm true
+                
+                if [ -n "${'$'}(git status --porcelain)" ]; then
+                  echo "Your git status is not clean. Aborting.";
+                  exit 1;
+                fi
+                
+                npm install
+                npm run bootstrap
+                npm run build
+                # Reset possibly changed lock to avoid "git status is not clear" error
+                git checkout package.json package-lock.json packages/*/package-lock.json
+                npm whoami
+                npm run release-ci -- %lerna.publish.options%
+                
+                cat package.json
+                
+                function publishBuildNumber {
+                    local VERSION=${'$'}(node -p 'require("./package.json").version')
+                    echo "##teamcity[buildNumber '${'$'}VERSION']"
+                }
+                
+                publishBuildNumber
+                
+                #chmod 777 ~/.ssh/config
+            """.trimIndent()
+        }
+    }
+
     triggers {
         val trigger1 = find<RetryBuildTrigger> {
             retryBuild {
