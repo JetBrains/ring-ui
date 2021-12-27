96a1f49650e14a3078ef42608a00ae8b76a07b5f
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2021-12-27 20:59:47 +0100

    Add auto theme

--- src/global/theme.tsx
+++ src/global/theme.tsx
@@ -8,7 +8,7 @@ import React, {
   PropsWithoutRef,
   FunctionComponent,
   HTMLAttributes,
-  useMemo
+  useMemo, useState, useEffect
 } from 'react';
 import classNames from 'classnames';
 
@@ -18,32 +18,53 @@ import styles from './variables_dark.css';
 import getUID from './get-uid';
 
 enum Theme {
+  AUTO= 'auto',
   LIGHT= 'light',
   DARK= 'dark'
 }
 
+const darkMatcher = window.matchMedia('(prefers-color-scheme: dark)');
+
+export function useTheme() {
+  const [dark, setDark] = useState(darkMatcher.matches);
+
+  useEffect(() => {
+    const onChange = (e: MediaQueryListEvent) => setDark(e.matches);
+    darkMatcher.addEventListener('change', onChange);
+
+    return () => darkMatcher.removeEventListener('change', onChange);
+  }, []);
+
+  return dark ? Theme.DARK : Theme.LIGHT;
+}
+
 export interface ThemeProviderProps extends HTMLAttributes<HTMLDivElement> {
-  theme: Theme
+  theme?: Theme
   passToPopups?: boolean
 }
 
 export function ThemeProvider({
-  theme,
+  theme = Theme.AUTO,
   className,
   passToPopups,
   children,
   ...restProps
 }: ThemeProviderProps) {
+  const systemTheme = useTheme();
+  const resolvedTheme = theme === Theme.AUTO ? systemTheme : theme;
   const id = useMemo(() => getUID('popups-with-theme-'), []);
   return (
-    <div className={classNames(className, {[styles.dark]: theme === Theme.DARK})} {...restProps}>{
-      passToPopups
-        ? (
-          <PopupTarget id={id}>
-            {target => <>{children}{target}</>}
-          </PopupTarget>
-        )
-        : children}
+    <div
+      className={classNames(className, {[styles.dark]: resolvedTheme === Theme.DARK})}
+      {...restProps}
+    >{
+        passToPopups
+          ? (
+            <PopupTarget id={id}>
+              {target => <>{children}{target}</>}
+            </PopupTarget>
+          )
+          : children}
     </div>
   );
 }
