835c04e590794130286a0ed9deca85c06871e1f6
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2022-05-24 11:16:19 +0200

    Customize puppeter browser to fix "Protocol error (Target.createTarget): Target closed" error

--- a11y-audit.test.js
+++ a11y-audit.test.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import fs from 'fs';
 
+import Puppeteer from 'puppeteer';
+
 import initStoryshots from '@storybook/addon-storyshots';
 import {axeTest} from '@storybook/addon-storyshots-puppeteer';
 
@@ -9,7 +11,12 @@ const isTeamCity = process.env.TEAMCITY_VERSION != null;
 const test = axeTest({
   storybookUrl: isTeamCity ? `file://${path.resolve(__dirname, 'storybook-dist')}` : 'http://localhost:9999?block-auth',
   customizePage: page => page.setViewport({width: 1000, height: 600}),
-  testTimeout: 20000
+  testTimeout: 20000,
+  // Customized like said in https://github.com/puppeteer/puppeteer/issues/1947#issuecomment-879500626
+  getCustomBrowser: () => Puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process'],
+    headless: true
+  })
 });
 
 const suite = 'Accessibility audit';
