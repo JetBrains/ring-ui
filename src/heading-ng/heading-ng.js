1ed865373ee2fd393636176f92726e07e5c7c25f
Max Maximov <max.maximov@gmail.com>
2022-08-11 19:29:57 +0200

    Add rgHeading angular wrapper for Heading component [publish]

---
+++ src/heading-ng/heading-ng.js
@@ -0,0 +1,8 @@
+/**
+ * @name Heading Ng
+*/
+
+import angularComponentFactory from '../global/angular-component-factory';
+import HeadingNg from '../heading/heading';
+
+export default angularComponentFactory(HeadingNg, 'Heading').name;
