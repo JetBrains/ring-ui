cf9c6be35f36760a97bab5c57a3430d4bb53cb9f
katriyna <katriyna@gmail.com>
2022-11-08 18:12:12 +0100

    RG-2220 toggle class .ring-ui-them-dark when theme is changed

--- src/global/theme.tsx
+++ src/global/theme.tsx
@@ -41,10 +41,10 @@ export function applyTheme(theme: Theme.DARK | Theme.LIGHT, container: HTMLEleme
   if (theme === Theme.DARK) {
     container.classList.remove(defaultStyles.light);
     container.classList.add(styles.dark);
-    container.classList.add(styles['ring-ui-theme-dark']);
+    container.classList.add('ring-ui-theme-dark');
   } else {
     container.classList.remove(styles.dark);
-    container.classList.remove(styles['ring-ui-theme-dark']);
+    container.classList.remove('ring-ui-theme-dark');
     container.classList.add(defaultStyles.light);
   }
 }
