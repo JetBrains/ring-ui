0d541bf340f6569019106c47a09a02bd63c06780
Max Maximov <max.maximov@gmail.com>
2022-12-06 05:45:14 +0100

    Editable Heading. Support "multiline" mode

--- src/editable-heading/editable-heading.examples.tsx
+++ src/editable-heading/editable-heading.examples.tsx
@@ -106,6 +106,16 @@ export const basic = () => {
       </ExampleEditableHeading>
       {lorem}
 
+      <ExampleEditableHeading multiline>
+        Multiline Heading.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio tellus.
+        Suspendisse ut elit orci. Pellentesque sagittis pharetra lacus, id vehicula mauris.
+        Donec lacinia tincidunt risus, et sollicitudin risus dignissim sit amet.
+        Duis eu lectus ac odio pharetra egestas in eget ipsum. Curabitur luctus lacinia molestie.
+        Nullam id metus tortor.
+      </ExampleEditableHeading>
+      {lorem}
+
       <ExampleEditableHeading error="">Invalid Heading</ExampleEditableHeading>
       {lorem}
 
