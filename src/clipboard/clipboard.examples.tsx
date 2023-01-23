324f7680cdd2c63e8eed8665fdcdf5b2d5128f6b
katriyna <katriyna@gmail.com>
2023-01-23 13:55:30 +0100

    RG-2252 Update login and email presentation on User Card in ring-ui

---
+++ src/clipboard/clipboard.examples.tsx
@@ -0,0 +1,36 @@
+import React, {Fragment} from 'react';
+
+import reactDecorator from '../../.storybook/react-decorator';
+import Link from '../link/link';
+
+import clipboard from './clipboard';
+
+export default {
+  title: 'Components/Clipboard',
+  decorators: [reactDecorator()],
+
+  parameters: {
+    notes: 'Displays a link which copies test to clipboard.'
+  }
+};
+
+const DEMO_TEXT = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt' +
+  'ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.' +
+  'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur' +
+  'sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.' +
+  'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata' +
+  'sanctus est Lorem ipsum dolor sit amet.';
+
+export const copyToClipboard = () => (
+  <Fragment>
+    <div>{DEMO_TEXT}</div>
+    <div>
+      <Link
+        onClick={() => clipboard.copyText(DEMO_TEXT, 'Text copied!', 'Text copying error')}
+        pseudo
+      >
+        Copy
+      </Link>
+    </div>
+  </Fragment>
+);
