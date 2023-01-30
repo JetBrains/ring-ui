bc4f8747baab619d6bfd17f02d26aa83de9c2326
katriyna <katriyna@gmail.com>
2023-01-30 13:30:29 +0100

    RG-2252 api of redesigned user-card should be backward-compatible

--- src/user-card/card.tsx
+++ src/user-card/card.tsx
@@ -26,10 +26,10 @@ export interface UserCardWording {
   banned: string
   online: string
   offline: string
-  copyToClipboard: string
-  copiedToClipboard: string
-  copingToClipboardError: string
-  unverified: string
+  copyToClipboard?: string
+  copiedToClipboard?: string
+  copingToClipboardError?: string
+  unverified?: string
 }
 export interface UserCardProps extends HTMLAttributes<HTMLDivElement> {
   user: UserCardUser
@@ -80,12 +80,15 @@ export default class UserCard extends PureComponent<UserCardProps> {
   };
 
   copyEmail = () => {
-    const {user, wording} = this.props;
-    clipboard.copyText(user.email || '', wording.copiedToClipboard, wording.copingToClipboardError);
+    const wording = {...UserCard.defaultProps.wording, ...this.props.wording};
+    clipboard.copyText(
+      this.props.user.email || '', wording.copiedToClipboard, wording.copingToClipboardError
+    );
   };
 
   render() {
-    const {children, info, className, user, wording, avatarInfo, ...restProps} = this.props;
+    const {children, info, className, user, avatarInfo, ...restProps} = this.props;
+    const wording = {...UserCard.defaultProps.wording, ...this.props.wording};
 
     const classes = classNames(className, {});
     const userActiveStatusClasses = classNames(
