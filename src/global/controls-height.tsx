7bad310084392ce13fb02275e35fc0f1864c313b
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2023-03-22 16:58:51 +0100

    JT-73698 add a way to configure default controls height for AlertService

--- src/global/controls-height.tsx
+++ src/global/controls-height.tsx
@@ -7,3 +7,14 @@ export enum ControlsHeight {
 }
 
 export const ControlsHeightContext = createContext(ControlsHeight.M);
+
+let globalControlsHeight = ControlsHeight.M;
+
+// This can be used if React Context is not applicable, for example for alertService or Auth dialog
+export function configureGlobalControlsHeight(value: ControlsHeight) {
+  globalControlsHeight = value;
+}
+
+export function getGlobalControlsHeight() {
+  return globalControlsHeight;
+}
