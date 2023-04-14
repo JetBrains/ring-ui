b2c6e1eb8836387882ba7c887e4d3a0ce792328c
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2023-04-14 18:53:04 +0200

    Fix Storybook custom header and dark theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,41 @@
+import {paramCase} from 'change-case';
+import {create} from '@storybook/theming';
+
+export const darkMatcher = window.matchMedia('(prefers-color-scheme: dark)');
+// Proxy support is more or less the same as one of CSS custom properties
+const variables: Record<string, string> = window.Proxy
+  ? new Proxy(
+    {},
+    {
+      get: (_, prop) => getComputedStyle(document.documentElement).getPropertyValue(`--ring-${paramCase(prop as string)}`).trim()
+    },
+  )
+  : {};
+
+const variablesSupported = variables.textColor != null;
+export const theme = create({
+  base: darkMatcher.matches ? 'dark' : 'light',
+  brandTitle: 'JetBrains Ring UI',
+  ...(variablesSupported
+    ? {
+      // Storybook-specific color palette
+      colorSecondary: variables.mainColor,
+
+      // UI
+      appBorderColor: variables.lineColor,
+      appBorderRadius: parseInt(variables.borderRadius, 10),
+
+      // Typography
+      fontBase: variables.fontFamily,
+      fontCode: variables.fontFamilyMonospace,
+
+      // Toolbar default and active colors
+      barTextColor: variables.secondaryColor,
+      barSelectedColor: variables.mainColor,
+
+      // Form colors
+      inputBorder: variables.bordersColor,
+      inputTextColor: variables.textColor
+    }
+    : {})
+});
