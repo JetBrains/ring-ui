b33351e342a5998585f3b96eccc8914dda0b78ca
andrey.volfman <andrey.volfman@jetbrains.com>
2023-06-06 19:34:26 +0300

    Revert "Revert "fix preventDefault for months scrolling in date-picker""
    This reverts commit f89931047f190e3db5294cf80f04b38af05ff569.

---
+++ src/global/use-event-callback.ts
@@ -0,0 +1,19 @@
+import {useCallback, useLayoutEffect, useRef} from 'react';
+
+export default function useEventCallback<I extends unknown[], O>(
+  fn: (...args: I) => O,
+): (...args: I) => O {
+  const ref = useRef<(...args: I) => O>();
+  useLayoutEffect(() => {
+    ref.current = fn;
+  });
+  return useCallback((...args) => {
+    const {current} = ref;
+
+    if (current == null) {
+      throw new Error('callback created in useEventCallback can only be called from event handlers');
+    }
+
+    return current(...args);
+  }, []);
+}
