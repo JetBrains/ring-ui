7ff8477f1c6ac2a35208264ddd6dca1932d1fdb8
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-02-05 15:39:18 +0100

    RG-2343 add example of nested dropdown menu usage

--- src/dropdown-menu/dropdown-menu.stories.tsx
+++ src/dropdown-menu/dropdown-menu.stories.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 
-import DropdownMenu from './dropdown-menu';
+import chevronRight from '@jetbrains/icons/chevron-right';
+
+import {Directions} from '../popup/popup.consts';
+import Icon from '../icon';
+import Group from '../group/group';
+
+import {ListDataItem} from '../list/consts';
+
+import DropdownMenu, {DropdownMenuProps} from './dropdown-menu';
 
 export default {
   title: 'Components/DropdownMenu',
@@ -43,3 +51,140 @@ export const basic = () => {
 };
 
 basic.storyName = 'DropdownMenu';
+
+export const nested = () => {
+  interface NestedMenuProps {
+    title: string;
+    data?: ListDataItem[];
+    children?: React.ReactNode;
+  }
+
+  interface MenuItem extends ListDataItem {
+    submenu?: MenuItem[] | React.ReactNode;
+  }
+
+  const NestedMenuItem = (props: NestedMenuProps) => {
+    const anchor: DropdownMenuProps['anchor'] = ({active}, ariaProps = {}) => (
+      <Group
+        role="menu"
+        className="nested-menu-item"
+        {...{'aria-expanded': active, 'aria-label': props.title}}
+        {...ariaProps}
+      >
+        <span className="nested-menu-title">{props.title}</span>
+
+        <Icon glyph={chevronRight} className="chevron-icon"/>
+      </Group>
+    );
+
+    const dropdownProps: DropdownMenuProps = {
+      hoverMode: true,
+      hoverShowTimeOut: 50,
+      hoverHideTimeOut: 100,
+      anchor
+    };
+
+    const menuProps: DropdownMenuProps['menuProps'] = {
+      directions: [
+        Directions.RIGHT_BOTTOM, Directions.LEFT_BOTTOM, Directions.RIGHT_TOP, Directions.LEFT_TOP
+      ],
+      left: 20,
+      top: -12,
+      minWidth: 150,
+      ['data-test']: 'nested-menu',
+      hidden: false,
+      activateFirstItem: false
+    };
+
+    if (props.data) {
+      // dropdown menu has automatic support for aria-navigation
+      return <DropdownMenu {...dropdownProps} data={props.data} menuProps={menuProps}/>;
+    }
+
+    return (
+      <DropdownMenu {...(dropdownProps)} menuProps={menuProps}/>
+    );
+  };
+
+  const menu: MenuItem[] = [
+    {label: 'Option 1', key: 'item1'},
+    {label: 'Option 2', key: 'nested-parent', submenu: [
+      {label: 'Option 2-1', key: 'nested-item1'},
+      {label: 'Option 2-2', key: 'nested-item2'},
+      {label: 'Option 2-3', key: 'nested-item3'},
+      {label: 'Option 2-4', key: 'nested-item4'}
+    ]},
+    {label: 'Option 3', key: 'item3', submenu: [
+      {label: 'Option 3-1', key: 'nested-item1'},
+      {label: 'Option 3-2', key: 'nested-item2'},
+      {label: 'Option 3-3', key: 'nested-item3'},
+      {label: 'Option 3-4', key: 'nested-item4'}
+    ]}
+  ];
+
+  const data: ListDataItem[] = menu.reduce((acc, menuItem) => {
+    const {submenu, ...rest} = menuItem;
+
+    if (!submenu) {
+      acc.push(menuItem);
+      return acc;
+    }
+
+    const secondLevelRoot: ListDataItem = {
+      rgItemType: DropdownMenu.ListProps.Type.CUSTOM,
+      key: menuItem.key,
+      template: (
+        <button type="button" className="nested-menu-button" onClick={e => e.stopPropagation()}>
+          <NestedMenuItem
+            title={rest.label as string}
+            data={Array.isArray(submenu) ? (submenu as ListDataItem[]) : undefined}
+          >
+            {submenu as React.ReactNode}
+          </NestedMenuItem>
+        </button>
+      )
+    };
+    acc.push(secondLevelRoot);
+
+    return acc;
+  }, [] as ListDataItem[]);
+
+  return <DropdownMenu anchor="Click me" data={data} menuProps={{minWidth: 200}}/>;
+};
+
+nested.storyName = 'DropdownMenu nested';
+
+nested.parameters = {
+  hermione: {
+    actions: [
+      {type: 'click', selector: '[data-test~=ring-dropdown]'},
+      {type: 'click', selector: '[aria-label="Option 2"]'},
+      {type: 'mouseMove', selector: '[title="Option 2-1"]'},
+      {
+        type: 'capture',
+        name: 'dropdown',
+        selector: ['[data-test~=ring-dropdown]', '[data-test~=ring-popup]', '[data-test~=nested-menu]']
+      }]
+  },
+  storyStyles: `
+<style>
+  .chevron-icon {
+    color: var(--ring-secondary-color)
+  }
+  .nested-menu-item {
+    display: flex;
+    width: 100%;
+  }
+  .nested-menu-title {
+    flex-grow: 1;
+  }
+  .nested-menu-button {
+    all: unset;
+    display: flex;
+    width: 100%;
+  }
+  .nested-menu-button > * {
+    width: 100%;
+  }
+</style>`
+};
