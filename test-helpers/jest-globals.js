7991c80cc3533579f6c5de56eb2b80e42352b156
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-06-15 00:31:08 +0200

    Add native HTML dialog support [publish]

--- test-helpers/jest-globals.js
+++ test-helpers/jest-globals.js
@@ -48,3 +48,7 @@ Object.defineProperty(window, 'matchMedia', {
 });
 
 setProjectAnnotations(require('../.storybook/preview'));
+
+HTMLDialogElement.prototype.show = jest.fn();
+HTMLDialogElement.prototype.showModal = jest.fn();
+HTMLDialogElement.prototype.close = jest.fn();
