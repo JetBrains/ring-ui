7035ffa552fc683cb2e3ad8a6e18e3651f44f93a
Veniamin Krol <veniamin.krol@jetbrains.com>
2024-07-02 21:15:12 +0200

    Deprecate composeRefs export

--- src/global/composeRefs.ts
+++ src/global/composeRefs.ts
@@ -1,7 +1,8 @@
 import {Ref, MutableRefObject} from 'react';
 import memoizeOne from 'memoize-one';
+import deprecate from 'util-deprecate';
 
-export default function composeRefs<T>(...refs: (Ref<T> | undefined)[]) {
+function composeRefs<T>(...refs: (Ref<T> | undefined)[]) {
   return (value: T | null) => refs.forEach(ref => {
     if (typeof ref === 'function') {
       ref(value);
@@ -10,6 +11,9 @@ export default function composeRefs<T>(...refs: (Ref<T> | undefined)[]) {
     }
   });
 }
+
+// TODO remove export in 7.0, composeRefs should be used only in createComposedRef and in useComposedRefs in the future
+export default deprecate(composeRefs, 'composeRefs is deprecated and will be removed in 7.0. Use createComposedRef instead.');
 
 export function createComposedRef<T>() {
   return memoizeOne(composeRefs<T>);
