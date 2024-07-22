364b9228303e444ad560cdf3db09feec3f7a5a7c
Sergey Kochetkov <skoch13@pm.me>
2024-07-22 12:47:15 +0200

    RG-2457 feat(simple-table): add simple table component

---
+++ src/table/simple-table.tsx
@@ -0,0 +1,55 @@
+import {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+import classNames from 'classnames';
+
+import style from './table.css';
+
+import Table, {TableAttrs} from './table';
+import Selection, {SelectionItem} from './selection';
+
+const {
+  selection: __selection__,
+  onSelect: __onSelect__,
+  selectable: __selectable__,
+  disabledHover: __disabledHover__,
+  ...restPropTypes
+} = Table.propTypes ?? {};
+
+export interface SimpleTableProps<T extends SelectionItem> extends
+  Omit<TableAttrs<T>, 'selection' | 'onSelect' | 'selectable'> {
+  narrowFirstColumn?: boolean;
+}
+class SimpleTable<T extends SelectionItem> extends PureComponent<SimpleTableProps<T>> {
+  static propTypes = {
+    narrowFirstColumn: PropTypes.bool,
+    ...restPropTypes
+  };
+
+  static defaultProps = {
+    selectable: false
+  };
+
+
+  state = {
+    selection: new Selection({
+      data: this.props.data
+    })
+  };
+
+  classes = classNames(style.disabledHover,
+    {[style.narrowFirstColumn]: this.props.narrowFirstColumn}, this.props.className);
+
+  render() {
+    return (
+      <Table
+        className={this.classes}
+        {...this.props}
+        {...this.state}
+      />
+    );
+  }
+}
+
+export default SimpleTable;
+
