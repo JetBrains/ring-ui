ac975756e9083335ee7f46128841944461a698d9
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-08-05 21:16:40 +0200

    RG-2341 put styles to layers: "ring-ui_global" for global variables and "ring-ui" for rest library styles

--- postcss.config.js
+++ postcss.config.js
@@ -13,7 +13,18 @@ module.exports = () => {
     require('postcss-font-family-system-ui')({browsers: ['last 2 versions']}),
     require('postcss-flexbugs-fixes')(),
     require('@jetbrains/postcss-require-hover')(),
-    require('postcss-calc')({mediaQueries: true})
+    require('postcss-calc')({mediaQueries: true}),
+    require('postcss-assign-layer')([
+      {
+        include: '**/global/*.css',
+        layerName: 'ring-ui_global'
+      },
+      {
+        include: '**/!(global)/*.css',
+        // Hack to make sure `ring-ui_global` layer is always defined first – and has less priority
+        layerName: 'ring-ui_global {} @layer ring-ui'
+      }
+    ])
   ];
 
   return {plugins};
