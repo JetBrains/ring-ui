8010eca3394bec3ddfb12bd43bd198d05f951052
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-10-04 20:58:18 +0200

    RTL migration: heading.test.tsx

--- src/heading/heading.test.tsx
+++ src/heading/heading.test.tsx
@@ -1,23 +1,27 @@
-import {shallow} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 
 import Heading, {H2, HeadingProps} from './heading';
 
 describe('Heading', () => {
-  const shallowHeading = (props?: HeadingProps) => shallow(<Heading {...props}/>);
+  const renderHeading = (props?: HeadingProps) => render(<Heading {...props}/>);
 
   it('should wrap children with h1 by default', () => {
-    shallowHeading().should.have.tagName('h1');
+    renderHeading();
+    screen.getByRole('heading').should.have.tagName('h1');
   });
 
   it('should accept level prop', () => {
-    shallowHeading({level: Heading.Levels.H3}).should.have.tagName('h3');
+    renderHeading({level: Heading.Levels.H3});
+    screen.getByRole('heading').should.have.tagName('h3');
   });
 
   it('should export helpers', () => {
-    shallow(<H2/>).should.have.tagName('h2');
+    render(<H2/>);
+    screen.getByRole('heading').should.have.tagName('h2');
   });
 
   it('should use passed className', () => {
-    shallowHeading({className: 'test-class'}).should.have.className('test-class');
+    renderHeading({className: 'test-class'});
+    screen.getByRole('heading').should.have.class('test-class');
   });
 });
