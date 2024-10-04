c7c150785a009dd9840b72ad27721014fa781426
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-10-04 20:56:32 +0200

    RTL migration: header.test.tsx

--- src/header/header.test.tsx
+++ src/header/header.test.tsx
@@ -1,20 +1,18 @@
-import {mount} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 
 import Header, {HeaderAttrs} from './header';
 
 describe('Header', () => {
-  const mountHeader = (props?: HeaderAttrs) => mount(<Header {...props}/>);
-  const getHeaderDiv = (props?: HeaderAttrs) => mountHeader(props).find('header');
-
-  it('should create component', () => {
-    mountHeader().type().should.equal(Header);
-  });
+  const getHeaderDiv = (props?: HeaderAttrs) => {
+    render(<Header {...props}/>);
+    return screen.getByRole('banner');
+  };
 
   it('should wrap children with header', () => {
     getHeaderDiv().should.exist;
   });
 
   it('should use passed className', () => {
-    getHeaderDiv({className: 'test-class'}).should.have.className('test-class');
+    getHeaderDiv({className: 'test-class'}).should.have.class('test-class');
   });
 });
