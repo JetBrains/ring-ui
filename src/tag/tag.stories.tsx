7d2d3fced73048b95b4737864fc6228fa5a8febb
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-11-04 14:36:28 +0100

    RG-2506 don't show "cross" on Tag if "onRemove" is not passed to make it better replacement for Badge

--- src/tag/tag.stories.tsx
+++ src/tag/tag.stories.tsx
@@ -12,6 +12,8 @@ export default {
   },
 };
 
+const noop = () => {};
+
 export const demo = () => {
   const avatarUrl =
     'data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgNDAgNDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+ICAgIDxkZWZzPiAgICAgICAgPGxpbmVhckdyYWRpZW50IGlkPSJncmFkaWVudCIgeDE9IjAiIHkxPSIwIiB4Mj0iMCIgeTI9IjEiPiAgICAgICAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiNENTBGNkIiIG9mZnNldD0iMCIvPiAgICAgICAgICAgIDxzdG9wIHN0b3AtY29sb3I9IiNFNzNBRTgiIG9mZnNldD0iMSIvPiAgICAgICAgPC9saW5lYXJHcmFkaWVudD4gICAgPC9kZWZzPiAgICA8Zz4gICAgICAgIDxyZWN0IGZpbGw9InVybCgjZ3JhZGllbnQpIiAgICAgICAgICAgICAgeD0iMCIgeT0iMCIgd2lkdGg9IjQwIiBoZWlnaHQ9IjQwIiAgICAgICAgICAgICAgcng9IjMiIHJ5PSIzIi8+ICAgICAgICA8dGV4dCB4PSI1IiB5PSIxOSIgICAgICAgICAgICAgIGZvbnQtZmFtaWx5PSJBcmlhbCwgSGVsdmV0aWNhLCBzYW5zLXNlcmlmIiAgICAgICAgICAgICAgZm9udC1zaXplPSIxNXB4IiAgICAgICAgICAgICAgbGV0dGVyLXNwYWNpbmc9IjEiICAgICAgICAgICAgICBmaWxsPSIjRkZGRkZGIj4gICAgICAgICAgICA8dHNwYW4+SkI8L3RzcGFuPiAgICAgICAgICAgIDx0c3BhbiB4PSI2IiB5PSIyOCI+XzwvdHNwYW4+ICAgICAgICA8L3RleHQ+ICAgIDwvZz48L3N2Zz4=';
@@ -20,16 +22,19 @@ export const demo = () => {
     render() {
       return (
         <div>
-          <Tag>Simple</Tag>
+          <Tag onRemove={noop}>Simple</Tag>
           <Tag readOnly>Read-only</Tag>
-          <Tag className="limited-width">With long truncated text</Tag>
-          <Tag rgTagIcon={tagIcon} rgTagTitle="I am an icon title">
+          <Tag className="limited-width" onRemove={noop}>
+            With long truncated text
+          </Tag>
+          <Tag rgTagIcon={tagIcon} rgTagTitle="I am an icon title" onRemove={noop}>
             With icon title
           </Tag>
-          <Tag avatar={avatarUrl} readOnly={false}>
+          <Tag avatar={avatarUrl} onRemove={noop}>
             With avatar
           </Tag>
           <Tag
+            onRemove={noop}
             render={({children, ...restProps}) => (
               <a href="/" {...restProps}>
                 {children}
