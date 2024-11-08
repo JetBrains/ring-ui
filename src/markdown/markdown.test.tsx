ef06fb9da7154f22593bc62bd4670380b4348650
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-11-08 19:52:32 +0100

    RTL migration: markdown.test.tsx

--- src/markdown/markdown.test.tsx
+++ src/markdown/markdown.test.tsx
@@ -1,20 +1,22 @@
-import {shallow} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 
 import Markdown, {MarkdownProps} from './markdown';
 
 describe('Markdown', () => {
-  const shallowMarkdown = (props?: Partial<MarkdownProps>) =>
-    shallow(
+  const renderMarkdown = (props?: Partial<MarkdownProps>) => {
+    render(
       <Markdown {...props}>
         <div dangerouslySetInnerHTML={{__html: '<p>test</p>'}} />
       </Markdown>,
     );
+    return screen.getByTestId('ring-markdown');
+  };
 
   it('should wrap children with div', () => {
-    shallowMarkdown().should.have.tagName('div');
+    renderMarkdown().should.have.tagName('div');
   });
 
   it('should use passed className', () => {
-    shallowMarkdown({className: 'test-class'}).should.have.className('test-class');
+    renderMarkdown({className: 'test-class'}).should.have.class('test-class');
   });
 });
