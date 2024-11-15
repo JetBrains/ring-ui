1bbe2248ab7fb7d826aa6fc95042ef5b94a1229b
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-11-15 19:47:48 +0100

    RTL migration: progress-bar.test.tsx

--- src/progress-bar/progress-bar.test.tsx
+++ src/progress-bar/progress-bar.test.tsx
@@ -1,20 +1,22 @@
-import {shallow, mount} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 
 import ProgressBar, {ProgressBarAttrs} from './progress-bar';
 import styles from './progress-bar.css';
 
 describe('Progress Bar', () => {
-  const shallowProgressBar = (props?: ProgressBarAttrs) => shallow(<ProgressBar label="Progress" {...props} />);
-  const mountProgressBar = (props?: ProgressBarAttrs) =>
-    mount(<ProgressBar label="Progress" {...props} />).find<ProgressBar>(ProgressBar);
+  const renderProgressBar = (props?: ProgressBarAttrs) => {
+    render(<ProgressBar label="Progress" {...props} />);
+  };
 
   it('should create component', () => {
-    shallowProgressBar().should.exist;
+    renderProgressBar();
+    screen.getByRole('progressbar').should.exist;
   });
 
   describe('default value for attributes', () => {
     it('should set default value for max attribute', () => {
-      mountProgressBar().should.have.prop('max', 1.0);
+      renderProgressBar();
+      screen.getByRole('progressbar', {value: {max: 1.0}}).should.exist;
     });
   });
 
@@ -22,37 +24,37 @@ describe('Progress Bar', () => {
     it('should set max value for progress bar', () => {
       const MAX = 100;
 
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         max: MAX,
       });
 
-      wrapper.should.have.prop('max', MAX);
+      screen.getByRole('progressbar', {value: {max: MAX}}).should.exist;
     });
 
     it('should set progress task value', () => {
       const MIDDLE = 0.5;
 
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         value: MIDDLE,
       });
 
-      wrapper.should.have.prop('value', MIDDLE);
+      screen.getByRole('progressbar', {value: {now: MIDDLE}}).should.exist;
     });
 
     it('should set additional classes(modifiers) to the component', () => {
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         className: 'test-class',
       });
 
-      wrapper.instance().progressbarWrapper!.should.have.class('test-class');
+      screen.getByRole('progressbar').parentElement!.should.have.class('test-class');
     });
 
     it('should set global modifier', () => {
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         global: true,
       });
 
-      wrapper.instance().progressbarWrapper!.should.have.class(styles.globalMode);
+      screen.getByRole('progressbar').parentElement!.should.have.class(styles.globalMode);
     });
   });
 
@@ -62,47 +64,34 @@ describe('Progress Bar', () => {
    */
   describe('#render', () => {
     it('should set min value to equal zero', () => {
-      const wrapper = mountProgressBar();
-      wrapper.instance().progressbar!.should.have.attr('aria-valuemin', '0');
-    });
-
-    it('should update max value in DOM', () => {
-      const wrapper = mountProgressBar({
-        max: 100,
-      });
-
-      wrapper.instance().progressbar!.should.have.attr('aria-valuemax', '100');
+      renderProgressBar();
+      screen.getByRole('progressbar', {value: {min: 0}}).should.exist;
     });
 
     it('should update progress value in DOM', () => {
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         value: 0.5,
       });
 
-      wrapper.instance().progressbar!.should.have.attr('aria-valuenow', '0.5');
-      wrapper
-        .instance()
-        .progressbar!.should.have.attr('style')
-        .match(/width: 50%;/);
+      const progressBar = screen.getByRole('progressbar', {value: {now: 0.5}});
+      progressBar.should.exist;
+      progressBar.should.have.style('width', '50%');
     });
 
     it('should set width equal 100% if progress value more than max value', () => {
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         max: 1.0,
         value: 10,
       });
 
-      wrapper
-        .instance()
-        .progressbar!.should.have.attr('style')
-        .match(/width: 100%;/);
+      screen.getByRole('progressbar').should.have.style('width', '100%');
     });
 
     it('should not set style if value is not a number', () => {
-      const wrapper = mountProgressBar({
+      renderProgressBar({
         value: undefined,
       });
-      wrapper.instance().progressbar!.should.not.have.attr('style');
+      screen.getByRole('progressbar').should.not.have.attr('style');
     });
   });
 });
