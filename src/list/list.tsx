23dae7bec896b506da287314c404d4893f63f506
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2024-12-20 14:40:32 +0100

    Don't render list item description if it is an empty string when trimmed. JT-86886 related.

--- src/list/list.tsx
+++ src/list/list.tsx
@@ -581,6 +581,9 @@ export default class List<T = unknown> extends Component<ListProps<T>, ListState
       if (cleanedProps.href) {
         cleanedProps.rgItemType = Type.LINK;
       }
+      cleanedProps.description = typeof cleanedProps.description === 'string'
+        ? cleanedProps.description.trim()
+        : cleanedProps.description;
       const itemProps =
         Object.assign({rgItemType: DEFAULT_ITEM_TYPE}, restProps) as ListDataItemProps<T>;
 
