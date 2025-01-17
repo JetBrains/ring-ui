90f21e9985f28b66c8d05cb06ce0a6239b1e973a
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2025-01-17 13:51:11 +0100

    RG-2552 List: use the same styles for links and regular items

--- src/list/list__custom.tsx
+++ src/list/list__custom.tsx
@@ -1,12 +1,11 @@
 import {PureComponent, ReactNode} from 'react';
 import * as React from 'react';
-import classNames from 'classnames';
 
 import dataTests from '../global/data-tests';
 import getEventKey from '../global/get-event-key';
 
-import styles from './list.css';
 import {ListDataItemProps} from './consts';
+import {getListClasses} from './list__classes';
 
 export default class ListCustom<T> extends PureComponent<ListDataItemProps<T>> {
   handleKeyPress = (event: React.KeyboardEvent) => {
@@ -17,34 +16,15 @@ export default class ListCustom<T> extends PureComponent<ListDataItemProps<T>> {
   };
 
   render() {
-    const {
-      scrolling,
-      hover,
-      className,
-      disabled,
-      template,
-      rgItemType,
-      tabIndex,
-      onClick,
-      onCheckboxChange,
-      onMouseOver,
-      onMouseUp,
-      role,
-      tagName,
-      ...restProps
-    } = this.props;
-    const classes = classNames(styles.item, className, {
-      [styles.action]: !disabled,
-      [styles.hover]: hover && !disabled,
-      [styles.scrolling]: scrolling,
-    });
+    const {disabled, template, tabIndex, onClick, onMouseOver, onMouseUp, role, tagName} = this.props;
+    const classes = getListClasses(this.props);
 
     const dataTest = dataTests(
       'ring-list-item-custom',
       {
         'ring-list-item-action': !disabled,
       },
-      restProps['data-test'],
+      this.props['data-test'],
     );
 
     const content: ReactNode = typeof template === 'function' ? template(this.props as ListDataItemProps<T>) : template;
