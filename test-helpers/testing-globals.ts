a312e0a3ac4d7ac4c6acfb8e90ff8f21eb86d89e
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2025-02-07 21:35:32 +0100

    RTL migration: query-assist.test.tsx

--- test-helpers/testing-globals.ts
+++ test-helpers/testing-globals.ts
@@ -30,6 +30,7 @@ declare global {
 Object.assign(window, windowExtension);
 
 afterEach(function restoreSandbox() {
+  vi.useRealTimers();
   window.sandbox.restore();
 
   Array.from(document.body.children).forEach(child => {
@@ -40,3 +41,15 @@ afterEach(function restoreSandbox() {
 });
 
 window.global = window as never;
+
+Range.prototype.getBoundingClientRect = () => ({
+  x: 0,
+  y: 0,
+  bottom: 0,
+  height: 0,
+  left: 0,
+  right: 0,
+  top: 0,
+  width: 0,
+  toJSON: () => {},
+});
