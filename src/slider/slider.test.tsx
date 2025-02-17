b5edf11b7c5e9148537e29721d7999051a0d130f
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2025-02-17 11:48:37 +0100

    RTL migration: Slider

--- src/slider/slider.test.tsx
+++ src/slider/slider.test.tsx
@@ -1,52 +1,58 @@
-import {mount} from 'enzyme';
-import {ComponentProps} from 'react';
-
 import {fireEvent, render, screen} from '@testing-library/react';
+import {ComponentProps} from 'react';
 
 import {Slider} from './slider';
 
 const DEFAULT_VALUE = 42;
 
 describe('Slider', () => {
-  const mountSlider = (params?: ComponentProps<typeof Slider>) => mount(<Slider {...params} />);
-  const renderSlider = (params?: ComponentProps<typeof Slider>) => render(<Slider {...params} />);
+  const renderSlider = (props?: ComponentProps<typeof Slider>) => render(<Slider {...props} />);
 
   it('should create component', () => {
-    mountSlider().should.have.type(Slider);
+    const {container} = renderSlider();
+    container.firstElementChild?.should.exist;
   });
 
   it('should use passed className', () => {
-    mountSlider({className: 'test-class'}).find(Slider).should.have.className('test-class');
+    const {container} = renderSlider({className: 'test-class'});
+    container.querySelector('.test-class')?.should.exist;
   });
 
   it('should use min value when defaultValue or value are unspecified', () => {
     renderSlider({min: DEFAULT_VALUE, showTag: true});
-    screen.getByText(DEFAULT_VALUE);
-    should.exist(screen.queryByRole('tooltip'));
+    screen.getByText(DEFAULT_VALUE).should.exist;
+    screen.getByRole('tooltip').should.exist;
   });
 
   it('should use values in range', () => {
     const RIGHT = 101;
     renderSlider({defaultValue: [-1, RIGHT], showTag: true});
-    const tooltips = screen.queryAllByRole('tooltip');
-    tooltips[0]?.should.have.text('0');
-    tooltips[1]?.should.have.text('100');
+    const tooltips = screen.getAllByRole('tooltip');
+    tooltips[0]?.textContent?.should.equal('0');
+    tooltips[1]?.textContent?.should.equal('100');
   });
 
   it('should display a formatted tag', () => {
-    renderSlider({defaultValue: DEFAULT_VALUE, showTag: true, renderTag: value => `%${value}%`});
-    screen.queryByRole('tooltip')?.should.have.text(`%${DEFAULT_VALUE}%`);
+    renderSlider({
+      defaultValue: DEFAULT_VALUE,
+      showTag: true,
+      renderTag: value => `%${value}%`,
+    });
+    screen.getByRole('tooltip').textContent?.should.equal(`%${DEFAULT_VALUE}%`);
   });
 
   it.skip('should handle only 2 values in range', () => {
     const NEW_VALUE = 5;
     const onChange = sandbox.spy();
-    const {
-      container: {firstElementChild},
-    } = renderSlider({defaultValue: [1, 2, 1, 0], onChange});
-    should.exist(firstElementChild);
-    fireEvent.mouseDown(firstElementChild!, {clientX: 50});
-    fireEvent.mouseUp(firstElementChild!, {clientX: 50});
+    const {container} = renderSlider({
+      defaultValue: [1, 2, 1, 0],
+      onChange,
+    });
+
+    const slider = container.firstElementChild!;
+    fireEvent.mouseDown(slider, {clientX: 50});
+    fireEvent.mouseUp(slider, {clientX: 50});
+
     onChange.should.have.been.calledWith([1, NEW_VALUE]);
   });
 
@@ -55,10 +61,13 @@ describe('Slider', () => {
     const RIGHT = 40;
     const NEW_VALUE = 5;
     const onChange = sandbox.spy();
-    const wrapper = renderSlider({defaultValue: [LEFT, RIGHT], onChange});
-    const [, thumb] = wrapper.queryAllByRole('slider');
-    fireEvent.mouseDown(thumb!);
-    fireEvent.mouseUp(wrapper.container, {clientX: 50});
+
+    renderSlider({defaultValue: [LEFT, RIGHT], onChange});
+    const thumbs = screen.getAllByRole('slider');
+
+    fireEvent.mouseDown(thumbs[1]); // Second thumb
+    fireEvent.mouseUp(document.body, {clientX: 50});
+
     onChange.should.have.been.calledWith([NEW_VALUE, LEFT]);
   });
 });
