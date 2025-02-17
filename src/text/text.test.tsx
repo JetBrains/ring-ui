178ad017b13c9af9af6b4a16649ceede64b01541
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2025-02-17 11:30:26 +0100

    RTL migration: Text

--- src/text/text.test.tsx
+++ src/text/text.test.tsx
@@ -1,24 +1,28 @@
-import {shallow, mount} from 'enzyme';
+import {render} from '@testing-library/react';
 
 import Text, {TextProps} from './text';
 
 describe('Text', () => {
-  const shallowText = (props?: TextProps) => shallow(<Text {...props} />);
-  const mountText = (props?: TextProps) => mount(<Text {...props} />);
+  const renderText = (props?: TextProps) => render(<Text {...props} />);
 
   it('should create component', () => {
-    mountText().should.have.type(Text);
+    const {container} = renderText();
+    container.querySelector('span')?.should.exist;
   });
 
   it('should wrap children with span', () => {
-    shallowText().should.have.tagName('span');
+    const {container} = renderText();
+    const element = container.firstElementChild;
+    element?.tagName.toLowerCase().should.equal('span');
   });
 
   it('should use passed className', () => {
-    shallowText({className: 'test-class'}).should.have.className('test-class');
+    const {container} = renderText({className: 'test-class'});
+    container.querySelector('.test-class')?.should.exist;
   });
 
   it('should use pass rest props to dom node', () => {
-    shallowText({'data-test': 'foo'}).should.have.data('test', 'foo');
+    const {container} = renderText({'data-test': 'foo'});
+    container.querySelector('[data-test="foo"]')?.should.exist;
   });
 });
