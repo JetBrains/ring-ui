31bf5b9509adca186a17c3e1de50c6db871d7019
Andrey Skladchikov <andrey.skladchikov@gmail.com>
2025-03-14 16:20:21 +0100

    Move Tabs test to RTL

--- src/tabs/tabs.test.tsx
+++ src/tabs/tabs.test.tsx
@@ -1,27 +1,29 @@
-import {mount} from 'enzyme';
+import {render, screen} from '@testing-library/react';
 
 import {Tabs, Tab} from './tabs';
 import {TabsAttrs} from './dumb-tabs';
 
 describe('Tabs', () => {
-  const mountTabs = (props?: Partial<TabsAttrs>) =>
-    mount(
+  const renderTabs = (props?: Partial<TabsAttrs>) =>
+    render(
       <Tabs {...props}>
         <Tab title="1" />
         <Tab title="2" />
       </Tabs>,
     );
-  const getTabsDiv = (props?: Partial<TabsAttrs>) => mountTabs(props).find('div').at(0);
 
   it('should create component', () => {
-    mountTabs().should.exist;
+    renderTabs();
+    screen.getByTestId('ring-dumb-tabs').should.exist;
   });
 
   it('should wrap children with div', () => {
-    getTabsDiv().should.exist;
+    renderTabs();
+    screen.getByTestId('ring-dumb-tabs').should.exist;
   });
 
   it('should use passed className', () => {
-    getTabsDiv({className: 'test-class'}).should.have.className('test-class');
+    renderTabs({className: 'test-class'});
+    screen.getByTestId('ring-dumb-tabs').should.have.class('test-class');
   });
 });
