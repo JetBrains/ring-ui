1ecbe9ce8d0cc6caa2caf010048371ab94d0b1cf
JetBrains Ring UI Automation <npmjs-buildserver@jetbrains.com>
2025-04-03 11:48:25 +0000

    Deploy master

--- master/sb-manager/globals-runtime.js
+++ master/sb-manager/globals-runtime.js
@@ -41018,7 +41018,7 @@ qe();
 var UM = P(Kn(), 1), mn = P($M(), 1);
 
 // src/manager-api/version.ts
-var jM = "8.6.11";
+var jM = "8.6.12";
 
 // src/manager-api/modules/versions.ts
 var { VERSIONCHECK: Bde } = z, VM = (0, UM.default)(1)(() => {
