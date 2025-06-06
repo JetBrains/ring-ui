4f6d097f5eb2708b3f1bff7d4ac537fee5f02225
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2025-06-06 17:30:33 +0200

    Use the lowest supported browser versions for screenshot tests

--- packages/screenshots/.testplane.conf.js
+++ packages/screenshots/.testplane.conf.js
@@ -52,7 +52,7 @@ module.exports = {
       desiredCapabilities: {
         browser: 'Chrome',
         pageLoadStrategy: 'normal',
-        browser_version: '131.0',
+        browser_version: '135.0',
         chromeOptions: {
           excludeSwitches: ['enable-automation'],
         },
@@ -69,7 +69,7 @@ module.exports = {
       desiredCapabilities: {
         browser: 'Firefox',
         pageLoadStrategy: 'normal',
-        browser_version: '137.0',
+        browser_version: '138.0',
         os: 'OS X',
         os_version: 'Sonoma',
         maxDuration,
