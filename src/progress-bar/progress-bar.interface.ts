8bde3a9654e0287da0a1494f643ca2d965d25992
Nik <25371847+lampmaster@users.noreply.github.com>
2025-09-12 17:28:18 +0300

    RG-2648 remove useless undefined for width calculation and add readonly for all props

--- src/progress-bar/progress-bar.interface.ts
+++ src/progress-bar/progress-bar.interface.ts
@@ -5,27 +5,27 @@ export interface ProgressBarProps extends HTMLAttributes<HTMLDivElement> {
    * A floating point number that specifies maximum progress value. Default value is 1.0.
    * @type {number}
    */
-  max?: number;
+  readonly max?: number;
   /**
    * A floating point number that specifies current progress value.
    * @type {number}
    */
-  value?: number;
-  label?: string;
+  readonly value?: number;
+  readonly label?: string;
   /**
    * Positions the progress bar globally at the top of the screen.
    * @type {boolean}
    */
-  global?: boolean | null | undefined;
+  readonly global?: boolean | null | undefined;
   /**
    * Disables progress bar color animation.
    * @type {boolean}
    */
-  disableAnimation?: boolean;
+  readonly disableAnimation?: boolean;
   /**
    * @deprecated Use `disableAnimation` instead.
    * Disables progress bar color animation.
    * @type {boolean}
    */
-  staticColor?: boolean;
+  readonly staticColor?: boolean;
 }
