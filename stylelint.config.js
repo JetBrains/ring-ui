60915fab826f719ed2ca50ec71b663cca0fc66c0
Anton Manokhin <18751423+amanokh@users.noreply.github.com>
2025-09-15 10:56:37 +0200

    refactor(eslint): add stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  extends: '@jetbrains/stylelint-config',
+  extends: ['stylelint-config-sass-guidelines', '@jetbrains/stylelint-config'],
   plugins: ['@csstools/stylelint-no-at-nest-rule'],
   rules: {
     '@csstools/stylelint-no-at-nest-rule': true,
-
+    'selector-class-pattern': null,
+    'selector-max-id': null,
+    'max-nesting-depth': null,
+    'scss/selector-no-redundant-nesting-selector': null,
+    'scss/load-no-partial-leading-underscore': null,
+    'scss/dollar-variable-pattern': null,
+    'color-hex-length': null,
+    'selector-max-compound-selectors': null,
+    'selector-no-qualifying-type': null,
+    'scss/at-import-no-partial-leading-underscore': null,
+    'scss/at-mixin-pattern': null,
+    'property-no-vendor-prefix': null,
+    'length-zero-no-unit': null,
+    'scss/at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['value'],
+      },
+    ],
     'selector-max-specificity': [
       '0,3,0',
       {
