c0916966878e0256416168ac486ec5c30f120075
Nik <25371847+lampmaster@users.noreply.github.com>
2025-10-14 15:15:34 +0300

    RG-2669 import set manually in docs
    added custom params importSubpath and importSubpath

--- .storybook/custom-docs/blocks/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Extracts the base component name (slug) from a Storybook file path.
- *
- * Example: getSlugFromPath('/src/button/button.stories.tsx') → 'button'
- */
-export const getSlugFromPath = (path: string) =>
-  (path.split(/[/\\]/).pop() ?? '').replace(/\.(stories|story)\.[^.]+$/i, '');
