6b3531352271fceb9335dc72714ec817c458ac2c
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2025-10-17 20:14:10 +0200

    RG-2661 Replace the old loader with the new one

--- src/loader/loader.stories.tsx
+++ src/loader/loader.stories.tsx
@@ -1,4 +1,4 @@
-import {type StoryFn} from '@storybook/react-webpack5';
+import {type StoryFn, type StoryObj} from '@storybook/react-webpack5';
 
 import Loader, {type LoaderProps} from './loader';
 
@@ -6,12 +6,16 @@ export default {
   title: 'Components/Loader',
 
   component: Loader,
-  parameters: {
-    screenshots: {skip: true},
-  },
 };
 
 export const basic: StoryFn<LoaderProps> = args => <Loader {...args} />;
 
 basic.storyName = 'Loader';
 basic.args = {message: 'Loading...'};
+basic.parameters = {
+  screenshots: {skip: true},
+};
+
+export const squares: StoryObj<LoaderProps> = {
+  args: {message: 'Loading...', squares: true},
+};
--- src/loader/loader.tsx
+++ src/loader/loader.tsx
@@ -1,9 +1,20 @@
 import {type HTMLAttributes, PureComponent} from 'react';
+import classNames from 'classnames';
 
 import LoaderCore, {type LoaderCoreProps} from './loader-core';
+import styles from './loader.css';
+
+declare module 'csstype' {
+  interface Properties {
+    '--ring-loader-color-1'?: string;
+    '--ring-loader-color-2'?: string;
+    '--ring-loader-color-3'?: string;
+  }
+}
 
 export interface LoaderProps extends Partial<LoaderCoreProps>, HTMLAttributes<HTMLElement> {
   'data-test'?: string | null | undefined;
+  squares?: boolean; // TODO make default in 8.0
 }
 
 /**
@@ -35,8 +46,27 @@ export default class Loader extends PureComponent<LoaderProps> {
   };
 
   render() {
-    const {message, size, colors, 'data-test': dataTest, stop, deterministic, ...restProps} = this.props;
+    const {message, size, colors, 'data-test': dataTest, stop, deterministic, squares, ...restProps} = this.props;
 
-    return <div {...restProps} ref={this.initLoader} />;
+    return squares ? (
+      <>
+        <div
+          {...restProps}
+          className={classNames(styles.canvas, styles.squares)}
+          style={{
+            '--ring-loader-color-1': colors?.[0] ? `rgb(${colors[0].r}, ${colors[0].g}, ${colors[0].b})` : undefined,
+            '--ring-loader-color-2': colors?.[1] ? `rgb(${colors[1].r}, ${colors[1].g}, ${colors[1].b})` : undefined,
+            '--ring-loader-color-3': colors?.[2] ? `rgb(${colors[2].r}, ${colors[2].g}, ${colors[2].b})` : undefined,
+          }}
+        >
+          <div className={classNames(styles.square, styles.outer)} />
+          <div className={classNames(styles.square, styles.middle)} />
+          <div className={classNames(styles.square, styles.inner)} />
+        </div>
+        {message && <div className={styles.text}>{message}</div>}
+      </>
+    ) : (
+      <div {...restProps} ref={this.initLoader} />
+    );
   }
 }
