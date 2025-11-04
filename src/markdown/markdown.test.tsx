f4ae85b36d43e3e7d71538a9ac69fd55245c47cf
Nik <25371847+lampmaster@users.noreply.github.com>
2025-11-04 20:21:35 +0200

    RG-2681 - increase test coverage

--- src/markdown/markdown.test.tsx
+++ src/markdown/markdown.test.tsx
@@ -2,6 +2,8 @@ import {render, screen} from '@testing-library/react';
 
 import Markdown, {type MarkdownProps} from './markdown';
 
+import styles from './markdown.css';
+
 describe('Markdown', () => {
   const renderMarkdown = (props?: Partial<MarkdownProps>) => {
     render(
@@ -18,5 +20,23 @@ describe('Markdown', () => {
 
   it('should use passed className', () => {
     expect(renderMarkdown({className: 'test-class'})).to.have.class('test-class');
+  });
+
+  it('should apply inline class when inline prop is true', () => {
+    expect(renderMarkdown({inline: true})).to.have.class(styles.inline);
+  });
+
+  it('should apply markdown class when inline is false or not provided', () => {
+    expect(renderMarkdown({inline: false})).to.have.class(styles.markdown);
+    expect(renderMarkdown()).to.have.class(styles.markdown);
+  });
+
+  it('should render children correctly', () => {
+    render(
+      <Markdown>
+        <span data-test='test-child'>{'Test content'}</span>
+      </Markdown>,
+    );
+    expect(screen.getByTestId('test-child')).to.exist;
   });
 });
