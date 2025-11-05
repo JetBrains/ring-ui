1005d5a36567845a03edd40b6c98e752b8ef730e
Nik <25371847+lampmaster@users.noreply.github.com>
2025-11-05 17:41:39 +0200

    RG-2682 - refactor naming and code style

--- src/breadcrumbs/breadcrumbs.tsx
+++ src/breadcrumbs/breadcrumbs.tsx
@@ -3,17 +3,18 @@ import {Children, Fragment, type ReactNode} from 'react';
 
 import styles from './breadcrumbs.css';
 
-export interface BreadcrumbsSeparatorAttrs {
+export interface BreadcrumbsProps {
   separatorClassName?: string | null | undefined;
   children?: ReactNode;
 }
 
-export default function Breadcrumbs({separatorClassName, children}: BreadcrumbsSeparatorAttrs) {
-  return Children.toArray(children).map((child, index) => (
+const Breadcrumbs = ({separatorClassName, children}: BreadcrumbsProps) =>
+  Children.toArray(children).map((child, index) => (
     // eslint-disable-next-line react/no-array-index-key
     <Fragment key={index}>
       {index > 0 && <span className={classNames(styles.separator, separatorClassName)}>{'/'}</span>}
       {child}
     </Fragment>
   ));
-}
+
+export default Breadcrumbs;
