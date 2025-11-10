d150c0269af5169dcde749521dada09b6bd6a62f
Nik <25371847+lampmaster@users.noreply.github.com>
2025-11-10 11:39:58 +0200

    RG-2682 Add restProps for the breadcrumbs and fix aria-label

--- src/breadcrumbs/breadcrumbs.tsx
+++ src/breadcrumbs/breadcrumbs.tsx
@@ -1,15 +1,15 @@
 import classNames from 'classnames';
-import {Children, Fragment, type ReactNode} from 'react';
+import {Children, Fragment, type HTMLAttributes, type ReactNode} from 'react';
 
 import styles from './breadcrumbs.css';
 
-export interface BreadcrumbsProps {
+export interface BreadcrumbsProps extends Omit<HTMLAttributes<HTMLElement>, 'children'> {
   separatorClassName?: string | null | undefined;
   children?: ReactNode;
 }
 
-const Breadcrumbs = ({separatorClassName, children}: BreadcrumbsProps) => (
-  <nav aria-label='breadcrumb'>
+const Breadcrumbs = ({separatorClassName, children, ...restProps}: BreadcrumbsProps) => (
+  <nav aria-label='breadcrumbs' {...restProps}>
     {Children.toArray(children).map((child, index) => (
       // eslint-disable-next-line react/no-array-index-key
       <Fragment key={index}>
