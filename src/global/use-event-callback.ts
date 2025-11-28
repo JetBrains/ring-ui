207152ab4f9081d5b6901ddb5ea5129a618a05d4
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2025-11-28 22:40:03 +0100

    chore: don't use useEffectEvent as it's only available in React 19.2+

--- src/global/use-event-callback.ts
+++ src/global/use-event-callback.ts
@@ -1,5 +1,6 @@
 import {useCallback, useLayoutEffect, useRef} from 'react';
 
+// TODO deprecate in favor of useEffectEvent after dropping support for React < 19.2
 export default function useEventCallback<I extends unknown[], O>(fn: (...args: I) => O): (...args: I) => O {
   const ref = useRef<(...args: I) => O>(null);
   useLayoutEffect(() => {
