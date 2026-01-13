b76165fb99c12b513f0bf0cf972de3938bf0a766
nikita.volkov <nikita.volkov@TeamCity>
2026-01-13 11:03:39 +0000

    VCS roots of 'Hub Auth Publish' build configuration were updated (TeamCity change in 'JetBrains Public Projects / JetBrains UI / Ring UI' project)

--- .teamcity/patches/buildTypes/HubAuthPublish.kts
+++ .teamcity/patches/buildTypes/HubAuthPublish.kts
@@ -38,6 +38,7 @@ create(DslContext.projectId, BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+        root(RelativeId("JetBrainsHubAuth"))
 
         branchFilter = "+:<default>"
     }
