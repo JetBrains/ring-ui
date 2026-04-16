e73c413200ba579814603f926ef0bad9e5d340a1
Aleksei Berezkin <16083785+aleksei-berezkin@users.noreply.github.com>
2026-04-16 13:22:19 +0200

    RG-2288 Build fixup

--- src/date-picker/scroll-arith.test.ts
+++ src/date-picker/scroll-arith.test.ts
@@ -43,21 +43,21 @@ describe('ScrollArith with months', () => {
       const midDate = new Date(TEST_YEAR, 1, 15, 12);
       const expectedScrollTop = (ITEMS_AROUND + 0.5) * ITEM_HEIGHT - VIEW_CENTER;
 
-      expect(arith.getScrollTop(items, midDate)).to.be.closeTo(expectedScrollTop, 1e-7);
+      expect(arith.getScrollTop(items, midDate, undefined)).to.be.closeTo(expectedScrollTop, 1e-7);
     });
 
     it('should calculate scrollTop when scrollDate is before the first item', () => {
       const beforeDate = addMonths(items[0], -10);
       const expectedScrollTop = -VIEW_CENTER;
 
-      expect(arith.getScrollTop(items, beforeDate)).to.be.closeTo(expectedScrollTop, 1e-7);
+      expect(arith.getScrollTop(items, beforeDate, undefined)).to.be.closeTo(expectedScrollTop, 1e-7);
     });
 
     it('should calculate scrollTop when scrollDate is after the last item', () => {
       const beforeDate = addMonths(items[0], 10);
       const expectedScrollTop = 2 * ITEMS_AROUND * ITEM_HEIGHT - VIEW_CENTER;
 
-      expect(arith.getScrollTop(items, beforeDate)).to.be.closeTo(expectedScrollTop, 1e-7);
+      expect(arith.getScrollTop(items, beforeDate, undefined)).to.be.closeTo(expectedScrollTop, 1e-7);
     });
   });
 
@@ -66,11 +66,16 @@ describe('ScrollArith with months', () => {
       const scrollTop = 3.5 * ITEM_HEIGHT - VIEW_CENTER;
       const expectedScrollDate = new Date(TEST_YEAR, 2, 16, 12);
 
-      expect(Number(arith.getScrollDate(items, scrollTop))).to.be.closeTo(Number(expectedScrollDate), 3600_000);
+      expect(Number(arith.getScrollDate(items, scrollTop, undefined))).to.be.closeTo(
+        Number(expectedScrollDate),
+        3600_000,
+      );
     });
 
     it('should return last item for very high scrollTop', () => {
-      expect(Number(arith.getScrollDate(items, ITEM_HEIGHT * 100))).to.equal(Number(items[items.length - 1]));
+      expect(Number(arith.getScrollDate(items, ITEM_HEIGHT * 100, undefined))).to.equal(
+        Number(items[items.length - 1]),
+      );
     });
   });
 
