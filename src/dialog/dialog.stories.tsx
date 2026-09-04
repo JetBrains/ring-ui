063e28142217195f4feb773d7ed8e00bdb7c57a5
Filipp Riabchun <Filipp.Riabchun@jetbrains.com>
2026-09-04 13:38:02 +0200

    Add RTL dialog story

--- src/dialog/dialog.stories.tsx
+++ src/dialog/dialog.stories.tsx
@@ -178,6 +178,15 @@ export const dense: StoryFn = () => (
   </Dialog>
 );
 
+export const rtl: StoryFn = () => (
+  <div dir='rtl'>
+    <Dialog show>
+      <Header>RTL dialog</Header>
+      <Content>The resize marker follows the inline end.</Content>
+    </Dialog>
+  </div>
+);
+
 const lorem = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
 Ipsum has been the industry's standard dummy text ever since the 1500s, when an
 unknown printer took a galley of type and scrambled it to make a type specimen
